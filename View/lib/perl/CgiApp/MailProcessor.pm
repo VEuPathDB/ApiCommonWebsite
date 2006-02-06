d327fb58c3b54c46bf4857d7254a9eea92e4d2db
yongchan <yongchan@_.com>
2006-02-06 20:18:30 +0000

    created
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@10671 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/CgiApp/MailProcessor.pm
@@ -0,0 +1,53 @@
+package ApiCommonWebsite::View::CgiApp::MailProcessor;
+
+
+use strict;
+use CGI;
+
+sub go {
+    my $Self = shift;
+    my $cgi = CGI->new();
+
+    my $to = join("", @{ $cgi->{'to'} });
+    my $subject = join("", @{ $cgi->{'subject'} });
+    my $replyTo = join("", @{ $cgi->{'replyTo'} }) || 'anonymous';
+    my $privacy = join("", @{ $cgi->{'privacy'} });
+    my $website = join("", @{ $cgi->{'website'} });
+    my $version = join("", @{ $cgi->{'version'} });
+    my $browser = join("", @{ $cgi->{'browser'} });
+    my $message = join("", @{ $cgi->{'message'} });
+
+    my $cfmMsg = "Sorry your message was not sent";
+    my $body;
+    if ($to) {
+      $body = "On behalf of: $replyTo" . "\n"
+            . "Privacy preference: $privacy" . "\n"
+            . "Website and version: $website $version" . "\n"
+            . "Browser information: $browser" . "\n\n"
+            . "$message" . "\n";
+
+      $to = 'ygan@pcbi.upenn.edu';
+      system("echo '$body' | mail -s '$subject' $to");
+
+      if ($?) {
+	$cfmMsg .= ": $!"
+                 . "\n\nPlease use your browser's back button to go back and try again.";
+      } else {
+	$cfmMsg = "Thank you! Your message has been sent."
+                . "\n\nPlease use you browser's back button to go back to the website.";
+      }
+    } else {
+      $cfmMsg .= ": no recipient is specified."
+               . "\n\nThis indicate a problem with the mail form."
+               . " Please contact the webmaster to report the problem.";
+    }
+
+    print $cgi->header('text/plain');
+    print "$cfmMsg";
+
+    if(0) {
+      print "\n\nDEBUG: mailed to $to with the following message:\n$body";
+    }
+}
+
+1;
