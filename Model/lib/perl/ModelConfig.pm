7109942ed71ed678d88efa578b3c41b538f9d878
Mark Heiges <mheiges@gmail.com>
2006-04-08 18:26:29 +0000

    add Perl module for access to WDK model-config.xml
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@11642 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/lib/perl/ModelConfig.pm
@@ -0,0 +1,147 @@
+package ApiCommonWebsite::Model::ModelConfig;
+
+use strict;
+use XML::Simple;
+
+sub new {
+    my ($class, $model) = @_;
+    my $self = {};
+    bless $self;
+   
+    my $cfg = XMLin("$ENV{GUS_HOME}/config/${model}-config.xml");
+    
+    for (keys %$cfg) {
+        $self->{$_} = $cfg->{$_}
+    }
+    
+    $self->{dbiDsn} = _jdbc2dbi($self->{connectionUrl});
+
+    return $self;
+}
+
+sub getDbiDsn             { $_[0]->{dbiDsn} }
+sub getLogin              { $_[0]->{login} }
+sub getPassword           { $_[0]->{password} }
+sub getConnectionUrl      { $_[0]->{connectionUrl} }
+sub getQueryInstanceTable { $_[0]->{queryInstanceTable} }
+sub getQueryHistoryTable  { $_[0]->{historyTable} }
+sub getPlatformClass      { $_[0]->{platformClass} }
+sub getMaxQueryParams     { $_[0]->{maxQueryParams} }
+sub getMaxIdle            { $_[0]->{maxIdle} }
+sub getMaxWait            { $_[0]->{maxWait} }
+sub getMaxActive          { $_[0]->{maxActive} }
+sub getMinIdle            { $_[0]->{minIdle} }
+sub getInitialSize        { $_[0]->{initialSize} }
+sub getWebServiceUrl      { $_[0]->{webServiceUrl} }
+
+sub _jdbc2dbi {
+# convert Oracle thin jdbc driver syntax to dbi syntax
+# jdbc:oracle:thin:@redux.rcc.uga.edu:1521:cryptoB
+# dbi:Oracle:host=redux.rcc.uga.edu;sid=cryptob
+    my ($jdbc) = @_;
+    my ($host, $port, $sid) = $jdbc =~ m/thin:[^\@]*\@([^:]+):([^:]+):([^:]+)/;
+    return "dbi:Oracle:host=$host;sid=$sid;port=$port";
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+ApiCommonWebsite::Model::ModelConfig - access to WDK model-config.xml properties
+
+=head1 SYNOPSIS
+
+    use ApiCommonWebsite::Model::ModelConfig;
+
+    my $cfg = new ApiCommonWebsite::Model::ModelConfig('toyModel');
+    
+    my $username = $cfg->getLogin;
+    my $password = $cfg->getPassword;
+
+    Retrieve the JDBC connectionUrl converted to Perl DBI syntax:
+    my $dsn = $cfg->getDbiDsn;
+    
+
+    You may also access by property name:
+        $cfg->{login}
+    but be aware that if the property name changes in the model-config.xml you
+    will need to update every script that uses this accessor.
+    
+    $cfg->{connectionUrl} is the JDBC connection string as written in the 
+    model-config.xml.
+    $cfg->{dbiDsn} is the Perl DBI version translated from the 
+    connectionUrl property.
+    
+=head1 DESCRIPTION
+
+Provides Perl access to properties in a WDK model-config.xml file.
+
+=head1 BUGS
+
+The conversion of the JDBC connectionUrl to Perl DBI only works for Oracle
+thin driver syntax, and even then not for all allowed syntax.
+Assumes connection strings of the format
+  jdbc:oracle:thin:@hostname.uga.edu:1521:toysite
+
+=head1 AUTHOR 
+
+Mark Heiges, mheiges@uga.edu
+
+=cut
+
+=head1 METHODS
+
+=head2 new
+
+ Usage   : my $cfg = new ApiCommonWebsite::Model::ModelConfig('toyModel');
+ Returns : object containing data parsed from the model configuration file.
+ Args    : the name of the model. This is used to find the Model's 
+           configuration XML file ($GUS_HOME/config/{model}-config.xml)
+
+=head2 getLogin
+ 
+ Usage : my $username = $cfg->getLogin;
+ Returns : login name for the database
+ 
+=head2 getPassword
+ 
+ Usage : my $passwd = $cfg->getPassword;
+ Returns : login password for the database
+ 
+=head2 getDbiDsn
+ 
+ Usage : my $dsn = $cfg->getDbiDsn;
+ Returns : perl dbi connection string. converted from the jdbc connection URL in the model-config.xml
+ Example : dbi:Oracle:host=redux.rcc.uga.edu;sid=toysite
+ 
+=head2 getConnectionUrl
+ 
+ Usage : my $jdbcUrl = $cfg->getConnectionUrl;
+ Returns : original jdbc connection string from model-config.xml
+
+
+
+=head2 getQueryInstanceTable
+
+=head2 getQueryHistoryTable 
+
+=head2 getPlatformClass     
+
+=head2 getMaxQueryParams    
+
+=head2 getMaxIdle           
+
+=head2 getMaxWait           
+
+=head2 getMaxActive         
+
+=head2 getMinIdle           
+
+=head2 getInitialSize       
+
+=head2 getWebServiceUrl     
+
+ 
+=cut
