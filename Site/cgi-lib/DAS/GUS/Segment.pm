2b848d6ea2ecd8fc0bfbfc88295779fc82b21e2a
yongchan <yongchan@_.com>
2006-05-05 20:09:13 +0000

    bundle subfeature queries
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-april-2006@11953 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/GUS/Segment.pm
+++ Site/cgi-lib/DAS/GUS/Segment.pm
@@ -369,17 +369,24 @@ sub features {
       or $self->throw("getting feature query failed");
 
     my @tempfeats = ();
+
     while (my $featureRow = $sth->fetchrow_hashref) {
-	push @tempfeats, $self->_makeFeature($featureRow, $factory);
+			push @tempfeats, $self->_makeFeature($featureRow, $factory);
     }
 
-	# filter out blastx. it is used by cryptodb
+	  # filter out blastx. it is used by cryptodb
     if($typeString =~ /blastx/i) { 
         warn "filtering blastx results for feature type: $type";
         @tempfeats = _blastx_filter(\@tempfeats);
     }
 
     push(@features, @tempfeats);
+
+		my $bulkSubFeatureSql = $factory->parser->getSQL("Feature.pm", "$type:bulksubfeatures");
+		next unless $bulkSubFeatureSql;
+		$bulkSubFeatureSql =~ s/(\$\w+)/eval $1/eg;
+  	$self->_addBulkSubFeatures(\@features, $bulkSubFeatureSql, $factory) 
+
   }
 
   if($iterator) {
@@ -388,6 +395,27 @@ sub features {
     return @features;
   } else {
     return \@features;
+  }
+}
+
+sub _addBulkSubFeatures {
+  my ($self, $features, $subFeatureSql, $factory) = @_;
+
+  my %featuresById;
+  map { $featuresById{$_->feature_id} = $_ } @$features;
+  my $sth = $factory->dbh->prepare($subFeatureSql);
+  $sth->execute()
+    or $self->throw("getting bulk subfeature query failed");
+
+  while (my $featureRow = $sth->fetchrow_hashref) {
+    my $feature = $featuresById{$$featureRow{'PARENT_ID'}};
+    if ($feature) {
+      $feature->_addSubFeatureFromRow($featureRow);
+    } else {
+      $self->warn("sub feature [" . $$featureRow{'FEATURE_ID'} . "]'s parent feature ["
+		  . $$featureRow{'PARENT_ID'} . "] could not be found. bulk subfeature query is:\n"
+		  . $subFeatureSql);
+    }
   }
 }
 
