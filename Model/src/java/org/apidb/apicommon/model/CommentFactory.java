fdf8c4d5cd049de7b7465499cd1af5b77f580a6b
Xin Gao <xingao@upenn.edu>
2006-08-03 20:16:34 +0000

    hard code the external database name & version to remove the backwrad dblink requiremnet
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@12789 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/CommentFactory.java
+++ Model/src/java/org/apidb/apicommon/model/CommentFactory.java
@@ -79,10 +79,11 @@ public class CommentFactory {
         CommentTarget target = null;
         PreparedStatement ps = null;
         try {
-            
-            ps = SqlUtils.getPreparedStatement (platform.getDataSource(), "SELECT * FROM " 
-    				+ schema + ".comment_target where comment_target_id = ?");
-        	ps.setString(1, internalValue);
+
+            ps = SqlUtils.getPreparedStatement(platform.getDataSource(),
+                    "SELECT * FROM " + schema
+                            + ".comment_target where comment_target_id = ?");
+            ps.setString(1, internalValue);
             rs = ps.executeQuery();
             if (!rs.next())
                 throw new WdkModelException("The comment target cannot be "
@@ -112,28 +113,31 @@ public class CommentFactory {
         PreparedStatement ps = null;
         // get a new comment id
         try {
-            //DataSource dataSource = platform.getDataSource();
+            // DataSource dataSource = platform.getDataSource();
             String commentId = platform.getNextId(schema, "comments");
-            
-            ps = SqlUtils.getPreparedStatement(platform.getDataSource(), 
-            		"INSERT INTO " + schema + ".comments (comment_id, email, "
-            		+ "comment_date, comment_target_id, stable_id, conceptual, "
-            		+ "project_name, project_version, headline, content, "
-            		+ "location_string, review_status_id) "
-					+ " VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-            
+
+            ps = SqlUtils.getPreparedStatement(
+                    platform.getDataSource(),
+                    "INSERT INTO "
+                            + schema
+                            + ".comments (comment_id, email, "
+                            + "comment_date, comment_target_id, stable_id, conceptual, "
+                            + "project_name, project_version, headline, content, "
+                            + "location_string, review_status_id) "
+                            + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+
             long currentMillis = System.currentTimeMillis();
-            
+
             ps.setInt(1, Integer.parseInt(commentId));
-            ps.setString (2, comment.getEmail());
-            ps.setDate(3, new java.sql.Date (currentMillis));
-            ps.setString (4, comment.getCommentTarget());
-            ps.setString (5, comment.getStableId());
-            ps.setBoolean (6, comment.isConceptual());
-            ps.setString (7, comment.getProjectName());
-            ps.setString (8, comment.getProjectVersion());
-            ps.setString (9, comment.getHeadline());
-            ps.setString (10, comment.getContent());
+            ps.setString(2, comment.getEmail());
+            ps.setDate(3, new java.sql.Date(currentMillis));
+            ps.setString(4, comment.getCommentTarget());
+            ps.setString(5, comment.getStableId());
+            ps.setBoolean(6, comment.isConceptual());
+            ps.setString(7, comment.getProjectName());
+            ps.setString(8, comment.getProjectVersion());
+            ps.setString(9, comment.getHeadline());
+            ps.setString(10, comment.getContent());
             ps.setString(11, comment.getLocationString());
             ps.setString(12, Comment.COMMENT_REVIEW_STATUS_UNKNOWN);
 
@@ -182,48 +186,61 @@ public class CommentFactory {
         }
     }
 
+    /**
+     * For the first release, this method hard-code the external database name
+     * and version, instead of looking up directly in GUS. This avoids the
+     * backward dblink requirement; The site should provide such information to
+     * the comment factory in future releases.
+     * 
+     * @param commentId
+     * @param comment
+     * @throws SQLException
+     */
     private void saveExternalDbs(String commentId, Comment comment)
             throws SQLException {
         String schema = config.getCommentSchema();
-        String dblink = config.getProjectDbLink();
-        String stableId = comment.getStableId();
+        // String dblink = config.getProjectDbLink();
+        // String stableId = comment.getStableId();
         DataSource dataSource = platform.getDataSource();
 
         // get a set of matched external databases
         StringBuffer sql = new StringBuffer();
-        sql.append("( SELECT DISTINCT ed.name AS external_database_name, "
-                + "    edr.version AS external_database_version ");
-        sql.append("  FROM sres.ExternalDatabase" + dblink + " ed, ");
-        sql.append("    sres.DbRef" + dblink + " dr, ");
-        sql.append("    sres.ExternalDatabaseRelease" + dblink + " edr, ");
-        sql.append("    dots.dbrefNaFeature" + dblink + " drnf, ");
-        sql.append("    dots.GeneFeature" + dblink + " gf ");
-        sql.append("  WHERE dr.external_database_release_id = edr.external_database_release_id "
-                + "     AND edr.external_database_id = ed.external_database_id "
-                + "     AND drnf.db_ref_id = dr.db_ref_id "
-                + "     AND drnf.na_feature_id = gf.na_feature_id ");
-        sql.append("    AND gf.source_id = ?) ");
-        sql.append("union "
-                + "( SELECT DISTINCT ed.name AS external_database_name, "
-                + "    edr.version AS external_database_version ");
-        sql.append("  FROM sres.ExternalDatabase" + dblink + " ed, ");
-        sql.append("sres.DbRef" + dblink + " dr, ");
-        sql.append("    sres.ExternalDatabaseRelease" + dblink + " edr, ");
-        sql.append("    dots.dbrefNaSequence" + dblink + " drns, ");
-        sql.append("dots.SplicedNaSequence" + dblink + " sns ");
-        sql.append("  WHERE dr.external_database_release_id = edr.external_database_release_id "
-                + "    AND edr.external_database_id = ed.external_database_id "
-                + "    AND drns.db_ref_id = dr.db_ref_id "
-                + "    AND drns.na_sequence_id = sns.na_sequence_id ");
-        sql.append("    AND sns.source_id = ?)");
-        ResultSet rs = null;
-        PreparedStatement ps = null, queryDb = null, insertDb = null, insertLink = null;
+        // sql.append("( SELECT DISTINCT ed.name AS external_database_name, "
+        // + " edr.version AS external_database_version ");
+        // sql.append(" FROM sres.ExternalDatabase" + dblink + " ed, ");
+        // sql.append(" sres.DbRef" + dblink + " dr, ");
+        // sql.append(" sres.ExternalDatabaseRelease" + dblink + " edr, ");
+        // sql.append(" dots.dbrefNaFeature" + dblink + " drnf, ");
+        // sql.append(" dots.GeneFeature" + dblink + " gf ");
+        // sql.append(" WHERE dr.external_database_release_id =
+        // edr.external_database_release_id "
+        // + " AND edr.external_database_id = ed.external_database_id "
+        // + " AND drnf.db_ref_id = dr.db_ref_id "
+        // + " AND drnf.na_feature_id = gf.na_feature_id ");
+        // sql.append(" AND gf.source_id = ?) ");
+        // sql.append("union "
+        // + "( SELECT DISTINCT ed.name AS external_database_name, "
+        // + " edr.version AS external_database_version ");
+        // sql.append(" FROM sres.ExternalDatabase" + dblink + " ed, ");
+        // sql.append("sres.DbRef" + dblink + " dr, ");
+        // sql.append(" sres.ExternalDatabaseRelease" + dblink + " edr, ");
+        // sql.append(" dots.dbrefNaSequence" + dblink + " drns, ");
+        // sql.append("dots.SplicedNaSequence" + dblink + " sns ");
+        // sql.append(" WHERE dr.external_database_release_id =
+        // edr.external_database_release_id "
+        // + " AND edr.external_database_id = ed.external_database_id "
+        // + " AND drns.db_ref_id = dr.db_ref_id "
+        // + " AND drns.na_sequence_id = sns.na_sequence_id ");
+        // sql.append(" AND sns.source_id = ?)");
+        // ResultSet rs = null;
+        // PreparedStatement ps = null;
+        PreparedStatement queryDb = null, insertDb = null, insertLink = null;
         try {
-        	ps = SqlUtils.getPreparedStatement(dataSource, sql.toString());
-        	ps.setString(1, stableId);
-        	ps.setString (2, stableId);
-        	
-            rs = ps.executeQuery();
+            // ps = SqlUtils.getPreparedStatement(dataSource, sql.toString());
+            // ps.setString(1, stableId);
+            // ps.setString (2, stableId);
+            //        	
+            // rs = ps.executeQuery();
 
             // construct prepared statements
             sql = new StringBuffer();
@@ -232,46 +249,51 @@ public class CommentFactory {
             sql.append("WHERE external_database_name = ? "
                     + "AND external_database_version = ?");
             queryDb = SqlUtils.getPreparedStatement(dataSource, sql.toString());
+
             sql = new StringBuffer();
             sql.append("INSERT INTO " + schema + ".external_databases ");
             sql.append("(external_database_id, external_database_name, "
                     + "external_database_version) VALUES (?, ?, ?)");
             insertDb = SqlUtils.getPreparedStatement(dataSource, sql.toString());
+
             sql = new StringBuffer();
             sql.append("INSERT INTO " + schema + ".comment_external_database ");
             sql.append("(external_database_id, comment_id) VALUES (?, ?)");
             insertLink = SqlUtils.getPreparedStatement(dataSource,
                     sql.toString());
 
-            while (rs.next()) {
-                String externalDbName = rs.getString("external_database_name");
-                String externalDbVersion = rs.getString("external_database_version");
-                int externalDbId;
+            // while (rs.next()) {
+            // String externalDbName = rs.getString("external_database_name");
+            // String externalDbVersion =
+            // rs.getString("external_database_version");
+            String externalDbName = "PlasmoDB_Temp";
+            String externalDbVersion = "ver_1.0";
+            int externalDbId;
 
-                // check if the external db entry exists
-                queryDb.setString(1, externalDbName);
-                queryDb.setString(2, externalDbVersion);
-                ResultSet rsDb = queryDb.executeQuery();
-                if (!rsDb.next()) { // no entry, add one
-                    externalDbId = Integer.parseInt(platform.getNextId(schema,
-                            "external_databases"));
-                    insertDb.setInt(1, externalDbId);
-                    insertDb.setString(2, externalDbName);
-                    insertDb.setString(3, externalDbVersion);
-                    insertDb.execute();
-                } else { // has entry, get the external_database_id
-                    externalDbId = rsDb.getInt("external_database_id");
-                }
-                rsDb.close();
-
-                // add the reference link
-                insertLink.setInt(1, externalDbId);
-                insertLink.setInt(2, Integer.parseInt(commentId));
-                insertLink.execute();
+            // check if the external db entry exists
+            queryDb.setString(1, externalDbName);
+            queryDb.setString(2, externalDbVersion);
+            ResultSet rsDb = queryDb.executeQuery();
+            if (!rsDb.next()) { // no entry, add one
+                externalDbId = Integer.parseInt(platform.getNextId(schema,
+                        "external_databases"));
+                insertDb.setInt(1, externalDbId);
+                insertDb.setString(2, externalDbName);
+                insertDb.setString(3, externalDbVersion);
+                insertDb.execute();
+            } else { // has entry, get the external_database_id
+                externalDbId = rsDb.getInt("external_database_id");
             }
+            rsDb.close();
+
+            // add the reference link
+            insertLink.setInt(1, externalDbId);
+            insertLink.setInt(2, Integer.parseInt(commentId));
+            insertLink.execute();
+            // }
         } finally {
             // close statements and result set
-            SqlUtils.closeResultSet(rs);
+            // SqlUtils.closeResultSet(rs);
             SqlUtils.closeStatement(queryDb);
             SqlUtils.closeStatement(insertDb);
             SqlUtils.closeStatement(insertLink);
@@ -287,11 +309,12 @@ public class CommentFactory {
         ResultSet rs = null;
         PreparedStatement ps = null;
         try {
-        	
-            ps = SqlUtils.getPreparedStatement(platform.getDataSource(), sql.toString());
+
+            ps = SqlUtils.getPreparedStatement(platform.getDataSource(),
+                    sql.toString());
             ps.setString(1, commentId);
             rs = ps.executeQuery();
-            
+
             if (!rs.next())
                 throw new WdkModelException("Comment of the given id '"
                         + commentId + "' cannot be found.");
@@ -335,14 +358,15 @@ public class CommentFactory {
         sql.append("coordinate_type FROM " + schema + ".locations ");
         sql.append("WHERE comment_id = ?");
         ResultSet rs = null;
-        
+
         PreparedStatement ps = null;
         try {
-        	
-            ps = SqlUtils.getPreparedStatement(platform.getDataSource(), sql.toString());
-            ps.setString (1, commentId);
+
+            ps = SqlUtils.getPreparedStatement(platform.getDataSource(),
+                    sql.toString());
+            ps.setString(1, commentId);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 long start = rs.getLong("location_start");
                 long end = rs.getLong("location_end");
@@ -363,16 +387,17 @@ public class CommentFactory {
         sql.append("SELECT ed.external_database_name, ed.external_database_version ");
         sql.append("FROM " + schema + ".external_databases ed, ");
         sql.append(schema + ".comment_external_database ced ");
-        sql.append("WHERE ced.comment_id = ?" );
+        sql.append("WHERE ced.comment_id = ?");
         sql.append(" AND ced.external_database_id = ed.external_database_id");
-        
+
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            ps = SqlUtils.getPreparedStatement(platform.getDataSource(), sql.toString());
-            ps.setString (1, commentId);
+            ps = SqlUtils.getPreparedStatement(platform.getDataSource(),
+                    sql.toString());
+            ps.setString(1, commentId);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 String externalDbName = rs.getString("external_database_name");
                 String externalDbVersion = rs.getString("external_database_version");
