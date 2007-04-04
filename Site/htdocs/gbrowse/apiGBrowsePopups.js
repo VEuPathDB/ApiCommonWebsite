29f7a07a5687ae6df2fe2b050d2716a5216efa94
Steve Fischer <sfischer@upenn.edu>
2007-04-04 15:20:30 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@15582 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/gbrowse/apiGBrowsePopups.js
+++ Site/htdocs/gbrowse/apiGBrowsePopups.js
@@ -1,9 +1,9 @@
 function table (rows) {
-  return '<table border=1>' + rows.join('') + </table>';
+  return '<table border=0>' + rows.join('') + '</table>';
 }
 
 function twoColRow(left, right) {
-  return '<tr><td>' + left + '</td><td>' + right + '</td></tr>;
+  return '<tr><td>' + left + '</td><td>' + right + '</td></tr>';
 }
 
 // plasmodb SNP title
@@ -12,87 +12,67 @@ function pst (tip, paramsString) {
   var v = new Array();
   v = paramsString.split(',');
 
-  // indexes into the array formed by splitting paramsString
-  const isCoding     = 0;
-  const posInCDS     = isCoding + 1;
-  const posInProtein = posInCDS + 1; 
-  const refStrain    = posInProtein + 1; 
-  const refAA        = refStrain + 1; 
-  const gene         = refAA + 1; 
-  const reversed     = gene + 1; 
-  const refNA        = reversed + 1; 
-  const nonSyn       = refNA + 1; 
-  const sourceId     = nonSyn + 1; 
-  const type         = sourceId + 1;
-  const variants     = type + 1; 
-  const start        = variants + 1;
+  var revArray = new Array();
+  revArray['A'] = 'T';
+  revArray['C'] = 'G';
+  revArray['T'] = 'A';
+  revArray['G'] = 'C';
+
+  const IS_CODING     = 0;
+  const POS_IN_CDS     = IS_CODING + 1;
+  const POS_IN_PROTEIN = POS_IN_CDS + 1; 
+  const REF_STRAIN    = POS_IN_PROTEIN + 1; 
+  const REF_AA        = REF_STRAIN + 1; 
+  const GENE         = REF_AA + 1; 
+  const REVERSED     = GENE + 1; 
+  const REF_NA        = REVERSED + 1; 
+  const NON_SYN       = REF_NA + 1; 
+  const SOURCE_ID     = NON_SYN + 1; 
+  const VARIANTS     = SOURCE_ID + 1; 
+  const START        = VARIANTS + 1;
 
   // expand minimalist input data
-  var link = "<a href=/plasmo/showRecord.do?name=SnpRecordClasses.SnpRecordClass&primary_key=" + v[sourceId] + ">" + v[sourceId] + "</a>";
+  var link = "<a href=/plasmo/showRecord.do?name=SnpRecordClasses.SnpRecordClass&primary_key=" + v[SOURCE_ID] + ">" + v[SOURCE_ID] + "</a>";
  
   var type = 'Non-coding';
-  var refNA = v[isReversed]? reverse(v[refNA]) : v[refNA];
+  var refNA = v[REVERSED]? revArray[v[REF_NA]] : v[REF_NA];
   var refAAString = '';
-  if (v[isCoding]) {
-    var non = v[nonSyn]? 'non-' : '';
+  if (v[IS_CODING]) {
+    var non = v[NON_SYN]? 'non-' : '';
     type = 'Coding (' + non + 'synonymous)';
-    refAAString = '&nbsp;&nbsp;&nbsp;&nbsp;AA=' + v[refAA];
+    refAAString = '&nbsp;&nbsp;&nbsp;&nbsp;AA=' + v[REF_AA];
   }
 
   // format into html table rows
   var rows = new Array();
   rows.push(twoColRow('SNP', link));
-  rows.push(twoColRow('Location', start));
-  if (v[gene] != '') rows.push(twoColRow('Gene', v[gene]));
-  if (isCoding) {
-    rows.push(twoColRow('Position in CDS', v[posInCDS]));
-    rows.push(twoColRow('Position in Protein', v[posInProtein]));
+  rows.push(twoColRow('Location', v[START]));
+  if (v[GENE] != '') rows.push(twoColRow('Gene', v[GENE]));
+  if (v[IS_CODING]) {
+    rows.push(twoColRow('Position in CDS', v[POS_IN_CDS]));
+    rows.push(twoColRow('Position in Protein', v[POS_IN_PROTEIN]));
   }
   rows.push(twoColRow('Type', type));
-  rows.push(twoColRow(refStrain + ' (reference)', 'NA=' + refNA + refAAString));  
+  rows.push(twoColRow(v[REF_STRAIN] + ' (reference)', 'NA=' + refNA + refAAString));  
 
   // make one row per SNP allele
   var variants = new Array();
-  variants = v[variants].split('|');
-  for (var i=0; i<variants.length(); i++) {
+  variants = v[VARIANTS].split('|');
+  for (var i=0; i<variants.length; i++) {
     var variant = new Array();
     variant = variants[i].split(':');
     var strain = variant[0];
     var na = variant[1];
-    if (v[isReversed]) na = reverse(na); 
+    if (v[REVERSED]) na = revArray[na]; 
     var aa = variant[2];
     var info = 
-     'NA=' + na + (v[isCoding]? '&nbsp;&nbsp;&nbsp;&nbsp;AA=' + aa : '');
+     'NA=' + na + (v[IS_CODING]? '&nbsp;&nbsp;&nbsp;&nbsp;AA=' + aa : '');
     rows.push(twoColRow(strain, info));    
   }
 
   tip.T_BGCOLOR = 'lightskyblue';
   tip.T_TITLE = 'SNP';
   return table(rows);
-}
-
-
-
-#########  Perl code  -- from .conf file  ################
-#
-# The sql code only formats one tag called 'params'
-#
-# this is what is returned:
-#    onmouseover="return escape(pst(this, '1,302,22,Dd2,J,PF11_0344,0,A,1,PS-12221,Dd2:G:K|HB3:A:|Ghana:C:L,10010126'))
-#
-#
-# and here is the perl code to format that
-  sub snpTitle {
-    my $f = shift;
-    my $params = $f->get_tag_values("params")[0];
-    my $variants = $f->bulkAttributes();
-   
-    my @vars;
-    foreach my $variant (@$variants) {
-      push(@vars, "$variant->{STRAIN}:$variant->{ALLELE}:$variant->{PRODUCT}";
-    }
-    my $varsString = join('|', @vars)
-    return qq{onmouseover="return escape(pst(this,'$params,$varsString,$f->start'))"};
 }
 
 
