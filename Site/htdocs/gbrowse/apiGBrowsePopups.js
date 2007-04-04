b941cfb47f3aa1db2d0bcea563c345ddc59a87ad
Steve Fischer <sfischer@upenn.edu>
2007-04-04 17:10:49 +0000

    add gene title function
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@15586 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/gbrowse/apiGBrowsePopups.js
+++ Site/htdocs/gbrowse/apiGBrowsePopups.js
@@ -6,7 +6,43 @@ function twoColRow(left, right) {
   return '<tr><td>' + left + '</td><td>' + right + '</td></tr>';
 }
 
-// plasmodb SNP title
+// plasmodb Gene title
+function pgt (tip, paramsString) {
+  // split paramsString on comma
+  var v = new Array();
+  v = paramsString.split(',');
+
+  const SOURCE_ID = 0;
+  const CHR = SOURCE_ID + 1;
+  const LOC = CHR + 1;
+  const SO_TERM =  LOC + 1;
+  const PRODUCT = SO_TERM + 1;
+  const TAXON = PRODUCT + 1;
+  const IS_PSEUDO =  TAXON + 1;
+
+  // expand minimalist input data
+  var cdsLink = "<a href=../../../cgi-bin/geneSrt?ids=" + v[SOURCE_ID]
+        + "&type=CDS&upstreamAnchor=Start&upstreamOffset=0&downstreamAnchor=End&downstreamOffset=0&go=Get+Sequences target=_blank>CDS</a>"
+  var proteinLink = "<a href=../../../cgi-bin/geneSrt?ids=" + v[SOURCE_ID]
+        + "&type=protein&upstreamAnchor=Start&upstreamOffset=0&downstreamAnchor=End&downstreamOffset=0&go=Get+Sequences target=_blank>protein</a>"
+
+  var type = v[IS_PSEUDO]? v[SO_TERM] : v[SO_TERM] + " (pseudogene)";	
+  var download = cdsLink + " | " + proteinLink;
+
+  // format into html table rows
+  var rows = new Array();
+  rows.push(twoColRow('Species:', v[TAXON]));
+  rows.push(twoColRow('ID', v[SOURCE_ID]));
+  rows.push(twoColRow('Gene Type', type));
+  rows.push(twoColRow('Description', v[PRODUCT]));
+  rows.push(twoColRow('Location', v[LOC]));
+  rows.push(twoColRow('Download', download)); 
+
+  tip.T_BGCOLOR = 'lightskyblue';
+  tip.T_TITLE = 'Annotated Gene ' + v[SOURCE_ID];
+  return table(rows);
+}
+
 function pst (tip, paramsString) {
   // split paramsString on comma
   var v = new Array();
@@ -74,5 +110,3 @@ function pst (tip, paramsString) {
   tip.T_TITLE = 'SNP';
   return table(rows);
 }
-
-
