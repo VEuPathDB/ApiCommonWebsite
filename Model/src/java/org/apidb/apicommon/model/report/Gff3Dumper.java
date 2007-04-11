02cbcd375fa63a616d2cd09073e4f40dac2ab2ee
Xin Gao <xingao@upenn.edu>
2007-04-11 15:13:17 +0000

    revise the help info for gffDump
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@15736 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/report/Gff3Dumper.java
+++ Model/src/java/org/apidb/apicommon/model/report/Gff3Dumper.java
@@ -28,88 +28,88 @@ import org.gusdb.wdk.model.report.Reporter;
  * 
  */
 public class Gff3Dumper {
-    
+
     /**
      * @param args
      * @throws WdkModelException
      * @throws WdkUserException
      * @throws IOException
      */
-    public static void main( String[ ] args ) throws WdkModelException,
+    public static void main(String[] args) throws WdkModelException,
             WdkUserException, IOException {
-        if ( args.length != 4 && args.length != 6 ) {
-            System.err.println( "Invalid parameters." );
+        if (args.length != 4 && args.length != 6) {
+            System.err.println("Invalid parameters.");
             printUsage();
-            System.exit( -1 );
+            System.exit(-1);
         }
-        Map< String, String > cmdArgs = new HashMap< String, String >();
+        Map<String, String> cmdArgs = new HashMap<String, String>();
         for (int i = 0; i < args.length - 1; i += 2) {
             cmdArgs.put(args[i].trim().toLowerCase(), args[i + 1].trim());
         }
-        
+
         // get params
-        String modelName = cmdArgs.get( "-model" );
-        String organismArg = cmdArgs.get( "-organism" );
+        String modelName = cmdArgs.get("-model");
+        String organismArg = cmdArgs.get("-organism");
         String baseDir = cmdArgs.get("-dir");
-        if ( modelName == null || organismArg == null ) {
-            System.err.println( "Missing parameters." );
+        if (modelName == null || organismArg == null) {
+            System.err.println("Missing parameters.");
             printUsage();
-            System.exit( -1 );
+            System.exit(-1);
         }
         if (baseDir == null || baseDir.length() == 0) baseDir = ".";
-        
+
         // TEST
-        System.out.println( "Initializing...." );
-        
+        System.out.println("Initializing....");
+
         // load config
-        Map< String, String > config = new LinkedHashMap< String, String >();
-        config.put( Reporter.FIELD_FORMAT, "text" );
-        
+        Map<String, String> config = new LinkedHashMap<String, String>();
+        config.put(Reporter.FIELD_FORMAT, "text");
+
         // construct wdkModel
-        WdkModel wdkModel = WdkModel.construct( modelName );
-        QuestionSet qset = wdkModel.getQuestionSet( "DataDumpQuestions" );
-        
-        String[ ] organisms = organismArg.split( "," );
-        for ( String organism : organisms ) {
-            dumpOrganism( qset, organism.trim(), config, baseDir );
+        WdkModel wdkModel = WdkModel.construct(modelName);
+        QuestionSet qset = wdkModel.getQuestionSet("DataDumpQuestions");
+
+        String[] organisms = organismArg.split(",");
+        for (String organism : organisms) {
+            dumpOrganism(qset, organism.trim(), config, baseDir);
         }
         System.out.println("Finished.");
     }
-    
-    private static void dumpOrganism( QuestionSet qset, String organism,
-            Map< String, String > config, String baseDir ) throws WdkUserException,
-            WdkModelException, IOException {
-        
+
+    private static void dumpOrganism(QuestionSet qset, String organism,
+            Map<String, String> config, String baseDir)
+            throws WdkUserException, WdkModelException, IOException {
+
         long start = System.currentTimeMillis();
         // TEST
-        System.out.println( "Collecting sequence data...." );
-        
+        System.out.println("Collecting sequence data....");
+
         // ask sequence dumper question
-        Question seqQuestion = qset.getQuestion( "SequenceGffQuestion" );
-        Map< String, Object > seqParams = new LinkedHashMap< String, Object >();
-        seqParams.put( "organism_with_sequences", organism );
-        Answer sqlAnswer = seqQuestion.makeAnswer( seqParams, 1, 1 );
-        
+        Question seqQuestion = qset.getQuestion("SequenceGffQuestion");
+        Map<String, Object> seqParams = new LinkedHashMap<String, Object>();
+        seqParams.put("organism_with_sequences", organism);
+        Answer sqlAnswer = seqQuestion.makeAnswer(seqParams, 1, 1);
+
         ByteArrayOutputStream seqOut = new ByteArrayOutputStream();
-        Reporter seqReport = sqlAnswer.createReport( "gff3", config );
-        seqReport.write( seqOut );
-        byte[ ] seqBuffer = seqOut.toByteArray();
-        
+        Reporter seqReport = sqlAnswer.createReport("gff3", config);
+        seqReport.write(seqOut);
+        byte[] seqBuffer = seqOut.toByteArray();
+
         // TEST
-        System.out.println( "Collecting gene data...." );
-        
+        System.out.println("Collecting gene data....");
+
         // ask gene dumper question
-        Question geneQuestion = qset.getQuestion( "GeneGffQuestion" );
-        Map< String, Object > geneParams = new LinkedHashMap< String, Object >();
-        geneParams.put( "organism", organism );
-        Answer geneAnswer = geneQuestion.makeAnswer( geneParams, 1, 1 );
-        
+        Question geneQuestion = qset.getQuestion("GeneGffQuestion");
+        Map<String, Object> geneParams = new LinkedHashMap<String, Object>();
+        geneParams.put("organism", organism);
+        Answer geneAnswer = geneQuestion.makeAnswer(geneParams, 1, 1);
+
         ByteArrayOutputStream geneOut = new ByteArrayOutputStream();
-        config.put( Gff3Reporter.FIELD_HAS_PROTEIN, "yes" );
-        Reporter geneReport = geneAnswer.createReport( "gff3", config );
-        geneReport.write( geneOut );
-        byte[ ] geneBuffer = geneOut.toByteArray();
-        
+        config.put(Gff3Reporter.FIELD_HAS_PROTEIN, "yes");
+        Reporter geneReport = geneAnswer.createReport("gff3", config);
+        geneReport.write(geneOut);
+        byte[] geneBuffer = geneOut.toByteArray();
+
         // decide the path-file name
         File dir = new File(baseDir, organism.replace(' ', '_'));
         if (!dir.exists() || !dir.isDirectory()) dir.mkdirs();
@@ -122,53 +122,58 @@ public class Gff3Dumper {
         File gffFile = new File(dir, fileName);
 
         // merge the result
-        BufferedReader seqIn = new BufferedReader( new InputStreamReader(
-                new ByteArrayInputStream( seqBuffer ) ) );
-        BufferedReader geneIn = new BufferedReader( new InputStreamReader(
-                new ByteArrayInputStream( geneBuffer ) ) );
-        PrintWriter gffOut = new PrintWriter( new FileWriter( gffFile ) );
+        BufferedReader seqIn = new BufferedReader(new InputStreamReader(
+                new ByteArrayInputStream(seqBuffer)));
+        BufferedReader geneIn = new BufferedReader(new InputStreamReader(
+                new ByteArrayInputStream(geneBuffer)));
+        PrintWriter gffOut = new PrintWriter(new FileWriter(gffFile));
         String line;
-        
+
         // read headers, and annotations from sequence gff
-        while ( ( line = seqIn.readLine() ) != null ) {
+        while ((line = seqIn.readLine()) != null) {
             line = line.trim();
-            if ( line.equalsIgnoreCase( "##FASTA" ) ) break;
-            gffOut.println( line );
+            if (line.equalsIgnoreCase("##FASTA")) break;
+            gffOut.println(line);
         }
         gffOut.flush();
-        
+
         // read annotations from gene gff
-        while ( ( line = geneIn.readLine() ) != null ) {
+        while ((line = geneIn.readLine()) != null) {
             line = line.trim();
-            if ( line.startsWith( "##" ) && !line.equalsIgnoreCase( "##FASTA" ) )
+            if (line.startsWith("##") && !line.equalsIgnoreCase("##FASTA"))
                 continue;
-            gffOut.println( line );
+            gffOut.println(line);
         }
         gffOut.flush();
-        
+
         // append genomic sequence
-        while ( ( line = seqIn.readLine() ) != null ) {
+        while ((line = seqIn.readLine()) != null) {
             line = line.trim();
-            gffOut.println( line );
+            gffOut.println(line);
         }
         gffOut.flush();
         gffOut.close();
-        
+
         // TEST
-        System.out.println( "GFF3 file saved at " + gffFile.getAbsolutePath()
-                + "." );
-        
+        System.out.println("GFF3 file saved at " + gffFile.getAbsolutePath()
+                + ".");
+
         long end = System.currentTimeMillis();
-        System.out.println( "Time spent " + ( ( end - start ) / 1000.0 )
-                + " seconds." );
+        System.out.println("Time spent " + ((end - start) / 1000.0)
+                + " seconds.");
     }
-    
+
     public static void printUsage() {
         System.out.println();
-        System.out.println( "Usage: gff3Dump -model <model_name> -organism <organism_list>" );
+        System.out.println("Usage: gff3Dump -model <model_name> -organism "
+                + "<organism_list> [-dir <base_dir>]");
         System.out.println();
-        System.out.println( "\t\t<model_name>:\tThe name of WDK supported model" );
-        System.out.println( "\t\t<organism_list>: a list of organism names, delimited by a comma" );
+        System.out.println("\t\t<model_name>:\tThe name of WDK supported model;");
+        System.out.println("\t\t<organism_list>: a list of organism names, "
+                + "delimited by a comma;");
+        System.out.println("\t\t<base_dir>: Optional, the base directory for "
+                + "the output files. If not specified, the current directory "
+                + "will be used.");
         System.out.println();
     }
 }
