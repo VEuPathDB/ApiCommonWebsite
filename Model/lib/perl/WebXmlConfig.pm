b026a65e30256a758cc59de485debb26b179d128
Mark Heiges <mheiges@gmail.com>
2007-04-17 20:33:47 +0000

    perl-based Tomcat web.xml partial parser
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@15878 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/lib/perl/WebXmlConfig.pm
@@ -0,0 +1,42 @@
+package ApiCommonWebsite::Model::WebXmlConfig;
+
+use strict;
+use Carp;
+use XML::Simple;
+use File::Basename;
+use Data::Dumper;
+
+sub new {
+    my ($class, $webxml) = @_;
+    my $self = {};
+    bless $self;
+       
+    my $cfg = XMLin(
+        $webxml ||= "$ENV{GUS_HOME}/config/web.xml",
+        forcearray => ['context-parm', 'error-page'],
+        KeyAttr => {'context-param' => 'param-name'},
+    );
+    
+    for (keys %$cfg) {
+        $self->{$_} = $cfg->{$_}
+    }
+    
+    return $self;
+}
+
+
+sub getModelConfigBasename { basename($_[0]->getWdkModelConfigXml()); }
+
+# context params
+sub getWdkCustomViewDir { $_[0]->{'context-param'}->{'wdkCustomViewDir_param'}->{'param-value'}; }
+sub getWdkModelSchema { $_[0]->{'context-param'}->{'wdkModelSchema_param'}->{'param-value'}; }
+sub getWdkModelProps { $_[0]->{'context-param'}->{'wdkModelProps_param'}->{'param-value'}; }
+sub getWdkModelConfigXml { $_[0]->{'context-param'}->{'wdkModelConfigXml_param'}->{'param-value'}; }
+sub getWdkModelParser { $_[0]->{'context-param'}->{'wdkModelParser_param'}->{'param-value'}; }
+sub getWdkXmlDataDir { $_[0]->{'context-param'}->{'wdkXmlDataDir_param'}->{'param-value'}; }
+sub getWdkXmlSchema { $_[0]->{'context-param'}->{'wdkXmlSchema_param'}->{'param-value'}; }
+sub getWdkAlwaysGoToSummary { $_[0]->{'context-param'}->{'wdkAlwaysGoToSummary_param'}->{'param-value'}; }
+sub getWdkModelXml { $_[0]->{'context-param'}->{'wdkModelXml_param'}->{'param-value'}; }
+
+
+1;
