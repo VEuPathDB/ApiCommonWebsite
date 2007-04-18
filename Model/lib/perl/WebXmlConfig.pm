bb856750de0f5198df8aef66016518004e0f2615
Mark Heiges <mheiges@gmail.com>
2007-04-18 03:48:29 +0000

    cleanup; add getModelName
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@15886 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/WebXmlConfig.pm
+++ Model/lib/perl/WebXmlConfig.pm
@@ -25,18 +25,20 @@ sub new {
 }
 
 
-sub getModelConfigBasename { basename($_[0]->getWdkModelConfigXml()); }
+sub getModelConfigBasename { basename($_[0]->getWdkModelConfigXml()) }
+
+sub getModelName { scalar fileparse($_[0]->getWdkModelXml(), '.xml') }
 
 # context params
-sub getWdkCustomViewDir { $_[0]->{'context-param'}->{'wdkCustomViewDir_param'}->{'param-value'}; }
-sub getWdkModelSchema { $_[0]->{'context-param'}->{'wdkModelSchema_param'}->{'param-value'}; }
-sub getWdkModelProps { $_[0]->{'context-param'}->{'wdkModelProps_param'}->{'param-value'}; }
-sub getWdkModelConfigXml { $_[0]->{'context-param'}->{'wdkModelConfigXml_param'}->{'param-value'}; }
-sub getWdkModelParser { $_[0]->{'context-param'}->{'wdkModelParser_param'}->{'param-value'}; }
-sub getWdkXmlDataDir { $_[0]->{'context-param'}->{'wdkXmlDataDir_param'}->{'param-value'}; }
-sub getWdkXmlSchema { $_[0]->{'context-param'}->{'wdkXmlSchema_param'}->{'param-value'}; }
-sub getWdkAlwaysGoToSummary { $_[0]->{'context-param'}->{'wdkAlwaysGoToSummary_param'}->{'param-value'}; }
-sub getWdkModelXml { $_[0]->{'context-param'}->{'wdkModelXml_param'}->{'param-value'}; }
+sub getWdkCustomViewDir { $_[0]->{'context-param'}->{'wdkCustomViewDir_param'}->{'param-value'} }
+sub getWdkModelSchema { $_[0]->{'context-param'}->{'wdkModelSchema_param'}->{'param-value'} }
+sub getWdkModelProps { $_[0]->{'context-param'}->{'wdkModelProps_param'}->{'param-value'} }
+sub getWdkModelConfigXml { $_[0]->{'context-param'}->{'wdkModelConfigXml_param'}->{'param-value'} }
+sub getWdkModelParser { $_[0]->{'context-param'}->{'wdkModelParser_param'}->{'param-value'} }
+sub getWdkXmlDataDir { $_[0]->{'context-param'}->{'wdkXmlDataDir_param'}->{'param-value'} }
+sub getWdkXmlSchema { $_[0]->{'context-param'}->{'wdkXmlSchema_param'}->{'param-value'} }
+sub getWdkAlwaysGoToSummary { $_[0]->{'context-param'}->{'wdkAlwaysGoToSummary_param'}->{'param-value'} }
+sub getWdkModelXml { $_[0]->{'context-param'}->{'wdkModelXml_param'}->{'param-value'} }
 
 
 1;
