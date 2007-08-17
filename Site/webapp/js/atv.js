39246018551ddd80dadc0496b8cc40b018398e38
Mark Heiges <mheiges@gmail.com>
2007-08-17 02:56:18 +0000

    atv.js: append any authN ticket from cookie to ATVapplet.jar url to work around failures on password protected dev sites with some browsers that do not send existing cookies on applet requests (I'm looking at you OSX/Firefox 2.0.0.1).
    also, misc cleanup to generate valid html
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@17859 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/js/atv.js
+++ Site/webapp/js/atv.js
@@ -1,8 +1,9 @@
 <!-- ATV applet (phylogenetic tree viewer) launchpad -->
 function openATVWin( dataurl ) {
   var atv_window = open("", "atv_window", 
-    "width=300,height=150,status=no,toolbar=no,menubar=no,resizable=no");
-  
+    "width=300,height=250,status=no,toolbar=no,menubar=no,resizable=no");
+  window.focus();
+
   var fullurl = /^http/;
   var datasource;
   
@@ -12,26 +13,39 @@ function openATVWin( dataurl ) {
     datasource = "http://" + location.host + "/" + dataurl;
   }
   
-  atv_window.document.open();
-  atv_window.document.writeln( "<HTML>" );
-  atv_window.document.writeln( "<HEAD>" );
-  atv_window.document.writeln( "<TITLE>ATV Launchpad</TITLE>" );
-  atv_window.document.writeln( "</HEAD>" );
-  atv_window.document.writeln( "<BODY TEXT=\"#FFFFFF\" BGCOLOR=\"#000000\">" );
-  atv_window.document.writeln( "<FONT FACE=\"HELVETICA,ARIAL\">" );
-  atv_window.document.writeln( "<CENTER>" );
-  atv_window.document.writeln( "<B>Please do not close this window as long as you want to use ATV.</B>" );
-  atv_window.document.write( "<APPLET ARCHIVE=\"/ATVapplet.jar\"" );
-  atv_window.document.write( " CODEBASE=\"/\"" );
-  atv_window.document.write( " CODE=\"forester.atv_awt.ATVapplet.class\"" );
-  atv_window.document.write( " NAME=\"ATV" );
-  atv_window.document.write( " WIDTH=\"220\" HEIGHT=\"60\"" );
-  atv_window.document.writeln( " ALT=\"ATV Applet is not working on your system (requires at least Sun Java 1.5)!\">" );
-  atv_window.document.writeln( "<PARAM NAME=\"url_of_tree_to_load\" VALUE=\"" + datasource + "\">" );
-  atv_window.document.writeln( "Your browser is completely ignoring the &lt;APPLET&gt; tag!" );
-  atv_window.document.writeln( "</APPLET>" );
-  atv_window.document.writeln( "</CENTER>" );
-  atv_window.document.writeln( "</BODY>" );
-  atv_window.document.writeln( "</HTML>" );
-  atv_window.document.close();  
+  var w = atv_window.document;
+  w.open("text/html", "replace");
+  w.writeln( "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://www.w3.org/TR/html4/loose.dtd'>" );
+  w.writeln( "<html>" );
+  w.writeln( "<head>" );
+  w.writeln( "<title>ATV Launchpad</title>" );
+  w.writeln( "</HEAD>" );
+  w.writeln( "<body text='#000000' bgcolor='#DDDDDD'>" );
+  w.writeln( "<center>" );
+  w.writeln( "<font face='helvetica,arial'>" );
+  w.writeln( "<b>Control window for ATV tree viewer.</b><br><br>" );
+  w.writeln( "<b>Please do not close this window as long as you want to use ATV.</b>" );
+  w.writeln( "</font>" );
+  w.writeln( "<hr>" );
+  w.writeln( "<font face='helvetica,arial'>" );
+  w.writeln( "<font size='-1'>ApiDB Bioinformatics Resource Center for Biodefense and Emerging/Re-emerging Infectious Diseases.</font>" );
+  w.writeln( "<br><br>" );
+  w.writeln( "<applet archive='/ATVapplet.jar?" + parameterizedCookie() + "'" ); // append any authN ticket
+  w.writeln( " codebase='/'" );
+  w.writeln( " code='forester.atv_awt.ATVapplet.class'" );
+  w.writeln( " align='middle'" );
+  w.writeln( " width='220' height='60'" );
+  w.writeln( " alt='ATV Applet is not working on your system (requires at least Sun Java 1.5)!'>" );
+  w.writeln( "<param name='url_of_tree_to_load' value='" + datasource + "'>" );
+  w.writeln( "Your browser is completely ignoring the &lt;APPLET&gt; tag!" );
+  w.writeln( "</applet>" );
+  w.writeln( "</font>" );
+  w.writeln( "</center>" );
+  w.writeln( "</body>" );
+  w.writeln( "</html>" );
+  w.close();  
+}
+
+function parameterizedCookie() {
+  return document.cookie.replace(/; /, "&");
 }
