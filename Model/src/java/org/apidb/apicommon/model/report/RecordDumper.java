169ae7dfb8c8a38a5393e8b698319a366887d2c3
Xin Gao <xingao@upenn.edu>
2007-09-05 16:52:05 +0000

    fix full record dumper to work with new model
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@18077 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/report/RecordDumper.java
+++ Model/src/java/org/apidb/apicommon/model/report/RecordDumper.java
@@ -15,7 +15,6 @@ import org.apache.log4j.Logger;
 import org.gusdb.wdk.model.Answer;
 import org.gusdb.wdk.model.Field;
 import org.gusdb.wdk.model.Question;
-import org.gusdb.wdk.model.QuestionSet;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
@@ -27,149 +26,146 @@ import org.gusdb.wdk.model.report.Reporter;
  * 
  */
 public class RecordDumper {
-    
+
     private static final Logger logger = Logger.getLogger(RecordDumper.class);
-    
+
     /**
      * @param args
      * @throws WdkModelException
      * @throws WdkUserException
      * @throws IOException
      */
-    public static void main( String[ ] args ) throws WdkModelException,
+    public static void main(String[] args) throws WdkModelException,
             WdkUserException, IOException {
-        if ( args.length != 6 && args.length != 8 ) {
-            System.err.println( "Invalid parameters." );
+        if (args.length != 6 && args.length != 8) {
+            System.err.println("Invalid parameters.");
             printUsage();
-            System.exit( -1 );
+            System.exit(-1);
         }
-        Map< String, String > cmdArgs = new HashMap< String, String >();
-        for ( int i = 0; i < args.length - 1; i += 2 ) {
-            cmdArgs.put( args[ i ].trim().toLowerCase(), args[ i + 1 ].trim() );
+        Map<String, String> cmdArgs = new HashMap<String, String>();
+        for (int i = 0; i < args.length - 1; i += 2) {
+            cmdArgs.put(args[i].trim().toLowerCase(), args[i + 1].trim());
         }
-        
-        // get params
-        String modelName = cmdArgs.get( "-model" );
-        String organismArg = cmdArgs.get( "-organism" );
-        String typeArg = cmdArgs.get( "-type" );
-        String baseDir = cmdArgs.get( "-dir" );
-        if ( modelName == null || organismArg == null ) {
-            System.err.println( "Missing parameters." );
-            printUsage();
-            System.exit( -1 );
-        }
-        if ( baseDir == null || baseDir.length() == 0 ) baseDir = ".";
-        
-        // TEST
-        System.out.println( "Initializing...." );
-        
-        // construct wdkModel
-        WdkModel model = WdkModel.construct( modelName );
-        QuestionSet qset = model.getQuestionSet( "DataDumpQuestions" );
-        
-        // get type list
-        String[ ] types = typeArg.split( "," );
-        for ( String type : types ) {
-            type = type.trim();
-            if ( !type.equalsIgnoreCase( "sequence" ) && !type.equalsIgnoreCase( "gene" ) ) {
-                System.err.println( "Unsupported record type: " + type );
-                printUsage();
-                System.exit( -1 );
-            }
-        }
-        
-        String[ ] organisms = organismArg.split( "," );
-        for ( String organism : organisms ) {
-            for ( String type : types ) {
-                dumpOrganism( qset, organism.trim(), type.trim(), baseDir );
-            }
-        }
-        System.out.println( "Finished." );
-    }
-    
-    private static void dumpOrganism( QuestionSet qset, String organism,
 
-    String type, String baseDir ) throws WdkUserException, WdkModelException,
-            IOException {
-        long start = System.currentTimeMillis();
-        
+        // get params
+        String modelName = cmdArgs.get("-model");
+        String organismArg = cmdArgs.get("-organism");
+        String typeArg = cmdArgs.get("-type");
+        String baseDir = cmdArgs.get("-dir");
+        if (modelName == null || organismArg == null) {
+            System.err.println("Missing parameters.");
+            printUsage();
+            System.exit(-1);
+        }
+        if (baseDir == null || baseDir.length() == 0) baseDir = ".";
+
         // TEST
-        logger.info( "Dumping " + type + " records for " + organism
-                + "..." );
-        
+        System.out.println("Initializing....");
+
+        // construct wdkModel
+        WdkModel model = WdkModel.construct(modelName);
+
+        // get type list
+        String[] types = typeArg.split(",");
+        for (String type : types) {
+            type = type.trim();
+            if (!type.equalsIgnoreCase("sequence")
+                    && !type.equalsIgnoreCase("gene")) {
+                System.err.println("Unsupported record type: " + type);
+                printUsage();
+                System.exit(-1);
+            }
+        }
+
+        String[] organisms = organismArg.split(",");
+        for (String organism : organisms) {
+            for (String type : types) {
+                dumpOrganism(model, organism.trim(), type.trim(), baseDir);
+            }
+        }
+        System.out.println("Finished.");
+    }
+
+    private static void dumpOrganism(WdkModel wdkModel, String organism,
+            String type, String baseDir) throws WdkUserException,
+            WdkModelException, IOException {
+        long start = System.currentTimeMillis();
+
+        // TEST
+        logger.info("Dumping " + type + " records for " + organism + "...");
+
         // decide which question to use, and the name of the parameter.
         Question question = null;
-        String organismParam = "organism";
+        final String organismParam = "organism";
         String reporterName = null;
-        if ( type.equalsIgnoreCase( "gene" ) ) {
-            question = qset.getQuestion( "GeneGffQuestion" );
+        if (type.equalsIgnoreCase("gene")) {
+            question = (Question) wdkModel.resolveReference("GeneDumpQuestions.GeneDumpQuestion");
             reporterName = "fullRecordDump";
-        } else if ( type.equalsIgnoreCase( "sequence" ) ) {
-            question = qset.getQuestion( "SequenceGffQuestion" );
+        } else if (type.equalsIgnoreCase("sequence")) {
+            question = (Question) wdkModel.resolveReference("SequenceDumpQuestions.SequenceDumpQuestion");
             reporterName = "fullRecord";
         } else { // something wrong here, not supported record type
-            throw new WdkModelException( "Unsupported Record Type: " + type );
+            throw new WdkModelException("Unsupported Record Type: " + type);
         }
-        
+
         // get report maker attributes and tables
-        Map< String, Field > fields = question.getReportMakerFields();
+        Map<String, Field> fields = question.getReportMakerFields();
         StringBuffer sbFields = new StringBuffer();
-        for ( String fieldName : fields.keySet() ) {
-            if ( sbFields.length() > 0 ) sbFields.append( "," );
-            sbFields.append( fieldName );
+        for (String fieldName : fields.keySet()) {
+            if (sbFields.length() > 0) sbFields.append(",");
+            sbFields.append(fieldName);
         }
-        
+
         // make the configuration for the reporter
-        Map< String, String > config = new LinkedHashMap< String, String >();
-        config.put( Reporter.FIELD_FORMAT, "text" );
-        config.put( FullRecordReporter.FIELD_SELECTED_COLUMNS,
-                sbFields.toString() );
-        config.put( FullRecordReporter.FIELD_HAS_EMPTY_TABLE, "yes" );
-        
+        Map<String, String> config = new LinkedHashMap<String, String>();
+        config.put(Reporter.FIELD_FORMAT, "text");
+        config.put(FullRecordReporter.FIELD_SELECTED_COLUMNS,
+                sbFields.toString());
+        config.put(FullRecordReporter.FIELD_HAS_EMPTY_TABLE, "yes");
+
         // ask the question
-        Map< String, Object > params = new LinkedHashMap< String, Object >();
-        params.put( organismParam, organism );
-        Answer sqlAnswer = question.makeAnswer( params, 1, 1 );
-        
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        params.put(organismParam, organism);
+        Answer sqlAnswer = question.makeAnswer(params, 1, 1);
+
         // decide the path-file name
-        File dir = new File( baseDir, organism.replace( ' ', '_' ) );
-        if ( !dir.exists() || !dir.isDirectory() ) dir.mkdirs();
-        int pos = organism.indexOf( " " );
+        File dir = new File(baseDir, organism.replace(' ', '_'));
+        if (!dir.exists() || !dir.isDirectory()) dir.mkdirs();
+        int pos = organism.indexOf(" ");
         String fileName = organism;
-        if ( pos >= 0 )
-            fileName = organism.substring( 0, 1 ).toLowerCase() + "_"
-                    + organism.substring( pos + 1 );
+        if (pos >= 0)
+            fileName = organism.substring(0, 1).toLowerCase() + "_"
+                    + organism.substring(pos + 1);
         fileName += "_" + type + ".txt";
-        File file = new File( dir, fileName );
-        
+        File file = new File(dir, fileName);
+
         // output the result
-        OutputStream out = new FileOutputStream( file );
-        Reporter seqReport = sqlAnswer.createReport( reporterName, config );
-        seqReport.write( out );
+        OutputStream out = new FileOutputStream(file);
+        Reporter seqReport = sqlAnswer.createReport(reporterName, config);
+        seqReport.write(out);
         out.close();
-        
+
         // TEST
-        System.out.println( "Dump file saved at " + file.getAbsolutePath()
-                + "." );
-        
+        System.out.println("Dump file saved at " + file.getAbsolutePath() + ".");
+
         long end = System.currentTimeMillis();
-        System.out.println( "Time spent " + ( ( end - start ) / 1000.0 )
-                + " seconds." );
+        System.out.println("Time spent " + ((end - start) / 1000.0)
+                + " seconds.");
     }
-    
+
     public static void printUsage() {
         System.out.println();
-        System.out.println( "Usage: wdkRecordDump -model <model_name> -organism "
-                + "<organism_list> -type <record_type_list> [-dir <base_dir>]" );
+        System.out.println("Usage: wdkRecordDump -model <model_name> -organism "
+                + "<organism_list> -type <record_type_list> [-dir <base_dir>]");
         System.out.println();
-        System.out.println( "\t\t<model_name>:\tThe name of WDK supported model" );
-        System.out.println( "\t\t<organism_list>: a list of organism names, "
-                + "delimited by a comma;" );
-        System.out.println( "\t\t<record_type_list>: a list of record types, "
-                + "current support: gene, sequence." );
-        System.out.println( "\t\t<base_dir>: Optional, the base directory for "
+        System.out.println("\t\t<model_name>:\tThe name of WDK supported model");
+        System.out.println("\t\t<organism_list>: a list of organism names, "
+                + "delimited by a comma;");
+        System.out.println("\t\t<record_type_list>: a list of record types, "
+                + "current support: gene, sequence.");
+        System.out.println("\t\t<base_dir>: Optional, the base directory for "
                 + "the output files. If not specified, the current directory "
-                + "will be used." );
+                + "will be used.");
         System.out.println();
     }
 }
