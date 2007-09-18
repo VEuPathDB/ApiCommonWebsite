c768e83cfb2557611e94e2e0be3dda39f30bf23b
Cristina Aurrecoechea <aurreco@uga.edu>
2007-09-18 14:15:37 +0000

    now it is resizable
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@18275 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/js/newwindow.js
+++ Site/webapp/js/newwindow.js
@@ -1,6 +1,6 @@
 var newwindow;
 function poptastic(url,name)
 {
-	newwindow=window.open(url,name,'scrollbars=yes,height=700,width=700');
+	newwindow=window.open(url,name,'resizable=yes,scrollbars=yes,height=700,width=700');
 	if (window.focus) {newwindow.focus()}
 }
