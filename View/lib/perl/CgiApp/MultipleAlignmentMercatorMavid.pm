b339fbbd4b9a8719854d19e8d5363f5b7a5da0f3
John Brestelli <jbrestell@gmail.com>
2007-11-01 23:31:44 +0000

    translate the input for all contigs so any genomic sequence can be used.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@18965 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/MultipleAlignmentMercatorMavid.pm
+++ View/lib/perl/CgiApp/MultipleAlignmentMercatorMavid.pm
@@ -27,16 +27,15 @@ sub getCndsrcBin {$_[0]->{cndsrc_bin}}
 sub run {
   my ($self, $cgi) = @_;
 
-  $| = 1;
-
   my $dbh = $self->getQueryHandle($cgi);
 
   print STDOUT $cgi->header('text/plain');
 
-  my ($ref, $contig, $start, $stop, $strand, $type) = validateParams($cgi, $dbh);
+  my ($contig, $start, $stop, $strand, $type) = validateParams($cgi, $dbh);
   my ($agpDir, $alignDir, $sliceAlign, $fa2clustal) = $self->validateMacros();
 
-  my ($mapStart, $mapStop) = &validateMapCoordinates($alignDir, $contig, $start, $stop);
+  my ($genome, $assembly, $assemblyStart, $assemblyStop, $assemblyStrand) = &translateCoordinates($contig, $agpDir, $start, $stop, $strand);
+  my ($mapStart, $mapStop) = &validateMapCoordinates($genome, $alignDir, $assembly, $assemblyStart, $assemblyStop);
 
   if($mapStart && $mapStop) {
     print STDOUT "The Genomic Coordinates provided fall outside a mapped region!\n\n";
@@ -44,11 +43,11 @@ sub run {
     exit(0);
   }
 
-  my $multiFasta = makeAlignment($alignDir, $agpDir, $sliceAlign, $ref, $contig, $start, $stop, $strand);
+  my $multiFasta = makeAlignment($alignDir, $agpDir, $sliceAlign, $genome, $assembly, $assemblyStart, $assemblyStop, $assemblyStrand);
 
   if($type eq 'fasta_ungapped') {
       my $seqs = &makeUngappedSeqs($multiFasta);
-      
+
       my $seqIO = Bio::SeqIO->new(-fh => \*STDOUT, -format => 'fasta');
       foreach my $seq(@$seqs) {
 	  $seqIO->write_seq($seq);
@@ -56,7 +55,7 @@ sub run {
       $seqIO->close();
   }
   elsif($type eq 'clustal') {
-      my $clustal = &makeClustal($fa2clustal, $multiFasta, $cgi);
+      my $clustal = &makeClustal($fa2clustal, $multiFasta, $cgi, $genome);
   }
   else {
       print STDOUT $multiFasta;
@@ -66,13 +65,91 @@ sub run {
 
 #--------------------------------------------------------------------------------
 
+sub translateCoordinates {
+  my ($contig, $agpDir, $start, $stop, $strand) = @_;
+
+  opendir(DIR, $agpDir) or &error("Could not open directory $agpDir for reading:$!");
+
+  my ($genome, $assembly);
+
+  while (defined (my $fn = readdir DIR) ) {
+    next unless($fn =~ /([\w\d_]+)\.agp$/);
+
+    my $thisGenome = $1;
+
+    open(AGP, "$agpDir/$fn") or &error("Cannot open file $fn for reading: $!");
+
+    while(<AGP>) {
+      chomp;
+      my @a = split(/\t/, $_);
+      my $assemblyName = $a[0];
+      my $assemblyStart = $a[1];
+      my $assemblyStop = $a[2];
+      my $contigName = $a[5];
+      my $contigStart = $a[6];
+      my $contigStop = $a[7];
+      my $contigStrand = $a[8];
+
+      next unless($contigName eq $contig);
+
+      if($genome) {
+        &error("Source_id $contig was found in multiple genomes: $genome and $thisGenome");
+      }
+      $genome = $thisGenome;
+      $assembly = $assemblyName;
+
+      if($start > $contigStop || $stop < $contigStart) {
+        &error("Please enter coordinates between $contigStart-$contigStop for $contig");
+      }
+
+      # The -1 is because sliceAlign has a 1 off error
+      if($contigStrand eq '+') {
+        $start = $assemblyStart + ($start - $contigStart) - 1;
+        $stop = $assemblyStop - ($contigStop - $stop);
+      }
+      else {
+        my $tmpStop = $stop;
+        $stop = $assemblyStop - ($start - $contigStart);
+        $start = $assemblyStart +  ($contigStop - $tmpStop) -  1;
+        $strand = $strand eq '+' ? '-' : '+';
+      }
+    }
+    close AGP;
+  }
+  close DIR;
+
+  unless($genome) {
+    &error("Genome not found for source_id $contig");
+  }
+  return($genome, $assembly, $start, $stop, $strand);
+}
+
+#--------------------------------------------------------------------------------
+
 sub validateMapCoordinates {
-  my ($alignDir, $query, $start, $stop) = @_;
+  my ($genome, $alignDir, $query, $start, $stop) = @_;
 
   my $mapfile = "$alignDir/map";
+  my $genomesFile = "$alignDir/genomes";
+
   unless(-e $mapfile) {
     &error("Map file $mapfile does not exist");
   }
+
+  unless(-e $genomesFile) {
+    &error("Genomes file $genomesFile does not exist");
+  }
+
+  my $index;
+  open(GENOME, $genomesFile) or &error("Cannot open file $genomesFile for reading: $!");
+  my $line = <GENOME>;
+  chomp $line;
+  my @genomes = split(/\t/, $line);
+  for(my $i = 0; $i < scalar(@genomes); $i++) {
+    $index = ($i + 1) * 4 if($genomes[$i] eq $genome);
+  }
+
+  close GENOME;
 
   open(MAP, $mapfile) or error("Cannot open file $mapfile for reading: $!");
 
@@ -83,9 +160,9 @@ sub validateMapCoordinates {
 
     my @a = split(/\t/, $_);
 
-    my $contig = $a[1];
-    my $mapStart = $a[2];
-    my $mapStop = $a[3];
+    my $contig = $a[$index - 3];
+    my $mapStart = $a[$index - 2];
+    my $mapStop = $a[$index - 1];
 
     if(my $hash = $mapped{$contig}) {
       $mapped{$contig}->{start} = $hash->{start} < $mapStart ? $hash->{start} : $mapStart;
@@ -98,7 +175,7 @@ sub validateMapCoordinates {
   close MAP;
 
   unless($mapped{$query}) {
-    &error("Could not find [$query] in the map file... perhaps this is not from the reference strain?\n");
+    &error("The coordinates $start-$stop fall outside a mapped region.");
   }
 
   my $mapStart = $mapped{$query}->{start};
@@ -144,27 +221,20 @@ sub validateMacros {
 sub validateParams {
   my ($cgi, $dbh) = @_;
 
-  my $genome       = $cgi->param('genome');
   my $contig       = $cgi->param('contig');
   my $start        = $cgi->param('start');
   my $stop         = $cgi->param('stop');
-  my $strand       = $cgi->param('strand');
+  my $revComp      = $cgi->param('revComp');
   my $type         = $cgi->param('type');
-
-  unless($genome) {
-    error("Reference Gemonome not specified");
-  }
 
   &validateContig($contig, $dbh);
 
-  if($strand eq 'forward') {
-      $strand = '+';
-  }
-  elsif($strand eq 'reverse') {
+  my $strand;
+  if($revComp eq 'on') {
       $strand = '-';
   }
   else {
-    &error("unrecognized strand [$strand]\n");
+    $strand = '+';
   }
 
   unless($type eq 'clustal' || $type eq 'fasta_gapped' || $type eq 'fasta_ungapped') {
@@ -181,7 +251,7 @@ sub validateParams {
   if ($start < 1 || $stop < 1 || $stop <= $start) {
     &error("Start and End must be positive, and Start must be less than End");
   }
-  return ($genome, $contig, $start, $stop, $strand, $type);
+  return ($contig, $start, $stop, $strand, $type);
 }
 
 #--------------------------------------------------------------------------------
@@ -203,7 +273,7 @@ EOSQL
   $sth->execute(uc($contig));
 
   unless(my ($id) = $sth->fetchrow_array()) {
-    #&error("Invalid source ID:  $contig\n");
+    &error("Invalid source ID:  $contig\n");
   }
   $sth->finish();
 
@@ -245,25 +315,26 @@ sub replaceAssembled {
     if($assembly eq $input && 
        (($start >= $assemblyStart && $start <= $assemblyStop) || ($stop >= $assemblyStart && $stop <= $assemblyStop))) {
       
-        my ($newStart, $newStop, $newStrand);
+      my ($newStart, $newStop, $newStrand);
 
-        if($contigStrand eq '+') {
-          $newStart = $start < $assemblyStart ? $contigStart : $start - $assemblyStart + $contigStart + 1;
-          $newStop = $stop > $assemblyStop ? $contigStop : $stop - $assemblyStart + $contigStart; 
-          $newStrand = $contigStrand;
-        }
-        else {
-          $newStart = $start < $assemblyStart ? $contigStop : $assemblyStop - $start + $contigStart - 1;
-          $newStop = $stop > $assemblyStop ? $contigStart : $assemblyStop - $stop + $contigStart;  
-          $newStrand = $strand eq '+' ? '-' : '+';
-        }
+      # the +1 and -1 is because of a 1 off error in the sliceAlign program
+      if($contigStrand eq '+') {
+        $newStart = $start < $assemblyStart ? $contigStart : $start - $assemblyStart + $contigStart + 1;
+        $newStop = $stop > $assemblyStop ? $contigStop : $stop - $assemblyStart + $contigStart; 
+        $newStrand = $strand;
+      }
+      else {
+        $newStart = $start < $assemblyStart ? $contigStop : $assemblyStop - $start + $contigStart - 1;
+        $newStop = $stop > $assemblyStop ? $contigStart : $assemblyStop - $stop + $contigStart;  
+        $newStrand = $strand eq '+' ? '-' : '+';
+      }
 
-	if($newStart <= $newStop) {
-	    push(@v, "$contig:$newStart-$newStop($newStrand)");
-	}
-	else {
-	    push(@v, "$contig:$newStop-$newStart($newStrand)");
-	}
+      if($newStart <= $newStop) {
+        push(@v, "$contig:$newStart-$newStop($newStrand)");
+      }
+      else {
+        push(@v, "$contig:$newStop-$newStart($newStrand)");
+      }
     }
   }
   close FILE;
@@ -284,12 +355,11 @@ sub makeAlignment {
   for(my $i = 0; $i < scalar (@lines); $i++) {
     my $line = $lines[$i];
 
-    if($line =~ /assembled/) {
-      my ($genome, $assembled, $start, $stop, $strand) = $line =~ />([a-zA-Z1-9_]+) (assembled\d+):(\d+)-(\d+)([-+])/;
-      my $replaced = &replaceAssembled($agpDir, $genome, $assembled, $start, $stop, $strand);
+    my ($genome, $assembled, $start, $stop, $strand) = $line =~ />([a-zA-Z1-9_]+) ([\w\d]+):(\d+)-(\d+)([-+])/;
+    next unless($genome);
+    my $replaced = &replaceAssembled($agpDir, $genome, $assembled, $start, $stop, $strand);
 
-      $lines[$i] = $replaced;
-    }
+    $lines[$i] = $replaced;
 
     if($line =~ />/) {
       $lines[$i] =~ s/([+|-])$/\($1\)/;
@@ -302,38 +372,39 @@ sub makeAlignment {
 #--------------------------------------------------------------------------------
 
 sub makeClustal {
-    my ($fa2clustal, $multiFasta, $cgi) =  @_;
+  my ($fa2clustal, $multiFasta, $cgi, $genome) =  @_;
 
-    my $rv;
+  my $rv;
 
-    my $genome = $cgi->param('genome');
+  # Print the deflines on top of the clustal output
+  my @lines = split(/\n/, $multiFasta);
 
-    # Print the deflines on top of the clustal output
-    my @lines = split(/\n/, $multiFasta);
+  my ($actualStart, $actualStop, $actualStrand);
 
-    my ($actualStart, $actualStop, $actualStrand);
+  foreach my $line (@lines) {
+    if($line =~ s/^>//) {
+      print STDOUT "$line\n";
 
-    foreach my $line (@lines) {
-	if($line =~ s/^>//) {
-	    print STDOUT "$line\n";
+      if($line =~ /^$genome [\w\d]+:(\d+)-(\d+)\(([+-])\)/) {
+        $actualStart = $1;
+        $actualStop = $2;
+        $actualStrand = $3;
 
-            if($line =~ /^$genome [\w\d]+:(\d+)-(\d+)\(([+-])\)/) {
-              $actualStart = $1;
-              $actualStop = $2;
-              $actualStrand = $3;
-            }
-	}
+        $actualStart-- if($actualStrand eq '+');
+        $actualStop++ if($actualStrand eq '-');
+      }
     }
+  }
 
-    print STDOUT "\n";
+  print STDOUT "\n";
 
-    # Should try to capture the output of fa2clustal and add numbers but...this doesn't work for > 30,000 bases
-    #my $command = "perl -e 'print \"$multiFasta\"'|$fa2clustal";
-    #my $clustal = `$command`;
-    #my $clustalMod = &addPositions($clustal, $actualStart, $actualStop, $actualStrand, $genome);
+  # Should try to capture the output of fa2clustal and add numbers but...this doesn't work for > 30,000 bases
+  #my $command = "perl -e 'print \"$multiFasta\"'|$fa2clustal";
+  #my $clustal = `$command`;
+  #my $clustalMod = &addPositions($clustal, $actualStart, $actualStop, $actualStrand, $genome);
 
-    # This is a bit of a hack because I couldn't get the stuff above to work
-    my $perlCommand = "perl -e 'my \$start=$actualStart; 
+  # This is a bit of a hack because I couldn't get the stuff above to work
+  my $perlCommand = "perl -e 'my \$start=$actualStart; 
                                 my \$stop=$actualStop;
                                 my \$strand=\"$actualStrand\";
                                 while(<>) {
@@ -360,12 +431,10 @@ sub makeClustal {
                                  }
                                }'";
 
-#    print STDERR $perlCommand;
-
-    open PIPE, "|$fa2clustal|$perlCommand" or die "Cannot open pipe:$!";
-    print PIPE $multiFasta;
-    close PIPE;
-
+  #    print STDERR $perlCommand;
+  open PIPE, "|$fa2clustal|$perlCommand" or die "Cannot open pipe:$!";
+  print PIPE $multiFasta;
+  close PIPE;
 }
 
 #--------------------------------------------------------------------------------
@@ -404,37 +473,37 @@ sub addPositions {
 #--------------------------------------------------------------------------------
 
 sub makeUngappedSeqs {
-    my ($multiFasta) = @_;
+  my ($multiFasta) = @_;
 
-    my @lines = split(/\n/, $multiFasta);
-    
-    my @rv;
+  my @lines = split(/\n/, $multiFasta);
 
-    my ($defline, $seq, $start);
-    foreach my $line (@lines) {
-	if($line =~ /^>/) {
-	    if($start) {
-		my $bioSeq = Bio::Seq->new(-description => $defline, 
-					   -seq => $seq,
-					   -alphabet => "dna");
-		push(@rv, $bioSeq);
-	    }
-	    $start = 1;
-	    $defline = $line;
-	    $defline =~ s/^>//;
-	    $seq = "";
-	}
-	else {
-	    $line =~ s/-//g;
-	    $seq = $seq . $line;
-	}
+  my @rv;
+
+  my ($defline, $seq, $start);
+  foreach my $line (@lines) {
+    if($line =~ /^>/) {
+      if($start) {
+        my $bioSeq = Bio::Seq->new(-description => $defline, 
+                                   -seq => $seq,
+                                   -alphabet => "dna");
+        push(@rv, $bioSeq);
+      }
+      $start = 1;
+      $defline = $line;
+      $defline =~ s/^>//;
+      $seq = "";
     }
-    # dont' forget the last one...
-    my $bioSeq = Bio::Seq->new(-display_id => $defline, 
-			       -seq => $seq,
-			       -alphabet => "dna");
-    push(@rv, $bioSeq);
-    return \@rv;
+    else {
+      $line =~ s/-//g;
+      $seq = $seq . $line;
+    }
+  }
+  # dont' forget the last one...
+  my $bioSeq = Bio::Seq->new(-display_id => $defline, 
+                             -seq => $seq,
+                             -alphabet => "dna");
+  push(@rv, $bioSeq);
+  return \@rv;
 }
 
 #--------------------------------------------------------------------------------
