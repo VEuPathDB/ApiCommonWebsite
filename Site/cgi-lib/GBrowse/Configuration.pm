f801879c1685b126c43d3a812c96a7a01c7712be
Mark Heiges <mheiges@gmail.com>
2008-01-23 18:06:35 +0000

    add module of methods for configuring attributes of gbrowse tracks. this version just for selected attributes of mass spec tracks to control the depth of redundant peptides shown.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@19904 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/cgi-lib/GBrowse/Configuration.pm
@@ -0,0 +1,79 @@
+package GBrowse::Configuration;
+
+
+sub new {
+  my $class = shift;
+  my $self  = {
+    track_no => 0,
+  };
+  bless ($self, $class);
+  return $self;
+}
+
+# For use with link_target.
+# Returns onmouseover popup for mass spec peptides
+# if 'bump density' is exceeded, the popup includes a note that
+# data is incomplete. If 'link_target density' is exceeded, undef
+# is returned and no popup will be displayed.
+# Example:
+# " onmouseover=";return escape('<TABLE><TR><TD></TD><TD>...</TD></TR></TABLE>')
+sub link_target_ms_peptides {  
+    my ($self, $feat, $panel, $track) = @_;
+
+    my $link_target_density = $track->option('link_target density');
+    my $bump_density = $track->option('bump density');
+
+    return if ( $link_target_density && 
+                @{$track->{parts}} > $link_target_density );
+    
+    my ($extdbname) = $feat->get_tag_values('ExtDbName');
+    my ($desc) = $feat->get_tag_values('Description');
+    my ($seq) = $feat->get_tag_values('Sequence');
+    $desc .= "sequence: $seq";
+    $desc =~ s/[\r\n]/<br>/g;
+
+    $extdbname =~ s/Wastling MassSpec/assay: /i;
+    $extdbname =~ s/Lowery MassSpec/assay: /i;
+    
+    my $content = "$extdbname<br>$desc";
+
+    if ( $bump_density && 
+         @{$track->{parts}} > $bump_density ) {
+         
+        my $span = $feat->{start} . '-' . $feat->{end};
+        if ($track->{'seen'}->{$span}++) {
+            return;
+        }
+        
+        $content = q(<font color='red'>non-redundant, representative data</font><br>) . $content;
+    }
+       
+    my @data;
+    push @data, [ '' => $content ];
+    $self->popup_template('', \@data);
+}
+
+# keep only $depth features for each span.
+# A feature may be used in multiple tracks so $name is required
+# to permitting counting features for each track.
+sub filter_to_depth {
+    my ($self, $feat, $name, $depth) = @_;
+    return 1 unless $depth;
+    my $span = $feat->{start} . '-' . $feat->{end} . ':' . $feat->{strand};
+    $self->{$name}->{$span}++ < $depth;
+}
+
+sub popup_template { 
+  my ($self, $name, $data) = @_; 
+  use HTML::Template; 
+  my $tmpl = HTML::Template->new(filename => $ENV{DOCUMENT_ROOT}.'/gbrowse/hover.tmpl'); 
+  $tmpl->param(DATA => [ map { { Key => $_->[0], Value => $_->[1], } } @$data ]); 
+  my $str = $tmpl->output; 
+  $str =~ s/'/\\'/g; 
+  $str =~ s/\s+$//; 
+  $str =~ s/\\n//; 
+  return qq{" onmouseover="$cmd;return escape('$str') };
+}
+
+
+1;
