d65e99e72f9edfb9fb73a5f23c30c8d8dbfc3885
Cary Pennington <carypen@_.com>
2008-02-05 18:25:20 +0000

    removed commented code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@20200 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/js/Top_menu.js
+++ Site/webapp/js/Top_menu.js
@@ -20,25 +20,6 @@ function navigation_toggle(area,p_name)
 			}
 		}
 	
-/*
-		if(document.getElementById('Eukaryotic Pathogens_area').style.display=="") {
-			document.getElementById('Eukaryotic Pathogens').className = "";
-			renameInputs('Eukaryotic Pathogens_area','none');
-			Effect.toggle('Eukaryotic Pathogens_area','slide',{duration: .5});
-		}
-		
-		if(document.getElementById('Apicomplexan_area').style.display==""){
-			document.getElementById('Apicomplexan').className = "";
-			renameInputs('Apicomplexan_area','none');
-			Effect.toggle('Apicomplexan_area','slide',{duration: .5}); 
-		}
-		
-		if(document.getElementById('Anaerobic Protists_area').style.display==""){
-			document.getElementById('Anaerobic Protists').className = "";
-			renameInputs('Anaerobic Protists_area','none');
-			Effect.toggle('Anaerobic Protists_area','slide',{duration: .5});
-		}
-*/		
 		var inputs = document.getElementById(area+'_area').getElementsByTagName('Input');
 		document.getElementById(area).className = "seled";
 		renameInputs(area+'_area','myMultiProp('+p_name+')');				
