e6de3cf3671baaa68528763c024ada17b4a53b25
Cary Pennington <carypen@_.com>
2008-04-18 19:38:30 +0000

    updated
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api_1-17-1_filter@21261 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/js/filter_menu.js
+++ Site/webapp/js/filter_menu.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 				var historyId = $("#history_id").val();
 				
 				var quesForm = $("form",data);
-				quesForm.prepend("<input type='radio' name='myProp(booleanExpression)' value='" + historyId + " AND'/>&nbsp;AND&nbsp;<input type='radio' name='myProp(booleanExpression)' value='" + historyId + " OR'>&nbsp;OR&nbsp;<input type='radio' name='myProp(booleanExpression)' value='" + historyId + " NOT'>&nbsp;NOT&nbsp<br>");
+				quesForm.prepend("<span class='radio'><b>Operator</b><input type='radio' name='myProp(booleanExpression)' value='" + historyId + " AND'/>&nbsp;AND&nbsp;<input type='radio' name='myProp(booleanExpression)' value='" + historyId + " OR'>&nbsp;OR&nbsp;<input type='radio' name='myProp(booleanExpression)' value='" + historyId + " NOT'>&nbsp;NOT&nbsp</span><br>");
 				var action = quesForm.attr("action").replace(/processQuestion/,"processFilter");
 				quesForm.attr("action",action);
 				$("#query_form").html(quesForm);
