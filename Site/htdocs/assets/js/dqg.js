c3e4a559865568ba521e8dccf0c8707fad7d0946
Cary Pennington <carypen@_.com>
2008-05-22 14:20:41 +0000

    new interface
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api_1-17-1_filter@21720 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/htdocs/assets/js/dqg.js
@@ -0,0 +1,38 @@
+var dqg_speed = "fast";
+var sidebar_speed = "fast";
+$(document).ready(function(){
+	//hide all of the sub list
+	$("div.sub_list").hide();
+//	$("ul.heading_list li img").hide();
+	$("ul.heading_list li a.heading").click(function(){
+		var sublist = $(this).parents("li").find("div.sub_list");
+		sublist.slideToggle(dqg_speed);
+		return false;
+	});
+	
+	$("span.small_links a").click(function(){
+		var val = $(this).attr("href");
+		var headList = $(this).parent().siblings("ul.heading_list");
+		if(val == "true"){
+			$("div.sub_list", headList).slideDown(dqg_speed);
+		}else{
+			$("div.sub_list", headList).slideUp(dqg_speed);
+		}
+		return false;
+	});
+	
+	var top_div = $("div#menu_lefttop");
+	$("div", top_div).hide();
+	var op = $("div:first", top_div);
+	$("div:first", top_div).show();
+	$("a.heading", top_div).click(function(){
+		var testOp = op.prev("a").text();
+		if(op.prev("a").text() != $(this).text()){
+			op.hide(sidebar_speed);
+			op = $(this).next("div");
+			$(this).next("div").show(sidebar_speed);
+		}
+		return false;
+	});
+});
+
