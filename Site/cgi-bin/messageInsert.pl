9e83c0f8e8658da02d24f900f5ed48ab13cd27bb
Ryan Thibodeau <ryanthib@_.com>
2008-05-28 20:55:39 +0000

    Removed redirect, added subroutine to validate data, added generated standardized form for both new message submits and edits.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@21823 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-bin/messageInsert.pl
+++ Site/cgi-bin/messageInsert.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -Tw 
 
-
 use CGI qw/:standard/;
 use strict;
 use warnings;
@@ -9,6 +8,7 @@ use DBI qw(:sql_types);
 use lib map { /(.*)/ } split /:/, $ENV{PERL5LIB}; # untaint PERL5LIB 
 use ApiCommonWebsite::Model::CommentConfig;
 use HTTP::Headers;
+use Time::Local;
 
 # Print the content and no-cache headers
 my $headers = HTTP::Headers->new(
@@ -39,31 +39,45 @@ my $editResult;
 my $updateResult;
 
 
-# Is this a new message submission?
-if ($query->param("newMessage")){
-   $insertResult=&insertMessage();
-   }
+if ($query->param("submitMessage"))
+  # Display new message form
+    {
+     &displayMessageForm();
+    exit(1);
+    }
+
 
 # Is this a message edit?
-if ($query->param("editMessageId")){
+if ($query->param("messageId")){
    $editResult=&editMessage();
    }
-
-
-# Is this a message update?
-if ($query->param("updateMessageId")){
+   
+   elsif ($query->param("updateMessageId")){
    $updateResult=&updateMessage();
    }
 
+    else{
+     # This is a new message submission
+       $insertResult=&insertMessage();
+       }
 
+# Is this a message update?
+#if ($query->param("updateMessageId")){
+  # $updateResult=&updateMessage();
+  # } 
+##########################################################
     sub insertMessage(){
         
         my $messageText=$query->param("messageText");
         my $messageCategory=$query->param("messageCategory");
-        my @selectedProjects=$query->param("projects");
+        my @selectedProjects=$query->param("selectedProjects");
         my $startDate=$query->param("startDate");
         my $stopDate=$query->param("stopDate");
         my $adminComments=$query->param("adminComments");
+
+       # Validate data from form
+       if (&validateData($messageCategory, \@selectedProjects, $messageText, $startDate, $stopDate, $adminComments)){
+
 
         ###Begin DB Transaction###
         eval{
@@ -102,31 +116,20 @@ if ($query->param("updateMessageId")){
         # Attempt DB commit, rollback if any errrors occur    
         $dbh->commit();
         };
-
         if ($@) {
           warn "Unable to process database transaction. Rolling back as a result of: $@\n";
           eval{ $dbh->rollback() };
           }
          ###End DB Transaction###
-
-         # XHTML redirect to updated table view
-         print<<_END_OF_TEXT_
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml" >
-        <head>
-        <meta http-equiv="REFRESH" content="0;url=http://ryanthib.giardiadb.org/cgi-bin/messageConsole.pl">
-        </head>
-        </html>
-_END_OF_TEXT_
-;
+       }
 
     } ## End insertMessage Subroutine
+###########################################################################
 
     # Retrieve message row from database for editing.
     sub editMessage(){
 
-        my $editMessageId=$query->param("editMessageId");
+        my $editMessageId=$query->param("messageId");
         
         my $sql=q(SELECT message_id, message_text, 
            message_category, 
@@ -141,89 +144,71 @@ _END_OF_TEXT_
         $sth->execute($editMessageId) or die "Can't excecute SQL";
 
          my @row;
-         
+         my $errorMessage;
+         my $messageId;
+         my $messageText;
+         my $messageCategory;
+         my $startDate;
+         my $stopDate;
+         my $adminComments;
+
          while (@row=$sth->fetchrow_array()) {
 		
-         my $editMessageId = $row[0];
-	 my $editMessageText = $row[1];
-	 my $editMessageCategory = $row[2];
-	 my $editStartDate = $row[3];
-	 my $editStopDate = $row[4];
-	 my $editAdminComments = $row[5];
-		
-		print<<_END_OF_TEXT_
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml" >
-        <head>	
-	<title>Edit Message</title>
-        <script language="javascript" type="text/javascript" src="../../include/datetimepicker.js">
-        </script>
-        <script language="JavaScript">
-        <!--
-        function refreshParent() {
-         window.opener.location.href = "../messageConsole.pl";
-
-        if (window.opener.progressWindow)
-                
-        {
-        window.opener.progressWindow.close()
-        }
-        window.close();
-        }
-        //-->
-        </script>
-
-        </head>
-	<body style="background-color: #e0e0eb"> 
-        <form method="get" name="submitEdit" action=messageInsert.pl>
-	<p><b>Message ID: $editMessageId</b>
-	<p><b>Message Category:</b>: 	
-	<select name="editMessageCategory">
-	<option value=$editMessageCategory>$editMessageCategory</option>
-	<option value ="Information">Information</option>
-	<option value ="Degraded">Degraded</option>
-	<option value ="Down">Down</option>
-	</select>
-        </p>
-        <p>Select affected systems:</p>
-        <div style="width: 140px; height: 105px; padding: 5px; line-height: 1.3; background-color: #EDE6DE; border-style: outset">
-        <input type="checkbox" name="editSelectedProjects" value="1">CryptoDB<br>
-        <input type="checkbox" name="editSelectedProjects" value="2">GiardiaDB<br>
-        <input type="checkbox" name="editSelectedProjects" value="3">PlasmodDB<br>
-        <input type="checkbox" name="editSelectedProjects" value="4">ToxoDB<br>
-        <input type="checkbox" name="editSelectedProjects" value="5">TrichDB<br>
-        </div>
-        <p><b>Message Text: </b></p>
-        <p><textarea name="editMessageText"  style="overflow: auto" rows ="5" cols="50">$editMessageText</textarea></p>     
-	<p><b>Start Date/Time:</b>
-	<textarea name="editStartDate" id="editStartDate" rows="1" cols="20" size="25">$editStartDate</textarea><a href="javascript:NewCal('editStartDate','mmddyyyy', 'true')"><img src="../../images/cal.png" width="16" height="16" border="0" alt="Pick a date"></a>	
-	<p><b>Stop Date/Time:</b>
-        <textarea name="editStopDate" id="editStopDate" rows="1" cols="20" size="25">$editStopDate</textarea><a href="javascript:NewCal('editStopDate','mmddyyyy', 'true')"><img src="../../images/cal.png" width="16" height="16" border="0" alt="Pick a date"></a>
-	<p><b>Admin Comments:</p></b> 
-	<textarea name="editAdminComments" rows="6" cols="50">$editAdminComments</textarea>
-        <input type="hidden" name="updateMessageId" value=$editMessageId>
-	<p align="center"><input type="submit" name="newInfo" value="Submit Edit"></p>
-	</form>
-        <!--Submit Form--> 
-        </body>
-	</html>
-_END_OF_TEXT_
-;
-}
+          $messageId=$row[0];
+	  $messageText=$row[1];
+	  $messageCategory=$row[2];
+	  $startDate=$row[3];
+	  $stopDate=$row[4];
+	  $adminComments=$row[5];
+         }
+         # Determine and re-select previously checked projects
+         my @selectedProjects=&getSelectedProjects($editMessageId);
+         
+         my $cryptoBox;
+         my $giardiaBox;
+         my $plasmoBox;
+         my $toxoBox;
+         my $trichBox; 
+       
+         # Pre-check previously checked project boxes  
+         foreach my $project (@selectedProjects){
+         if ($project=~/CryptoDB/){$cryptoBox="checked='checked'";}
+         if ($project=~/GiardiaDB/){$giardiaBox="checked='checked'";}
+         if ($project=~/PlasmoDB/){$plasmoBox="checked='checked'";}
+         if ($project=~/ToxoDB/){$toxoBox="checked='checked'";}
+         if ($project=~/TrichDB/){$trichBox="checked='checked'";}
+         }
+         # Display message form
+         &displayMessageForm($errorMessage,
+                             $messageId, 
+                             $messageCategory,
+                             \@selectedProjects,
+                             $messageText,
+                             $cryptoBox,
+                             $giardiaBox,
+                             $plasmoBox,
+                             $toxoBox,
+                             $trichBox,
+                             $startDate, 
+                             $stopDate, 
+                             $adminComments);
+       
 }### End editMessage subroutine
-
+##############################################################
+    
     # Write updated message record to the database.
     sub updateMessage() {
-        
-        my $updateMessageId = $query->param("updateMessageId");
-	my $updateMessageText = $query->param("editMessageText");
-        my $updateMessageCategory = $query->param("editMessageCategory");
-        my @updateSelectedProjects = $query->param("editSelectedProjects");
-        my $updateStartDate = $query->param("editStartDate");
-        my $updateStopDate =  $query->param("editStopDate");
-        my $updateAdminComments = $query->param("editAdminComments");
-        
+       
+        my $messageId = $query->param("updateMessageId");
+	my $messageText = $query->param("messageText");
+        my $messageCategory = $query->param("messageCategory");
+        my @selectedProjects = $query->param("selectedProjects");
+        my $startDate = $query->param("startDate");
+        my $stopDate =  $query->param("stopDate");
+        my $adminComments = $query->param("adminComments");
+
+       # Validate data from form
+        if (&validateData($messageCategory, \@selectedProjects, $messageText, $startDate, $stopDate, $adminComments)){
         ### Begin database transaction
         eval{
         my $sql=q(UPDATE MESSAGES SET 
@@ -234,38 +219,217 @@ _END_OF_TEXT_
                    WHERE message_id = ?);
 
        my $sth=$dbh->prepare($sql) or die "Could not prepare SQL. Check syntax.";
-       $sth->execute($updateMessageText, 
-                     $updateMessageCategory, 
-                     $updateStartDate, 
-                     $updateStopDate, 
-                     $updateAdminComments, 
-                     $updateMessageId)
+       $sth->execute($messageText, 
+                     $messageCategory, 
+                     $startDate, 
+                     $stopDate, 
+                     $adminComments, 
+                     $messageId)
           or die "Could not execute SQL.";
 
          # Delete existing message_projects rows to avoid redundant messages
          my $sqlDelete=q(DELETE FROM message_projects WHERE message_id = ?);
             $sth=$dbh->prepare($sqlDelete);
-            $sth->execute($updateMessageId)
+            $sth->execute($messageId)
               or die "Could not execute SQL.";
 
  
          # Bind message id's to revised selected projects in DB       
-        foreach my $projectID (@updateSelectedProjects) {
+        foreach my $projectID (@selectedProjects) {
             my $sqlInsert=q(INSERT INTO MESSAGE_PROJECTS (message_id, project_id) VALUES (?, ?));
                $sth=$dbh->prepare($sqlInsert);
-               $sth->execute($updateMessageId, $projectID);
+               $sth->execute($messageId, $projectID);
                }
 
        $sth->finish();
        $dbh->commit();
        };
+    }
+             if($@) {
+	     warn "Unable to process record update transaction. Rolling back as a result of: $@\n";
+	     $dbh->rollback();
+             }  
 
-    if($@) {
-	warn "Unable to process record update transaction. Rolling back as a result of: $@\n";
-	$dbh->rollback();
-    }  
+             else{
+             $updateResult="success";
+             return($updateResult);
+             }
+     
 
-}#End updateMessage Subroutine
+    }## End updateMessage Subroutine
+####################################
+sub displayMessageForm{
 
+
+        # Repopulate form with passed params
+         my $errorMessage=$_[0];
+         my $messageId=$_[1];
+         my $messageCategory=$_[2];
+         my (@selectedProjects)=@{($_[3])} if ($messageCategory);
+         my $messageText=$_[4];
+         my $cryptoBox=$_[5];
+         my $giardiaBox=$_[6];
+         my $plasmoBox=$_[7];
+         my $toxoBox=$_[8];
+         my $trichBox=$_[9];;
+         my $startDate=$_[10];
+         my $stopDate=$_[11];
+        my $adminComments=$_[12];
+        
+         if(!$messageId){
+         # Pre-check previously checked project boxes from a failed new message submission 
+         foreach my $project (@selectedProjects){
+         if ($project=~/1/){$cryptoBox="checked='checked'";}
+         if ($project=~/2/){$giardiaBox="checked='checked'";}
+         if ($project=~/3/){$plasmoBox="checked='checked'";}
+         if ($project=~/4/){$toxoBox="checked='checked'";}
+         if ($project=~/5/){$trichBox="checked='checked'";}
+         }
+         }
+
+    print<<_END_OF_TEXT_
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" >
+        <head>  
+        <title>Edit Message</title>
+        <script language="javascript" type="text/javascript" src="../../include/datetimepicker.js">
+        </script>
+        <script language="javascript" type="text/javascript">
+        function refreshParent() {
+        if (window.opener && !window.opener.closed) {
+        window.opener.location.reload();
+        }
+        //window.close();
+        }        
+        </script>
+        </head>
+        <body style="background-color: #e0e0eb"> 
+        <form method="get" name="submitEdit" action=messageInsert.pl>
+        <p style="color: red">$errorMessage</p>
+        <p><b>Message ID: $messageId</b>
+        <p><b>Message Category:</b>:    
+        <select name="messageCategory">
+        <option value=$messageCategory>$messageCategory</option>
+        <option value ="Information">Information</option>
+        <option value ="Degraded">Degraded</option>
+        <option value ="Down">Down</option>
+        </select>
+        </p>
+        <p>Select affected systems:</p>
+        <div style="width: 140px; height: 105px; padding: 5px; line-height: 1.3; background-color: #ede6de; border-style: outset">
+        <input type="checkbox" name="selectedProjects" value="1" $cryptoBox>CryptoDB<br>
+        <input type="checkbox" name="selectedProjects" value="2" $giardiaBox>GiardiaDB<br>
+        <input type="checkbox" name="selectedProjects" value="3" $plasmoBox>PlasmodDB<br>
+        <input type="checkbox" name="selectedProjects" value="4" $toxoBox>ToxoDB<br>
+        <input type="checkbox" name="selectedProjects" value="5" $trichBox>TrichDB<br>
+        </div>
+        <p><b>Message Text: </b></p>
+        <p><textarea name="messageText" style="overflow: auto" rows ="5" cols="50">$messageText</textarea></p>     
+        <p><b>Start Date/Time:</b>
+        <textarea name="startDate" id="startDate" rows="1" cols="20" size="25">$startDate</textarea><a href="javascript:NewCal('startDate','mmddyyyy', 'true')"><img src="../../images/cal.png" width="16" height="16" border="0" alt="Pick a date"></a>        
+        <p><b>Stop Date/Time:</b>
+        <textarea name="stopDate" id="stopDate" rows="1" cols="20" size="25">$stopDate</textarea><a href="javascript:NewCal('stopDate','mmddyyyy', 'true')"><img src="../../images/cal.png" width="16" height="16" border="0" alt="Pick a date"></a>
+        <p><b>Admin Comments:</p></b> 
+        <textarea name="adminComments" rows="6" cols="50">$adminComments</textarea>
+        <input type="hidden" name="updateMessageId" value="$messageId">
+        <p align="center"><input type="submit" name="newInfo" value="Submit Message" onClick="refreshParent();"></p>
+        </form>
+        <!--Submit Form--> 
+        </body>
+        </html>
+_END_OF_TEXT_
+;
+}
+
+
+####################################
+   sub getSelectedProjects(){
+
+     #Determine previously selected projects associated with given message ID.
+     my $messageID=$_[0];
+     my @selectedProjects;
+     my $sql=q(SELECT p.project_name FROM projects p, message_projects mp WHERE mp.message_ID = ? AND mp.project_ID = p.project_ID);
+     my $sth=$dbh->prepare($sql);
+     $sth->execute($messageID);
+
+     while (my @row=$sth->fetchrow_array()){
+     my $i=0;
+     push (@selectedProjects, $row[$i]);
+     $i++;
+     }
+   
+     return @selectedProjects;
+     }## End getProjects subroutine 
+
+####################################
+   sub validateData(){
+ 
+         # This is an edit. Parameters passed via call.
+         my $messageId;
+         my $messageCategory=shift;
+         my (@selectedProjects)=@{(shift)};
+         my $cryptoBox;
+         my $giardiaBox;
+         my $plasmoBox;
+         my $toxoBox;
+         my $trichBox;
+         my $messageText=shift;
+         my $startDate=shift;
+         my $stopDate=shift;
+         my $adminComments=shift;
+         my $errorMessage="";
+          
+           # Check to ensure that required fields are filled out
+           $errorMessage .= "*At least one project must be selected.<br/>" if (!@selectedProjects);
+           $errorMessage .= "*Message field is required.<br/>" if (!$messageText);
+       
+       my $convertedStartDate=$startDate;
+       my $convertedStopDate=$stopDate;
+       $convertedStartDate=~s/\s/-/;
+       $convertedStopDate=~s/\s/-/;
+       $convertedStartDate=~s/:/-/g;
+       $convertedStopDate=~s/:/-/g;
+       
+       # Convert date strings to seconds since epoch
+       (my $startMonth, my $startDay, my $startYear, my $startHour, my $startMinutes, my $startSeconds)=($convertedStartDate=~/(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)/); 
+       (my $stopMonth, my $stopDay, my $stopYear, my $stopHour, my $stopMinutes, my $stopSeconds)=($convertedStopDate=~/(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)/);
+      
+       $convertedStartDate = timelocal($startSeconds, $startMinutes, $startHour, $startDay, $startMonth-1, $startYear-1900);
+       $convertedStopDate = timelocal($stopSeconds, $stopMinutes, $stopHour, $stopDay, $stopMonth-1, $stopYear-1900);
+
+        $errorMessage .= "*Start date cannot be before stop date.<br/>" if  ($convertedStartDate >= $convertedStopDate);
+      
+        if ( $errorMessage )
+           {
+             # Errors with the form - redisplay it and return failure
+              &displayMessageForm($errorMessage,
+                                  $messageId,
+                                  $messageCategory,
+                                  \@selectedProjects,
+                                  $cryptoBox,
+                                  $giardiaBox,
+                                  $plasmoBox,
+                                  $toxoBox,
+                                  $trichBox,
+                                  $messageText,
+                                  $startDate, 
+                                  $stopDate,
+                                  $adminComments);
+
+             return 0;
+           }
+   
+        else
+           {
+        # Form OK - return success
+        return 1;
+           } 
+       
+       } 
+###################################
+ 
 #Finish and close DB connection
 $dbh->disconnect();
+
+
