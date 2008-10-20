403ff5168e64d9f7c57310a914080dcf5fa7c862
Mark Heiges <mheiges@gmail.com>
2008-10-20 01:55:09 +0000

    add apache server information
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@24310 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/admin/index.php
+++ Site/htdocs/admin/index.php
@@ -1,4 +1,13 @@
-<h3 class='banner' align='center'><a href='/'><? print $_SERVER['SERVER_NAME']?></a></h3>
+
+<h3 class='banner' align='center'><a href='/'>
+<? 
+  include_once "functions.php.inc";
+  print $_SERVER['SERVER_NAME'] . '<br>';
+  if ($upstreamServer = upstreamServer()) {
+    print "<font size='-1'>(upstream server: " . $upstreamServer . ")</font>";
+  }
+?>
+</a></h3>
 
 <fmt:formatDate type="both" pattern="${dateFormatStr}" value="<%=new Date()%>" />
 <? 
@@ -7,6 +16,7 @@ $pageMap = array(
     'Databases'     => "/a/admin/index.jsp?p=Databases",
     'WDK'           => "/a/admin/index.jsp?p=WDK",
     'Tomcat'        => "/a/admin/index.jsp?p=Tomcat",
+    'Apache'        => "/admin/apacheInfo.php",
     'Build'         => "/a/admin/index.jsp?p=Build",
     'Announcements' => "/cgi-bin/admin/messageConsole.pl"
     );
