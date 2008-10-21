851cc36bf2fa54a6817363145f91059cb2e6b8f1
Mark Heiges <mheiges@gmail.com>
2008-10-21 18:05:32 +0000

    add message when site is proxied
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@24371 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/admin/apacheInfo.php
+++ Site/htdocs/admin/apacheInfo.php
@@ -4,20 +4,42 @@ include_once "functions.php.inc";
 $modinfo = parsePHPModules();
 $apacheEnv = $modinfo['Apache Environment'];
 $httpHeaders = $modinfo['HTTP Headers Information'];
+$upstreamServer = upstreamServer();
+
+$headers = apache_request_headers();
+$proxyNotice = '';
+if ( isset($headers['Via']) ) {
+    $via = $headers['Via'];
+    $proxyNotice = "<p>This site is reverse-proxied via <i>'$via'</i> to upstream host $upstreamServer.";
+}
+?>
+<h2>Apache HTTP Server</h2>
+
+<? print $proxyNotice ?>
+<p>
+<b><a href="#" style="text-decoration:none" onclick="Effect.toggle('httpheaders','blind'); return false">HTTP Headers &#8593;&#8595;</a></b>
+<div id="httpheaders" style="padding: 5px; display: none;"><div>
+<? printTable($modinfo['HTTP Headers Information']); ?>
+</div></div>
+
+<p>
+
+<b><a href="#" style="text-decoration:none" onclick="Effect.toggle('apacheenv','blind'); return false">Apache Environment &#8593;&#8595;</a></b>
+<div id="apacheenv" style="padding: 5px; display: none;"><div>
+<? printTable($modinfo['Apache Environment']); ?>
+</div></div>
+
+<p>
+
+<b><a href="#" style="text-decoration:none" onclick="Effect.toggle('apachehandler','blind'); return false">Apache Internals &#8593;&#8595;</a></b>
+<div id="apachehandler" style="padding: 5px; display: none;"><div>
+<? printTable($modinfo['apache2handler']); ?>
+</div></div>
 
 
-printTable('HTTP Headers', $modinfo['HTTP Headers Information']);
-print '<br>';
-printTable('Apache Environment', $modinfo['Apache Environment']);
-print '<br>';
-printTable('Apache Internals', $modinfo['apache2handler']);
+<?
 
-#phpinfo(8);
-
-
-
-function printTable($title, $array) {
-  print "<b>$title</b>:";
+function printTable($array) {
   $tabledef = "<table border='0' cellspacing='3' cellpadding='2' align=''>";
   print $tabledef;
   print "<tr class='secondary3'><th><font size='-2'>Attribute</font></th><th><font size='-2'>Value</font></th></tr>";
