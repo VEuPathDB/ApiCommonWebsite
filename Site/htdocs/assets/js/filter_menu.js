7b459956a65f80e126a5d5292a24133f57c1d7fd
Cary Pennington <carypen@_.com>
2008-10-23 18:53:48 +0000

    added sub_diagram class to sub diagrams
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api_1-17-1_filter@24406 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/filter_menu.js
+++ Site/htdocs/assets/js/filter_menu.js
@@ -467,6 +467,13 @@ function ExpandStep(ele,url){
 		dataType: "html",
 		success: function(data){
 			var sub = $(".diagram",data);
+			sub.addClass("sub_diagram");
+			var parentStepId = sub.attr("id");
+			parentStepId = parentStepId.split("_")[2];
+			var parentStep = parent_strat.find("#stepId_" + parentStepId);
+			if(parentStep.attr("value") == false){
+				parentStep.text = 
+			}
 			var filter = $(".filter_link_div", data);
 			parent_strat.css({
 				height: "21em"
@@ -476,9 +483,9 @@ function ExpandStep(ele,url){
 				width: "97%",
 				top: "118px"
 			});
-			//strat_div.append(sub);
 			parent_strat.append(sub);
 			strat_div.append(filter);
+			
 		},
 		error: function(data, msg, e){
 			alert("ERROR \n " + msg + "\n" + e);
