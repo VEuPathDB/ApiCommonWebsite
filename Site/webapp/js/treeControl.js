84d6c056523d2683fde6afe549129f6c265032ab
Cary Pennington <carypen@_.com>
2008-10-29 15:34:09 +0000

    added file for interaction of the tree control
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@24503 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/webapp/js/treeControl.js
@@ -0,0 +1,75 @@
+//jQuery.noConflict();
+$(document).ready(function(){
+	var root = $(".tree");
+	var nodes = $("term-node");
+	var children = $(".term-children").hide();
+	var x = 1;
+});
+
+function toggleChildren(ele){
+	if($(ele).hasClass("plus")){
+		$(ele).attr("src","images/minus.gif");
+		$(ele).removeClass("plus");
+		$(ele).siblings(".term-children").show();
+	}else{
+		$(ele).attr("src","images/plus.gif");
+		$(ele).addClass("plus");
+		$(ele).siblings(".term-children").hide();
+	}
+}
+
+function toggleChildrenCheck(ele){
+	if($(ele).attr("checked")){
+		check(ele);
+		checkBranch(ele);
+		//$(ele).parent().parents(".term-node").children("input").attr("disabled",true).attr("checked", true);
+	}else{
+		uncheck(ele);
+		checkBranch(ele);
+	}
+}
+
+function checkBranch(ele){
+	if($(ele).parent().parent().hasClass("param")) return;
+	var any = false;
+	var all = true;
+	if(ele.checked) 
+		any = true;
+	else
+		all = false;
+	$(ele).parent().siblings("div.term-node").children("input").each(function(t){
+		if(this.checked){
+			any = true;
+		}else{ 
+			all = false;
+		}
+	});
+	if(!any)
+		all = true;
+	$(ele).parent().parent().parent().children("input").attr("disabled",!all).attr("checked", any);
+//	else
+//	$(ele).parent().parent().parent().children("input").attr("disabled",true).attr("checked", true);
+	checkBranch($(ele).parent().parent().parent().children("input")[0]);
+}
+
+function uncheck(ele){
+	$(ele).attr("checked",false);
+	var childDiv = $(ele).siblings(".term-children");
+	if(childDiv.length > 0){
+		var kids = $(childDiv).children(".term-node input");
+		for(var i=0;i<kids.length;i++){
+			uncheck($(kids[i]).children("input")[0]);
+		}
+	}
+}
+
+function check(ele){
+	$(ele).attr("checked",true);
+	var childDiv = $(ele).siblings(".term-children");
+	if(childDiv.length > 0){
+		var kids = $(childDiv).children(".term-node input");
+		for(var i=0;i<kids.length;i++){
+			check($(kids[i]).children("input")[0]);
+		}
+	}
+}
