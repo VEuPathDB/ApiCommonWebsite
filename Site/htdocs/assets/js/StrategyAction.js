0626c9e57ef6ba9f2ead1164bed5337dc79a5b01
Cary Pennington <carypen@_.com>
2008-12-04 21:21:38 +0000

    typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api_1-17-1_filter@25283 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/StrategyAction.js
+++ Site/htdocs/assets/js/StrategyAction.js
@@ -220,9 +220,9 @@ function createDetails(ele, strat, step){
 		"		<div class='crumb_menu'>"+
 		"			<a class='rename_step_link' href='javascript:void(0)' onclick='Rename_Step(this)'>Rename</a>&nbsp;|&nbsp;"+
 		"			<a class='view_step_link' onclick='NewResults(" + strat + "," + id + ")' href='javascript:void(0)'>View</a>&nbsp;|&nbsp;"+
-		"			<a class='edit_step_link' href='javascript:void(0)' onclick='Edit_Step(" + strat + "," + id + ",\"" + questionFullName + "\",\"" + urlParams + "\")' id='1|0|'>Edit</a>&nbsp;|&nbsp;"+
-		"			<a class='expand_step_link' href='javascript:void(0)' onclick='Expand_Step(" + strat + "," + id + ",\"" + collapsedName + "\")'>Expand</a>&nbsp;|&nbsp;"+
-		"			<a class='insert_step_link' id='1|0' href='javascript:void(0)' onclick='Insert_Step(" + strat + "," + id + ")'>Insert Before</a>"+
+		"			<a class='edit_step_link' href='javascript:void(0)' onclick='EditStep(" + strat + "," + id + ",\"" + questionFullName + "\",\"" + urlParams + "\")' id='1|0|'>Edit</a>&nbsp;|&nbsp;"+
+		"			<a class='expand_step_link' href='javascript:void(0)' onclick='ExpandStep(" + strat + "," + id + ",\"" + collapsedName + "\")'>Expand</a>&nbsp;|&nbsp;"+
+		"			<a class='insert_step_link' id='1|0' href='javascript:void(0)' onclick='InsertStep(" + strat + "," + id + ")'>Insert Before</a>"+
 		"			&nbsp;|&nbsp;"+
 		"			<a class='delete_step_link' href='javascript:void(0)' onclick='DeleteStep(" + strat + "," + id + ")'>Delete</a>"+
 		"			<span style='float: right; position: absolute; right: 6px;'>"+
@@ -388,10 +388,12 @@ function ExpandStep(f_strategyId, f_stepId, collapsedName){
 		url: url,
 		dataType: "html",
 		beforeSend: function(){
-			showLoading(parentStratNum);
+			showLoading(f_strategyId);
 		},
 		success: function(data){
-			var sub = $(".diagram",data);
+			loadModel(data);
+			x = 1;
+/*			var sub = $(".diagram",data);
 			sub.addClass("sub_diagram");
 			var parentStepId = sub.attr("id");
 			parentStepId = parentStepId.split("_")[2];
@@ -418,6 +420,7 @@ function ExpandStep(f_strategyId, f_stepId, collapsedName){
 			parent_strat.append(sub);
 			parent_strat.children("span#loadingGIF").remove();
 			strat_div.append(filter);
+			*/
 		},
 		error: function(data, msg, e){
 			alert("ERROR \n " + msg + "\n" + e);
