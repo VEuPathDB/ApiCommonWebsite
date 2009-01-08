2a808b18ccc090ca47d051c79cff94549129f6b0
Wei Li <weili1@pennmedicine.upenn.edu>
2009-01-08 21:25:32 +0000

    populate wdk_table_id column, using sequence, when writing to GeneTable
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@25964 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/report/Gff3Reporter.java
+++ Model/src/java/org/apidb/apicommon/model/report/Gff3Reporter.java
@@ -218,12 +218,15 @@ public class Gff3Reporter extends Reporter {
 
         // construct insert sql
         StringBuffer sqlInsert = new StringBuffer("INSERT INTO ");
-        sqlInsert.append(tableCache).append(" (");
+        sqlInsert.append(tableCache).append(" (wdk_table_id, ");
         sqlInsert.append("table_name, row_count, content");
         for (String column : pkColumns) {
             sqlInsert.append(", ").append(column);
         }
-        sqlInsert.append(") VALUES (?, ?, ?");
+        sqlInsert.append(") VALUES (");
+        sqlInsert.append(wdkModel.getUserPlatform().getNextIdSqlExpression("apidb", "wdkTable"));
+        sqlInsert.append(", ");
+        sqlInsert.append("?, ?, ?");
         for (int i = 0; i < pkColumns.length; i++) {
             sqlInsert.append(", ?");
         }
