e9b6fc005b74aa01c25ac163c348f0f4cf634d8b
Haiming Wang <hwang@uga.edu>
2009-01-15 21:06:29 +0000

    minor format changes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-dec-08@26079 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/Util/SynView.pm
+++ Site/cgi-lib/DAS/Util/SynView.pm
@@ -4,7 +4,7 @@ sub postgrid {
   my ($gd, $panel) = @_;
   $gd->alphaBlending(0);
   $gd->saveAlpha(1);
-  $panel->boxes();		# causes the layout to be calculated
+  $panel->boxes();    # causes the layout to be calculated
   use Tie::IxHash;
   my %orthologs;
   my %location;
@@ -18,10 +18,10 @@ sub postgrid {
       next unless @orthologs;
       my $gene = $feature->name;
       for my $ortholog (@orthologs) {
-	unless (exists $orthologs{$ortholog->[0]}) {
-	  tie(%{$orthologs{$ortholog->[0]}}, "Tie::IxHash");
-	}
-	$orthologs{$ortholog->[0]}->{$gene}++;
+        unless (exists $orthologs{$ortholog->[0]}) {
+          tie(%{$orthologs{$ortholog->[0]}}, "Tie::IxHash");
+        }
+        $orthologs{$ortholog->[0]}->{$gene}++;
       }
       my $yoffset = $panel->track_position($track);
       my $padleft = $panel->pad_left();
@@ -32,28 +32,28 @@ sub postgrid {
       $y2 += $yoffset + 2;
       $location{$gene} = [ $x1, $y1, $x2, $y2 ];
       if (exists $orthologs{$gene}) {
-	ORTHOLOGS : for my $ortholog (keys %{$orthologs{$gene}}) {
-	  unless (exists $location{$ortholog}) {
-	    warn "no location for $ortholog (ortholog of $gene)\n";
-	    next ORTHOLOGS;
-	  }
-	  if ($drawn{$ortholog}) {
-	    for my $coortholog (@orthologs) {
-	      next ORTHOLOGS if $drawn{$ortholog}->{$coortholog->[0]};
-	    }
-	  }
-	  $newdrawn{$ortholog}->{$gene}++;
-	  my ($ox1, $oy1, $ox2, $oy2) = @{$location{$ortholog}};
-	  my $polygon = GD::Polygon->new();
-	  $polygon->addPt($ox1, $oy2);
-	  $polygon->addPt($ox2, $oy2);
-	  $polygon->addPt($x2, $y1);
-	  $polygon->addPt($x1, $y1);
-	  $gd->filledPolygon($polygon, $gd->colorAllocateAlpha($panel->color_name_to_rgb("darkgray"), 100));
-	  # $gd->openPolygon($polygon, $gd->colorAllocateAlpha($panel->color_name_to_rgb("lightsteelblue"), 0));
-	  $gd->line($ox1, $oy2, $x1, $y1, $panel->translate_color("lightsteelblue"));
-	  $gd->line($ox2, $oy2, $x2, $y1, $panel->translate_color("lightsteelblue"));
-	}
+        ORTHOLOGS : for my $ortholog (keys %{$orthologs{$gene}}) {
+          unless (exists $location{$ortholog}) {
+            warn "no location for $ortholog (ortholog of $gene)\n";
+            next ORTHOLOGS;
+          }
+          if ($drawn{$ortholog}) {
+            for my $coortholog (@orthologs) {
+              next ORTHOLOGS if $drawn{$ortholog}->{$coortholog->[0]};
+            }
+          }
+          $newdrawn{$ortholog}->{$gene}++;
+          my ($ox1, $oy1, $ox2, $oy2) = @{$location{$ortholog}};
+          my $polygon = GD::Polygon->new();
+          $polygon->addPt($ox1, $oy2);
+          $polygon->addPt($ox2, $oy2);
+          $polygon->addPt($x2, $y1);
+          $polygon->addPt($x1, $y1);
+          $gd->filledPolygon($polygon, $gd->colorAllocateAlpha($panel->color_name_to_rgb("darkgray"), 100));
+          # $gd->openPolygon($polygon, $gd->colorAllocateAlpha($panel->color_name_to_rgb("lightsteelblue"), 0));
+          $gd->line($ox1, $oy2, $x1, $y1, $panel->translate_color("lightsteelblue"));
+          $gd->line($ox2, $oy2, $x2, $y1, $panel->translate_color("lightsteelblue"));
+        }
       }
     }
     while (my ($key, $value) = each %newdrawn) {
