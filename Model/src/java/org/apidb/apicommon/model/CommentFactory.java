4daadc7c91a15021a1d2cbe488c89c475885c245
John Iodice <iodice@pennmedicine.upenn.edu>
2009-01-29 18:12:16 +0000

    the application shouldn't move comments to TextSearchableComment. Triggers do that now.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-nov-08_2@26336 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/CommentFactory.java
+++ Model/src/java/org/apidb/apicommon/model/CommentFactory.java
@@ -173,36 +173,6 @@ public class CommentFactory {
             }
 	}
 
-        try {
-            // make new comment searchable by Oracle Text by updating TextSearchableComment
-	    String insertSql = new String(
-                    "insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content) \n"
-                            + "select c.comment_id, c.stable_id, c.project_name, c.organism, \n"
-                            + "c.headline || '|' || c.content || '|' || \n"
-                            + "u.first_name || ' ' || u.last_name || '(' || u.organization || ')' \n"
-                            + "from " + commentSchema + "comments c, " + userSchema + "users u \n"
-                            + "where c.email = u.email(+) \n"
-                            + "  and c.comment_id in \n"
-                            + "      (select comment_id from " + commentSchema + "comments minus select comment_id from apidb.TextSearchableComment)");
-
-	    //                            + "  and c.comment_id = " + commentId);
-
-	    logger.debug("insert SQL: " + insertSql);
-            ps = SqlUtils.getPreparedStatement(platform.getDataSource(), insertSql);
-            int result = ps.executeUpdate();
-            logger.debug("Copied row to TextSearchableComment: " + result);
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            throw new WdkModelException(ex);
-        } finally {
-            try {
-                SqlUtils.closeStatement(ps);
-            } catch (SQLException ex) {
-                throw new WdkModelException(ex);
-            }
-	}
-
             // print connection status
             printStatus();
     }
