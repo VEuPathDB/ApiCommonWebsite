64db07f5d7e70e088ff6878a7ae11c73e475c64f
John Brestelli <jbrestell@gmail.com>
2009-03-16 20:00:06 +0000

    synteny , proteomics and gene model filters
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@27456 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/GBrowse/Filter.pm
+++ Site/cgi-lib/GBrowse/Filter.pm
@@ -3,11 +3,33 @@ package GBrowse::Filter;
 use strict;
 
 
+sub filterByTaxon {
+  my ($f, $value) = @_;
+  my ($taxon) = $f->get_tag_values("Taxon");
+  $taxon eq qq /$value/;
+}
+
+
 sub filterByExtDbNameAndFeatureType {
   my ($f, $nm, $tp) = @_;
   my ($extdbname) = $f->get_tag_values('ExtDbName');
   my ($featuretype) = $f ->get_tag_values('FeatureType');
   (($extdbname =~ /$nm/i) && ($featuretype =~ /$tp/i));
+}
+
+
+sub filterByExtDbNameAndDescription { 
+  my ($f, $nm, $desc) = @_;
+  my $extdbname = $f->get_tag_values('ExtDbName');
+  my ($description) = $f->get_tag_values('Description');                  
+  (($extdbname =~ /$nm/i) && ($description =~ /$desc/i));
+}
+
+
+sub filterByDescription { 
+  my ($f, $desc)  = @_;
+  my $description = $f->get_tag_values('Description');
+  ($description !~ /$desc/i);
 }
 
 
