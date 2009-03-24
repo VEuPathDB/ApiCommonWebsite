62a11ce7f510924ab825e04af722a55ed55d6332
John Brestelli <jbrestell@gmail.com>
2009-03-24 21:38:06 +0000

    merge with cgi version of SqlParser.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@27628 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/SqlXmlParser.pm
+++ Model/lib/perl/SqlXmlParser.pm
@@ -1,40 +1,156 @@
 package ApiCommonWebsite::Model::SqlXmlParser;
 
 use strict;
-use Data::Dumper;
 use XML::Simple;
+use Data::Dumper;
 
-# parse an sql dictionary in xml, in the form expected by the gbrowse DAS adaptor.
-# in particular, it must be in this form:
-#  <DONTCARE>
-#    <module>
-#      <sqlQuery>
-#        <name>the_query_name</name>
-#        <sql>
-#         the sql here
-#        </sql>
-#      </sqlQuery>
-#    </module>
-#  </DONTCARE>
+sub new {
+	my $class = shift;
+	my $self = {};
+	my @file = @_;
 
-sub parseSqlXmlFile {
-  my($sqlXmlFile, $showParse) = @_;
+	my $xsl = XML::Simple->new();
+	my $tree = $xsl->XMLin(@file, Cache => 'memshare') or die "cannot open the sql file\n";
 
-  open(FILE, $sqlXmlFile) || die "can't open sql xml file '$sqlXmlFile' for reading\n";
-  my $simple = XML::Simple->new();
+	$self->{tree} = $tree;
 
-  # use forcearray so elements with one child are still arrays
-  # and, use keyattr so that handlers are given as an ordered list
-  # rather than a hash with name as key.  the ordering is needed
-  # so that undo operations are ordered.  also, the qualifiers retain
-  # the ordering found in the xml file.
-  my $data = $simple->XMLin($sqlXmlFile,
-			    forcearray => 1,
-	      	    KeyAttr => {});
-  if ($showParse) {
-    print Dumper($data);
-    print  "\n\n\n";
-  }
-  return $data;
+	bless( $self, $class );
+	return $self;
+
 }
+
+sub getTree {$_[0]->{tree}}
+
+sub setProjectId {$_[0]->{projectId} = $_[1]}
+sub getProjectId {
+  my ($self) = @_;
+
+  my $projectId = $self->{projectId};
+
+  unless($projectId) {
+    die "Project Id was not set!";
+  }
+  return $projectId;
+}
+
+sub getSQL {
+	my $self = shift;
+	my ($modulename, $key) = @_;
+
+	my $obj = $self->{tree}->{module}->{$modulename}->{sqlQuery};
+
+        return $obj->{sql} if (exists $obj->{sql});
+        my $sqlObj = $obj->{$key}->{sql};
+
+        return $self->_getSQL($sqlObj, $key);
+}
+
+
+sub print {
+	my $self = shift;
+
+        my $tree = $self->getTree();
+
+	print Dumper($tree->{module}->{'GUS.pm'});
+}
+
+sub _getSQL {
+  my ($self, $sqlObj, $key) = @_;
+
+  my $projectId = $self->getProjectId();
+
+  my $rv;
+  my $sqlCount = 0;
+
+  # If there is only one but it has an includeProjects attribute force it onto an array
+  if(ref($sqlObj) eq 'HASH') {
+    $sqlObj = [$sqlObj];
+  }
+
+  if(ref($sqlObj) eq 'ARRAY') {
+    my $alreadyIncluded;
+
+    foreach my $sql (@$sqlObj) {
+      if(ref($sql) eq 'HASH') {
+        next if($sql->{excludeProjects} && $sql->{excludeProjects} =~ /$projectId/);
+
+        if($sql->{includeProjects} && $sql->{includeProjects} =~ /$projectId/) {
+          $rv = $sql->{content};
+          $alreadyIncluded = 1;
+          $sqlCount++;
+        }
+        if($sql->{excludeProjects} && $sql->{excludeProjects} !~ /$projectId/) {
+          $rv = $sql->{content} unless($alreadyIncluded);
+          $sqlCount++;
+        }
+      }
+      else {
+        $rv = $sql unless($alreadyIncluded);
+        $sqlCount++;
+      }
+    }
+  }
+  else {
+    $rv = $sqlObj;
+    $sqlCount++;
+  }
+
+  if($sqlCount > 1) {
+    die "Multiple SQL statements found for $key" . Dumper $rv;
+  }
+
+  return $rv;
+
+}
+
+
+sub getQueryNamesHash {
+  my ($self) = @_;
+
+  my $tree = $self->getTree();
+  my @modules = keys %{$tree->{module}};
+
+  my %queryNames;
+  foreach my $module (@modules) {
+    my $sqlQueryHash = $tree->{module}->{$module}->{sqlQuery};
+
+    # Handle the GUS.pm Case where there is only one query
+    if($sqlQueryHash->{sql} && $sqlQueryHash->{name}) {
+      my $queryName = $sqlQueryHash->{name};
+      push @{$queryNames{$module}}, $queryName;
+    }
+
+    else {
+      foreach my $queryName (keys %{$tree->{module}->{$module}->{sqlQuery}}) {
+        push @{$queryNames{$module}}, $queryName;
+      }
+    }
+  }
+
+  return \%queryNames;
+}
+
+
+
+#sub parseSqlXmlFile {
+#  my($sqlXmlFile, $showParse) = @_;
+
+#  open(FILE, $sqlXmlFile) || die "can't open sql xml file '$sqlXmlFile' for reading\n";
+#  my $simple = XML::Simple->new();
+
+#  # use forcearray so elements with one child are still arrays
+#  # and, use keyattr so that handlers are given as an ordered list
+#  # rather than a hash with name as key.  the ordering is needed
+#  # so that undo operations are ordered.  also, the qualifiers retain
+#  # the ordering found in the xml file.
+#  my $data = $simple->XMLin($sqlXmlFile,
+#			    forcearray => 1,
+#	      	    KeyAttr => {});
+#  if ($showParse) {
+#    print Dumper($data);
+#    print  "\n\n\n";
+#  }
+#  return $data;
+#}
+
 1;
