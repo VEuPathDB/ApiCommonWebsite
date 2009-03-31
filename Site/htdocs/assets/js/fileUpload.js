47cb012ee06a23c7571e618ca13a11bb2371373e
Mark Heiges <mheiges@gmail.com>
2009-03-31 16:29:28 +0000

    add jquery.validate plugin and apply it to fileUpload.js
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@27760 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/fileUpload.js
+++ Site/htdocs/assets/js/fileUpload.js
@@ -3,6 +3,15 @@ var filesTable = '#fileSelTbl';
 
 $(document).ready(function(){
   addFileSelRow();
+
+  $('#uploadForm').validate({
+    rules: {
+      title: {
+          required: true,
+      },
+    }
+  });
+  
   
   $('#newfile').click(function(){
     addFileSelRow();
@@ -22,21 +31,24 @@ function addFileSelRow() {
   $(filesTable).append(
     '<tr><td><table style="border:1px solid black;">' + 
     '<td>Select a file:</td>' +
-    '<td><input name="file[' + fCount + ']" type="file">' +
+    '<td><input name="file[' + fCount + ']" type="file" class="required" id="file[' + fCount + ']">' +
     '<td id="f_rm"></td>' +
     '</tr>' + 
     '<tr>' +
     '<td style="vertical-align:top">Brief Description:<br>(4000 max characters)</td>' +
     '<td colspan="2"><textarea name="notes[' + fCount + 
-       ']" rows="3" cols="50"></textarea></td>' +
+       ']" rows="3" cols="50" class="required" maxlength="4000" ></textarea></td>' +
     '</table></td></tr>'
   );
+
 
   if (fCount > 0) {
     var rowCount = $(filesTable).find("table").length;
     $(filesTable + ' tr:nth-child(' + (rowCount -1) + 
         ') table:first tr:first td:last').replaceWith(remove);
   }
+
+//    $("file").rules("add", "required");
 
   zebraStripe();
   
