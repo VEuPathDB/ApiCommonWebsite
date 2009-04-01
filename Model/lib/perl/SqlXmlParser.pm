607efe45b279fd80a72c15db1ef9a8dcbd230028
Steve Fischer <sfischer@upenn.edu>
2009-04-01 18:43:20 +0000

    adjust and debug for unification and sanity test
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@27816 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/SqlXmlParser.pm
+++ Model/lib/perl/SqlXmlParser.pm
@@ -8,32 +8,21 @@ sub new {
   my ($class, $file, $project, $showParse) = @_;
   my $self = {};
 
+  die 'must supply $project arg to SqlXmlParser' unless $project;
+  $self->{project} = $project;
 
   my $xsl = XML::Simple->new();
-  my $tree = $xsl->XMLin($file, Cache => 'memshare', forcearray => 1, keyattr => { module => '+name', sqlQuery => '+name'}) or die "cannot open the sql file\n";
+  my $fullTree = $xsl->XMLin($file, Cache => 'memshare', forcearray => 1, keyattr => { module => '+name'}) or die "cannot open the sql file\n";
 
-  $self->{tree} = $tree;
-  $self->{projectId} = $project;
+  print Dumper $fullTree if ($showParse);
 
-  print Dumper $tree if($showParse);
+  $self->{prunedTree} = &pruneTree($fullTree, $project);
 
   bless( $self, $class );
   return $self;
 }
 
-sub getTree {$_[0]->{tree}}
-
-sub setProjectId {$_[0]->{projectId} = $_[1]}
-sub getProjectId {
-  my ($self) = @_;
-
-  my $projectId = $self->{projectId};
-
-  unless($projectId) {
-    die "Project Id was not set!";
-  }
-  return $projectId;
-}
+sub getTree {$_[0]->{prunedTree}}
 
 sub print {
   my $self = shift;
@@ -43,109 +32,49 @@ sub print {
   print Dumper($tree);
 }
 
+# remove exluded sqlQueries, and change shape from array of sqlQueries to hash
+sub pruneTree {
+  my ($fullTree, $project) = @_;
+
+  my $prunedTree = {};
+
+  $prunedTree->{sanityTestInfo} = $fullTree->{sanityTestInfo};
+
+  foreach my $moduleName (keys(%{$fullTree->{module}})) {
+    my $module = $fullTree->{module}->{$moduleName};
+
+    $prunedTree->{module}->{$moduleName} = {};
+
+    foreach my $sqlQuery (@{$module->{sqlQuery}}) {
+      if ($sqlQuery->{includeProjects} && $sqlQuery->{excludeProjects}) {
+	die "<sqlQuery name=\"$sqlQuery->{name}\"> as both 'includeProjects=' and 'excludeProjects='";
+      }
+
+      next if ($sqlQuery->{includeProjects}
+	       && $sqlQuery->{includeProjects} !~ /$project/);
+      next if ($sqlQuery->{excludeProjects}
+	       && $sqlQuery->{excludeProjects} != /$project/);
+
+      die "<sqlQuery name=\"$sqlQuery->{name}\"> is included more than once for $project"
+	if $prunedTree->{module}->{$moduleName}->{sqlQuery}->{$sqlQuery->{name}};
+
+      $prunedTree->{module}->{$moduleName}->{sqlQuery}->{$sqlQuery->{name}} = $sqlQuery;
+    }
+  }
+  return $prunedTree;
+}
 
 sub getSQL {
-	my $self = shift;
-	my ($modulename, $key) = @_;
+  my $self = shift;
+  my ($moduleName, $sqlQueryName) = @_;
 
-	my $sqlObj = $self->{tree}->{module}->{$modulename}->{sqlQuery}->{$key}->{sql};
-
-        return $self->_getSQL($sqlObj, $key);
+  return $self->{prunedTree}->{$moduleName}->{$sqlQueryName}->{sql}->[0];
 }
 
 
-sub _getSQL {
-  my ($self, $sqlObj, $key) = @_;
-
-  my $projectId = $self->getProjectId();
-
-  my $rv;
-  my $sqlCount = 0;
-
-  if(ref($sqlObj) eq 'ARRAY') {
-    my $alreadyIncluded;
-
-    foreach my $sql (@$sqlObj) {
-      if(ref($sql) eq 'HASH') {
-        next if($sql->{excludeProjects} && $sql->{excludeProjects} =~ /$projectId/);
-
-        if($sql->{includeProjects} && $sql->{includeProjects} =~ /$projectId/) {
-          $rv = $sql->{content};
-          $alreadyIncluded = 1;
-          $sqlCount++;
-        }
-        if($sql->{excludeProjects} && $sql->{excludeProjects} !~ /$projectId/) {
-          $rv = $sql->{content} unless($alreadyIncluded);
-          $sqlCount++;
-        }
-      }
-      else {
-        $rv = $sql unless($alreadyIncluded);
-        $sqlCount++;
-      }
-    }
-  }
-  else {
-    $rv = $sqlObj;
-    $sqlCount++;
-  }
-
-  if($sqlCount > 1) {
-    die "Multiple SQL statements found for $key" . Dumper $rv;
-  }
-
-  return $rv;
-
+sub getSqlStringFromSqlQuery {
+  my ($self, $sqlQuery) = @_;
+  return $sqlQuery->{sql}->[0];
 }
-
-
-sub getQueryNamesHash {
-  my ($self) = @_;
-
-  my $tree = $self->getTree();
-  my @modules = keys %{$tree->{module}};
-
-  my %queryNames;
-  foreach my $module (@modules) {
-    my $sqlQueryHash = $tree->{module}->{$module}->{sqlQuery};
-
-    # Handle the GUS.pm Case where there is only one query
-    if($sqlQueryHash->{sql} && $sqlQueryHash->{name}) {
-      my $queryName = $sqlQueryHash->{name};
-      push @{$queryNames{$module}}, $queryName;
-    }
-
-    else {
-      foreach my $queryName (keys %{$tree->{module}->{$module}->{sqlQuery}}) {
-        push @{$queryNames{$module}}, $queryName;
-      }
-    }
-  }
-
-  return \%queryNames;
-}
-
-
-
-#sub parseSqlXmlFile {
-#  my($sqlXmlFile, $showParse) = @_;
-
-#  open(FILE, $sqlXmlFile) || die "can't open sql xml file '$sqlXmlFile' for reading\n";
-#  my $simple = XML::Simple->new();
-
-#  # use forcearray so elements with one child are still arrays
-#  # and, use keyattr so that handlers are given as an ordered list
-#  # rather than a hash with name as key.  the ordering is needed
-#  # so that undo operations are ordered.  also, the qualifiers retain
-#  # the ordering found in the xml file.
-#  my $data = $simple->XMLin($sqlXmlFile,
-#			    forcearray => 1,
-#	      	    KeyAttr => {});
-#  if ($showParse) {
-#    print Dumper($data);
-#    print  "\n\n\n";
-#  }
-#  return $data;
-#}
 
 1;
