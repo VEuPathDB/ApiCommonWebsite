1a55064c1eaf2f93287f28971476eb51dd58869b
Steve Fischer <sfischer@upenn.edu>
2009-04-02 01:25:31 +0000

    another round of simplification
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@27829 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/SqlXmlParser.pm
+++ Model/lib/perl/SqlXmlParser.pm
@@ -12,38 +12,31 @@ sub new {
   $self->{project} = $project;
 
   my $xsl = XML::Simple->new();
-  my $fullTree = $xsl->XMLin($file, Cache => 'memshare', forcearray => 1, keyattr => { module => '+name'}) or die "\ncannot open the sql file\n";
+  my $fullTree = $xsl->XMLin($file, Cache => 'memshare', forcearray => 1, keyattr => {}) or die "\ncannot open the sql file\n";
 
   print Dumper $fullTree if ($showParse);
 
-  $self->{prunedTree} = &pruneTree($fullTree, $project);
+  &pruneTree($self, $fullTree, $project);
 
   bless( $self, $class );
   return $self;
 }
 
-sub getTree {$_[0]->{prunedTree}}
-
-sub print {
-  my $self = shift;
-
-  my $tree = $self->getTree();
-
-  print Dumper($tree);
-}
-
-# remove exluded sqlQueries, and change shape from array of sqlQueries to hash
+# post-process structure from XML simple to prune away excluded sqlQueries
+# and produce structures we need
+#  (1) hash of modules and sqlQueries for use by gbrowse
+#  (2) array of sqlQueries for the sanity test
+#  (3) hash of sanityTestInfo for sanity test
 sub pruneTree {
-  my ($fullTree, $project) = @_;
+  my ($self, $fullTree, $project) = @_;
 
-  my $prunedTree = {};
+  $self->{moduleHash} = {};
+  $self->{sqlQueryArray} = [];
+  $self->{sanityTestInfo} = $fullTree->{sanityTestInfo};
 
-  $prunedTree->{sanityTestInfo} = $fullTree->{sanityTestInfo};
+  foreach my $module (@{$fullTree->{module}}) {
 
-  foreach my $moduleName (keys(%{$fullTree->{module}})) {
-    my $module = $fullTree->{module}->{$moduleName};
-
-    $prunedTree->{module}->{$moduleName} = {};
+    $self->{moduleHash}->{$module->{name}} = {};
 
     foreach my $sqlQuery (@{$module->{sqlQuery}}) {
       if ($sqlQuery->{includeProjects} && $sqlQuery->{excludeProjects}) {
@@ -55,26 +48,48 @@ sub pruneTree {
       next if ($sqlQuery->{excludeProjects}
 	       && $sqlQuery->{excludeProjects} != /$project/);
 
-      die "\n<sqlQuery name=\"$sqlQuery->{name}\"> is included more than once for $project\n"
-	if $prunedTree->{module}->{$moduleName}->{sqlQuery}->{$sqlQuery->{name}};
+      if ($self->{moduleHash}->{$module->{name}}->{$sqlQuery->{name}}) {
+	die "\n<sqlQuery name=\"$sqlQuery->{name}\"> is included more than once for $project\n"
+      }
 
-      $prunedTree->{module}->{$moduleName}->{sqlQuery}->{$sqlQuery->{name}} = $sqlQuery;
+      $self->{moduleHash}->{$module->{name}}->{$sqlQuery->{name}} = $sqlQuery;
+      push(@{$self->{sqlQueryArray}}, $sqlQuery);
     }
   }
-  return $prunedTree;
 }
 
+# used by gbrowse adaptor
 sub getSQL {
   my $self = shift;
   my ($moduleName, $sqlQueryName) = @_;
 
-  return $self->{prunedTree}->{$moduleName}->{$sqlQueryName}->{sql}->[0];
+  return $self->{moduleHash}->{$moduleName}->{$sqlQueryName}->{sql}->[0];
 }
 
-
+# used by sanity test
 sub getSqlStringFromSqlQuery {
   my ($self, $sqlQuery) = @_;
   return $sqlQuery->{sql}->[0];
+}
+
+# used by sanity test
+sub getSqlQueryArray {
+  my ($self) = @_;
+  return $self->{sqlQueryArray}
+}
+
+# used by sanity test
+sub getSanityTestInfo {
+  my ($self) = @_;
+  return $self->{sanityTestInfo};
+}
+
+sub print {
+  my $self = shift;
+
+  my $tree = $self->getTree();
+
+  print Dumper($tree);
 }
 
 1;
