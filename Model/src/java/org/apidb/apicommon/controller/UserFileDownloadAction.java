5851fba0e21c8c5ee37e2de99d6b6636dda9bcfc
Mark Heiges <mheiges@gmail.com>
2009-04-13 18:05:15 +0000

    refactor UserFileDownloadAction and handle directories
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-nov-08_2@28067 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/controller/UserFileDownloadAction.java
+++ Model/src/java/org/apidb/apicommon/controller/UserFileDownloadAction.java
@@ -15,16 +15,42 @@ import org.apidb.apicommon.model.CommentFactory;
 import org.gusdb.wdk.model.Utilities;
 
 public class UserFileDownloadAction extends DownloadAction {
+    
+    File file;
+    String gusHome;
+    String projectId;
+ 
 
     public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest   request, HttpServletResponse   response)
+            HttpServletRequest request, HttpServletResponse response)
             throws Exception   {
 
-        StreamInfo info = getStreamInfo(mapping, form, request, response);
-        
-        if (info == null)
-            return mapping.findForward("fileNotFound");
+        ServletContext application = getServlet().getServletContext();
+        gusHome = application.getRealPath(application.getInitParameter(Utilities.SYSTEM_PROPERTY_GUS_HOME));
+        projectId = application.getInitParameter(Utilities.ARGUMENT_PROJECT_ID);
+ 
+        String fname = (String) request.getParameter("fname");
 
+        file = getFile(fname);
+
+        if (!file.exists()) {
+            return mapping.findForward("fileNotFound");
+        } else if (file.isDirectory()) {
+            ActionForward forwardBase = mapping.findForward("isDir");
+            String dirPath = "../" + forwardBase.getPath() + "/" + projectId + "/" + file.getName();
+            System.out.println("dirPath " + dirPath);
+            return new ActionForward(dirPath, true);
+        } else {
+            StreamInfo info = getStreamInfo(mapping, form, request, response);
+            sendStreamResponse(response, info);
+        }
+    
+        return null;        
+    }
+
+    protected void sendStreamResponse(HttpServletResponse response, 
+              StreamInfo info) throws Exception {
+        
         String   contentType = info.getContentType();
         InputStream   stream = info.getInputStream();
 
@@ -37,45 +63,37 @@ public class UserFileDownloadAction extends DownloadAction {
             }
         }
 
-        // Tell Struts that we are done with the response.
-        return null;
     }
 
-    protected StreamInfo getStreamInfo(
-           ActionMapping mapping, 
-           ActionForm form,
-           HttpServletRequest request, 
-           HttpServletResponse response) throws Exception {
+    protected File getFile(String fname) throws Exception {
         
-        ServletContext application = getServlet().getServletContext();
-        
-        String gusHome = application.getRealPath(application.getInitParameter(Utilities.SYSTEM_PROPERTY_GUS_HOME));
-        String projectId = application.getInitParameter(Utilities.ARGUMENT_PROJECT_ID);
-
         CommentFactory.initialize(gusHome, projectId);
         CommentFactory factory = CommentFactory.getInstance();
         CommentConfig commentConfig = factory.getCommentConfig();
         
         String uploadPath = commentConfig.getUserFileUploadDir();
 
-        String fname = (String) request.getParameter("fname");
         
         String filePath = uploadPath + "/" + projectId + "/" + fname;
         
-        File file = new File(filePath);
+        return new File(filePath);
         
-        if (!file.exists()) {
-            return null;
-        } else {   
-            /** Note: content-disposition is broken in Internet Explorer 5.5 
-                Service Pack 1 (SP1). See
-                http://support.microsoft.com/kb/q279667/
-            **/
-            response.setHeader("Content-disposition", 
-                               "attachment; filename=" + file.getName());        
-            String contentType = "application/octet-stream";
-            return new FileStreamInfo(contentType, file);
-        }
+    }
+    
+    protected StreamInfo getStreamInfo(
+       ActionMapping mapping, 
+       ActionForm form,
+       HttpServletRequest request, 
+       HttpServletResponse response) throws Exception {
+    
+        /** Note: content-disposition is broken in Internet Explorer 5.5 
+            Service Pack 1 (SP1). See
+            http://support.microsoft.com/kb/q279667/
+        **/
+        response.setHeader("Content-disposition", 
+                           "attachment; filename=" + file.getName());        
+        String contentType = "application/octet-stream";
+        return new FileStreamInfo(contentType, file);
     }
 
 }
