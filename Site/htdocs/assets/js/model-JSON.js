ce1dac7d09a27d6b179f2ceca907bceba484dfe1
Cary Pennington <carypen@_.com>
2009-04-16 17:54:42 +0000

    re apply changes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@28160 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/model-JSON.js
+++ Site/htdocs/assets/js/model-JSON.js
@@ -128,7 +128,7 @@ Utility Functions
 *****************************************************/
 	
 function getStep(strat,id){
-	for(i=0;i<strats.length;i++){
+	for(i in strats){
 		if(strats[i].frontId == strat){
 			for(j=0;j<strats[i].Steps.length;j++){
 				if(strats[i].Steps[j].frontId == id)
