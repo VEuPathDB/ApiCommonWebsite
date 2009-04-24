f9022e20d8efc3956beda32c1dc4901a2786c677
Cary Pennington <carypen@_.com>
2009-04-24 17:40:44 +0000

    added clock to the init function
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@28398 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/js-utils.js
+++ Site/htdocs/assets/js/js-utils.js
@@ -1,25 +1,43 @@
 //show the loading icon in the upper right corner of the strategy that is being operated on
 function showLoading(divId){
-	var d = $("#diagram_" + divId);
+	var d = null;
+	var l = 0;
+	var t = 0;
+	if(divId == undefined){
+		d = $("#Strategies");
+		le = "225px";
+		t = "40px";
+		l_gif = "loading.gif";
+		sz = "45";
+	}else{
+		d = $("#diagram_" + divId);
+		le = "10px";
+		t = "10px";
+		l_gif = "loading2.gif";
+		sz = "35";
+	}
 	var l = document.createElement('span');
 	$(l).attr("id","loadingGIF");
 	var i = document.createElement('img');
-	$(i).attr("src","/assets/images/loading2.gif");
-	$(i).attr("height","23");
+	$(i).attr("src","/assets/images/" + l_gif);
+	$(i).attr("height",sz);
 	//$(l).html("<p style='position:relative;top:-17px;z-index:300'>Loading...</p>");
 	$(l).prepend(i);
 	$(l).css({
 		"text-align": "center",
 		position: "absolute",
-		left: "10px",
-		top: "10px"
+		left: le,
+		top: t
 	});
 	$(d).append(l);
 }
 
 // remove the loading icon for the given strategy
 function removeLoading(divId){
-	$("#diagram_" + divId + " span#loadingGIF").remove();
+	if(divId == undefined)
+		$("#Strategies span#loadingGIF").remove();
+	else
+		$("#diagram_" + divId + " span#loadingGIF").remove();
 }
 
 // parses the inputs of the question form to be sent via ajax call
