6c9996f821b87eb5fefbd8fc6e8389374870932a
Charles Treatman <ctreatma@_.com>
2009-05-27 18:52:44 +0000

    Dynamic height for unsaved strategies tables.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29119 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/history.js
+++ Site/htdocs/assets/js/history.js
@@ -139,6 +139,7 @@ function displayHist(type) {
 	}
 	else $(".history_controls").show();
 	$("div.panel_" + type).show();
+	if ($("div.panel_" + type + " .unsaved-strategies-body").height() > 250) $("div.panel_" + type + " .unsaved-strategies-body").height(250);
 	setCurrentTabCookie('search_history');
 	setCurrentTabCookie(type, true);
 }
