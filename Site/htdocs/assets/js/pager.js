ae7d7d4d4ec5e38c5d0273c97bc7b56c65d4de5c
Cary Pennington <carypen@_.com>
2009-06-04 19:35:02 +0000

    fixed javascript loading problem
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29359 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/pager.js
+++ Site/htdocs/assets/js/pager.js
@@ -55,3 +55,51 @@ function ResultsToGrid(data) {
 	flexifluid.init();
 */
 }
+
+function updatePageCount(pager_id) {
+    var resultSize = parseInt($("input#resultSize").val());
+    var psSelect = document.getElementById(pager_id + "_pageSize");
+    var index = psSelect.selectedIndex;
+    var pageSize = psSelect.options[index].value;
+    var pageCount = Math.ceil(resultSize / pageSize);
+    if (pageCount * pageSize < resultSize) pageCount++;
+    var span = document.getElementById(pager_id + "_pageCount");
+    span.innerHTML = pageCount;
+}
+
+function gotoPage(pager_id) {
+    //alert("hello");
+    var pageNumber = document.getElementById(pager_id + "_pageNumber").value;
+    var psSelect = document.getElementById(pager_id + "_pageSize");
+    var pageSize = psSelect.options[psSelect.selectedIndex].value;
+
+    var pageUrl = document.getElementById("pageUrl").value;
+    
+    var pageOffset = (pageNumber - 1) * pageSize;
+    var gotoPageUrl = pageUrl.replace(/\&pager\.offset=\d+/, "");
+    gotoPageUrl = gotoPageUrl.replace(/\&pageSize=\d+/, "");
+    gotoPageUrl += "&pager.offset=" + pageOffset;
+    gotoPageUrl += "&pageSize=" + pageSize;
+    GetResultsPage(gotoPageUrl, true);
+}
+
+function openAdvancedPaging(element){
+    var button = $(element);
+   
+    var isOpen = (button.val() == "Advanced Paging");
+    var panel = button.next(".advanced-paging");
+    var offset = button.position();
+    offset.left += button.width() + 20;
+    offset.top -= 20;
+	if(isOpen){
+                    panel.css({"display" : "block",
+                               "left": offset.left + "px",
+                               "top": offset.top + "px",
+			   "width": "256px", 
+                               "z-index" : 500});
+                    button.val("Cancel");
+	}else{
+                    panel.css({"display" : "none"});
+                    button.val("Advanced Paging");
+	}
+}
