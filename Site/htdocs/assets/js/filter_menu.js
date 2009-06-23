3ac0242961532cc6510d38c466ff102e215a078d
Cary Pennington <carypen@_.com>
2009-06-23 13:55:50 +0000

    close details boxes when Add Step is clicked
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29679 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/filter_menu.js
+++ Site/htdocs/assets/js/filter_menu.js
@@ -25,8 +25,8 @@ function showSaveForm(stratId, save, share){
 	closeModal();
 	$("div.save_strat_div").addClass("hidden");
 	var saveForm = $("div#save_strat_div_" + stratId);
-	var stratName = saveForm.parent().parent().find("span:first").text();
-	$("input[name='name']", saveForm).attr("value", stratName);
+	var name = saveForm.parent().parent().find("span:first").text();
+	$("input[type=text]", saveForm).attr("value", name);
        if (save){
          $("form", saveForm).attr("action", "javascript:saveOrRenameStrategy(" + stratId + ", true, true, false)");
          $("span.h3left", saveForm).text("Save As");
@@ -41,8 +41,6 @@ function showSaveForm(stratId, save, share){
          $("input[type=submit]", saveForm).attr("value", "Rename");
        }
 	saveForm.show();
-	$("input[name='name']", saveForm).focus().select();
-	
 }
 
 function closeModal(){
@@ -276,6 +274,7 @@ function OpenOperationBox(stratId, insertId) {
 }
 
 function openFilter(dtype,strat_id,step_id,isAdd){
+	hideDetails();
 	var isFirst = false;
 	steps = getStrategy(strat_id).Steps;
 	if(step_id == undefined){
