0dcffdbd9c2f006780c995629557e46922fd52b0
Haiming Wang <hwang@uga.edu>
2009-07-02 15:22:19 +0000

    added a gene identifier validator to check the associated gene/isolate identifiers from user comment.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29885 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/GeneIdValidator.java
@@ -0,0 +1,56 @@
+package org.apidb.apicommon.model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet; 
+import java.sql.SQLException;
+
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.dbms.DBPlatform;
+import org.gusdb.wdk.model.dbms.SqlUtils;
+import org.gusdb.wdk.model.query.ColumnType;
+
+public class GeneIdValidator {
+
+    private DBPlatform platform;
+
+    public GeneIdValidator (String gusHome, String projectId) {
+		  try {
+        WdkModel wdkModel = WdkModel.construct(projectId, gusHome);
+				platform = wdkModel.getQueryPlatform();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+    }
+
+    public boolean checkStableIds(String sourceId) {
+        ResultSet rs = null;
+
+        StringBuffer sql = new StringBuffer();
+        sql.append("SELECT source_id FROM ApiDB.GeneAttributes ");
+        sql.append("WHERE source_id = ? ");
+        sql.append("UNION ");
+        sql.append("SELECT source_id FROM ApiDB.IsolateAttributes ");
+        sql.append("WHERE source_id = ? ");
+        sql.append("UNION ");
+        sql.append("SELECT source_id FROM DoTS.ExternalNASequence ");
+        sql.append("WHERE source_id = ? ");
+
+        try {
+            PreparedStatement ps = SqlUtils.getPreparedStatement(
+                    platform.getDataSource(), sql.toString());
+            ps.setString(1, sourceId);
+            ps.setString(2, sourceId);
+            ps.setString(3, sourceId);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String id = rs.getString("source_id");
+								return true;
+						}
+        } catch(Exception e) {
+				    e.printStackTrace();
+						return false;
+				}
+				return false;
+    }
+}
