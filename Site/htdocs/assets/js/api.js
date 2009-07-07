36752a1671f1e9a62479c4284935e05c5f71588c
Charles Treatman <ctreatma@_.com>
2009-07-07 22:02:36 +0000

    Popup when user comes in with an unsupported browser.  Dummy link for going to older version of the site.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29933 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/api.js
+++ Site/htdocs/assets/js/api.js
@@ -9,6 +9,11 @@ the version that works on Netscape doesn't work at all on Safari, as it seems th
 in sayShowOrHide don't work
 
 */
+var oldSiteUrl = "/";
+
+$(document).ready(function() {
+	popUnsupported();
+});
 
 function toggleLayer(controllingLayerName, textLayerName) {
     //alert("toggleLayer: " + controllingLayerName);
@@ -422,6 +427,13 @@ function popLogin(){
 
 function popRegister(){
 	$.blockUI({message : $("#registerForm"), css : {cursor: 'auto',width: '80%',top:'10%',left:'10%' } });
+}
+
+function popUnsupported(){
+	if (!$.browser.mozilla && !$.browser.safari && !($.browser.msie && $.browser.version < 7) && $.cookie('api-unsupported') == null){
+		var message = "<div id='browser-info'><h2>Your browser is not supported</h2><p>For the best experience using our website, we recommend that you download one of the supported browsers listed below.</p><h3>Download a Supported Browser:</h3><ul id='supported-browsers'><li class='browser-link'><a href='http://www.mozilla.com/firefox/'>Firefox</a></li><li class='browser-link'><a href='http://www.apple.com/safari/'>Safari</a></li><li class='browser-link win'><a href='http://www.microsoft.com/windows/internet-explorer/default.aspx'>Internet Explorer 8</a></li><li class='browser-link'><a href='http://www.microsoft.com/windows/internet-explorer/ie7/'>Internet Explorer 7</a></li><!--<li class='browser-link'><a href='http://www.opera.com/download/'>Opera</a></li>--></ul><p>If you are unable to install a supported browser, you may want to use the older version of this site, available <a href='" + oldSiteUrl + "'>here</a>.</p><p>If you would like to continue using this site without upgrading your browser, simply click 'Ignore' below.</p><input type='submit' value='Ignore' onclick=\"$.cookie('api-unsupported',true,{path:'/'});$.unblockUI();\"/></div>"
+		$.blockUI({message : message, css : {cursor:'auto',top:'25%'}});
+	}
 }
 
 function checkboxAll(field) {
