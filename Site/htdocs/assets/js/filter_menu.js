7ba9a6b1fd52f8b4f9f7c9344a30b029421c8452
John Brestelli <jbrestell@gmail.com>
2009-07-08 15:33:21 +0000

    rm function for syntenic ortholog filter
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@29940 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/filter_menu.js
+++ Site/htdocs/assets/js/filter_menu.js
@@ -388,19 +388,6 @@ function openOrthologFilter(strat_id, step_id){
 	getQueryForm(url, true, true, strat.frontId);
 }
 
-function openSyntenicOrthologFilter(strat_id, step_id){
-	var strat = getStrategyFromBackId(strat_id);
-	isInsert = step_id;
-	current_Front_Strategy_Id = strat.frontId;
-	var url = 'showQuestion.do?questionFullName=InternalQuestions.GenesBySyntenicOrthologs&gene_result=' + step_id;
-	$("#query_form").remove();
-	$("#Strategies div a#filter_link span").css({opacity: 1.0});
-	$("#Strategies div#diagram_" + current_Front_Strategy_Id + " a#filter_link span").css({opacity: 0.4});
-	$("body").append("<div id='query_form' class='jqDnR' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
-//	setDraggable($("#query_form"), ".handle");
-	getQueryForm(url, true, true, strat.frontId);
-}
-
 function close(ele){
 	cd = $("#query_form");
 	$(cd).html(original_Query_Form_Text);
