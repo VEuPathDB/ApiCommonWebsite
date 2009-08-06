2c43bbf663c521adbde4659a5f8f51be2154e527
Xin Gao <xingao@upenn.edu>
2009-08-06 15:36:17 +0000

    add missing function back
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@30628 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/SqlXmlParser.pm
+++ Model/lib/perl/SqlXmlParser.pm
@@ -98,4 +98,26 @@ sub print {
   print Dumper($tree);
 }
 
+# used by orthomcl
+sub parseSqlXmlFile {
+  my($sqlXmlFile, $showParse) = @_;
+
+  open(FILE, $sqlXmlFile) || die "can't open sql xml file '$sqlXmlFile' for reading\n";
+  my $simple = XML::Simple->new();
+
+  # use forcearray so elements with one child are still arrays
+  # and, use keyattr so that handlers are given as an ordered list
+  # rather than a hash with name as key.  the ordering is needed
+  # so that undo operations are ordered.  also, the qualifiers retain
+  # the ordering found in the xml file.
+  my $data = $simple->XMLin($sqlXmlFile,
+			    forcearray => 1,
+	      	    KeyAttr => {});
+  if ($showParse) {
+    print Dumper($data);
+    print  "\n\n\n";
+  }
+  return $data;
+}
+
 1;
