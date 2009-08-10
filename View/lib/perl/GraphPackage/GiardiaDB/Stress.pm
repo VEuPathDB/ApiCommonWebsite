32d613379dcd124b84a72dde41dbb2a372289d92
John Brestelli <jbrestell@gmail.com>
2009-08-10 19:16:57 +0000

    moved GiardiaDB Graphs to ApiCOmmonWebsite
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@30726 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/GiardiaDB/Stress.pm
@@ -0,0 +1,32 @@
+package GiardiaDBWebsite::View::GraphPackage::Stress;
+
+=pod
+
+=head1 Summary
+
+apidb.ProfileSet "Stress Response in Giardia lamblia Trophozoites-Averaged"
+
+=cut
+
+# ========================================================================
+# ----------------------------- Declarations -----------------------------
+# ========================================================================
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BasicBarPlot );
+
+use ApiCommonWebsite::View::GraphPackage::BasicBarPlot;
+
+use Time::HiRes qw ( time );
+
+# ========================================================================
+# ----------------------- Create, Init, and Access -----------------------
+# ========================================================================
+
+# ========================================================================
+# ---------------------------- End of Package ----------------------------
+# ========================================================================
+
+1;
---
+++ View/lib/perl/GraphPackage/GiardiaDB/StressPercentile/Ver2.pm
@@ -0,0 +1,58 @@
+package GiardiaDBWebsite::View::GraphPackage::StressPercentile::Ver2;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( GiardiaDBWebsite::View::GraphPackage::StressPercentile );
+
+use GiardiaDBWebsite::View::GraphPackage::StressPercentile;
+
+use ApiCommonWebsite::Model::CannedQuery::Profile;
+use ApiCommonWebsite::Model::CannedQuery::ProfileSet;
+use ApiCommonWebsite::Model::CannedQuery::ElementNames;
+
+sub init {
+  my $Self = shift;
+
+  $Self->SUPER::init(@_);
+
+  my $_ttl  = 'Stress Response percentiles by varying DTT incubation time-red values';
+
+  $Self->setNumQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => '_data',
+        ProfileSet   => $_ttl,
+      )
+    );
+
+  $Self->setNumNamesQuery
+    ( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+      ( Name         => '_names',
+        ProfileSet   => $_ttl,
+      )
+	);
+
+  $_ttl  = 'Stress Response percentiles by varying DTT incubation time-green values';
+
+  $Self->setDenQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => '_data',
+        ProfileSet   => $_ttl,
+      )
+    );
+
+  $Self->setDenNamesQuery
+    ( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+      ( Name         => '_names',
+        ProfileSet   => $_ttl,
+      )
+    );
+
+  $Self->setPctYaxisLabel('percentile');
+
+  $Self->setPctIsDecimal(0);
+
+  return $Self;
+}
+
+1;
