c8d27dbf849b6d2b8dd90bfe02457926ae98db1c
John Brestelli <jbrestell@gmail.com>
2009-08-10 20:05:14 +0000

    move TriTrypDB Graph packages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@30735 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/TriTrypDB/MylerLinfantum/Ver1.pm
@@ -0,0 +1,87 @@
+package TriTrypDBWebsite::View::GraphPackage::MylerLinfantum::Ver1;
+@ISA = qw( TriTrypDBWebsite::View::GraphPackage::MylerLinfantum );
+
+# ========================================================================
+# ----------------------------- Declarations -----------------------------
+# ========================================================================
+
+use strict;
+
+use ApiCommonWebsite::View::GraphPackage;
+use ApiCommonWebsite::Model::CannedQuery::Profile;
+use ApiCommonWebsite::Model::CannedQuery::ElementNames;
+
+use TriTrypDBWebsite::View::GraphPackage::MylerLinfantum;
+
+# ========================================================================
+# ----------------------- Create, Init, and Access -----------------------
+# ========================================================================
+
+# --------------------------------- init ---------------------------------
+
+sub init {
+  my $Self = shift;
+
+  $Self->SUPER::init(@_);
+
+  my $name = 'Lmajor promastigote time-course biorep01';
+
+  $Self->setExpressionNames
+    ( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+      ( Name => 'expressionNames',
+        ProfileSet => $name,
+      ),
+    );
+
+  $Self->setBioRep01ExpressionQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => 'PromastigoteTime-courseBiorep01',
+        ProfileSet   => $name,
+      )
+    );
+
+  $name = 'Lmajor promastigote time-course biorep02';
+
+  $Self->setBioRep02ExpressionQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => 'PromastigoteTime-courseBiorep02',
+        ProfileSet   => $name,
+      )
+    );
+
+  $name = 'Percents of the Lmajor promastigote time-course biorep01';
+
+
+  $Self->setPercentileNames
+    ( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+      ( Name => 'percentileNames',
+        ProfileSet => $name,
+      ),
+    );
+
+  $Self->setBioRep01PercentileQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => 'PercentsPromastigoteTime-courseBiorep01',
+        ProfileSet   => $name,
+      )
+    );
+
+  $name = 'Percents of the Lmajor promastigote time-course biorep02';
+
+  $Self->setBioRep02PercentileQuery
+    ( ApiCommonWebsite::Model::CannedQuery::Profile->new
+      ( Name         => 'PercentsPromastigoteTime-courseBiorep02',
+        ProfileSet   => $name,
+      )
+    );
+
+
+
+   return $Self;
+}
+
+# ========================================================================
+# ---------------------------- End of Package ----------------------------
+# ========================================================================
+
+1;
