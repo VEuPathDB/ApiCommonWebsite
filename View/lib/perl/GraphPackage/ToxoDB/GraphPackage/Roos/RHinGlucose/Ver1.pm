27fa1728e5f075af356fc37ea114abcecc78bf6a
John Brestelli <jbrestell@gmail.com>
2009-08-10 20:16:08 +0000

    moved from graph packages from ToxoDBWebsite
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@30738 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/ToxoDB/GraphPackage/Roos/RHinGlucose/Ver1.pm
@@ -0,0 +1,62 @@
+
+package ToxoDBWebsite::View::GraphPackage::Roos::RHinGlucose::Ver1;
+
+=pod
+
+=head1 Description
+
+Provides initialization parameters for the Cowman data.
+
+=cut
+
+# ========================================================================
+# ----------------------------- Declarations -----------------------------
+# ========================================================================
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BasicBarPlot );
+
+use ApiCommonWebsite::View::GraphPackage::BasicBarPlot;
+use ApiCommonWebsite::Model::CannedQuery::Profile;
+use ApiCommonWebsite::Model::CannedQuery::ProfileSet;
+use ApiCommonWebsite::Model::CannedQuery::ElementNames;
+
+# ========================================================================
+# --------------------------- Required Methods ---------------------------
+# ========================================================================
+
+sub init {
+	my $Self = shift;
+
+	$Self->SUPER::init(@_);
+
+  my $name = 'Effects of Glucose Starvation in Toxoplasma gondii';
+
+	$Self->setDataQuery
+	( ApiCommonWebsite::Model::CannedQuery::Profile->new
+		( Name         => 'data',
+      ProfileSet   => $name,
+		)
+	);
+
+	$Self->setNamesQuery
+	( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+		( Name         => 'names',
+      ProfileSet   => $name,
+		)
+	);
+
+  $Self->setYaxisLabel('Log2 expression value');
+  $Self->setColors( [ 'rgb(1.0,0.4,0.4)', 'rgb(0.6,0.2,0.2)' ] );
+  $Self->setTagRx ( 'RH \d (.+) (rep\d)' );
+
+	return $Self;
+}
+
+# ========================================================================
+# ---------------------------- End of Package ----------------------------
+# ========================================================================
+
+1;
---
+++ View/lib/perl/GraphPackage/ToxoDB/GraphPackage/Roos/ToxoLineages/Ver1.pm
@@ -0,0 +1,48 @@
+package ToxoDBWebsite::View::GraphPackage::Roos::ToxoLineages::Ver1;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BasicBarPlot );
+
+#use ToxoDBWebsite::View::GraphPackage::Roos::ThreeClonalStrains;
+use ApiCommonWebsite::View::GraphPackage::BasicBarPlot;
+use ApiCommonWebsite::Model::CannedQuery::Profile;
+use ApiCommonWebsite::Model::CannedQuery::ProfileSet;
+use ApiCommonWebsite::Model::CannedQuery::ElementNames;
+
+sub init {
+	my $Self = shift;
+
+	$Self->SUPER::init(@_);
+
+  my $name = 'Expression profiling of the 3 archetypal T. gondii lineages';
+
+	$Self->setDataQuery
+	( ApiCommonWebsite::Model::CannedQuery::Profile->new
+		( Name         => 'data',
+      ProfileSet   => $name,
+		)
+	);
+
+	$Self->setNamesQuery
+	( ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+		( Name         => 'names',
+      ProfileSet   => $name,
+		)
+	);
+
+  $Self->setYaxisLabel('Log2 expression value');
+  $Self->setColors(['#4682B4', '#B22222', '#8FBC8F', '#6A5ACD', '#87CEEB', '#CD853F']);
+  $Self->setYMax(10);
+
+#  $Self->setTagRx('(.+) (rep\d)');
+
+	return $Self;
+}
+
+# ========================================================================
+# ---------------------------- End of Package ----------------------------
+# ========================================================================
+
+1;
