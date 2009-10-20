ef5683c997dea3b469780ef89cbc6761d7e4ce74
John Brestelli <jbrestell@gmail.com>
2009-10-20 22:10:35 +0000

    R graphs made a bit easier
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@31828 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/AbstractPlot.pm
@@ -0,0 +1,191 @@
+package ApiCommonWebsite::View::GraphPackage::AbstractPlot;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage );
+use ApiCommonWebsite::View::GraphPackage;
+
+use ApiCommonWebsite::Model::CannedQuery::ElementNames;
+use ApiCommonWebsite::Model::CannedQuery::Profile;
+use ApiCommonWebsite::View::MultiScreen;
+
+use Data::Dumper;
+
+#--------------------------------------------------------------------------------
+
+sub getProfileSetsHash           { $_[0]->{'_profile_sets_hash'           } }
+sub setProfileSetsHash           { $_[0]->{'_profile_sets_hash'           } = $_[1]; $_[0] }
+
+sub getMultiScreen               { $_[0]->{'_multi_screen'                } }
+sub setMultiScreen               { $_[0]->{'_multi_screen'                } = $_[1]; $_[0] }
+
+sub getFileHandle                { $_[0]->{'_file_handle'                 } }
+sub setFileHandle                { $_[0]->{'_file_handle'                 } = $_[1]; $_[0] }
+
+sub getPlotWidth                 { $_[0]->{'_plot_width'                  } }
+sub setPlotWidth                 { $_[0]->{'_plot_width'                  } = $_[1]; $_[0] }
+
+#--------------------------------------------------------------------------------
+# Abstract methods
+#--------------------------------------------------------------------------------
+
+sub makeRPlotStrings {}
+
+#--------------------------------------------------------------------------------
+
+sub init {
+  my ($self) = @_;
+
+  $self->SUPER::init(@_);
+
+  # Default 
+  $self->setPlotWidth(600);
+
+  $self;
+}
+
+#--------------------------------------------------------------------------------
+
+sub makeR {
+  my ($self, $rPlotStringsHash) = @_;
+
+  my @rv;
+
+  my $profileSetsHash = $self->getProfileSetsHash();
+
+  my $thumb_b   = $self->getThumbnail();
+
+  my $r_f = $self->getOutputFile(). '.R';
+  my $r_fh = FileHandle->new(">$r_f") || die "Can not open R file '$r_f': $!";
+  my $out_f     = $self->getOutputFile();
+
+  push(@rv, $r_f, $out_f);
+
+  my $parts = [];
+  foreach my $ps (keys %$profileSetsHash) {
+    push(@$parts, { Name => "$ps",   Size => $self->getScreenSize() });
+  }
+
+  my $mS = ApiCommonWebsite::View::MultiScreen->new
+    ( Parts => $parts,
+      VisibleParts => $self->getVisibleParts(),
+      Thumbnail    => $thumb_b
+    );
+
+  $self->setMultiScreen($mS);
+
+  my $width       = $self->getPlotWidth();
+  my $totalHeight = $mS->totalHeight();
+
+  if ($thumb_b) {
+    $width       *= 0.75;
+    $totalHeight *= 0.75;
+  }
+
+  # used in R code to set locations of screens
+  my $screens     = $mS->rScreenVectors();
+  my $parts_n     = $mS->numberOfVisibleParts();
+
+  my $open_R      = $self->rOpenFile($width, $totalHeight);
+  my $preamble_R  = $self->_rStandardComponents($thumb_b);
+
+  my @rStrings = @{$self->makeRPlotStrings()};
+  my $rStrings = join("\n", @rStrings);
+
+  my $rcode =  <<RCODE;
+
+# ------------------------------- Prepare --------------------------------
+
+$preamble_R
+
+$open_R;
+
+plasmodb.par();
+
+screen.dims <- t(array(c($screens),dim=c(4,$parts_n)));
+screens     <- split.screen(screen.dims, erase=T);
+screens;
+screen.i    <- 1;
+
+ticks <- function() {
+  axis(1, at=seq(x.min, x.max, 1), labels=F, col="gray75");
+  axis(1, at=seq(5*floor(x.min/5+0.5), x.max, 5), labels=F, col="gray50");
+  axis(1);
+}
+
+# --------------------------------- Add Plots ---------------------------------
+
+$rStrings
+
+
+# --------------------------------- Done ---------------------------------
+
+close.screen(all.screens=T);
+dev.off();
+quit(save="no")
+
+RCODE
+
+
+  print $r_fh $rcode;
+#  print STDERR $rcode;
+
+  $r_fh->close();
+
+  return @rv;
+}
+
+#--------------------------------------------------------------------------------
+
+sub writeProfileFiles {
+  my ($self, $profileSetName, $suffix) = @_;
+
+  my $_qh   = $self->getQueryHandle();
+  my $_dict = {};
+
+  my $r_fh = $self->getFileHandle();
+
+  my $profile = ApiCommonWebsite::Model::CannedQuery::Profile->new
+    ( Name         => "_data_$suffix",
+      Id           => $self->getId(),
+      ProfileSet   => $profileSetName,
+    );
+
+  my $elementNames = ApiCommonWebsite::Model::CannedQuery::ElementNames->new
+      ( Name         => "_names_$suffix",
+        Id           => $self->getId(),
+        ProfileSet   => $profileSetName,
+      );
+
+  my @errors;
+
+  my $profile_fn = eval { $profile->makeTabFile($_qh, $_dict) }; $@ && push(@errors, $@);
+  my $elementNames_fn = eval { $elementNames->makeTabFile($_qh, $_dict) }; $@ && push(@errors, $@);
+
+  my @rv = ($profile_fn, $elementNames_fn);
+
+  if (@errors) {
+    $self->reportErrorsAndBlankGraph($r_fh, @errors);
+  }
+
+  return \@rv;
+}
+
+
+#--------------------------------------------------------------------------------
+
+sub rStringVectorFromArray {
+  my ($self, $stringArray, $name) = @_;
+
+  return "$name = c(" . join(',', map {"\"$_\""} @$stringArray) . ");";
+}
+
+
+#--------------------------------------------------------------------------------
+
+
+
+
+
+1;
---
+++ View/lib/perl/GraphPackage/BarPlot.pm
@@ -0,0 +1,163 @@
+package ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::AbstractPlot );
+use ApiCommonWebsite::View::GraphPackage::AbstractPlot;
+
+use Data::Dumper;
+
+#--------------------------------------------------------------------------------
+
+sub getScreenSize            { $_[0]->{'_screen_size'         }}
+sub setScreenSize            { $_[0]->{'_screen_size'         } = $_[1]; $_[0] }
+
+sub getColors                { $_[0]->{'_colors'              }}
+sub setColors                { $_[0]->{'_colors'              } = $_[1]; $_[0] }
+
+sub getYAxisLabels           { $_[0]->{'_yaxis_labels'        }}
+sub setYAxisLabels           { $_[0]->{'_yaxis_labels'        } = $_[1]; $_[0] }
+
+sub getLegend                { $_[0]->{'_legend'              }}
+sub setLegend                { $_[0]->{'_legend'              } = $_[1]; $_[0] }
+
+sub getRAdjustProfile        { $_[0]->{'_r_adjust_profile'    }}
+sub setRAdjustProfile        { $_[0]->{'_r_adjust_profile'    } = $_[1]; $_[0] }
+
+sub getRAdjustNames          { $_[0]->{'_r_adjust_names'      }}
+sub setRAdjustNames          { $_[0]->{'_r_adjust_names'      } = $_[1]; $_[0] }
+
+#--------------------------------------------------------------------------------
+
+sub init {
+  my $self = shift;
+  my $args = ref $_[0] ? shift : {@_};
+
+  $self->SUPER::init($args);
+
+  # Defaults
+  $self->setScreenSize(250);
+
+  return $self;
+}
+
+#--------------------------------------------------------------------------------
+
+sub makeRPlotStrings {
+  my ($self) = @_;
+
+  my @rv;
+
+  my $profileSetsHash = $self->getProfileSetsHash();
+
+
+
+  my $ms = $self->getMultiScreen();
+
+  my %isVis_b = $ms->partIsVisible();
+
+  foreach my $part (keys %$profileSetsHash) {
+    next unless ($isVis_b{$part});
+
+    my (@profileFiles, @elementNamesFiles);
+
+    my $colors = $profileSetsHash->{$part}->{colors};
+    my $rColorsString = $self->rStringVectorFromArray($colors, 'the.colors');
+
+    my $rXAxisLabelsString;
+    if(my $xAxisLabels = $profileSetsHash->{$part}->{x_axis_labels}) {
+      $rXAxisLabelsString = $self->rStringVectorFromArray($xAxisLabels, 'element.names');
+    }
+
+    my $legend = $profileSetsHash->{$part}->{legend};
+    my $rLegendString = $self->rStringVectorFromArray($legend, 'the.legend');
+
+    my $rAdjustProfile = $profileSetsHash->{$part}->{r_adjust_profile};
+    my $yAxisLabel = $profileSetsHash->{$part}->{y_axis_label};
+
+    # each part can have several profile sets
+    foreach my $profileSetName (@{$profileSetsHash->{$part}->{profiles}}) {
+      my ($profileFile, $elementNamesFile) = @{$self->writeProfileFiles($profileSetName, $part)};
+
+      push(@profileFiles, $profileFile);
+      push(@elementNamesFiles, $elementNamesFile);
+    }
+    my $profileFilesString = $self->rStringVectorFromArray(\@profileFiles, 'profile.files');
+    my $elementNamesString = $self->rStringVectorFromArray(\@elementNamesFiles, 'element.names.files');
+
+    my $rCode = $self->rString($profileFilesString, $elementNamesString, $rColorsString, $rLegendString, $yAxisLabel, $rXAxisLabelsString, $rAdjustProfile);
+
+    unshift @rv, $rCode;
+  }
+
+  return \@rv;
+}
+
+#--------------------------------------------------------------------------------
+
+sub rString {
+  my ($self, $profileFiles, $elementNamesFiles, $colorsString, $legend, $yAxisLabel, $rAdjustNames, $rAdjustProfile) = @_;
+
+  $yAxisLabel = $yAxisLabel ? $yAxisLabel : "You Forgot the Label";
+  $rAdjustProfile = $rAdjustProfile ? $rAdjustProfile : "";
+  $rAdjustNames = $rAdjustNames ? $rAdjustNames : "";
+
+  my $rv = "
+# ---------------------------- BAR PLOT ----------------------------
+
+$profileFiles
+$elementNamesFiles
+$colorsString
+$legend
+
+screen(screens[screen.i]);
+screen.i <- screen.i + 1;
+
+profile = vector();
+for(i in 1:length(profile.files)) {
+  tmp = read.table(profile.files[i], header=T, sep=\"\t\");
+  profile = rbind(profile, tmp\$VALUE);
+}
+
+element.names = vector(\"character\");
+for(i in 1:length(element.names.files)) {
+  tmp = read.table(element.names.files[i], header=T, sep=\"\t\");
+  element.names = rbind(element.names, as.vector(tmp\$NAME));
+}
+
+par(mar       = c(8,4,1,10), xpd=TRUE);
+
+# Allow Subclass to fiddle with the data structure and x axis names
+$rAdjustProfile
+$rAdjustNames
+
+d.max = max(1.1 * profile, 10);
+
+barplot(profile,
+        col       = the.colors,
+        ylab      = '$yAxisLabel',
+        ylim      = c(0, d.max),
+        beside    = TRUE,
+        names.arg = element.names,
+        space=c(0,.5),
+        las = 2
+       );
+
+if(length(the.legend) > 0) {
+  legend(11, d.max, legend=the.legend, cex=0.9, fill=the.colors, inset=0.2) ;
+}
+
+box();
+
+
+";
+
+  return $rv;
+}
+
+
+
+
+
+1;
