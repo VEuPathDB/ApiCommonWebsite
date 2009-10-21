fbce99a2c52b578c890a06628575bd10cd6913d2
John Brestelli <jbrestell@gmail.com>
2009-10-21 21:55:14 +0000

    add Dzierszinski line plot
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@31848 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/ToxoDB/Dzierszinski/TzBz.pm
@@ -0,0 +1,34 @@
+package ApiCommonWebsite::View::GraphPackage::ToxoDB::Dzierszinski::TzBz;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::LinePlot );
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+
+  $self->setProfileSetsHash
+    ({rma => {profiles => ['expression profiles of VEG strain CO2-starvation bradyzoite inducing conditions (by Florence Dzierszinski)',
+                           'expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite inducing conditions (by Florence Dzierszinski)',
+                          ],
+              y_axis_label => 'RMA Value (log2)',
+              x_axis_label => 'Days',
+              colors => ['pink', 'purple'],
+              plot_title => 'CO2-Starvation Bradyzoite Inducing Conditions (Pru and VEG)',
+              default_y_max => 8,
+              default_y_min => 6,
+             },
+     });
+
+  return $self;
+}
+
+
+
+1;
