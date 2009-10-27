6dcbf9cd827a46e61b44aa0e7b17b2be13ee0bb1
John Brestelli <jbrestell@gmail.com>
2009-10-27 20:29:08 +0000

    make percentile graph look like the rma
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@31935 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/White/TzBz.pm
+++ View/lib/perl/GraphPackage/ToxoDB/White/TzBz.pm
@@ -12,8 +12,8 @@ sub init {
 
   $self->SUPER::init(@_);
 
-  $self->setScreenSize(250);
-  $self->setBottomMarginSize(8);
+  $self->setScreenSize(200);
+  $self->setBottomMarginSize(6);
 
   $self->setProfileSetsHash
     ({rma => {profiles => ['expression profiles of three Tgondii strains under both normal-tachyzoite and induced-bradyzoite conditoins'],
@@ -27,6 +27,10 @@ sub init {
 
       pct => {profiles => ['expression profile percentiles of three Tgondii strains under both normal-tachyzoite and induced-bradyzoite conditoins'],
               y_axis_label => 'percentile',
+              colors => ['#616D7E', '#806D7E', '#4E387E'],
+              x_axis_labels => ['Tachyzoite', 'Compound 1', 'pH=8.2'],
+              r_adjust_profile => 'profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
+              legend => ["GT1", "ME49", "CTGara"],
               plot_title => 'Normal-tachyzoite vs. Induced-bradyzoite Percentiles - 3 Tgondii Strains',
              }
      });
