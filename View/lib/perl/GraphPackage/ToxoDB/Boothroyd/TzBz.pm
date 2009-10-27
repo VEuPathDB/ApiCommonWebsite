d4f3d41e6046a4907575a0dd2b68f20e20e58e2c
John Brestelli <jbrestell@gmail.com>
2009-10-27 20:12:05 +0000

    switch from barplot to lineplot
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@31934 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Boothroyd/TzBz.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Boothroyd/TzBz.pm
@@ -3,8 +3,8 @@ package ApiCommonWebsite::View::GraphPackage::ToxoDB::Boothroyd::TzBz;
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::LinePlot );
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
 
 
 sub init {
@@ -12,23 +12,30 @@ sub init {
 
   $self->SUPER::init(@_);
 
+  my $colors = ['purple'];
+
   $self->setProfileSetsHash
     ({rma => {profiles => ['expression profiles of T. gondii Matt_Tz-Bz time series'],
               y_axis_label => 'RMA Value (log2)',
-              colors => ['#7F525D','#348781',  '#A0CFEC', '#AF7817'],
+              x_axis_label => 'Days',
+              colors => $colors,
               plot_title => 'Tachyzoite to Bradyzoite Differentiation Time Series',
+              default_y_max => 8,
+              default_y_min => 6,
              },
 
       pct => {profiles => ['expression profile percentiles of T. gondii Matt_Tz-Bz time series'],
               y_axis_label => 'percentile',
-              colors => ['#7F525D','#348781',  '#A0CFEC', '#AF7817'],
+              x_axis_label => 'Days',
+              colors => $colors,
               plot_title => 'Tachyzoite to Bradyzoite Differentiation Time Series - Percentiles',
+              default_y_max => 50,
+              default_y_min => 0,
              }
      });
 
   return $self;
 }
-
 
 
 1;
