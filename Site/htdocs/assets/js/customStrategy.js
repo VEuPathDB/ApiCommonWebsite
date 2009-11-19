a01526cb694ccf253ac2b47d2e5e3feff8dd8eb3
Charles Treatman <ctreatma@_.com>
2009-11-19 18:46:13 +0000

    Override strat instructions javascript.  Capitalize required file names in wdkCustomization.  Add styles for instructions divs to AllSites.css.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-refactor-09@32284 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/customStrategy.js
+++ Site/htdocs/assets/js/customStrategy.js
@@ -32,3 +32,25 @@ function customCreateDetails(jsonstep, modelstep) {
    }
    return orthologs;
 }
+
+function showInstructions(){
+	$("#strat-instructions").remove();
+	$("#strat-instructions-2").remove();
+	$("#Strategies").removeAttr("style"); // DO NOT DELETE.  This is for IE.
+	var instr = document.createElement('div');
+	id = "strat-instructions";
+	instr_text = "<br>Click '<a href='queries_tools.jsp'>New Search</a>' <br/> to start a strategy";
+	instr_text2 = "Or Click on '<a href=\"javascript:showPanel('search_history')\">Browse Strategies</a>' to view your available strategies.";
+	arrow_image = "<img id='ns-arrow' alt='Arrow pointing to New Search Button' src='wdk/images/lookUp.png' width='45px'/>"; 
+	arrow_image2 = "<img id='bs-arrow' alt='Arrow pointing to Browse Strategy Tab' src='wdk/images/lookUp2.png' width='45px'/>"; 
+	as = $("#mysearch").text();
+	as = as.substring(as.indexOf(":") + 2);
+	if(as != "0"){
+		instr_text = instr_text + "<br>" + instr_text2;
+		id = id + "-2";
+		arrow_image = arrow_image + arrow_image2;
+	}
+	$(instr).attr("id",id).html(arrow_image + instr_text);
+	$("#Strategies").css({'overflow' : 'visible'}); // DO NOT DELETE.  This is for IE to display instructions correctly.
+	$("#Strategies").append(instr);
+}
