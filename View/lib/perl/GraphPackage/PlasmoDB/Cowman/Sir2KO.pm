01ccddec805ea00ecee0147c3287dd1360fd1d5f
John Brestelli <jbrestell@gmail.com>
2009-12-08 20:09:04 +0000

    x axis labels are now r,t,s.  Also, the profile set name for percents profile changed
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@32528 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Cowman/Sir2KO.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Cowman/Sir2KO.pm
@@ -16,19 +16,21 @@ sub init {
   $self->setBottomMarginSize(6);
 
   my $colors = ['#6495ED', '#E9967A', '#2F4F4F' ];
+  my $xAxisLabels = ['ring', 'trophozoite', 'schizont'];
+
 
   $self->setProfileSetsHash
     ({rma => {profiles => ['Profiles of E-TABM-438 from Cowman'],
               y_axis_label => 'RMA Value (log2)',
-              x_axis_labels => ['ring', 'schizont', 'trophozoite'],
+              x_axis_labels => $xAxisLabels,
               colors => $colors,
               plot_title => 'Transcription profiling of WT, Pfsir2A and Pfsir2B KO intra-erythrocytic stages',
               r_adjust_profile => 'profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
               legend => ['Wild Type', 'sir2A KO', 'sir2B KO'],
              },
-      pct => {profiles => ['Percents of E-TABM-438 from Cowman'],
+      pct => {profiles => ['Percents of of E-TABM-438 from Cowman '],
               y_axis_label => 'percentile',
-              x_axis_labels => ['ring', 'schizont', 'trophozoite'],
+              x_axis_labels => $xAxisLabels,
               colors => $colors,
               plot_title => 'Percentiles of WT, Pfsir2A and Pfsir2B KO intra-erythrocytic stages',
               r_adjust_profile => 'profile = profile * 100;profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
