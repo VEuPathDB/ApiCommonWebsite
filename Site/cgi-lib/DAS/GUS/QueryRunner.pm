6a217e47927bddd3bfc0365da97d967da8046e63
Steve Fischer <sfischer@upenn.edu>
2009-12-17 01:52:27 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@32707 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/cgi-lib/DAS/GUS/QueryRunner.pm
@@ -0,0 +1,21 @@
+package  DAS::GUS::QueryRunner;
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(executeQuery); 
+
+use Time::HiRes qw ( time );
+$now_fractions = time;
+
+sub executeQuery {
+    my ($sth, $sql, $queryName, $logging);
+    my $start_time = time();
+    $sth->execute();
+    if ($logging) {
+	my $elapsed_time = time() - $start_time;
+	print STDERR "=========================================================\n";
+	print STDERR "QUERYTIME\t" . localtime() . "\t$queryName\t" . sprintf("%.2f sec", $elapsed_time) . "\n";
+	print STDERR "=========================================================\n";
+	print STDERR "$sql\n\n"
+    }
+}
