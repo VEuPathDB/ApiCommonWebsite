eaaf175e6e8cd0ae76a9928f7d2e14c5b21e4d55
Steve Fischer <sfischer@upenn.edu>
2009-12-17 14:11:08 +0000

    make into object, and use dedicated file handle
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@32710 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/GUS/QueryRunner.pm
+++ Site/cgi-lib/DAS/GUS/QueryRunner.pm
@@ -5,16 +5,29 @@ require Exporter;
 @EXPORT_OK = qw(executeQuery); 
 
 use Time::HiRes qw ( time );
+use FileHandle;
+
+sub new {
+    my ($class) = @_;
+    my $self = {};
+
+    if ($ENV{GBROWSE_SQL_LOG}) {
+	$self->{logHandle} = FileHandle->new("> $ENV{GBROWSE_SQL_LOG}");
+	die "Can't open gbrowse log file (as set in env variable \$GBROWSE_SQL_LOG)\n";
+    }
+    bless($self,$class);
+    return $self;
+}
 
 sub executeQuery {
-    my ($sth, $sql, $queryName, $logging);
+    my ($self, $sth, $sql, $queryName, $logging);
     my $start_time = time();
     $sth->execute();
-    if ($logging) {
+    if ($self->{logHandle}) {
 	my $elapsed_time = time() - $start_time;
-	print STDERR "=========================================================\n";
-	print STDERR "QUERYTIME\t" . localtime() . "\t$queryName\t" . sprintf("%.2f sec", $elapsed_time) . "\n";
-	print STDERR "=========================================================\n";
-	print STDERR "$sql\n\n"
+	print $self->{logHandle} "=========================================================\n";
+	print $self->{logHandle} "QUERYTIME\t" . localtime() . "\t$queryName\t" . sprintf("%.2f sec", $elapsed_time) . "\n";
+	print $self->{logHandle} "=========================================================\n";
+	print $self->{logHandle} "$sql\n\n"
     }
 }
