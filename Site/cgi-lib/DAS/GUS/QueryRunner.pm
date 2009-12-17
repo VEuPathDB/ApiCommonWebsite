63ce78a8ba6098ed847f5d7337d0c0ae261a935b
Steve Fischer <sfischer@upenn.edu>
2009-12-17 19:38:09 +0000

    pass unit test
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@32729 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/GUS/QueryRunner.pm
+++ Site/cgi-lib/DAS/GUS/QueryRunner.pm
@@ -2,7 +2,7 @@ package  DAS::GUS::QueryRunner;
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT_OK = qw(executeQuery); 
+@EXPORT_OK = qw(executeQuery);
 
 use Time::HiRes qw ( time );
 use FileHandle;
@@ -13,22 +13,22 @@ sub new {
 
     if ($ENV{GBROWSE_SQL_LOG}) {
 	$self->{logHandle} = FileHandle->new("> $ENV{GBROWSE_SQL_LOG}");
-	die "Can't open gbrowse log file (as set in env variable \$GBROWSE_SQL_LOG)\n";
+	die "Can't open gbrowse log file (as set in env variable \$GBROWSE_SQL_LOG)\n" unless $self->{logHandle};
     }
     bless($self,$class);
     return $self;
 }
 
 sub executeQuery {
-    my ($self, $sth, $sql, $queryName, $logging);
+    my ($self, $sth, $sql, $queryName) = @_;
     my $start_time = time();
     $sth->execute();
     if ($self->{logHandle}) {
         my $fh = $self->{logHandle};
 	my $elapsed_time = time() - $start_time;
-	print $fh "=========================================================\n";
+	print $fh "============================================================================\n";
 	print $fh "QUERYTIME\t" . localtime() . "\t$queryName\t" . sprintf("%.2f sec", $elapsed_time) . "\n";
-	print $fh "=========================================================\n";
+	print $fh "============================================================================\n";
 	print $fh "$sql\n\n"
     }
 }
