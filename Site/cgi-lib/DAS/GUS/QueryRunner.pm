d154914feca5efa3e585d9ff7df98719dcd33469
Steve Fischer <sfischer@upenn.edu>
2009-12-18 01:31:59 +0000

    add locking
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@32745 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/GUS/QueryRunner.pm
+++ Site/cgi-lib/DAS/GUS/QueryRunner.pm
@@ -5,15 +5,15 @@ require Exporter;
 @EXPORT_OK = qw(executeQuery);
 
 use Time::HiRes qw ( time );
-use FileHandle;
+use Fcntl qw(:flock SEEK_END);;
 
 sub new {
     my ($class) = @_;
     my $self = {};
 
     if ($ENV{GBROWSE_SQL_LOG}) {
-	$self->{logHandle} = FileHandle->new("> $ENV{GBROWSE_SQL_LOG}");
-	die "Can't open gbrowse log file (as set in env variable \$GBROWSE_SQL_LOG)\n" unless $self->{logHandle};
+	open($self->{logHandle},">> $ENV{GBROWSE_SQL_LOG}") ||
+	  die "Can't open gbrowse log file (as set in env variable \$GBROWSE_SQL_LOG)\n";
     }
     bless($self,$class);
     return $self;
@@ -26,9 +26,25 @@ sub executeQuery {
     if ($self->{logHandle}) {
         my $fh = $self->{logHandle};
 	my $elapsed_time = time() - $start_time;
+        lock($fh);
 	print $fh "============================================================================\n";
 	print $fh "QUERYTIME\t" . localtime() . "\t$queryName\t" . sprintf("%.2f sec", $elapsed_time) . "\n";
 	print $fh "============================================================================\n";
-	print $fh "$sql\n\n"
+	print $fh "$sql\n\n";
+	unlock($fh);
     }
 }
+
+sub lock {
+  my ($fh) = @_;
+  flock($fh, LOCK_EX) or die "Cannot lock gbrowse log - $!\n";
+
+  # and, in case someone appended while we were waiting...
+  seek($fh, 0, SEEK_END) or die "Cannot seek - $!\n";
+}
+
+sub unlock {
+  my ($fh) = @_;
+  flock($fh, LOCK_UN) or die "Cannot unlock gbrowse log - $!\n";
+}
+
