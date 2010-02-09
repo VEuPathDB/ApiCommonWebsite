10698b3c12b40552f51422e4af960a0ee4b5e57a
John Brestelli <jbrestell@gmail.com>
2010-02-09 23:04:04 +0000

    Overlay time series data with other stage data which we do not connect the points with a line.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@33259 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/LinePlot.pm
+++ View/lib/perl/GraphPackage/LinePlot.pm
@@ -112,70 +112,139 @@ if(length(profile.files) != length(element.names.files)) {
   stop(\"profile.files length not equal to element.names.files length\");
 }
 
-element.names = list();
-profile = list();
-
 x.min = $xMin;
 x.max = $xMax;
 
 y.min = $yMin;
 y.max = $yMax;
 
+# Create Data Frames to collect values to be plotted
+lines.df = as.data.frame(matrix(nrow=length(profile.files)));
+lines.df\$V1 = NULL;
+
+points.df = as.data.frame(matrix(nrow=length(profile.files)));
+points.df\$V1 = NULL;
+
 for(i in 1:length(profile.files)) {
-  profile.tmp = read.table(profile.files[i], header=T, sep=\"\\t\");
-  profile[[i]] = profile.tmp\$VALUE;
+  profile.df = read.table(profile.files[i], header=T, sep=\"\\t\");
+  profile = profile.df\$VALUE;
 
-  profile.max = max(profile[[i]]);
-  profile.min = min(profile[[i]]);
+  element.names.df = read.table(element.names.files[i], header=T, sep=\"\\t\");
+  element.names = as.character(element.names.df\$NAME);
+  is.numeric.element.names = !is.na(as.numeric(sub(\" *[a-z-A-Z]+ *\", \"\", element.names, perl=T)));
 
-  if(profile.max > y.max) y.max = profile.max;
-  if(profile.min < y.min) y.min = profile.min;
 
-  element.names.tmp = read.table(element.names.files[i], header=T, sep=\"\\t\");
-  element.names[[i]] = as.numeric(sub(\" *[a-z-A-Z]+ *\", \"\", element.names.tmp\$NAME, perl=T));
+  for(j in 1:length(element.names)) {
+    this.name = element.names[j];
 
-  element.max = max(element.names[[i]]);
-  element.min = min(element.names[[i]]);
+    if(is.null(points.df[[this.name]])) {
+      points.df[[this.name]] = NA;
+    }
 
-  if(element.max > x.max) x.max = element.max;
-  if(element.min < x.min) x.min = element.min;
+    if(is.null(lines.df[[this.name]])) {
+      lines.df[[this.name]] = NA;
+    }
+
+    if(is.numeric.element.names[j]) {
+      lines.df[[this.name]][i] = profile[j];
+
+    } else {
+      points.df[[this.name]][i] = profile[j];
+    }
+  }
 }
 
-#plasmodb.par
+isTimeSeries = FALSE;
+
+x.coords = as.numeric(sub(\" *[a-z-A-Z]+ *\", \"\", colnames(lines.df), perl=T));
+x.coords.rank = rank(x.coords);
+
+# if the points df is all NA's that means we can plot as Time Series
+if(sum(is.na(points.df)) == ncol(points.df) * nrow(points.df)) {
+  x.min = min(x.min, x.coords, na.rm=TRUE);
+  x.max = max(x.max, x.coords, na.rm=TRUE);
+
+  y.max = max(y.max, max(lines.df, na.rm=T), na.rm=TRUE);
+  y.min = min(y.min, min(lines.df, na.rm=T), na.rm=TRUE);
+
+  x.coords = sort(x.coords);
+
+  isTimeSeries = TRUE;
+} else {
+  x.min = 1;
+  x.max = length(x.coords);
+
+  y.max = max(y.max, max(points.df, na.rm=T), max(lines.df, na.rm=T), na.rm=TRUE);
+  y.min = min(y.min, min(points.df, na.rm=T), min(lines.df, na.rm=T), na.rm=TRUE);
+
+  x.coords = seq(x.min, x.max);
+}
+
+
+new.points = as.data.frame(matrix(NA, ncol=ncol(points.df), nrow=nrow(points.df)));
+new.lines = as.data.frame(matrix(NA, ncol=ncol(lines.df), nrow=nrow(lines.df)));
+
+for(j in 1:length(x.coords.rank)) {
+  colRank = x.coords.rank[j];
+
+  new.lines[[colRank]] = lines.df[[j]];
+  new.points[[colRank]] = points.df[[j]];
+
+  colnames(new.lines)[colRank] = colnames(lines.df)[j];
+  colnames(new.points)[colRank] = colnames(points.df)[j];
+}
+
 par(mar       = c($bottomMargin,4,1,10), xpd=TRUE);
 
-my.pch = 19;
+my.pch = 15;
 
-for(i in 1:length(profile)) {
+for(i in 1:nrow(lines.df)) {
 
   if(!is.null(points.pch)) {
     my.pch = points.pch[i];
   }
 
   if(i == 1) {
-    plot(element.names[[i]],
-         profile[[i]],
-         col  = the.colors[i],
-         bg   = the.colors[i],
-         type = \"b\",
-         pch  = my.pch,
-         cex  = 1.5,
+    plot(x.coords,
+         type = \"n\",
          xlab = \"$xAxisLabel\",
          xlim = c(x.min, x.max),
+         xaxt = \"n\",
          ylab = \"$yAxisLabel\",
          ylim = c(y.min, y.max)
         );
+
+    if(isTimeSeries) {
+      axis(1);
+    } else {
+      axis(1, at=x.coords.rank, labels=colnames(lines.df));
+    }
   }
-  else {
-    lines(element.names[[i]],
-         profile[[i]],
-         col  = the.colors[i],
-         bg   = the.colors[i],
-         type = \"o\",
-         pch  = my.pch,
-         cex  = 1.5
-         );
-  }
+
+  # To have connected lines... you can't have NA's
+  y.coords = new.lines[i,];
+  colnames(y.coords) = as.character(x.coords);
+
+  y.coords = y.coords[,!is.na(colSums(y.coords))];
+  x.coords.line = as.numeric(sub(\" *[a-z-A-Z]+ *\", \"\", colnames(y.coords), perl=T));
+
+  lines(x.coords.line,
+       y.coords,
+       col  = the.colors[i],
+       bg   = the.colors[i],
+       type = \"o\",
+       pch  = my.pch,
+       cex  = 1.5
+       );
+
+  points(x.coords,
+       new.points[i,],
+       col  = the.colors[i],
+       bg   = the.colors[i],
+       type = \"o\",
+       pch  = my.pch,
+       cex  = 1.5
+       );
 }
 
 #plasmodb.grid();
