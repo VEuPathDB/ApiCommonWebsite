5d663a1e1d9b25a7a8f1c209e16eaa87db496651
John Brestelli <jbrestell@gmail.com>
2010-02-10 21:29:26 +0000

    initial check in for R.T. Tc transcriptome profile
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@33278 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/TriTrypDB/Tarleton/TcTranscriptome.pm
@@ -0,0 +1,51 @@
+package ApiCommonWebsite::View::GraphPackage::TriTrypDB::Tarleton::TcTranscriptome;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(180);
+  $self->setBottomMarginSize(3);
+
+  my $colors =['#E9967A', '#66CDAA', '#8B4513', '#DDA0DD'];
+
+  my $xAxisLabels = ['A', 'T', 'E', 'M'];
+
+  my $legend = ["amastigotes", "trypomastigotes", "epimastigotes",  "metacyclics"];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend});
+
+  $self->setProfileSetsHash
+    ({rma => {profiles => ['Profiles of T.cruzi Tarleton Rick array data'],
+              y_axis_label => 'log2(R/G)',
+              colors => $colors,
+              plot_title => 'Transcriptome analysis of the Trypanosoma cruzi life-cycle',
+              default_y_max => 1.5,
+              default_y_min => -1.5,
+              x_axis_labels => $xAxisLabels,
+             },
+      pct => {profiles => ['Percents of T.cruzi Tarleton Rick array data(red)',
+                           'Percents of T.cruzi Tarleton Rick array data(green)'
+                          ],
+              y_axis_label => 'Percentile',
+              default_y_max => 50,
+              colors => ['#B22222', '#228B22'],
+              plot_title => 'Transcriptome analysis of the Trypanosoma cruzi life-cycle - Percentile',
+              x_axis_labels => $xAxisLabels,
+             },
+     });
+
+  return $self;
+}
+
+
+
+1;
