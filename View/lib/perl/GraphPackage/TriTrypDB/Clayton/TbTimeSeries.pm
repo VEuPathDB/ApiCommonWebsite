cc40e3e943a833379852a28e4356bd0ff7a4b2b2
John Brestelli <jbrestell@gmail.com>
2010-02-10 19:21:11 +0000

    initial checkin of Clayton Tb Time series graph
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@33271 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/TriTrypDB/Clayton/TbTimeSeries.pm
@@ -0,0 +1,51 @@
+package ApiCommonWebsite::View::GraphPackage::TriTrypDB::Clayton::TbTimeSeries;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::LinePlot );
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $colors = ['#483D8B'];
+
+  my $pch = [15];
+
+  $self->setProfileSetsHash
+    ({rma => {profiles => ['Expression profiling of T. brucei microarray data from Christine Clayton',
+                          ],
+              y_axis_label => 'log2(R/G)',
+              x_axis_label => ' ',
+              colors => $colors,
+              plot_title => 'Expression profiling of T. brucei time series',
+              default_y_max => 2,
+              default_y_min => -2,
+              default_x_min => 0,
+              points_pch => $pch,
+             },
+
+      pct => {profiles => ['Percentiles of T. brucei microarray data from Christine Clayton',
+                          ],
+              y_axis_label => 'Percentile',
+              x_axis_label => ' ',
+              colors => $colors,
+              plot_title => 'Percentiles of T. brucei time series',
+              default_y_max => 10,
+              default_y_min => 4,
+              default_x_min => 0,
+              points_pch => $pch,
+             },
+
+     });
+
+  return $self;
+}
+
+
+
+1;
