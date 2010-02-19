92fb6e7aabd3821d140b8072fa406e9285d092cc
John Brestelli <jbrestell@gmail.com>
2010-02-19 20:56:12 +0000

    rm
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@33482 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/TriTrypDB/Estevez/Drbd3Depletion.pm
+++ View/lib/perl/GraphPackage/TriTrypDB/Estevez/Drbd3Depletion.pm
@@ -17,21 +17,14 @@ sub init {
   my $colors =['#D8BFD8'];
 
   $self->setProfileSetsHash
-    ({'m' => {profiles => ['Expression profiling of Tbrucei Procyclic TbDRBD3 depletion'],
-              y_axis_label => 'log2(R/G)',
-              colors => $colors,
-              default_y_max => 1.5,
-              default_y_min => -1.5,
-              force_x_axis_label_horizontal => 1,
-              plot_title => 'Effect of TbDRBD3 depletion on procyclic T. brucei transcriptome',
-             },
-      pct => {profiles => ['Percent of Expression profiling of Tbrucei Procyclic TbDRBD3 depletion'],
-              y_axis_label => 'Percentile',
-              default_y_max => 50,
-              colors => $colors,
-              force_x_axis_label_horizontal => 1,
-              plot_title => 'Effect of TbDRBD3 depletion on procyclic T. brucei transcriptome',
-             },
+    ({'fold_induction' => {profiles => ['Expression profiling of Tbrucei Procyclic TbDRBD3 depletion'],
+                           y_axis_label => 'Fold Induction',
+                           colors => $colors,
+                           default_y_max => 1.5,
+                           default_y_min => -1.5,
+                           force_x_axis_label_horizontal => 1,
+                           plot_title => 'Effect of TbDRBD3 depletion on procyclic T. brucei transcriptome',
+                          },
      });
 
   return $self;
