4f02580d09f9fb42b247d0edce00207c01806d0a
John Brestelli <jbrestell@gmail.com>
2010-06-20 23:45:09 +0000

    initial check in for m White cell cycle
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@35622 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/ToxoDB/White/CellCycle.pm
@@ -0,0 +1,56 @@
+package ApiCommonWebsite::View::GraphPackage::ToxoDB::White::CellCycle;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::LinePlot );
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(250);
+  $self->setBottomMarginSize(6.5);
+
+  my $colors = ['#CD853F'];
+
+  my $pch = [19];
+
+  my $legend = ['Cell Cycle'];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend, points_pch => $pch});
+
+  $self->setProfileSetsHash
+    ({rma => {profiles => ['M.White Cell Cycle Microarray profiles'
+                          ],
+              y_axis_label => 'RMA Value (log2)',
+              x_axis_label => 'Time Point',
+              colors => $colors,
+              default_y_max => 10,
+              default_y_min => 4,
+              default_x_min => 0,
+              points_pch => $pch,
+              x_axis_labels => ['a', 'b', 1,2,3,4,5,6,7,8,9,10,11,12],
+             },
+      pct => {profiles => ['M.White Cell Cycle Microarray profile pcts'
+                          ],
+              y_axis_label => 'percentile',
+              x_axis_label => 'Time Point',
+              colors => $colors,
+              default_y_max => 50,
+              default_y_min => 0,
+              default_x_min => 0,
+              points_pch => $pch,
+              x_axis_labels => ['a', 'b', 1,2,3,4,5,6,7,8,9,10,11,12],
+       }
+     });
+
+  return $self;
+}
+
+
+
+1;
