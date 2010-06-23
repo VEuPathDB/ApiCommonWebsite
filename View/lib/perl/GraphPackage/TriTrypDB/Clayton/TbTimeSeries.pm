563a7e19ec69806312a6746cc3b2a54f53641d71
John Brestelli <jbrestell@gmail.com>
2010-06-23 15:41:42 +0000

    Relabel left y axis as expression value
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@35663 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/TriTrypDB/Clayton/TbTimeSeries.pm
+++ View/lib/perl/GraphPackage/TriTrypDB/Clayton/TbTimeSeries.pm
@@ -17,8 +17,8 @@ sub init {
   my $pch = [15];
 
   $self->setProfileSetsHash
-    ({'fold_induction' => {profiles => ['Expression profiling of T. brucei microarray data from Christine Clayton'],
-                           y_axis_label => 'Fold Induction',
+    ({'expr_val' => {profiles => ['Expression profiling of T. brucei microarray data from Christine Clayton'],
+                           y_axis_label => 'Expression Value',
                            x_axis_label => ' ',
                            colors => $colors,
                            make_y_axis_fold_incuction => 1,
