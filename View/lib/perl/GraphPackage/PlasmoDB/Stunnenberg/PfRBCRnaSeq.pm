23227e7088347dbc1b8436f0ecbde93cea1062e5
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2010-08-10 20:35:54 +0000

    Added directory and Plots for stunnenberg RNA Seq dataset
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@36383 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/PlasmoDB/Stunnenberg/PfRBCRnaSeq.pm
@@ -0,0 +1,44 @@
+package ApiCommonWebsite::View::GraphPackage::PlasmoDB::Stunnenberg::PfRBCRnaSeq;
+
+use strict;
+use vars qw( @ISA );
+
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(250);
+  $self->setBottomMarginSize(8);
+#  $self->setLegendSize(60);
+
+  my $colors =['#E9967A', '#8B4513','#66CDAA', '#556B2F', '#87CEEB','#008080', '#F0E68C','#DAA520'];
+
+  my $xAxisLabels = ['5 hours','10 hours','15 hours','20 hours','25 hours','30 hours','35 hours','40 hours'];
+
+  $self->setProfileSetsHash
+    ({'rma' => {profiles => ['Profiles of P.falciparum Stunnenberg mRNA Seq data'],
+                           y_axis_label => 'Normalized Coverage',
+                           default_y_max => 15,
+                           colors => $colors,
+                           plot_title => 'P. falciparum post infection RNA Seq Profiles',
+                           x_axis_labels => $xAxisLabels,
+                          },
+      pct => {profiles => ['Percents of P. falciparum Stunnenberg mRNA Seq data'],
+              y_axis_label => 'Percentile',
+              default_y_max => 50,
+              colors => $colors,
+              plot_title => 'P. falciparum post infection RNA Seq Profiles',
+              x_axis_labels => $xAxisLabels,
+             },
+     });
+
+  return $self;
+}
+
+1;
