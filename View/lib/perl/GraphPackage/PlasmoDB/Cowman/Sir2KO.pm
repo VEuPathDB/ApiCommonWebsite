25f760cfabebd071273901825400113a0be4d0f0
John Brestelli <jbrestell@gmail.com>
2010-08-30 20:46:52 +0000

    change the pct profile name; do not scale pct; rm plot title; add legend
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@36594 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Cowman/Sir2KO.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Cowman/Sir2KO.pm
@@ -18,32 +18,31 @@ sub init {
   my $colors = ['#6495ED', '#E9967A', '#2F4F4F' ];
   my $xAxisLabels = ['ring', 'trophozoite', 'schizont'];
 
+  my $legend = ['Wild Type', 'sir2A', 'sir2B'];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend, cols => 3});
+
 
   $self->setProfileSetsHash
     ({rma => {profiles => ['Profiles of E-TABM-438 from Cowman'],
               y_axis_label => 'RMA Value (log2)',
               x_axis_labels => $xAxisLabels,
               colors => $colors,
-              plot_title => 'Transcription profiling of WT, Pfsir2A and Pfsir2B KO intra-erythrocytic stages',
+              default_y_max => 6,
               r_adjust_profile => 'profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
               legend => ['Wild Type', 'sir2A KO', 'sir2B KO'],
              },
-      pct => {profiles => ['Percents of of E-TABM-438 from Cowman '],
+      pct => {profiles => ['Percentiles of of E-TABM-438 from Cowman'],
               y_axis_label => 'percentile',
               x_axis_labels => $xAxisLabels,
               colors => $colors,
-              plot_title => 'Percentiles of WT, Pfsir2A and Pfsir2B KO intra-erythrocytic stages',
-              r_adjust_profile => 'profile = profile * 100;profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
+              r_adjust_profile => 'profile = rbind(profile[1,1:3], profile[1,4:6], profile[1,7:9]);',
               legend => ['Wild Type', 'sir2A KO', 'sir2B KO'],
              },
       });
 
   return $self;
 }
-
-
-
-#Percents of E-TABM-438 from Cowman
 
 
 1;
