a4d4eca181925219b2788fdb4d6db8e44c14060c
John Brestelli <jbrestell@gmail.com>
2010-09-20 14:57:49 +0000

    add scaled profiles for stunnenberg mrna seq data; delete extra module
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@36918 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Stunnenberg/PfRBCRnaSeq.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Stunnenberg/PfRBCRnaSeq.pm
@@ -17,23 +17,33 @@ sub init {
 #  $self->setBottomMarginSize(8);
 #  $self->setLegendSize(60);
 
-  my $colors =['#E9967A'];
+  my $pch = [19,24];
+
+  my $colors = ['#E9967A', '#4682B4'];
+
+  my $legend = ['Normal', 'Scaled'];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend, points_pch => $pch});
 
   $self->setProfileSetsHash
-    ({coverage => {profiles => ['Profiles of P.falciparum Stunnenberg mRNA Seq data'],
+    ({coverage => {profiles => ['Profiles of P.falciparum Stunnenberg mRNA Seq data',
+                                'Scaled Profiles of P.falciparum Stunnenberg mRNA Seq data',
+                               ],
                    y_axis_label => 'Normalized Coverage (log2)',
                    x_axis_label => 'Hours Post Infection',
                    default_y_max => 15,
                    default_y_min => 0,
                    r_adjust_profile => 'profile[profile < 1] = 1; profile = log2(profile); ',
                    colors => $colors,
+                   points_pch => $pch,
                   },
       pct => {profiles => ['Percents of P. falciparum Stunnenberg mRNA Seq data'],
               y_axis_label => 'Percentile',
               x_axis_label => 'Hours Post Infection',
               default_y_max => 50,
               default_y_min => 0,
-              colors => $colors,
+              colors => ['#E9967A'],
+              points_pch => [19],
              },
      });
 
