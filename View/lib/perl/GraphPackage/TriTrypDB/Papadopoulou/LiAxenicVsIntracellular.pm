72a88161831cf9d40541fde473a70159f3738808
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2010-10-14 21:46:45 +0000

    Minor display/color corrections to Linfantum PaGE datasets. Corrected queryList code to assort questions by organism instead of prefixes (which can change for the same org).
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@37309 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/TriTrypDB/Papadopoulou/LiAxenicVsIntracellular.pm
+++ View/lib/perl/GraphPackage/TriTrypDB/Papadopoulou/LiAxenicVsIntracellular.pm
@@ -15,11 +15,11 @@ sub init {
   $self->setBottomMarginSize(5);
   $self->setLegendSize(40);
 
-  my $colors =['#43C6DB', '#4AA02C'];
+  my $colors =['#43C6DB', '#32CD32'];
 
   my $legend = ["Intracellular Amastigote vs Promastigote", "Axenic Amastigote vs Promastigote"];
 
-  $self->setMainLegend({colors => ['#43C6DB','#4AA02C'], short_names => $legend, cols => 1});
+  $self->setMainLegend({colors => ['#43C6DB','#32CD32'], short_names => $legend, cols => 1});
 
   $self->setProfileSetsHash
     ({'expr_val' => {profiles => ['Profiles of Linfantum axenic and intracellular amastigote array data'],
@@ -29,15 +29,16 @@ sub init {
                            default_y_min => -1.5,
                            force_x_axis_label_horizontal => 1,
                            make_y_axis_fold_incuction => 1,
-                           x_axis_labels => ['Ic.A vs P', 'Ax.A vs P'],
+                           x_axis_labels => ['IC.A vs P', 'Ax.A vs P'],
                            plot_title => 'Comparison of Axenic and Intracellular Amastigotes (with Promastigotes) using transcription profiling',
                           },
       pct => {profiles => ['Linfantum axenic and intracellular amastigote array data profile percents(red)',
                            'Linfantum axenic and intracellular amastigote array data profile percents(green)'],
                            y_axis_label => 'Percentile',
                            default_y_max => 50,
-                           colors => ['#43C6DB', '#F4A460','#4AA02C','#F4A460'],
-                           x_axis_labels => ['Ic.A vs P', 'Ax.A vs P'],
+                           force_x_axis_label_horizontal => 1,
+                           colors => ['#43C6DB', '#F4A460','#32CD32','#F4A460'],
+                           x_axis_labels => ['IC.A vs P', 'Ax.A vs P'],
                            plot_title => 'Comparison of Axenic and Intracellular Amastigotes (with Promastigotes) using transcription profiling',
              },
      });
