72caa4396c037f86c28654088b9d0a4b6fc2d37d
John Brestelli <jbrestell@gmail.com>
2010-10-19 21:31:00 +0000

    combine the line and bar graphs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@37355 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/DeRisiHalfLife/3D7.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/DeRisiHalfLife/3D7.pm
@@ -13,9 +13,11 @@ sub init {
   $self->SUPER::init(@_);
 
   my $colors = ['purple', 'darkred', 'green', 'orange']; # as in the paper!
+
   my $pch = [19,24,20,23];
 
   my $legend = ['Ring', 'Trophozoite', 'Schizont', 'Late schiz.'];
+
 
   $self->setMainLegend({colors => $colors, short_names => $legend, points_pch=> $pch});
 
@@ -25,15 +27,16 @@ sub init {
 				'Profiles of Derisi HalfLife-Schizont',
 				'Profiles of Derisi HalfLife-Late_Schizont',
                           ],
-              y_axis_label => 'expression Value (log2)',
-              x_axis_label => 'Minutes',
-              colors => $colors,
-              plot_title => '',
-              default_y_max => 1,
-              default_y_min => 0, #-0.5,
-              points_pch => $pch,
-#              r_adjust_profile => 'profile = 2^profile',
-             },
+                   y_axis_label => 'expression Value (log2)',
+                   x_axis_label => 'Minutes',
+                   colors => $colors,
+                   r_adjust_profile => 'profile = 2^profile/2^profile[2]; profile = profile[2:length(profile)]; element.names = element.names[2:length(element.names)]; ',
+                   plot_title => '',
+                   default_y_max => 1,
+#              smooth_spline => 1,
+#              spline_approx_n => 60,
+                   points_pch => $pch,
+                  },
      });
 
 
