0b381bb35bc8f0e5987842dc0e1cf6ac92bee9e4
Bindu Gajria <bindu.gajria@gmail.com>
2010-12-07 22:24:44 +0000

    add in percentile graph
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@38144 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/TriTrypDB/Carrington/TbCombined.pm
+++ View/lib/perl/GraphPackage/TriTrypDB/Carrington/TbCombined.pm
@@ -18,17 +18,31 @@ sub init {
   $self->setMainLegend({colors => $colors, short_names => $legend});
 
   $self->setProfileSetsHash
-    ({coverage => {profiles => ['T. brucei under heat shock array data','T. brucei DHH1 wild type array data','T. brucei DHH1 DEAD-DQAD mutant array data'],
-                   y_axis_label => 'normalized coverage',
+    ({expr_val => {profiles => ['T. brucei under heat shock array data','T. brucei DHH1 wild type array data','T. brucei DHH1 DEAD-DQAD mutant array data'],
                    x_axis_labels => ['heat shock','wild type','mutant'],
+                   y_axis_label => 'Expression Value',
                    colors => $colors,
                    # take transpose to change the columns to rows to columns, to get in-between spacing
                    r_adjust_profile => 'profile=t(profile);',
-                   plot_title => 'T. brucei DHH1 wild type',
-		   default_y_max => 1,
+                   plot_title => 'Transcriptional profiling of T. brucei - expression value',
+                   default_y_max => 1,
                    default_y_min => -1,
-		   force_x_axis_label_horizontal => 1,
+                   force_x_axis_label_horizontal => 1,
                   },
+      pct => {profiles => ['T. brucei under heat shock array data profile percents (red)',
+                           'T. brucei under heat shock array data profile percents (green)',
+                           'T. brucei DHH1 wild type array data profile percents (red)',
+                           'T. brucei DHH1 wild type array data profile percents (green)',
+                           'T. brucei DHH1 DEAD-DQAD mutant array data profile percents (red)',
+                           'T. brucei DHH1 DEAD-DQAD mutant array data profile percents (green)'],
+              x_axis_labels => ['heat shock','wild type','mutant'],
+              y_axis_label => 'Percentile',
+              force_x_axis_label_horizontal => 1,
+              r_adjust_profile => 'profile=t(profile);profile = cbind(profile[1,1:2], profile[1,3:4],profile[1,5:6]);',
+              colors => ['#CCCCCC','#DDDDDD','#999999','#AAAAAA','#555555','#666666'],
+              plot_title => 'Transcriptional profiling of T. brucei - percentile',
+             },
+
      });
 
   return $self;
