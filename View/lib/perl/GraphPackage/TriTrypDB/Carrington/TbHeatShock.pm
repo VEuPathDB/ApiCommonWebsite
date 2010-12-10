b0646c735326ea22a2e2f548b028d6fb080de497
Bindu Gajria <bindu.gajria@gmail.com>
2010-12-10 18:13:16 +0000

    separate graph for "heat shock" experiment as control for it different
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@38178 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/TriTrypDB/Carrington/TbHeatShock.pm
+++ View/lib/perl/GraphPackage/TriTrypDB/Carrington/TbHeatShock.pm
@@ -1,4 +1,4 @@
-package ApiCommonWebsite::View::GraphPackage::TriTrypDB::Carrington::TbCombined;
+package ApiCommonWebsite::View::GraphPackage::TriTrypDB::Carrington::TbHeatShock;
 
 use strict;
 use vars qw( @ISA );
@@ -12,14 +12,14 @@ sub init {
 
   $self->SUPER::init(@_);
 
-  my $colors =['#999999','#555555'];
-  my $legend = ["DHH1 wild type vs Control","DHH1 DEAD DQAD mutant vs Control"];
+  my $colors =['#CCCCCC'];
+  my $legend = ["heat shock vs. Control Strain"];
 
   $self->setMainLegend({colors => $colors, short_names => $legend});
 
   $self->setProfileSetsHash
-    ({expr_val => {profiles => ['T. brucei DHH1 wild type array data','T. brucei DHH1 DEAD-DQAD mutant array data'],
-                   x_axis_labels => ['wild type','mutant'],
+    ({expr_val => {profiles => ['T. brucei under heat shock array data'],
+                   x_axis_labels => ['heat shock'],
                    y_axis_label => 'Expression Value',
                    colors => $colors,
                    # take transpose to change the columns to rows to columns, to get in-between spacing
@@ -29,15 +29,13 @@ sub init {
                    default_y_min => -1,
                    force_x_axis_label_horizontal => 1,
                   },
-      pct => {profiles => ['T. brucei DHH1 wild type array data profile percents (red)',
-                           'T. brucei DHH1 wild type array data profile percents (green)',
-                           'T. brucei DHH1 DEAD-DQAD mutant array data profile percents (red)',
-                           'T. brucei DHH1 DEAD-DQAD mutant array data profile percents (green)'],
-              x_axis_labels => ['wild type','mutant'],
+      pct => {profiles => ['T. brucei under heat shock array data profile percents (red)',
+                           'T. brucei under heat shock array data profile percents (green)'],
+              x_axis_labels => ['heat shock'],
               y_axis_label => 'Percentile',
               force_x_axis_label_horizontal => 1,
-              r_adjust_profile => 'profile=t(profile);profile = cbind(profile[1,1:2], profile[1,3:4]);',
-              colors => ['#999999','dark blue','#555555','dark blue'],
+#              r_adjust_profile => 'profile=t(profile);profile = cbind(profile[1,1:2], profile[1,3:4],profile[1,5:6]);',
+              colors => ['#CCCCCC','dark blue'],
               plot_title => 'Transcriptional profiling of T. brucei - percentile',
              },
 
