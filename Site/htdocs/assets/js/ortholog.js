dd27bff92ff452ff673cbf16876c5296faec6665
Charles Treatman <ctreatma@_.com>
2011-01-11 19:15:55 +0000

    Fix position of ortholog popup in trunk.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@38403 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/ortholog.js
+++ Site/htdocs/assets/js/ortholog.js
@@ -12,7 +12,7 @@ function openOrthologFilter(strat_id, step_id){
 	$("#query_form").remove();
 	$("#Strategies div a#filter_link span").css({opacity: 1.0});
 	$("#Strategies div#diagram_" + current_Front_Strategy_Id + " a#filter_link span").css({opacity: 0.4});
-	$("body").append("<div id='query_form' class='jqDnR' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
+	$("body").append("<div id='query_form' class='formPopup' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
 //	setDraggable($("#query_form"), ".handle");
 	getQueryForm(url, true, true, strat.frontId);
 }
@@ -25,7 +25,7 @@ function openSyntenicOrthologFilter(strat_id, step_id){
 	$("#query_form").remove();
 	$("#Strategies div a#filter_link span").css({opacity: 1.0});
 	$("#Strategies div#diagram_" + current_Front_Strategy_Id + " a#filter_link span").css({opacity: 0.4});
-	$("body").append("<div id='query_form' class='jqDnR' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
+	$("body").append("<div id='query_form' class='formPopup' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
 //	setDraggable($("#query_form"), ".handle");
 	getQueryForm(url, true, true, strat.frontId);
 }
