2290e28e1ea26b2fe3739bf7101ee1960b519cc8
Haiming Wang <hwang@uga.edu>
2011-01-25 21:33:28 +0000

    updates the NcRnaSeq graph: force_x_axis_label_horizontal => 1 and inherit screen size from the parent.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@38598 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Reid/NcRnaSeq.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Reid/NcRnaSeq.pm
@@ -12,9 +12,6 @@ sub init {
 
   $self->SUPER::init(@_);
 
-  $self->setScreenSize(180);
-  $self->setBottomMarginSize(4);
-
   my $colors =['#66CDAA', '#D87093'];
 
   my $legend = ["day 3", "day 4"];
@@ -24,12 +21,14 @@ sub init {
   $self->setProfileSetsHash
     ({coverage => {profiles => ['N. caninum Day 3-4 Tachyzoite-profiles'],
                    y_axis_label => 'normalized coverage',
+									 force_x_axis_label_horizontal => 1,
                    colors => $colors,
                    r_adjust_profile => 'profile=profile + 1; profile = log2(profile);',
                    plot_title => 'T.gondii VEG Day 3-4 tachyzoite forms RNA Sequence Coverage',
                   },
       pct => {profiles => ['N. caninum Day 3-4 Tachyzoite-percentiles'],
               y_axis_label => 'Percentile',
+						  force_x_axis_label_horizontal => 1,
               default_y_max => 50,
               colors => $colors,
               r_adjust_profile => 'profile = profile;',
