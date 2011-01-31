0cfa47c9a9713033f62d74c41b1d296fee29d56e
Haiming Wang <hwang@uga.edu>
2011-01-31 22:47:17 +0000

    updates toxo rna seq graph
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@38691 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Reid/TgVEGRnaSeq.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Reid/TgVEGRnaSeq.pm
@@ -1,4 +1,4 @@
-package ApiCommonWebsite::View::GraphPackage::ToxoDB::Reid::NcRnaSeq;
+package ApiCommonWebsite::View::GraphPackage::ToxoDB::Reid::TgVEGRnaSeq;
 
 use strict;
 use vars qw( @ISA );
@@ -12,6 +12,9 @@ sub init {
 
   $self->SUPER::init(@_);
 
+  #$self->setScreenSize(180);
+  #$self->setBottomMarginSize(4);
+
   my $colors =['#66CDAA', '#D87093'];
 
   my $legend = ["day 3", "day 4"];
@@ -19,20 +22,20 @@ sub init {
   $self->setMainLegend({colors => $colors, short_names => $legend});
 
   $self->setProfileSetsHash
-    ({coverage => {profiles => ['N. caninum Day 3-4 Tachyzoite-profiles'],
+    ({coverage => {profiles => ['T. gondii VEG Day 3-4 Tachyzoite aligned to the VEG Genome-profiles'],
                    y_axis_label => 'normalized coverage',
                    force_x_axis_label_horizontal => 1,
                    colors => $colors,
                    r_adjust_profile => 'profile=profile + 1; profile = log2(profile);',
-                   plot_title => 'N.caninum Day 3-4 tachyzoite forms RNA Sequence Coverage',
+                   plot_title => 'T.gondii VEG Day 3-4 tachyzoite forms RNA Sequence Coverage',
                   },
-      pct => {profiles => ['N. caninum Day 3-4 Tachyzoite-percentiles'],
+      pct => {profiles => ['T. gondii VEG Day 3-4 Tachyzoite aligned to the VEG Genome-percentiles'],
               y_axis_label => 'Percentile',
               force_x_axis_label_horizontal => 1,
               default_y_max => 50,
               colors => $colors,
               r_adjust_profile => 'profile = profile;',
-              plot_title => 'N.caninum Day 3-4 tachyzoite forms RNA Sequence Coverage',
+              plot_title => 'T.gondii VEG Day 3-4 tachyzoite forms RNA Sequence Coverage',
              },
      });
 
