2227c43fa6f745a183881d64496b4849ef476179
Xin Gao <xingao@upenn.edu>
2011-03-14 13:05:22 +0000

    create a handler to process ortholog shortcut before sending to proceeStep
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@39575 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/ortholog.js
+++ Site/htdocs/assets/js/ortholog.js
@@ -4,17 +4,20 @@
 
 */
 
-function openOrthologFilter(strat_id, step_id){
-	var strat = getStrategyFromBackId(strat_id);
+function openOrthologFilter(strat_back_id, step_id){
+        
+	var strat = getStrategyFromBackId(strat_back_id);
 	isInsert = step_id;
 	current_Front_Strategy_Id = strat.frontId;
-	var url = 'showQuestion.do?questionFullName=InternalQuestions.GenesByOrthologs&gene_result=' + step_id + '&partial=true';
-	$("#query_form").remove();
-	$("#Strategies div a#filter_link span").css({opacity: 1.0});
-	$("#Strategies div#diagram_" + current_Front_Strategy_Id + " a#filter_link span").css({opacity: 0.4});
-	$("body").append("<div id='query_form' class='formPopup' style='min-height:140px; display:none'><span class='dragHandle'><div class='modal_name'><h1 id='query_form_title'></h1></div><a id='close_filter_query' href='javascript:closeAll()'><img src='/assets/images/Close-X-box.png' alt='Close'/></a></span></div>");
-//	setDraggable($("#query_form"), ".handle");
-	getQueryForm(url, true, true, strat.frontId);
+
+        if(openDetail != null) hideDetails(); 
+
+        var url = "wizard.do?stage=show_ortholog&action=add";
+        url += "&strategy=" + strat_back_id + "&step=" + step_id;
+        url += "&questionFullName=InternalQuestions.GenesByOrthologs";
+
+        // display the stage
+        callWizard(url,null,null,null,'next'); 
 }
 
 function openSyntenicOrthologFilter(strat_id, step_id){
