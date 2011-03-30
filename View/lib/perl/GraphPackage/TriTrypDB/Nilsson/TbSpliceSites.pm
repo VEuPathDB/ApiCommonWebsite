5d767b8a2227d5aae6ccfe83fbce5bb5d5ad0661
Bindu Gajria <bindu.gajria@gmail.com>
2011-03-30 14:30:08 +0000

    initial commit for graphs of Myler and Nilsson Splice Sites data (refs #3703). The percentiles are not loaded yet in database
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@39906 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/TriTrypDB/Nilsson/TbSpliceSites.pm
@@ -0,0 +1,46 @@
+package ApiCommonWebsite::View::GraphPackage::TriTrypDB::Nilsson::TbSpliceSites;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $colors =['#D87093', '#DDDDDD'];
+
+  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
+  my $xAxisLabels = ["bf long slender","bf Short Stumpy","bf Lister 427","procyclic form","pf Alba 1 non-induced","pf Alba 1 induced","pf Alba 3 and 4 non-induced","pf Alba 3 and 4 induced"];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend});
+
+  $self->setProfileSetsHash
+    ({coverage => {profiles => ['T.brucei RNASeq Spliced Leader And Poly A Sites from Nilsson uniqProfile','T.brucei RNASeq Spliced Leader And Poly A Sites from Nilsson nonUniqProfile'],
+                   y_axis_label => 'log 2 (RPKM)',
+                   x_axis_labels => $xAxisLabels,
+                   colors => $colors,
+                   force_x_axis_label_horizontal => 1,
+                   r_adjust_profile => 'profile=profile + 1; profile = log2(profile);',
+                   stack_bars => 1,
+                  },
+#      pct => {profiles => [''],
+#              y_axis_label => 'Percentile',
+#              x_axis_labels => $xAxisLabels,
+#              default_y_max => 50,
+#              colors => [$colors->[0]],
+#              force_x_axis_label_horizontal => 1,
+#             },
+     });
+
+  return $self;
+}
+
+
+
+1;
+
