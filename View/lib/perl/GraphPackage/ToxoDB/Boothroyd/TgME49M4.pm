b43381db6074a6e23e3d311bad2246e89b40b7c8
Bindu Gajria <bindu.gajria@gmail.com>
2011-04-18 20:39:55 +0000

    specify alternate profile names, fix color and legend, and correct type - refs #4002
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@40296 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Boothroyd/TgME49M4.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Boothroyd/TgME49M4.pm
@@ -16,21 +16,23 @@ sub init {
   $self->setBottomMarginSize(6);
 
 
-  my $colors = ['#FFC0CB', '#D87093', '#D87093', '#FFDAB9', '#AFEEEE' ,'#AFEEEE', '#AFEEEE'];
+  my $colors = ['#D87093', '#D87093', '#D87093', '#FFDAB9', '#AFEEEE' ,'#AFEEEE', '#AFEEEE'];
 
-  my $legend = ["oocyst", "sporozoite", "tachyzoite", "bradyzoite"];
+  my $legend = ["oocyst", "tachyzoite", "bradyzoite"];
 
   $self->setMainLegend({colors => ['#FFC0CB', '#D87093', '#FFDAB9', '#AFEEEE'], short_names => $legend, cols=> 4});
 
   $self->setProfileSetsHash
     ({rma => {profiles => ['Expression profiles of Tgondii ME49 Boothroyd experiments'],
+	      profile_display_names => ['Expression profiles'],
+	      x_axis_labels => ['d0 unsporulated', 'd4 sporulation', 'd10 sporulation', 'd2 in vitro Tz', 'd4 in vitro Bz', 'd8 in vitro Bz', 'd21 IN VIVO Bz'],
               y_axis_label => 'RMA Value (log2)',
               colors => $colors,
-							x_axis_labels => ['d0 unsporulated', 'd4 sporulation', 'd10 sporulation', 'd2 in intro Tz', 'd4 in vitro Bz', 'd8 in vitro Bz', 'd21 IN VIVO Bz'],
               plot_title => 'Tachyzoite comparison of archetypal T.gondii lineages',
              },
       pct => {profiles => ['Expression percentile profiles of Tgondii ME49 Boothroyd experiments'],
-							x_axis_labels => ['d0 unsporulated', 'd4 sporulation', 'd10 sporulation', 'd2 in intro Tz', 'd4 in vitro Bz', 'd8 in vitro Bz', 'd21 IN VIVO Bz'],
+	      profile_display_names => ['Expression percentile profiles'],
+	      x_axis_labels => ['d0 unsporulated', 'd4 sporulation', 'd10 sporulation', 'd2 in vitro Tz', 'd4 in vitro Bz', 'd8 in vitro Bz', 'd21 IN VIVO Bz'],
               y_axis_label => 'Percentile',
               colors => $colors,
             }
