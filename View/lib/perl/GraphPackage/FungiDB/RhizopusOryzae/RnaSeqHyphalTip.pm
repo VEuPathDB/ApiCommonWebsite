1dfadb514c26b062f97fae45f5fdd59c939b8746
dborch <dborch@_.com>
2011-09-29 20:57:02 +0000

    added profile graph for Rory
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@42887 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/FungiDB/RhizopusOryzae/RnaSeqHyphalTip.pm
+++ View/lib/perl/GraphPackage/FungiDB/RhizopusOryzae/RnaSeqHyphalTip.pm
@@ -1,10 +1,10 @@
-package FungiDBWebsite::View::GraphPackage::FungiDB::RhizopusOryzae::RnaSeqHyphalTip;
+package ApiCommonWebsite::View::GraphPackage::FungiDB::RhizopusOryzae::RnaSeqHyphalTip;
 
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( FungiDBWebsite::View::GraphPackage::BarPlot );
-use FungiDBWebsite::View::GraphPackage::BarPlot;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
 
 
 sub init {
