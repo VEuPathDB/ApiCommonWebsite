3e4ff6f4abfb5847dcd1c07372622a16f93f2700
Ja’Shon Cade <jcade@upenn.edu>
2011-10-19 22:03:45 +0000

    adding GraphPackage modules for Matrajt Microarray Datasets (bld13)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@43224 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/ToxoDB/Matrajt/BzMutant.pm
@@ -0,0 +1,41 @@
+package ApiCommonWebsite::View::GraphPackage::ToxoDB::Matrajt::BzMutant;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(300);
+  $self->setPlotWidth(450);
+  $self->setBottomMarginSize(7);
+
+  my $colors = ['#996600', '#996600', '#996600', '#FF0000', '#FF0000', '#FF6600', '#FF6600','#FFFF00', '#FFFF00','#33FF66', '#33FF66', '#009900', '#009900', '#0000CC', '#0000CC', '#660033', '#660033',];
+
+  my $legend = ['Wild Type', '12K Mutant', '13P Mutant', 'B7 Mutant', '11P Mutant', '11K Mutant', '7K Mutant', 'P11 Mutant',];
+
+  $self->setMainLegend({colors => ['#996600', '#FF0000', '#FF6600','#FFFF00','#33FF66', '#009900', '#0000CC', '#660033',], short_names => $legend, cols=> 5});
+
+    $self->setProfileSetsHash
+    ({rma => {profiles => ['expression profiles of TgRH_Matrajt_GSE23174_Bz_WildType_V_Mutant'],
+              stdev_profiles => ['expression profile standard errors of TgRH_Matrajt_GSE23174_Bz_WildType_V_Mutant'],
+               #  x_axis_labels => [],
+               y_axis_label => 'RMA Value (log2)',
+               colors => $colors,
+             },
+      pct => {profiles => ['expression profile percentiles of TgRH_Matrajt_GSE23174_Bz_WildType_V_Mutant'],
+              # x_axis_labels => [],
+              y_axis_label => 'Percentile',
+              colors => $colors,
+            }
+     });
+
+  return $self;
+}
+
+1;
