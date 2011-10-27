8051101b0d1da9e9d02c50e7a78963eff91057aa
Ja’Shon Cade <jcade@upenn.edu>
2011-10-27 16:12:25 +0000

    Graphs for Gilchrist Microarray dataset - bld 13
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@43402 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/AmoebaDB/Gilchrist/EhG3Hm1Imss.pm
@@ -0,0 +1,41 @@
+package ApiCommonWebsite::View::GraphPackage::AmoebaDB::Gilchrist::EhG3Hm1Imss;
+
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(250);
+  $self->setPlotWidth(450);
+  $self->setBottomMarginSize(3);
+
+  my $colors = ['#FFCC33', '#006699',];
+
+  my $legend = ['G3', 'HM1:IMSS',];
+
+  $self->setMainLegend({colors => ['#FFCC33', '#006699',], short_names => $legend, cols=> 2});
+
+    $self->setProfileSetsHash
+    ({rma => {profiles => ['Ehist_Gilchrist_G3_V_HM1:IMSS'],
+              stdev_profiles => ['standard error - Ehist_Gilchrist_G3_V_HM1:IMSS'],
+               #  x_axis_labels => [],
+               y_axis_label => 'RMA Value (log2)',
+               colors => $colors,
+             },
+      pct => {profiles => ['percentile - Ehist_Gilchrist_G3_V_HM1:IMSS'],
+              # x_axis_labels => [],
+              y_axis_label => 'Percentile',
+              colors => $colors,
+            }
+     });
+
+  return $self;
+}
+1;
