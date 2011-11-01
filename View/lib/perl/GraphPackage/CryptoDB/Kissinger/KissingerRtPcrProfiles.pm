6f62a496044f7a0f1256e614b23b801475a06a0a
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2011-11-01 21:20:49 +0000

    added graph for Rt Pcr experiment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@43507 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/CryptoDB/Kissinger/KissingerRtPcrProfiles.pm
@@ -0,0 +1,37 @@
+package ApiCommonWebsite::View::GraphPackage::CryptoDB::Kissinger::KissingerRtPcrProfiles;
+
+use strict;
+use vars qw( @ISA );
+
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setScreenSize(250);
+  $self->setBottomMarginSize(8);
+
+  my $colors =['#E9967A', '#8B4513','#66CDAA', '#556B2F', '#87CEEB','#008080', '#C9BE62'];
+
+  my $xAxisLabels = ['2 Hrs', '6 Hrs','12 Hrs', '24 Hrs','36 Hrs','48 Hrs','72 Hrs'];
+
+
+  $self->setProfileSetsHash
+    ({'rma' => {profiles => ['Cparvum_RT_PCR_Kissinger'],
+                           y_axis_label => 'Median Expr (standardized)',
+                           default_y_max => 1,
+                           colors => $colors,
+                           plot_title => 'C.parvum Real Time PCR Expression Profiles',
+                           x_axis_labels => $xAxisLabels,
+                          }
+    });
+
+  return $self;
+}
+
+1;
