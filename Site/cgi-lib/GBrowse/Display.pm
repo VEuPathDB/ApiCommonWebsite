dab989e7b362caaf1c23260773d1cb4413848824
John Brestelli <jbrestell@gmail.com>
2011-12-14 20:53:25 +0000

    colorByRnaSeq method
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@44558 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/GBrowse/Display.pm
+++ Site/cgi-lib/GBrowse/Display.pm
@@ -472,6 +472,45 @@ sub colorFromBinaryColor {
   }
 }
 
+
+sub colorByRnaSeq {
+  my ($f, $optColor) = @_;
+  my ($isReversed) = $f->get_tag_values('is_reversed');
+  my ($multiple) = $f->get_tag_values('multiple');
+  my $score = $f->score();
+
+  # pos strand unique = BLUE
+  if($isReversed eq '0' && $multiple eq '0') {
+    return 'blue';
+  }
+  # neg strand unique = BLUE
+  if($isReversed eq '1' && $multiple eq '0') {
+    return 'red';
+  }
+
+  # pos strand multiple align = lightblue
+  if($isReversed eq '0' && $multiple eq '1') {
+    return 'mediumslateblue';
+  }
+  # neg strand multiple align = pink
+  if($isReversed eq '1' && $multiple eq '1') {
+    return 'hotpink';
+  }
+
+  if($multiple eq '0' && $optColor) {
+    return $optColor;
+  }
+
+  # multiple aligners w/o strand info
+  if($multiple eq '1' || $score < 0) {
+    return 'lightslategray';
+  }
+
+  return 'black';
+}
+
+
+
 sub colorFromBinaryColorScore {
   my ($f, $first, $second) = @_;
   my ($binColor) = $f->get_tag_values('binaryColor');
