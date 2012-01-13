2a51fdeb846dd8f04b8d75d2e038903b9df1f19f
Haiming Wang <hwang@uga.edu>
2012-01-13 19:40:01 +0000

    patch for build 13
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-build-13@45131 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/Util/SynView.pm
+++ Site/cgi-lib/DAS/Util/SynView.pm
@@ -13,9 +13,11 @@ sub postgridGB2 {
   my %newdrawn;
   my @group;
   my %seen;
+  my $exon_x2 = 0;
   foreach my $box (@{$panel->boxes}) {
       my ($f, $x1, $y1, $x2, $y2) = @$box;
-      $location{$f->name} = [$x1, $y1, $x2, $y2];
+      $exon_x2 = $x2 and next if ($f->type =~ /exon/i);
+      $location{$f->name} = [$x1, $y1, $exon_x2, $y2];
       next unless $f->isa("Bio::SeqFeatureI");
       next if $f->isa("Bio::Das::Feature");
       my $gene = $f->name;
