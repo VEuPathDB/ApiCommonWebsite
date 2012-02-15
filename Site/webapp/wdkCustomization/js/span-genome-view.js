819c5b73ea7e56a0897f1fc6dad2e0f3d51e34b5
Xin Gao <xingao@upenn.edu>
2012-02-15 19:44:47 +0000

    handle genomic view for both dyna-spans & genes, and use datatables for the display
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/alter-views@45885 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/span-genome-view.js
+++ Site/webapp/wdkCustomization/js/span-genome-view.js
@@ -1,33 +1,8 @@
-function initializeSpanGenomeView() {
-    // get the max length of the sequences
-    var maxLength = 0;
-    $("#genome-view .sequence").each(function() {
-        var length = parseInt($(this).attr("length"));
-        if (length > maxLength) maxLength = length;
+function initializeGenomeView() {
+    // register click events
+    $("#genome-view .sequence .span").click(function() {
+        window.location.href = $(this).attr("url");
     });
-
-    $("#genome-view .sequence").each(function() {
-        // scale sequence ruler
-        var length = parseInt($(this).attr("length"));
-        var rulerWidth = length * 100.0 / maxLength;
-        var spans = $(this).find(".spans");
-        spans.children(".ruler").css("width", rulerWidth + "%");
-
-        // locate each span
-        spans.children(".span").each(function() {
-            // compute the start
-            var start = parseInt($(this).attr("start"));
-            var end = parseInt($(this).attr("end"));
-            var spanStart = start * 100.0 / maxLength;
-            var spanWidth = (end - start + 1) * 100.0 / maxLength;
-            $(this).css("left", spanStart + "%")
-                   .css("width", spanWidth + "%");
-            var width = $(this).width();
-            
-            // register click event
-            $(this).click(function() {
-                window.location.href = $(this).attr("url");
-            })
-        });
-    });
+    // register datatables
+    $("#genome-view").dataTable();
 }
