b15219ec2459a29e1cbda963682fa135b6817a3c
Xin Gao <xingao@upenn.edu>
2012-02-15 20:13:22 +0000

    fix the genomic view display for genes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/alter-views@45888 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/span-genome-view.js
+++ Site/webapp/wdkCustomization/js/span-genome-view.js
@@ -4,5 +4,8 @@ function initializeGenomeView() {
         window.location.href = $(this).attr("url");
     });
     // register datatables
-    $("#genome-view").dataTable();
+    $("#genome-view").dataTable({
+        "aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+        "iDisplayLength": 50
+    });
 }
