5582e3482776e6324c6f04ae4a42e4cf7d6756a6
Ja’Shon Cade <jcade@upenn.edu>
2012-03-19 15:49:23 +0000

    Created an example file of the new graph file format
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46398 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/Examples/RNASeqPct.pm
@@ -0,0 +1,50 @@
+package ApiCommonWebsite::View::GraphPackage::Examples::RNASeqPct;
+
+use strict;
+use vars qw( @ISA );
+
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
+use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+use ApiCommonWebsite::View::GraphPackage::PercentilePlot;
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $colors = ['#6495ED', '#E9967A', '#2F4F4F' ];
+  my $legend = ['Wild Type', 'sir2A', 'sir2B'];
+
+  $self->setMainLegend({colors => $colors, short_names => $legend, cols => 3});
+
+  my $graphObject = ApiCommonWebsite::View::GraphPackage::PercentilePlot->new();
+
+  $graphObject->setProfileSetNames(['Percentiles of of E-TABM-438 from Cowman']);
+  $graphObject->setPartName('bar');
+  $graphObject->setColors($colors);
+  $graphObject->setPlotTitle('This is a test of the emergency graph generation system');
+  
+  my $GO1 = ApiCommonWebsite::View::GraphPackage::PercentilePlot->new();
+  $GO1->setProfileSetNames(['Percentiles of of E-TABM-438 from Cowman']);
+  $GO1->setColors($colors);
+  $GO1->setPlotTitle('This is a test of the emergency graph generation system');
+
+  my $GO2 = ApiCommonWebsite::View::GraphPackage::LinePlot->new();
+  $GO2->setProfileSetNames(['Profiles of P.falciparum Newbold mRNA Seq data']);
+  $GO2->setColors(['#000080']);
+  $GO2->setPartName('line');
+  $GO2->setPlotTitle('This is a test of the emergency graph generation system');
+  $GO2->setYaxisLabel('Normalized Coverage (log2)');
+  $GO2->setAdjustProfile('profile[profile < 1] = 1; profile = log2(profile); ');
+  $GO2->setXaxisLabel('Hours');
+
+$self->setGraphObjects($graphObject,$GO1,$GO2);
+
+  return $self;
+}
+
+
+1;
+
