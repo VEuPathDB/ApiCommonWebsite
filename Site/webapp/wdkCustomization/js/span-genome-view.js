8b586ba3c93354cc37992e6e613fd048084121d3
Xin Gao <xingao@upenn.edu>
2012-03-20 20:27:41 +0000

    add a delayed tooltip to the genes on genomic summaryt view
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46428 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/span-genome-view.js
+++ Site/webapp/wdkCustomization/js/span-genome-view.js
@@ -1,7 +1,55 @@
 function initializeGenomeView() {
     // register click events
-    $("#genome-view .sequence .span").click(function() {
-        window.location.href = $(this).attr("url");
+    $("#genome-view .sequence .span").each(function() {
+        var span = $(this);
+        //span.click(function() {
+        //    window.location.href = $(this).attr("url");
+        //});
+        var content = $(this).children(".tooltip");
+        span.qtip({ content : content,
+                       show: {
+                               solo: true,
+                               event: 'click mouseenter'
+                             },
+                       hide: {
+                               event: 'click'
+                             },
+                       events: {
+                                 show: function(event, api) {
+                                       // qtip2 assigns an ID of "ui-tooltip-<id>" to the tooltip div
+                                       var tipSelector = '#ui-tooltip-' + api.get('id');
+
+                                       // define functions for hiding tooltip timer.
+                                       var cancelDelayedHide = function() {
+                                           // clear the previous timers
+                                           var timer = $(tipSelector).attr("timer");
+                                           if (timer != undefined) {
+                                               clearTimeout(timer);
+                                               $(tipSelector).removeAttr("timer");
+                                           }
+                                       }
+                                       var hide = function(){
+                                           $(tipSelector).qtip('hide'); 
+                                           cancelDelayedHide();
+                                       };
+                                       var delayedHide = function() {
+                                           cancelDelayedHide();
+                                           var timer = setTimeout("$('" + tipSelector + "').qtip('hide');", 1000);
+                                           $(tipSelector).attr("timer", timer);
+                                       };
+                                      
+                                       //
+                                       // when mouseout the span, start delayed hiding tip.
+                                       span.mouseout(delayedHide);
+                                       // when mouse in tip, cancel delayed hiding; 
+                                       // when clicking tip, hide tip immediately;
+                                       // when mouse out tip, start delayed hiding.
+                                       $(tipSelector).mouseover(cancelDelayedHide)
+                                                     .click(hide)
+                                                     .mouseout(delayedHide);
+                               }
+                          }
+                     });
     });
     $("#genome-view .sequence .ruler").click(function() {
         window.location.href = $(this).parents(".sequence").find(".sequence-id a").attr("href");
@@ -22,7 +70,7 @@ function initializeGenomeView() {
     $("#genome-view").dataTable({
         "bJQueryUI": true,
         "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-        "iDisplayLength": 10,
+        "iDisplayLength": 25,
         "aoColumns": [ null,
                        null,
                        null,
@@ -30,6 +78,7 @@ function initializeGenomeView() {
                        { "bSortable": false },
                        { "bSortable": false } ]
     });
+
 }
 
 function zoomInGenomeView(ele) {
