4b23bb244455c201f3ce79048f7c6bf63f5e90da
Ryan Doherty <tech@conical.org>
2012-03-22 21:31:09 +0000

    #8288: add custom JS file for Binding Site question
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46490 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/htdocs/assets/js/bindingSiteQuestion.js
@@ -0,0 +1,14 @@
+
+$(function() {
+	// set the initial description image
+	swapTfbsImage();
+	// change image when user changes TFBS name
+	$('#tfbs_name').click(swapTfbsImage);
+});
+
+
+function swapTfbsImage(x){
+	var names = $('#tfbs_name')[0];
+	var newVal = names.options[names.selectedIndex].value;
+	$('#tfbs_image').attr('src', '/a/images/pf_tfbs/' + newVal + '.png');
+}
