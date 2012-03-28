faffa17d4a76baa6d8d08a01816604eaa1d85b14
Haiming Wang <hwang@uga.edu>
2012-03-28 19:21:39 +0000

    updates: retrieve gps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46595 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/view/GeoIsolateViewHandler.java
+++ Model/src/java/org/apidb/apicommon/model/view/GeoIsolateViewHandler.java
@@ -8,19 +8,23 @@ public class GeoIsolateViewHandler extends IsolateViewHandler {
     @Override
     public String prepareSql(String idSql) throws WdkModelException,
             WdkUserException {
-        StringBuilder sql = new StringBuilder("select country as country, count(country) as total, data_type from ( SELECT ");
+        StringBuilder sql = new StringBuilder("select country as country, count(country) as total, data_type, lat, lng from ( SELECT ");
         sql.append("    i.source_id , ");
         sql.append("    i.data_type ,");
+        sql.append("    g.lat ,");
+        sql.append("    g.lng ,");
         sql.append("    v.term  as country    ");
         sql.append(" FROM apidb.isolatevocabulary v, ");
         sql.append("       apidb.isolatemapping m , ");
         sql.append("       ApidbTuning.IsolateAttributes i, ");
+        sql.append("       apidb.IsolateGPS g, ");
         sql.append("      (" + idSql + ") idq ");
         sql.append(" WHERE v.type = 'geographic_location' ");
         sql.append("  AND v.isolate_vocabulary_id = m.isolate_vocabulary_id ");
         sql.append("  AND i.na_sequence_id = m.na_sequence_id ");
+        sql.append("  AND g.country = v.term ");
         sql.append("  AND i.source_id = idq.source_id ");
-        sql.append(" ) group by country, data_type ");
+        sql.append(" ) group by country, data_type, lat, lng ");
 
         return sql.toString();
     }
