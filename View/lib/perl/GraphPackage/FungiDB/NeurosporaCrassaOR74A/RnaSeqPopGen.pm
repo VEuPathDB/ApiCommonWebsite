e35192bebf700ce844b989aa436ccc41cd1da053
John Brestelli <jbrestell@gmail.com>
2012-04-10 13:48:01 +0000

    update fungi graphs to use new classes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46837 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/FungiDB/NeurosporaCrassaOR74A/RnaSeqPopGen.pm
+++ View/lib/perl/GraphPackage/FungiDB/NeurosporaCrassaOR74A/RnaSeqPopGen.pm
@@ -6,8 +6,7 @@ use vars qw( @ISA );
 @ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
 use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
 
-use ApiCommonWebsite::View::GraphPackage::PercentilePlot;
-use ApiCommonWebsite::View::GraphPackage::RNASeqStackedBarPlot;
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
 
 sub init {
   my $self = shift;
@@ -21,7 +20,7 @@ sub init {
   my $sampleNames =["FGSC_1131", "FGSC_1132", "FGSC_1133", "FGSC_1165", "FGSC_2229b", "FGSC_2229", "FGSC_3199", "FGSC_3200", "FGSC_3211", "FGSC_3223", "FGSC_3943", "FGSC_3968", "FGSC_3975", "FGSC_4708", "FGSC_4712", "FGSC_4713", "FGSC_4715", "FGSC_4716", "FGSC_4730", "FGSC_4824", "FGSC_5910", "FGSC_5914", "FGSC_6203", "FGSC_851", "FGSC_8783", "FGSC_8784", "FGSC_8787", "FGSC_8789", "FGSC_8790", "FGSC_8816", "FGSC_8819", "FGSC_8829", "FGSC_8845", "FGSC_8848", "FGSC_8850", "FGSC_8851", "FGSC_8870", "FGSC_8871", "FGSC_8872", "FGSC_8874", "FGSC_8876", "FGSC_8878", "Perkins_4450", "Perkins_4452", "Perkins_4455", "Perkins_4465", "Perkins_4476", "Perkins_4496"];
  
   $self->setMainLegend({colors => $colors, short_names => $legend});
-  my $stackedCoverage = ApiCommonWebsite::View::GraphPackage::RNASeqStackedBarPlot->new();
+  my $stackedCoverage = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new();
   $stackedCoverage->setProfileSetNames(['N Crassa population genomics', 
                                         'N Crassa population genomics-diff']);
   $stackedCoverage->setColors($colors);
@@ -29,7 +28,7 @@ sub init {
   $stackedCoverage->setSampleLabels($sampleNames);
   $stackedCoverage->setBottomMarginSize(5);
 
-  my $percentile = ApiCommonWebsite::View::GraphPackage::PercentilePlot->new();
+  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new();
   $percentile->setProfileSetNames(['percentile - N Crassa population genomics']);
 #  $percentile->setForceHorizontalXAxis(1);
   $percentile->setColors([$colors->[0]]);
