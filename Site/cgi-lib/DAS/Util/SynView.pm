528a1cb161df0485cd4a3e9fde51778a7e3b5428
Haiming Wang <hwang@uga.edu>
2012-04-16 16:43:01 +0000

    calculate boundaries for exon/cds/intron
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@46986 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/DAS/Util/SynView.pm
+++ Site/cgi-lib/DAS/Util/SynView.pm
@@ -16,7 +16,7 @@ sub postgridGB2 {
   my $exon_x2 = 0;
   foreach my $box (@{$panel->boxes}) {
       my ($f, $x1, $y1, $x2, $y2) = @$box;
-      $exon_x2 = $x2 and next if ($f->type =~ /exon/i);
+      $exon_x2 = $x2 and next if ($f->type =~ /exon|CDS|UTR/i);
       $location{$f->name} = [$x1, $y1, $exon_x2, $y2];
       next unless $f->isa("Bio::SeqFeatureI");
       next if $f->isa("Bio::Das::Feature");
