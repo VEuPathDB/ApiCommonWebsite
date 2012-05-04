5d5f1df9d24e366e211f344bf76b9022b04a557c
Mark Heiges <mheiges@gmail.com>
2012-05-04 15:34:44 +0000

    adjustments to database name display
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47490 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/dashboard/view/databaseInfo.php
+++ Site/dashboard/view/databaseInfo.php
@@ -23,21 +23,15 @@ Related Links
     <li><a href="https://www.cbil.upenn.edu/dba/">Penn databases</a></li>
   </ul>
 </div>
-
+<p>
+<b>Service Name</b>:
 <?= ($adb->get('service_name')) ? $adb->get('service_name') : "<span class='warn'>service name not available</span>"; ?>
-
+</p>
 
 <p>
-<b>Identifiers</b>:
+<b>Other Identifiers</b>:
 <table border="0" cellspacing="3" cellpadding="2" align="">
 <tr class="secondary3"><th><font size="-2">Identifier</font></th><th><font size="-2">Value</font></th><th></th></tr>
-<tr class="rowLight"><td>Service Name</td><td><?= strtolower($adb->get('service_name')) ?></td>
-    <td><a href='javascript:void()' style="text-decoration:none"
-        onmouseover="return overlib(
-         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'service_name\')&nbsp;from&nbsp;dual</i>'
-        )"
-        onmouseout = "return nd();"><sup>[?]</sup></a></td>
-</tr>
 <tr class="rowMedium"><td>Instance Name</td><td><?= strtolower($adb->get('instance_name')) ?></td>
     <td><a href='javascript:void()' style="text-decoration:none" 
         onmouseover="return overlib(
