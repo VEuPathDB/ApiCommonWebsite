964e2995770e7576fda362535560c433fe76f726
John Brestelli <jbrestell@gmail.com>
2012-05-04 21:04:46 +0000

    fix uninitialized string warning;  add other fungi rnaseq graphs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47522 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/FungiDB/CryptococcusNeoformansGrubiiH99/RnaSeqCapReg.pm
+++ View/lib/perl/GraphPackage/FungiDB/CryptococcusNeoformansGrubiiH99/RnaSeqCapReg.pm
@@ -1,42 +1,35 @@
 package ApiCommonWebsite::View::GraphPackage::FungiDB::CryptococcusNeoformansGrubiiH99::RnaSeqCapReg;
 
+
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
-use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
 
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
 
 sub init {
   my $self = shift;
 
   $self->SUPER::init(@_);
 
-
-  my $colors =['#29ACF2', '#DDDDDD'];
-  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
   my $sampleNames =["WT-30C", "WT-37C", "ada2-30C", "ada2-37C", "cir1-30C", "cir1-37C", "nrg1-30C", "nrg1-37C"];
- 
-  $self->setMainLegend({colors => $colors, short_names => $legend});
-  my $stackedCoverage = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new();
-  $stackedCoverage->setProfileSetNames(['C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis', 
-                                        'C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis-diff']);
-  $stackedCoverage->setColors($colors);
-#  $stackedCoverage->setForceHorizontalXAxis(1);
-  $stackedCoverage->setSampleLabels($sampleNames);
-  $stackedCoverage->setBottomMarginSize(5);                                  
-                   
 
-  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new();
-  $percentile->setProfileSetNames(['percentile - C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis']);
-#  $percentile->setForceHorizontalXAxis(1);
-  $percentile->setColors([$colors->[0]]);
-  $percentile->setSampleLabels($sampleNames);
-  $self->setGraphObjects($stackedCoverage, $percentile);
+  $self->setSampleNames($sampleNames);
+  $self->setMinRpkmProfileSet('C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis');
+  $self->setDiffRpkmProfileSet('C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis-diff');
+  $self->setPctProfileSet('percentile - C.neoformans- ada2-delta, nrg1-delta, cir1-delta and KN99-alpha analysis');
+  $self->setColor('#29ACF2');
+  $self->makeGraphs(@_);
+
+  $self->setBottomMarginSize(6);
+
   return $self;
 }
 
 
-
 1;
+
+
+ 
+
--- View/lib/perl/GraphPackage/FungiDB/CryptococcusNeoformansGrubiiH99/RnaSeqNrg1.pm
+++ View/lib/perl/GraphPackage/FungiDB/CryptococcusNeoformansGrubiiH99/RnaSeqNrg1.pm
@@ -3,39 +3,30 @@ package ApiCommonWebsite::View::GraphPackage::FungiDB::CryptococcusNeoformansGru
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
-use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
 
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
 
 sub init {
   my $self = shift;
 
   $self->SUPER::init(@_);
 
+  $self->setBottomMarginSize(5.5);
 
-  my $colors =['#D87093', '#DDDDDD'];
-  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
+  my $sampleNames = ['Wild Type', 'nrg1 KO', 'nrg1 overexp'];
 
-  $self->setMainLegend({colors => $colors, short_names => $legend});
-  my $stackedCoverage = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new();
-  $stackedCoverage->setProfileSetNames(['C.neoformans NRG1 Expression', 
-                                        'C.neoformans NRG1 Expression-diff']);
-  $stackedCoverage->setColors($colors);
-  $stackedCoverage->setForceHorizontalXAxis(1);
-  $stackedCoverage->setSampleLabels(['H99 Wildtype',
-                                     'nrg1 KO',
-                                     'nrg1 Over-expression']);
+  $self->setSampleNames($sampleNames);
+  $self->setMinRpkmProfileSet('C.neoformans NRG1 Expression');
+  $self->setDiffRpkmProfileSet('C.neoformans NRG1 Expression-diff');
+  $self->setPctProfileSet('percentile - C.neoformans NRG1 Expression');
+  $self->setColor("#D87093");
+  $self->makeGraphs(@_);
 
-  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new();
-  $percentile->setProfileSetNames(['percentile - C.neoformans NRG1 Expression']);
-  $percentile->setForceHorizontalXAxis(1);
-  $percentile->setColors([$colors->[0]]);
-  
-  $self->setGraphObjects($stackedCoverage, $percentile);
+
   return $self;
 }
 
 
-
 1;
+
