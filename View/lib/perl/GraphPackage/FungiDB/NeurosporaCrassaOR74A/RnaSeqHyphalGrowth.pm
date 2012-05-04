5840031d83d70ed5d902498dbc7b75ac51a09ff9
John Brestelli <jbrestell@gmail.com>
2012-05-04 18:13:12 +0000

    New package for Standard RNASeq Stacked Barplots;  update 2 for Fungidb
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47502 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/FungiDB/NeurosporaCrassaOR74A/RnaSeqHyphalGrowth.pm
+++ View/lib/perl/GraphPackage/FungiDB/NeurosporaCrassaOR74A/RnaSeqHyphalGrowth.pm
@@ -1,42 +1,24 @@
 package ApiCommonWebsite::View::GraphPackage::FungiDB::NeurosporaCrassaOR74A::RnaSeqHyphalGrowth;
+
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
-use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
-
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
 
 sub init {
   my $self = shift;
 
   $self->SUPER::init(@_);
 
-  my $colors =['#D87093', '#DDDDDD'];
-  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
+  $self->setMinRpkmProfileSet("NcraOR74A Hyphal Growth RNASeq");
+  $self->setDiffRpkmProfileSet("NcraOR74A Hyphal Growth RNASeq - diff");
+  $self->setPctProfileSet("percentile - NcraOR74A Hyphal Growth RNASeq");
+  $self->setColor("#D87093");
+  $self->makeGraphs(@_);
 
-  $self->setMainLegend({colors => $colors, short_names => $legend});
-
-  my $stackedCoverage = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new();
-  $stackedCoverage->setProfileSetNames(['NcraOR74A Hyphal Growth RNASeq', 
-                                        'NcraOR74A Hyphal Growth RNASeq - diff'
-                                        ]);
-  $stackedCoverage->setColors($colors);
-  $stackedCoverage->setForceHorizontalXAxis(1);
-  $stackedCoverage->setSampleLabels(['3 HR', 
-                                     '5 HR', 
-                                     '20 HR']);
-
-  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new();
-  $percentile->setProfileSetNames(['percentile - NcraOR74A Hyphal Growth RNASeq']);
-  $percentile->setForceHorizontalXAxis(1);
-  $percentile->setColors([$colors->[0]]);
-
-  $self->setGraphObjects($stackedCoverage, $percentile);
+  $self->setBottomMarginSize(4);
   return $self;
-
 }
-
-
 
 1;
---
+++ View/lib/perl/GraphPackage/SimpleRNASeq.pm
@@ -0,0 +1,87 @@
+package ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
+use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+use ApiCommonWebsite::View::GraphPackage::Util;
+
+sub getMinRpkmProfileSet { $_[0]->{_min_rpkm_profile_set} }
+sub setMinRpkmProfileSet { $_[0]->{_min_rpkm_profile_set} = $_[1] }
+
+sub getDiffRpkmProfileSet { $_[0]->{_diff_rpkm_profile_set} }
+sub setDiffRpkmProfileSet { $_[0]->{_diff_rpkm_profile_set} = $_[1] }
+
+sub getPctProfileSet { $_[0]->{_pct_profile_set} }
+sub setPctProfileSet { $_[0]->{_pct_profile_set} = $_[1] }
+
+sub getAdditionalRCode { $_[0]->{_additional_r_code} }
+sub setAdditionalRCode { $_[0]->{_additional_r_code} = $_[1] }
+
+sub getColor { $_[0]->{_color} }
+sub setColor { $_[0]->{_color} = $_[1] }
+
+
+sub setBottomMarginSize {
+  my ($self, $ms) = @_;
+
+  # set the margin size for the mixed plot
+  $self->SUPER::setBottomMarginSize($ms);
+
+
+  # set the margin size for each of the parts if already set
+  my $graphObjects = $self->getGraphObjects();
+  foreach(@{$graphObjects}) {
+    $_->setElementNameMarginSize($ms);
+  }
+
+}
+
+
+sub makeGraphs {
+  my $self = shift;
+
+  my $minRpkmProfileSet = $self->getMinRpkmProfileSet();
+  my $diffRpkmProfileSet = $self->getDiffRpkmProfileSet();
+  my $pctProfileSet = $self->getPctProfileSet();
+  my $additionalRCode = $self->getAdditionalRCode();
+  my $color = $self->getColor() ? $self->getColor() : 'blue';
+
+  my @colors = ($color, '#DDDDDD');
+  my @legend = ("Uniquely Mapped", "Non-Uniquely Mapped");
+
+  $self->setMainLegend({colors => \@colors, short_names => \@legend, cols => 2});
+
+  my @profileArray = ([$minRpkmProfileSet],
+                      [$diffRpkmProfileSet],
+                     );
+
+  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileArray);
+  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets([[$pctProfileSet]]);
+
+  my $stacked = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
+  $stacked->setProfileSets($profileSets);
+  $stacked->setColors(\@colors);
+  $stacked->addAdjustProfile($additionalRCode);
+
+  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new(@_);
+  $percentile->setProfileSets($percentileSets);
+  $percentile->setColors([$colors[0]]);
+  $percentile->addAdjustProfile($additionalRCode);
+
+  if(my $bottomMargin = $self->getBottomMarginSize()) {
+    $stacked->setElementNameMarginSize($bottomMargin);
+    $percentile->setElementNameMarginSize($bottomMargin);
+  }
+
+
+  $self->setGraphObjects($stacked, $percentile);
+
+  return $self;
+}
+
+
+1;
