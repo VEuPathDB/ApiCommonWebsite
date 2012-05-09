aeacd0cf31798ecbda40dc38c7f9ac25edabc6db
Raghuraman Ramamurthy <rraghu@_.com>
2012-05-09 03:42:50 +0000

    added sample names
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47625 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/FungiDB/CandidaAlbicansSC5314/RnaSeqCompAnn.pm
+++ View/lib/perl/GraphPackage/FungiDB/CandidaAlbicansSC5314/RnaSeqCompAnn.pm
@@ -11,6 +11,9 @@ sub init {
 
   $self->SUPER::init(@_);
 
+  my $sampleNames =["Cellwall Damaging Environment", "High Oxidative Stress", "Low Oxidative Stress", "Media Containing Serum", "medium at PH4", "medium at PH8", "Nitrosative Stress", "Non Cellwall Damage Environment", "Non Nitrosative Environment", "Non Oxidation Stress", "YPD", "YPD media", "YPD media containing Serum"];
+
+  $self->setSampleNames($sampleNames);
   $self->setMinRpkmProfileSet("C albicans Comprehensive Annotation");
   $self->setDiffRpkmProfileSet("C albicans Comprehensive Annotation-diff");
   $self->setPctProfileSet("percentile - C albicans Comprehensive Annotation");
