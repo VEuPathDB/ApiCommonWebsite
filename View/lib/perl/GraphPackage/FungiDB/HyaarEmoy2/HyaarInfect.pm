7e54004d08b094a298fcd9560d87e631cf8291f6
Sucheta Tripathy <tsucheta@gmail.com>
2012-05-17 20:06:08 +0000

    updated all perl packages for graphPackage for Oomycetes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47867 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/FungiDB/HyaarEmoy2/HyaarInfect.pm
@@ -0,0 +1,27 @@
+package ApiCommonWebsite::View::GraphPackage::FungiDB::HyaarEmoy2::HyaarInfect;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $sampleNames =["Experiment1", "Experiment2", "Experiment3", "Experiment4", "Experiment5", "Experiment6"];
+
+  $self->setSampleNames($sampleNames);
+  $self->setMinRpkmProfileSet("H arabidopsidis Comprehensive Annotation");
+  $self->setDiffRpkmProfileSet("H arabidopsidis Comprehensive Annotation-diff");
+  $self->setPctProfileSet("percentile - H arabidopsidis Comprehensive Annotation");
+  $self->setColor("#D87093");
+  $self->makeGraphs(@_);
+
+  $self->setBottomMarginSize(8);
+  return $self;
+}
+
+1;
