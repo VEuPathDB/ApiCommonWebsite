38e905285a08f847e2971baed7272189602656a3
Raghuraman Ramamurthy <rraghu@_.com>
2012-05-18 22:19:55 +0000

    graph for Hyaar rnaseq
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@47915 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/FungiDB/HyaarEmoy2/RnaSeqInfection.pm
@@ -0,0 +1,27 @@
+package ApiCommonWebsite::View::GraphPackage::FungiDB::HyaarEmoy2::RnaSeqInfection;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $sampleNames =["Experiment1", "Experiment2", "Experiment3", "Experiment4", "Experiment5", "Experiment6"];
+
+  $self->setSampleNames($sampleNames);
+  $self->setMinRpkmProfileSet('H. arabidopsidis expression during infection');
+  #$self->setDiffRpkmProfileSet("H. arabidopsidis expression during infection profile");
+  $self->setPctProfileSet('percentile - H. arabidopsidis expression during infection');
+  $self->setColor("#D87093");
+  $self->makeGraphs(@_);
+
+  $self->setBottomMarginSize(8);
+  return $self;
+}
+
+1;
