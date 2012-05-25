c66c27f2a8e7f69721cda820559dc08eae1fa743
Mark Heiges <mheiges@gmail.com>
2012-05-25 16:15:00 +0000

    add mbean with servlet api attributes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@48168 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/jmx/MBeanSet.java
+++ Model/src/java/org/apidb/apicommon/jmx/MBeanSet.java
@@ -13,15 +13,16 @@ public class MBeanSet {
     Map<String, String> map = new HashMap<String, String>() {
     	private static final long serialVersionUID = -1L;
     	{
-    		put("org.apidb.apicommon.jmx.mbeans.wdk.Meta",          "org.gusdb.wdk:type=Meta");
-    		put("org.apidb.apicommon.jmx.mbeans.wdk.Cache",         "org.gusdb.wdk:type=Cache");
-    		put("org.apidb.apicommon.jmx.mbeans.wdk.ModelConfig",   "org.gusdb.wdk:type=ModelConfig");
-    		put("org.apidb.apicommon.jmx.mbeans.wdk.CommentConfig", "org.gusdb.wdk:type=CommentConfig");
-    		put("org.apidb.apicommon.jmx.mbeans.wdk.Properties",    "org.gusdb.wdk:type=Properties");
-    		put("org.apidb.apicommon.jmx.mbeans.apidb.UserDBExt",   "org.apidb.wdk:group=Databases,type=UserDB");
-    		put("org.apidb.apicommon.jmx.mbeans.apidb.AppDBExt",    "org.apidb.wdk:group=Databases,type=AppDB");
-    		put("org.apidb.apicommon.jmx.mbeans.LoggerManagement",  "org.apidb.wdk:type=Log4J");
-    	}
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.Meta",            "org.gusdb.wdk:type=Meta");
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.ServletVersions", "org.gusdb.wdk:type=ServletVersions");
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.Cache",           "org.gusdb.wdk:type=Cache");
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.ModelConfig",     "org.gusdb.wdk:type=ModelConfig");
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.CommentConfig",   "org.gusdb.wdk:type=CommentConfig");
+    		put("org.apidb.apicommon.jmx.mbeans.wdk.Properties",      "org.gusdb.wdk:type=Properties");
+    		put("org.apidb.apicommon.jmx.mbeans.apidb.UserDBExt",     "org.apidb.wdk:group=Databases,type=UserDB");
+    		put("org.apidb.apicommon.jmx.mbeans.apidb.AppDBExt",      "org.apidb.wdk:group=Databases,type=AppDB");
+    		put("org.apidb.apicommon.jmx.mbeans.LoggerManagement",    "org.apidb.wdk:type=Log4J");
+    	}  
     };
 
     return Collections.unmodifiableMap(map);
