19fca5bf7b5a0c1a340ef4c273dfe1c02ecdbc36
Cristina Aurrecoechea <aurreco@uga.edu>
2012-07-12 21:37:46 +0000

    refs # 6384 warning IE users that they can install Chrome Frame which together with a new <meta> tag will make their experience better.
    popup.js: adding 2 spaces indentation
    this file should live in WDK....
    
    footer: contains the "content" div...  (content itself in verbiage.xml)
    maybe these divs should be in specific tag files?
    
    This commit requires change in WDK: 
    - verbiage.xml for "content"
    - wdkCommon.css affecting this popup 
    (not sure why this css was here when the js function is not....)
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@48776 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/popups.js
+++ Site/htdocs/assets/js/popups.js
@@ -1,31 +1,32 @@
 /*
   popup.js
-
+  
   Functions for displaying login,register, and unsupported browser popups
+  
+*/
 
-*/jQuery
-jQuery(document).ready(function() {
-	//popUnsupported();
+jQuery(document).ready(function(){
+  popUnsupported();
 });
 
 function popLogin(destination){
-	jQuery.blockUI({message : jQuery("#loginForm"), css : {cursor: 'auto',width:'30%',top:'40%',left:'35%' }});
-	if (destination) {
-		jQuery("#loginForm input:hidden[name=refererUrl]").val(destination);
-	}
+  jQuery.blockUI({message : jQuery("#loginForm"), css : {cursor: 'auto',width:'30%',top:'40%',left:'35%' }});
+  if (destination) {
+    jQuery("#loginForm input:hidden[name=refererUrl]").val(destination);
+  }
 }
 
 function popRegister(){
-	jQuery.blockUI({message : jQuery("#registerForm"), css : {cursor: 'auto',width: '80%',top: '40px',left:'10%' } });
+  jQuery.blockUI({message : jQuery("#registerForm"), css : {cursor: 'auto',width: '80%',top: '40px',left:'10%' } });
 }
 
 
 //  && !(jQuery.browser.msie && parseInt(jQuery.browser.version) > 6) 
 function popUnsupported(){
-	var userAgent = navigator.userAgent.toLowerCase();
-	if (!jQuery.browser.mozilla && !jQuery.browser.safari  && !jQuery.browser.webkit //In case we upgrade to jQuery 1.4+
-            && jQuery.cookie('api-unsupported') == null){
-		var message = "<div id='browser-info'><h2>Your browser is not fully supported</h2><p>For the best experience, we recommend that you download one of the browsers listed below.</p><h3>Download a Supported Browser:</h3><ul id='supported-browsers'><li class='browser-link'><a href='http://www.mozilla.com/firefox/'>Firefox</a></li><li class='browser-link'><a href='http://www.apple.com/safari/'>Safari</a></li><li class='browser-link'><a href='http://www.google.com/chrome/'>Chrome</a></li><!--<li class='browser-link win'><a href='http://www.microsoft.com/windows/internet-explorer/default.aspx'>Internet Explorer 8</a></li><li class='browser-link'><a href='http://www.microsoft.com/windows/internet-explorer/ie7/'>Internet Explorer 7</a></li><li class='browser-link'><a href='http://www.opera.com/download/'>Opera</a></li>--></ul><p>IE 7,8,9 work for the most part but you might encounter minor problems.</p><p>If you are unable to upgrade your browser, please <a href='mailto:" + helpEmail + "'>contact us</a>.</p><p>To continue using this site with your current browser, click 'Ignore.'</p><input type='submit' value='Ignore' onclick=\"jQuery.cookie('api-unsupported',true,{path:'/'});jQuery.unblockUI();\"/></div>"
-		jQuery.blockUI({message : message, css : {cursor:'auto',top:'25%'}});
-	}	
+  var userAgent = navigator.userAgent.toLowerCase();
+  if (!jQuery.browser.mozilla && !jQuery.browser.safari  && !jQuery.browser.webkit //In case we upgrade to jQuery 1.4+
+      && jQuery.cookie('api-unsupported') == null){
+    var message = jQuery("#wdk-dialog-IE-warning");
+    jQuery.blockUI({message : message, css : {cursor:'auto',top:'5%'}});
+  }	
 }
