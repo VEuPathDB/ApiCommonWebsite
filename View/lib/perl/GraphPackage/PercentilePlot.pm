a53f64118881364e3517e7b9b7c888851ac8d6d9
Ja’Shon Cade <jcade@upenn.edu>
2012-08-13 20:16:08 +0000

    1. Removed LegendPlot.pm and references to LegendPlot.pm 2. updated CortesTransVar Crossstrains to use same color for all strains of the same subclonal line.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@49352 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/PercentilePlot.pm
@@ -0,0 +1,23 @@
+package ApiCommonWebsite::View::GraphPackage::PercentilePlot;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlot );
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+use ApiCommonWebsite::View::GraphPackage::Util;
+
+use Data::Dumper;
+
+#--------------------------------------------------------------------------------
+
+sub new {
+   my ($class, $args) = @_;
+   my $self = $class->SUPER::new($args);
+   $self->setPartName('percentile');
+   $self->setYaxisLabel('Percentile');
+   $self->setDefaultYMax(50);
+   $self->setDefaultYMin(0);
+   $self->setIsLogged(0);
+   return $self;
+}
