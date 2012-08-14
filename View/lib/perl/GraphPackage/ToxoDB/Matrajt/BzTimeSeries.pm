98cd8e8d6a22eddc86d44ff5c048d8a0072f5890
Ja’Shon Cade <jcade@upenn.edu>
2012-08-14 20:20:45 +0000

    fixes (#7512), Changed So that TZ is orange, as in the Fritz/Bucholz experiment, and Bz samples are blue, with increaing intensity over the time course
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@49392 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Matrajt/BzTimeSeries.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Matrajt/BzTimeSeries.pm
@@ -15,11 +15,11 @@ sub init {
   $self->setPlotWidth(450);
   $self->setBottomMarginSize(9);
 
-  my $colors = ['#FF0000', '#FF6600','#FFFF00', '#009900', '#0000CC',];
+  my $colors = [ '#E9967A', '#87CEFA', '#00BFFF', '#4169E1', '#0000FF', ];
 
   my $legend = ['Extracellular\nTachyzoite (0 hrs)', 'Bradyzoite (24 hrs)', 'Bradyzoite (36 hrs)',' Bradyzoite (48 hrs)', 'Bradyzoite (72 hrs)'];
 
-  $self->setMainLegend({colors => ['#FF0000', '#FF6600','#FFFF00', '#009900', '#0000CC'], short_names => $legend, cols=> 3});
+  $self->setMainLegend({colors => [ '#E9967A', '#87CEFA', '#00BFFF','#4169E1', '#0000FF', ], short_names => $legend, cols=> 3});
 
     $self->setProfileSetsHash
     ({rma => {profiles => ['expression profiles of TgRH_Matrajt_GSE23174_Bz_Time_Series'],
