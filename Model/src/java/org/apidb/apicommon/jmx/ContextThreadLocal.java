0de69962e31f156c8ad5fe6585c0549aee06fda8
Mark Heiges <mheiges@gmail.com>
2012-08-25 03:23:35 +0000

    minor formating cleanup
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@49563 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/jmx/ContextThreadLocal.java
+++ Model/src/java/org/apidb/apicommon/jmx/ContextThreadLocal.java
@@ -2,6 +2,9 @@ package org.apidb.apicommon.jmx;
 
 import javax.servlet.ServletContext;
 
+/**
+ * A ThreadLocal to store the ServletContext for the webapp.
+ */
 public class ContextThreadLocal {
 
   public static final ThreadLocal<ServletContext> wdkThreadLocal = new ThreadLocal<ServletContext>();
