d89751f39d7df7dbb05ad38184e87fda7fdd156c
Edward Liaw <ed.liaw@gmail.com>
2012-09-28 15:02:03 +0000

    Added Fungidb Cimm RNAseq experiment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@50446 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/FungiDB/CimmRS/RnaSeqCimmComTran.pm
@@ -0,0 +1,29 @@
+package ApiCommonWebsite::View::GraphPackage::FungiDB::CimmRS::RnaSeqCimmComTran;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
+
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  #$self->setPlotWidth(800);
+  #$self->setBottomMarginSize(7);
+
+  $self->setAdditionalRCode("colnames(profile.df) =  sub(\".fastq\", \"\", colnames(profile.df));");
+  $self->setMinRpkmProfileSet("C immitis saprobic hyphae and parasitic spherules");
+  $self->setDiffRpkmProfileSet("C immitis saprobic hyphae and parasitic spherules-diff");
+  $self->setPctProfileSet("percentile - C immitis saprobic hyphae and parasitic spherules");
+  $self->setColor("#29ACF2");
+  $self->makeGraphs(@_);
+
+  return $self;
+}
+
+
+1;
