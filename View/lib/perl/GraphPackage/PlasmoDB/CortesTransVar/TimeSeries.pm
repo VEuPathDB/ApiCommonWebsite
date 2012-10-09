36bbe6513fbd3ed8a6a287516e205d699b91146e
Ja’Shon Cade <jcade@upenn.edu>
2012-10-09 19:36:36 +0000

    fixed Graphs following the reload of the Cortes Microarray data
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@50674 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/CortesTransVar/TimeSeries.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/CortesTransVar/TimeSeries.pm
@@ -17,14 +17,14 @@ sub init {
   $self->SUPER::init(@_);
   
 
-  my $profileBase = "Transcriptional variation in Plasmodium falciparum";
-  my $ref_strain = ['3d7a',]
+  my $profileBase = 'Profiles of transcriptional variation in Plasmodium falciparum';
+  my $ref_strain = ['3d7a',];
   my $parental_strains = ['7g8','hb3a','d10'];
   my $_3d7strains = ['3d7a','10g','1_2b','3d7b','w41'];
   my $_7g8strains = ['kg7','7g8','ld10', 'we5', 'zf8'];
   my $hb3strains = ['hb3b','ab10','hb3a','ab6','bb8','bc4'];
   my $d10strains = ['e3','f1','g2','d10','g4'];
-  my $parental_strains = ['3D7A','7g8','hb3a','d10'];
+  my $parental_strains = ['7g8','hb3a','d10'];
 
   my @colorSet = ('#FF0000','#FF6600','#FFFF00','#009900','#0000CC','#660033',);
   my @colors = (@colorSet[0..4],@colorSet[0..4],@colorSet[0..5],@colorSet[0..4],@colorSet[0..3],);
@@ -37,7 +37,7 @@ sub init {
   $self->setMainLegend({colors => \@legendColors, short_names => \@legend, points_pch => \@pointsPCH, cols => 2, fill=> 0},);
 
   $self->setPlotWidth(450);
-  my @refColor = @colorSet[0];
+  my @refColor = ($colorSet[0],);
   my @_3d7Colors = @colorSet[0..4];
   my @_7g8Colors = @colorSet[0..4];
   my @hb3Colors = @colorSet[0..5];
@@ -54,7 +54,7 @@ sub init {
   push @hb3Pch, $pointsPCH[2] for 1 .. 6;
   push @d10Pch, $pointsPCH[3] for 1 .. 5;
   
-  my @refGraph = $self->defineGraphs('Reference_3D7-A',$ref_strain, \@refColors, $profileBase, 'red percentile', \@_3d7PCH);
+  my @refGraph = $self->defineGraphs('Reference_3D7-A',$ref_strain, \@refColor, $profileBase, 'red percentile', \@_3d7Pch);
   my @parentalGraphs = $self->defineGraphs('Parental',$parental_strains, \@parentalColors, $profileBase, 'red percentile', \@pointsPCH); 
   my @_3d7Graphs = $self->defineGraphs('3D7_derived',$_3d7strains, \@_3d7Colors, $profileBase, 'red percentile', \@_3d7Pch );
   my @_7g8Graphs = $self->defineGraphs('7G8_derived',$_7g8strains, \@_7g8Colors, $profileBase, 'red percentile', \@_7g8Pch );
@@ -79,44 +79,46 @@ sub defineGraphs {
     my @percentileSetName = ("$percentile_prefix - $profile_base $name");
     push(@profileSetNames, [@profileSetName]);
     push(@percentileSetNames, [@percentileSetName]);
-
+    
   }
 
-  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetNames);
-  my $line = ApiCommonWebsite::View::GraphPackage::LinePlot::LogRatio->new(@_);
-  $line->setProfileSets($profileSets);
-  $line->setColors($color);
-  $line->setPointsPch($pointsPch);
-  $line->setPartName("exprn_val_$tag");
-  $line->setScreenSize(250);
-  $line->setElementNameMarginSize($bottomMargin);
-  $percentile->setSmoothLines(1);
-  $percentile->setSplineApproxN(200);
-  $line->setHasExtraLegend(1);
-  $line->setExtraLegendSize(4.5);
-  $line->setLegendLabels($names);
-  my $lineTitle = $line->getPlotTitle();
-  $line->setPlotTitle("$tag - $lineTitle");
-  $line->setXaxisLabel("Hours");
 
-  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetNames);
-  my $percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
-  $percentile->setProfileSets($percentileSets);
-  $percentile->setPointsPch($pointsPch);
-  $percentile->setColors($color);
-  $percentile->setPartName("percentile_$tag");
-  my $pctTitle = $percentile->getPlotTitle();
-  $percentile->setScreenSize(250);
-  $percentile->setElementNameMarginSize($bottomMargin);
-  $percentile->setSmoothLines(1);
-  $percentile->setSplineApproxN(200);
-  $percentile->setHasExtraLegend(1);
-  $percentile->setExtraLegendSize(4.5);
-  $percentile->setLegendLabels($names);
-  $percentile->setPlotTitle("$tag - $pctTitle");
-  $percentile->setXaxisLabel("Hours");
+   my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetNames);
+   my $line = ApiCommonWebsite::View::GraphPackage::LinePlot::LogRatio->new(@_);
+   $line->setProfileSets($profileSets);
+   $line->setColors($color);
+   $line->setPointsPch($pointsPch);
+   $line->setPartName("exprn_val_$tag");
+   $line->setScreenSize(250);
+   $line->setElementNameMarginSize($bottomMargin);
+   $line->setSmoothLines(1);
+   $line->setSplineApproxN(200);
+   $line->setHasExtraLegend(1);
+   $line->setExtraLegendSize(6.5);
+   $line->setLegendLabels($names);
+   my $lineTitle = $line->getPlotTitle();
+   $line->setPlotTitle("$tag - $lineTitle");
+   $line->setXaxisLabel("Hours");
 
-  return( $line, $percentile );
+   my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetNames);
+   my $percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
+   $percentile->setProfileSets($percentileSets);
+   $percentile->setPointsPch($pointsPch);
+   $percentile->setColors($color);
+   $percentile->setPartName("percentile_$tag");
+   my $pctTitle = $percentile->getPlotTitle();
+   $percentile->setScreenSize(250);
+   $percentile->setElementNameMarginSize($bottomMargin);
+   $percentile->setSmoothLines(1);
+   $percentile->setSplineApproxN(200);
+   $percentile->setHasExtraLegend(1);
+   $percentile->setExtraLegendSize(6.5);
+   $percentile->setLegendLabels($names);
+   $percentile->setPlotTitle("$tag - $pctTitle");
+   $percentile->setXaxisLabel("Hours");
+
+   return( $line, $percentile );
+
 
 }
 1;
