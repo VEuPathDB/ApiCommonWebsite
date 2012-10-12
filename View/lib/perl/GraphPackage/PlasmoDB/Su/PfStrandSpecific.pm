0636b2a46dc49d0495899b83d9f50b3f50891ed2
John Brestelli <jbrestell@gmail.com>
2012-10-12 21:59:54 +0000

    unify strand specific graphs for Su
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@50806 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/PlasmoDB/Su/PfStrandSpecific.pm
@@ -0,0 +1,30 @@
+package ApiCommonWebsite::View::GraphPackage::PlasmoDB::Su::PfStrandSpecific;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleStrandSpecificRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleStrandSpecificRNASeq;
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  $self->setMinSenseRpkmProfileSet("P. falciparum Su Strand Specific RNA Seq data - sense strand");
+  $self->setMinAntisenseRpkmProfileSet("P. falciparum Su Strand Specific RNA Seq data - antisense strand");
+
+  $self->setDiffSenseRpkmProfileSet("P. falciparum Su Strand Specific RNA Seq data - sense strand - diff");
+  $self->setDiffAntisenseRpkmProfileSet("P. falciparum Su Strand Specific RNA Seq data - antisense strand - diff");
+
+  $self->setPctSenseProfileSet("percentile - P. falciparum Su Strand Specific RNA Seq data - sense strand");
+  $self->setPctAntisenseProfileSet("percentile - P. falciparum Su Strand Specific RNA Seq data - antisense strand");
+
+  $self->setColor("#8F006B");
+
+  $self->makeGraphs(@_);
+
+  return $self;
+
+
+}
