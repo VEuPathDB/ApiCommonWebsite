6c6d3211820a106fcf55167301f109bc6ca12b82
John Brestelli <jbrestell@gmail.com>
2012-10-12 18:58:56 +0000

    update Reid Grahps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@50780 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Reid/NcRnaSeq.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Reid/NcRnaSeq.pm
@@ -3,44 +3,23 @@ package ApiCommonWebsite::View::GraphPackage::ToxoDB::Reid::NcRnaSeq;
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
-use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
-
-use ApiCommonWebsite::View::GraphPackage::Util;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
 
 sub init {
   my $self = shift;
-
   $self->SUPER::init(@_);
 
-  my $colors =['#6A5ACD', '#DDDDDD'];
-
-  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
-
-  my $xAxisLabels = ["day 3", "day 4"];
-
-  $self->setMainLegend({colors => $colors, short_names => $legend, cols => 2});
-
-   my @profileSetsArray = (['N. caninum Day 3-4 Tachyzoite', '', ''],
-                           ['N. caninum Day 3-4 Tachyzoite-diff', '', ''],
-                          );
-  my @percentileSetsArray = (['percentile - N. caninum Day 3-4 Tachyzoite', '','']);
-
-  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetsArray);
-  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetsArray);
-
-  my $stacked = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stacked->setProfileSets($profileSets);
-  $stacked->setColors($colors);
-
-  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new(@_);
-  $percentile->setProfileSets($percentileSets);
-  $percentile->setColors([@$colors[0]]);
-
-  $self->setGraphObjects($stacked, $percentile);
+  $self->setMinRpkmProfileSet("N. caninum Day 3-4 Tachyzoite");
+  $self->setDiffRpkmProfileSet("N. caninum Day 3-4 Tachyzoite - diff");
+  $self->setPctProfileSet("percentile - N. caninum Day 3-4 Tachyzoite");
+  $self->setColor("#6A5ACD");
+  $self->makeGraphs(@_);
+  $self->setBottomMarginSize(4.5);
 
   return $self;
 }
 
 1;
+
+
--- View/lib/perl/GraphPackage/ToxoDB/Reid/TgVEGRnaSeq.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Reid/TgVEGRnaSeq.pm
@@ -1,43 +1,21 @@
-package ApiCommonWebsite::View::GraphPackage::ToxoDB::Reid::TgME49RnaSeq;
+package ApiCommonWebsite::View::GraphPackage::ToxoDB::Reid::TgVEGRnaSeq;
 
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
-use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
-use ApiCommonWebsite::View::GraphPackage::BarPlot;
-
-use ApiCommonWebsite::View::GraphPackage::Util;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeq );
+use ApiCommonWebsite::View::GraphPackage::SimpleRNASeq;
 
 sub init {
   my $self = shift;
-
   $self->SUPER::init(@_);
 
-  my $colors =['#6A5ACD', '#DDDDDD'];
-
-  my $legend = ["Uniquely Mapped", "Non-Uniquely Mapped"];
-  my $xAxisLabels = ["day 3", "day 4"];
-
-  $self->setMainLegend({colors => $colors, short_names => $legend, cols => 2});
-
-   my @profileSetsArray = (['T. gondii VEG Day 3-4 Tachyzoite aligned to the VEG Genome', '', ''],
-                           ['T. gondii VEG Day 3-4 Tachyzoite aligned to the VEG Genome-diff', '', ''],
-                          );
-  my @percentileSetsArray = (['percentile - T. gondii VEG Day 3-4 Tachyzoite aligned to the VEG Genome', '','']);
-
-  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetsArray);
-  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetsArray);
-
-  my $stacked = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stacked->setProfileSets($profileSets);
-  $stacked->setColors($colors);
-
-  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new(@_);
-  $percentile->setProfileSets($percentileSets);
-  $percentile->setColors([@$colors[0]]);
-
-  $self->setGraphObjects($stacked, $percentile);
+  $self->setMinRpkmProfileSet("T. gondii VEG Day 3-4 Tachyzoite aligned to the TgME49 Genome");
+  $self->setDiffRpkmProfileSet("T. gondii VEG Day 3-4 Tachyzoite aligned to the TgME49 Genome - diff");
+  $self->setPctProfileSet("percentile - T. gondii VEG Day 3-4 Tachyzoite aligned to the TgME49 Genome");
+  $self->setColor("#E6CC80");
+  $self->makeGraphs(@_);
+  $self->setBottomMarginSize(4.5);
 
   return $self;
 }
