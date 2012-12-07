7db8469a0b8d971a9dd1b95f0e7fdda80e3f8114
John Brestelli <jbrestell@gmail.com>
2012-12-07 20:34:45 +0000

    add Uniprot module
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@52056 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/CgiApp/UniprotDBXRefs.pm
@@ -0,0 +1,36 @@
+package ApiCommonWebsite::View::CgiApp::UniprotDBXRefs;
+@ISA = qw( ApiCommonWebsite::View::CgiApp );
+
+use strict;
+use ApiCommonWebsite::View::CgiApp;
+
+sub run {
+  my ($self, $cgi) = @_;
+
+  print STDOUT $cgi->header('text/plain');
+
+  my $dbh = $self->getQueryHandle($cgi);
+  my $sql = "select ga.project_id, 
+       ga.source_id, 
+       gi.id as uniprot
+from apidbtuning.geneid gi, apidbtuning.geneattributes ga
+where ga.source_id = gi.gene
+and (gi.database_name like '%uniprot_dbxref_RSRC'
+or gi.database_name like '%dbxref_gene2Uniprot_RSRC'
+or gi.database_name = 'Links to Uniprot Genes')";
+  
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+
+  print "uniprotID\teupathid\n";
+      
+  while (my ($project, $source_id, $uniprot) = $sth->fetchrow_array) {
+    print "$uniprot\t$project:$source_id\n";
+  }
+  
+  $sth->finish();
+
+}
+
+1;
+
