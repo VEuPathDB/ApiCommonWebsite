0df4d5e95ca2c153fb49728c037b83927787db35
Ja’Shon Cade <jcade@upenn.edu>
2012-12-17 18:43:21 +0000

    updated graphs for Giardia rebuild
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@52232 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/GiardiaDB/Hehl/Stress1.pm
+++ View/lib/perl/GraphPackage/GiardiaDB/Hehl/Stress1.pm
@@ -3,37 +3,39 @@ package ApiCommonWebsite::View::GraphPackage::GiardiaDB::Hehl::Stress1;
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::BarPlotSet );
-use ApiCommonWebsite::View::GraphPackage::BarPlotSet;
-
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet);
+use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
 
 sub init {
   my $self = shift;
 
   $self->SUPER::init(@_);
 
-  $self->setScreenSize(200);
-  $self->setBottomMarginSize(6);
+  my $colors= ['darkgreen', 'grey'];
 
-  $self->setProfileSetsHash
-    ({'expr_val' => {profiles => ['Stress Response in Giardia lamblia Trophozoites-Averaged'],
-                           y_axis_label => 'Expression Value',
-                           colors => ['darkgreen'],
-                           make_y_axis_fold_incuction => 1,
-                           default_y_max => 1,
-                           default_y_min => -1,
-                          },
-      pct => {profiles => ['Stress Response percentiles by temperature or varying DTT concentrations-red values',
-                           'Stress Response percentiles by temperature or varying DTT concentrations-green values'
-                          ],
-              y_axis_label => 'Percentile',
-              default_y_max => 50,
-              colors =>  ['grey', '#191970'],
-              x_axis_labels => ['Control', '40 degrees C', '7.1 mM DTT', '14.2 mM DTT', '21.3 mM DTT'],
-             },
-     });
+  my @profileSetsArray = (['Stress Response profiles by temperature or varying DTT concentrations', 'standard error - Stress Response profiles by temperature or varying DTT concentrations', ]);
+  my @percentileSetsArray = (['red percentile - Stress Response profiles by temperature or varying DTT concentrations', '',],
+                             ['green percentile - Stress Response profiles by temperature or varying DTT concentrations', '',]);
+
+  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetsArray);
+  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetsArray);
+
+  my $ratio = ApiCommonWebsite::View::GraphPackage::BarPlot::LogRatio->new(@_);
+  $ratio->setProfileSets($profileSets);
+  $ratio->setColors([$colors->[0]]);
+  $ratio->setElementNameMarginSize (6);
+  $ratio->setScreenSize(250);
+
+  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new(@_);
+  $percentile->setProfileSets($percentileSets);
+  $percentile->setColors($colors);
+  $percentile->setElementNameMarginSize (6);
+
+  $self->setGraphObjects($ratio, $percentile,);
 
   return $self;
+
 }
 
 
