6e73e99495444b6294a5378149429c95543e08fa
John Brestelli <jbrestell@gmail.com>
2013-01-16 19:04:20 +0000

    add instance var for bototm margin size so subclass can change
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@52727 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/SimpleStrandSpecificRNASeq.pm
+++ View/lib/perl/GraphPackage/SimpleStrandSpecificRNASeq.pm
@@ -37,10 +37,23 @@ sub getSampleNames { $_[0]->{_sample_names} }
 sub setSampleNames { $_[0]->{_sample_names} = $_[1] }
 
 
+sub setBottomMarginSize { $_[0]->{_bottom_margin_size} = $_[1] }
+sub getBottomMarginSize { $_[0]->{_bottom_margin_size} }
+
+
+sub init {
+  my $self = shift;
+    
+  $self->SUPER::init(@_);
+  $self->setBottomMarginSize(8);
+}
+
 sub makeGraphs {
   my $self = shift;
 
   my $color = $self->getColor();
+  my $bottomMarginSize = $self->getBottomMarginSize();
+
   my $lighterColor = ApiCommonWebsite::View::GraphPackage::Util::getLighterColorFromHex($color);
 
   my @colors = ($color, '#DDDDDD', $lighterColor, '#DDDDDD');
@@ -53,7 +66,7 @@ sub makeGraphs {
   $sense->setPctProfileSet($self->getPctSenseProfileSet());
   $sense->setColor($color);
   $sense->makeGraphs(@_);
-  $sense->setBottomMarginSize(8);
+  $sense->setBottomMarginSize($bottomMarginSize);
   $sense->setAdditionalRCode($self->getAdditionalRCode());
   $sense->setSampleNames($self->getSampleNames);
 
@@ -69,7 +82,7 @@ sub makeGraphs {
   $antisense->setPctProfileSet($self->getPctAntisenseProfileSet());
   $antisense->setColor($lighterColor);
   $antisense->makeGraphs(@_);
-  $antisense->setBottomMarginSize(8);
+  $antisense->setBottomMarginSize($bottomMarginSize);
   $antisense->setAdditionalRCode($self->getAdditionalRCode());
   $antisense->setSampleNames($self->getSampleNames);
 
