f35cb499ba5487e6e0c44974eacec6d634b16072
John Brestelli <jbrestell@gmail.com>
2013-01-16 22:26:57 +0000

    add sporozoite sample to the graph
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@52748 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/MicrosporidiaDB/Troemel/CelegansInfectionTimeSeries.pm
+++ View/lib/perl/GraphPackage/MicrosporidiaDB/Troemel/CelegansInfectionTimeSeries.pm
@@ -3,22 +3,59 @@ package ApiCommonWebsite::View::GraphPackage::MicrosporidiaDB::Troemel::Celegans
 use strict;
 use vars qw( @ISA );
 
-@ISA = qw( ApiCommonWebsite::View::GraphPackage::SimpleRNASeqLinePlot );
-use ApiCommonWebsite::View::GraphPackage::SimpleRNASeqLinePlot;
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::MixedPlotSet );
+use ApiCommonWebsite::View::GraphPackage::MixedPlotSet;
+
+use ApiCommonWebsite::View::GraphPackage::LinePlot;
+use ApiCommonWebsite::View::GraphPackage::BarPlot;
+use ApiCommonWebsite::View::GraphPackage::Util;
 
 
 sub init {
   my $self = shift;
   $self->SUPER::init(@_);
 
-  $self->setMinRpkmProfileSet("C. elegans Time Series - Infected");
-  $self->setDiffRpkmProfileSet("C. elegans Time Series - Infected-diff");
-  $self->setPctProfileSet("percentile - C. elegans Time Series - Infected");
-  $self->setColor("#D87093");
+#  $self->setXAxisLabel("hours");
+  my @colors = ('#D87093', '#DDDDDD', '#D87093');
+  my @legend = ("Uniquely Mapped", "Non-Uniquely Mapped");
 
-  $self->setXAxisLabel("hours");
-  $self->makeGraphs(@_);
-  $self->setBottomMarginSize(6);
+  
+
+  $self->setMainLegend({colors => \@colors, short_names => \@legend, cols => 2});
+
+  # Draw the diff first in light grey ... then the min rpkm will go on top
+  my @profileArray = (['Nematocida parisii ERTm1 Spores', '', ''],
+                      ['C. elegans Time Series - Infected-diff', '', ''],
+                      ['C. elegans Time Series - Infected', '', ''],
+
+                     );
+
+  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileArray);
+  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets([['percentile - Nematocida parisii ERTm1 Spores', '', ''],
+                                                                                    ['percentile - C. elegans Time Series - Infected', '', '']
+                                                                                   ]);
+
+
+  my $additionalRCode = "lines.df[2,] = lines.df[2,] + lines.df[3,];";
+
+
+  my $stacked = ApiCommonWebsite::View::GraphPackage::LinePlot::RNASeq->new(@_);
+  $stacked->setProfileSets($profileSets);
+  $stacked->setColors(\@colors);
+
+  $stacked->addAdjustProfile($additionalRCode);
+  $stacked->setXaxisLabel("hours");
+  $stacked->setPointsPch([19,'NA','NA']);
+
+  my $percentile = ApiCommonWebsite::View::GraphPackage::BarPlot::Percentile->new(@_);
+  $percentile->setProfileSets($percentileSets);
+  $percentile->setColors([$colors[0]]);
+
+  $stacked->setElementNameMarginSize(6);
+  $percentile->setElementNameMarginSize(6);
+
+
+  $self->setGraphObjects($stacked, $percentile);
 
 
   return $self;
