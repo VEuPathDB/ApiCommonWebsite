713b7ee4fe5449d875144af5831213b1e6e85520
John Brestelli <jbrestell@gmail.com>
2013-04-18 03:04:01 +0000

    add graph package for quantile normalization
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@54907 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/Templates/Microarray/Quantile.pm
@@ -0,0 +1,14 @@
+package ApiCommonWebsite::View::GraphPackage::Templates::Microarray::Quantile;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::Templates::Microarray );
+use ApiCommonWebsite::View::GraphPackage::Templates::Microarray;
+
+
+1;
+
+#--------------------------------------------------------------------------------
+
+# TEMPLATE_ANCHOR microarraySimpleQuantileGraph
