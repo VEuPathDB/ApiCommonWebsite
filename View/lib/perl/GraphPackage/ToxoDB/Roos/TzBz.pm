08582ba0581fb65bd3fbafaf3500f4863d6a5985
Ja’Shon Cade <jcade@upenn.edu>
2013-04-26 03:27:19 +0000

    moved Graphs for Roos TzBz and Matrajt datasets in to one plotset
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@55224 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/ToxoDB/Roos/TzBz.pm
+++ View/lib/perl/GraphPackage/ToxoDB/Roos/TzBz.pm
@@ -12,39 +12,74 @@ sub init {
 
   $self->SUPER::init(@_);
 
-  my $pch = [19,24,20,23];
+  $self->setPlotWidth(450);
+  $self->setBottomMarginSize(7);
 
-  my $colors = ['#4682B4', '#6B8E23', '#00FF00', '#2E8B57'];
+  my $roos_pch = [19,24,20,23];
 
-  my $legend = ['RH Alkaline', 'Pru Sodium Nitroprusside', 'Pru CO2-starvation', 'Pru Alkaline'];
+  my $roos_colors = ['#4682B4', '#6B8E23', '#00FF00', '#2E8B57'];
 
-  $self->setMainLegend({colors => $colors, short_names => $legend, points_pch => $pch});
-
-     my @profileSetsArray = (['expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', 'standard error - expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', ''],
+  my @roos_profileSetsArray = (['expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', 'standard error - expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', ''],
                              ['expression profiles of Pru dHXGPRT strain Alkaline bradyzoite-inducing conditions (media pH 8.2)','', ''],
                              ['expression profiles of Pru dHXGPRT strain sodium nitroprusside bradyzoite-inducing conditions', '', ''],
                              ['expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite-inducing conditions','standard error - expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite-inducing conditions', ''],
                             );
-  my @percentileSetsArray = (['percentile - expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', '',''],
+  my @roos_percentileSetsArray = (['percentile - expression profiles of RH delta-HXGPRT delta-UPRT strain Alkaline bradyzoite-inducing conditions', '',''],
                              ['percentile - expression profiles of Pru dHXGPRT strain Alkaline bradyzoite-inducing conditions (media pH 8.2)','',''],
                              ['percentile - expression profiles of Pru dHXGPRT strain sodium nitroprusside bradyzoite-inducing conditions','',''],
                              ['percentile - expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite-inducing conditions','',''],
                             );
 
-  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileSetsArray);
-  my $percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@percentileSetsArray);
+  my $roos_profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@roos_profileSetsArray);
+  my $roos_percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@roos_percentileSetsArray);
 
-  my $rma = ApiCommonWebsite::View::GraphPackage::LinePlot::RMA->new(@_);
-  $rma->setProfileSets($profileSets);
-  $rma->setColors($colors);
-  $rma->setPointsPch($pch);
+  my $roos_rma = ApiCommonWebsite::View::GraphPackage::LinePlot::RMA->new(@_);
+  $roos_rma->setProfileSets($roos_profileSets);
+  $roos_rma->setColors($roos_colors);
+  $roos_rma->setPointsPch($roos_pch);
+  $roos_rma->setPartName('roos_rma');
+  $roos_rma->setPlotTitle("Percentile - $id");
+  $roos_rma->setPlotTitle("0 - 72 hours : RMA Expression Value - $id");
 
-  my $percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
-  $percentile->setProfileSets($percentileSets);
-  $percentile->setColors($colors);
-  $percentile->setPointsPch($pch);
-  
-  $self->setGraphObjects($rma, $percentile);
+  my $roos_percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
+  $roos_percentile->setProfileSets($roos_percentileSets);
+  $roos_percentile->setColors($roos_colors);
+  $roos_percentile->setPointsPch($roos_pch);
+  $roos_percentile->setPartName('roos_percentile');
+  $roos_percentile->setPlotTitle("0 - 72 hours : Percentile - $id");
+
+
+
+   my $dzierszinski_colors = ['#CD853F', '#8FBC8F'];
+
+   my $dzierszinski_pch = [19,24];
+
+   my @dzierszinski_profileSetsArray = (['expression profiles of VEG strain CO2-starvation bradyzoite inducing conditions : 2-6 days (by Florence Dzierszinski)','', ''],
+                           ['expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite inducing conditions : 2-14 days (by Florence Dzierszinski)', '', '']
+                           );
+
+   my @dzierszinski_percentileSetsArray = (['percentile - expression profiles of VEG strain CO2-starvation bradyzoite inducing conditions : 2-6 days (by Florence Dzierszinski)', '',''],
+                              ['percentile - expression profiles of Pru dHXGPRT strain CO2-starvation bradyzoite inducing conditions : 2-14 days (by Florence Dzierszinski)','','']);
+
+   my $dzierszinski_profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@dzierszinski_profileSetsArray);
+   my $dzierszinski_percentileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@dzierszinski_percentileSetsArray);
+
+   my $dzierszinski_rma = ApiCommonWebsite::View::GraphPackage::LinePlot::RMA->new(@_);
+   $dzierszinski_rma->setProfileSets($dzierszinski_profileSets);
+   $dzierszinski_rma->setColors($dzierszinski_colors);
+   $dzierszinski_rma->setPointsPch($dzierszinski_pch);
+   $dzierszinski_rma->setPartName('dzierszinski_rma');
+   $dzierszinski_rma->setPlotTitle("2 - 14 days : RMA Expression Value - $id");
+
+   my $dzierszinski_percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
+   $dzierszinski_percentile->setProfileSets($dzierszinski_percentileSets);
+   $dzierszinski_percentile->setColors($dzierszinski_colors);
+   $dzierszinski_percentile->setPointsPch($dzierszinski_pch);
+   $dzierszinski_percentile->setPartName('dzierszinski_percentile');
+   $dzierszinski_percentile->setPlotTitle("2 - 14 days : Percentile - $id");
+
+
+  $self->setGraphObjects($roos_rma, $roos_percentile, $dzierszinski_rma, $dzierszinski_percentile);
 
   return $self;
 }
