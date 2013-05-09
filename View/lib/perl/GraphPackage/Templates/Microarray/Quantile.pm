adeb2cd4ba4131d1e69336bc2dbe11bff249af7b
John Brestelli <jbrestell@gmail.com>
2013-05-09 18:22:00 +0000

    add r code for matthews graph and new method for final adjustments (fixes #13017)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@55660 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/Microarray/Quantile.pm
+++ View/lib/perl/GraphPackage/Templates/Microarray/Quantile.pm
@@ -9,6 +9,27 @@ use ApiCommonWebsite::View::GraphPackage::Templates::Microarray;
 
 1;
 
+
+
+
+# This is an example of customizing a graph.  The template will provide things like colors (ie. we still inject stuff for it below!!
+package ApiCommonWebsite::View::GraphPackage::Templates::Microarray::Quantile::tbruTREU927_microarrayExpression_GSE17026_Matthews_Keith_RSRC;
+use base qw( ApiCommonWebsite::View::GraphPackage::Templates::Microarray::Quantile );
+use strict;
+
+sub getProfileRAdjust { return 'points.df = points.df - lines.df[[2]]; lines.df = lines.df - lines.df[[2]];'}
+
+sub finalProfileAdjustments {
+  my ($self, $profile) = @_;
+
+   $profile->setDefaultYMax(1);
+   $profile->setDefaultYMin(-1);
+
+  return $profile;
+}
+
+1;
+
 #--------------------------------------------------------------------------------
 
 # TEMPLATE_ANCHOR microarraySimpleQuantileGraph
