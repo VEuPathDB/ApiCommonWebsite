712213a0957a971ffc37b9cdc11b09a46a61e46b
John Brestelli <jbrestell@gmail.com>
2013-05-16 18:06:06 +0000

    scale giardia cell cycle to 1 hour timepoint
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@55814 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/Microarray/Quantile.pm
+++ View/lib/perl/GraphPackage/Templates/Microarray/Quantile.pm
@@ -32,6 +32,29 @@ sub finalProfileAdjustments {
 
 1;
 
+
+# This is an example of customizing a graph.  The template will provide things like colors (ie. we still inject stuff for it below!!
+package ApiCommonWebsite::View::GraphPackage::Templates::Microarray::Quantile::gassAWB_microarrayExpression_Troell_v1swapped_reps_RSRC;
+use base qw( ApiCommonWebsite::View::GraphPackage::Templates::Microarray::Quantile );
+use strict;
+
+sub getProfileRAdjust { return 'points.df = points.df - lines.df[[2]]; lines.df = lines.df - lines.df[[2]];'}
+
+sub finalProfileAdjustments {
+  my ($self, $profile) = @_;
+
+  $profile->setDefaultYMax(1);
+  $profile->setDefaultYMin(-1);
+
+  $profile->setMakeYAxisFoldInduction(1);
+
+  return $profile;
+}
+
+1;
+
+
+
 #--------------------------------------------------------------------------------
 
 # TEMPLATE_ANCHOR microarraySimpleQuantileGraph
