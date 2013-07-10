30b3960c1710c7a4dd526d897ed4bee5630b34e8
Dave Falke <dmfalke@gmail.com>
2013-07-10 01:02:15 +0000

    move search type names from header to table cells (as buttons) and update UX
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/internal-search@56862 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/dataset-searches.js
+++ Site/webapp/wdkCustomization/js/dataset-searches.js
@@ -1,6 +1,5 @@
 function datasetSearches($element, $attrs) {
   "use strict";
-  //console.log($attrs);
 
   var $datasetRecords = $element.find($attrs.table);
 
@@ -10,16 +9,30 @@ function datasetSearches($element, $attrs) {
   var toggleSource = $element.find("#toggle").html();
   var toggleTmpl = Handlebars.compile(toggleSource);
 
+  var $questionWrapper = $("#question-wrapper");
+
+  var questionTabsCache = {
+    cache: {},
+    // id: datasetId
+    // html: tabs source
+    add: function(id, html) {
+      return (this.cache[id] = html);
+    },
+    // id: datasetId
+    get: function(id) {
+      return this.cache[id];
+    }
+  };
+
   var questionCollection = {
     datasetId: null,
     questions: []
   };
 
-  // dataTable, but without jQueryUI so we can keep slanted headers
   var dataTableOpts = {
-    aoColumns: $datasetRecords.find("th").map(function(idx, th) {
-      return $(th).hasClass("skew") ? {bSortable: false} : [null];
-    }).toArray(),
+    aoColumnDefs: [
+    {bVisible: false, aTargets: [2]},
+    ],
     bPaginate: false,
     oLanguage: {
       sSearch: "Filter table:",
@@ -34,18 +47,56 @@ function datasetSearches($element, $attrs) {
     collapse = typeof collapse !== "undefined" ? collapse : !$datasetRecords.hasClass("collapsed");
     $datasetRecords.toggleClass("collapsed", collapse);
     $tableToggle.html(toggleTmpl({collapsed: collapse}));
-    $($attrs.table + "_filter").toggleClass("disabled", collapse)
-      .find("input").prop("disabled", collapse);
   }
+
+  var colors = [
+    "blue",
+    "red",
+    "green",
+    "brown"
+  ];
+
+  $datasetRecords.find("tbody tr").each(function() {
+    $(this).find(".search-mechanism").each(function(idx, td) {
+      var $div = $(td).find("a");
+      var color = colors[idx % colors.length];
+      $div.addClass("btn btn-" + color);
+    });
+  });
+
+  $element.find(".legend .search-mechanism").each(function(idx, span) {
+    var color = colors[idx % colors.length];
+    $(span).addClass("btn btn-active btn-" + color);
+  });
 
   var dataTable = $datasetRecords.dataTable(dataTableOpts);
   //new FixedHeader(dataTable);
 
-  $element.find("th.skew").contents().wrap("<div><span/></div>");
-    // redraw table
-  dataTable.fnDraw();
+  // filter
+  //   - remove active search page and expand table
+  //   - when input has content, display clear button
+  $($attrs.table + "_filter input")
+    .on("keyup change", function(e) {
+      $(this).toggleClass("content", this.value.length > 0);
+      toggleTable(false);
+      $("#question-wrapper").html("").removeClass("active");
+      questionCollection.datasetId = null;
+      questionCollection.questions = [];
+      $datasetRecords.find(".btn-active").removeClass("btn-active");
+      $datasetRecords.find("tbody tr").removeClass("active");
+      $tableToggle.hide();
+      $questionWrapper.find(".tabs").hide();
+    }).after(
+      $('<a href="#">clear</a>')
+        .addClass("filter-clear")
+        .on("click", function(e) {
+          e.preventDefault();
+          dataTable.fnFilter("");
+          $($attrs.table + "_filter input").change().select();
+        })
+    );
 
-  $($attrs.table + "_filter input").qtip({
+  $($attrs.table + "_filter input").wdkTooltip({
     content: {
       text: "Type anything to filter this table, " +
             "such as investigator or organism name. "
@@ -68,16 +119,21 @@ function datasetSearches($element, $attrs) {
 
     $tableToggle.show();
 
+    $(this).addClass("btn-active");
+
+    $datasetRecords.find(".btn-active").not(this).removeClass("btn-active");
+
     tabIdx = $delegate.find(".search-mechanism a").index(this);
 
     if ($data.datasetId === questionCollection.datasetId) {
       // select appropriate tab
-      $("#question-wrapper").find(".tabs").tabs("option", "selected", tabIdx);
+      $("#question-wrapper").find("#question-set-" + questionCollection.datasetId).tabs("option", "selected", tabIdx);
     } else {
       // update active row
       $datasetRecords.find("tbody tr").removeClass("active");
       $delegate.addClass("active");
       toggleTable.call($tableToggle.get(0), true);
+      $questionWrapper.find(".tabs").hide();
 
       questionCollection.datasetId = $data.datasetId;
       questionCollection.questions = [];
@@ -90,12 +146,15 @@ function datasetSearches($element, $attrs) {
         });
       });
 
-      $("#question-wrapper")
-        .html(datasetTabsTmpl(questionCollection))
-        .addClass("active")
-        .find(".tabs").tabs({
-          selected: tabIdx
-        });
+      if (!$questionWrapper.find("#question-set-" + questionCollection.datasetId).show().tabs("option", "selected", tabIdx).length) {
+        $questionWrapper
+          .append(datasetTabsTmpl(questionCollection))
+          .addClass("active")
+          .find(".tabs").tabs({
+            cache: true,
+            selected: tabIdx
+          });
+      }
     }
   });
 
