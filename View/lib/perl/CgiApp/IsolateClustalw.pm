6c2f0241b2230eb41944280ab09b01b99898e2b4
Haiming Wang <hwang@uga.edu>
2013-07-11 20:17:35 +0000

    minor changes - refs #13561
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@56913 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/IsolateClustalw.pm
+++ View/lib/perl/CgiApp/IsolateClustalw.pm
@@ -64,11 +64,10 @@ sub handleIsolates {
     #$ids =~ s/'(\w)/'$sid\.$1/g;
     $ids .= ",'$sid'";   # always compare with reference isolate
     $sql = <<EOSQL;
-select CASE WHEN source_id='$sid' THEN source_id
-            ELSE replace(source_id, '$sid.', '')
-            END as source_id, 
-            substr(nas.sequence, $start,$end-$start+1) as sequence from dots.nasequence nas
-where nas.source_id in ($ids) 
+SELECT source_id, 
+       substr(nas.sequence, $start,$end-$start+1) as sequence 
+FROM   dots.nasequence nas
+WHERE  nas.source_id in ($ids) 
 EOSQL
   } else {  # regular isolates
     $sql = <<EOSQL;
@@ -82,7 +81,7 @@ EOSQL
   my $sth = $dbh->prepare($sql);
   $sth->execute();
   while(my ($id, $seq) = $sth->fetchrow_array()) {
-    $id =~ s/^[^\.]+\.//g unless ($project_id =~ /ToxoDB/i);
+    $id =~ s/^[^\.]+\.// unless ($project_id =~ /ToxoDB/i);
     $sequence .= ">$id\n$seq\n";
   }
 
@@ -162,7 +161,7 @@ EOSQL
              $id =~ s/$sid\.//; 
           } 
         }  else {
-          $id =~ s/^[^\.]+\.//g;
+          $id =~ s/^[^\.]+\.//;
         }
         $origins{$id} = $start;
      }
