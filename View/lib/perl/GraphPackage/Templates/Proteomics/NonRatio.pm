d157b924850e153cda03ffce35312d37f466e78e
John Brestelli <jbrestell@gmail.com>
2013-08-13 22:07:38 +0000

    add class for absolute mass spec data
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@57529 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/GraphPackage/Templates/Proteomics/NonRatio.pm
@@ -0,0 +1,16 @@
+package ApiCommonWebsite::View::GraphPackage::Templates::Proteomics::NonRatio;
+
+use strict;
+use vars qw( @ISA );
+
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::Templates::Expression );
+use ApiCommonWebsite::View::GraphPackage::Templates::Expression;
+
+
+
+1;
+
+
+#--------------------------------------------------------------------------------
+
+# TEMPLATE_ANCHOR proteomicsSimpleNonRatio
