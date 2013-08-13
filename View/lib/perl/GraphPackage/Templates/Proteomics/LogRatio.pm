437d1187e91bf18240268e2b4b6fe8fcd62f698f
John Brestelli <jbrestell@gmail.com>
2013-08-13 18:58:34 +0000

    no sense in having separate plot part for logged quat data.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@57508 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/Proteomics/LogRatio.pm
+++ View/lib/perl/GraphPackage/Templates/Proteomics/LogRatio.pm
@@ -16,8 +16,18 @@ sub getPercentileColors {
 
 1;
 
-
 #--------------------------------------------------------------------------------
 
+# This is an example of customizing a graph.  The template will provide things like colors (ie. we still inject stuff for it below!!
+package ApiCommonWebsite::View::GraphPackage::Templates::Proteomics::LogRatio::tbruTREU927_quantitativeMassSpec_Urbaniak_CompProt_RSRC;
+use base qw( ApiCommonWebsite::View::GraphPackage::Templates::Proteomics::LogRatio );
+use strict;
+
+sub getProfileRAdjust { return 'profile.df = log2(profile.df);'}
+
+1;
+
+
+#--------------------------------------------------------------------------------
 
 # TEMPLATE_ANCHOR proteomicsSimpleLogRatio
