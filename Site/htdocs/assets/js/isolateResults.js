200789e46f61ecf755c47ac305741341f8d9d5dd
Haiming Wang <hwang@uga.edu>
2013-08-27 19:15:04 +0000

    limits the max number of hts isolates - refs #14084
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@57862 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/isolateResults.js
+++ Site/htdocs/assets/js/isolateResults.js
@@ -38,3 +38,30 @@ function goToIsolate(ele,type,source_id,start,end) {
   // focus the window
   w.focus();
 }
+
+function goToHTSStrain(ele,type,source_id,start,end) {
+  var $ = jQuery;
+	var form = $(ele).parents("form[name=checkHandleForm]");
+	var cbs = form.find('input:checkbox[name=selectedFields]:checked');
+	//alert("cbs length is " + cbs.length);
+	if(cbs.length < 2) {
+		alert("Please select at least two strains to run ClustalW");
+		return false;
+	} else if(cbs.length > 3) {
+		alert("Please select no more than three strains to run ClustalW"); 
+		return false;
+  }
+
+	var url = "/cgi-bin/isolateClustalw?project_id=" + wdk.modelName() + ";type=" + type + ";sid=" + source_id + ";start=" + start + ";end=" + end + ";isolate_ids=";
+	cbs.each(function(){
+		url += $(this).val() + ",";
+	});
+	//alert(url);
+	/* code if we want to popup a new window */
+	var w = open ('', 'clustalwResult', 'width=800,height=500,titlebar=1,menubar=1,resizable=1,scrollbars=1,toolbar=1');
+	w.document.open();
+	w.location.href=url;
+	//window.location.href = url;
+  // focus the window
+  w.focus();
+}
