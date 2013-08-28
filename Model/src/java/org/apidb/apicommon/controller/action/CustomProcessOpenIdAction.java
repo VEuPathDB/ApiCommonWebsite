e7501b16d1a400abe2c5141583fe4ecf7f1a65dc
Ryan Doherty <tech@conical.org>
2013-08-28 06:32:38 +0000

    #13966: place gbrowse login page into iframe and handle login failure redirection
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@57881 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/controller/action/CustomProcessOpenIdAction.java
+++ Model/src/java/org/apidb/apicommon/controller/action/CustomProcessOpenIdAction.java
@@ -1,5 +1,6 @@
 package org.apidb.apicommon.controller.action;
 
+import org.gusdb.wdk.controller.action.user.ProcessLoginAction;
 import org.gusdb.wdk.controller.action.user.ProcessOpenIdAction;
 import org.gusdb.wdk.controller.actionutil.ActionResult;
 
@@ -8,5 +9,11 @@ public class CustomProcessOpenIdAction extends ProcessOpenIdAction {
   @Override
   protected ActionResult getSuccessfulLoginResult(String redirectUrl, int wdkCookieMaxAge) {
     return CustomProcessLoginAction.getGbrowseLoginUrl(getWdkModel(), redirectUrl, wdkCookieMaxAge);
+  }
+  
+  @Override
+  protected ActionResult getFailedLoginResult(Exception e) {
+    return CustomProcessLoginAction.getFailedLoginUrl(getRequestData(),
+        ProcessLoginAction.getOriginalReferrer(getParams(), getRequestData()), e);
   }
 }
