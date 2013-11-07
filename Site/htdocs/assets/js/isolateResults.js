71a73703045d81402d7b13b898a218a3505ee7e0
Haiming Wang <hwang@uga.edu>
2013-11-07 16:29:45 +0000

    minor changes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@58971 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/isolateResults.js
+++ Site/htdocs/assets/js/isolateResults.js
@@ -2,58 +2,62 @@
 
 /* now using generic functions provided in WDK's  api.js
 function checkboxAll(ele) {
-	var form = $(ele).parents("form[name=checkHandleForm]");
-	var cbs = form.find('input:checkbox[name=selectedFields]');
-	cbs.each(function(){
-		this.checked = true;
-	});
+  var form = $(ele).parents("form[name=checkHandleForm]");
+  var cbs = form.find('input:checkbox[name=selectedFields]');
+  cbs.each(function(){
+    this.checked = true;
+  });
 }
 function checkboxNone(ele) {
-	var form = $(ele).parents("form[name=checkHandleForm]");
-	var cbs = form.find('input:checkbox[name=selectedFields]');
-	cbs.each(function(){
-		this.checked = false;
-	});
+  var form = $(ele).parents("form[name=checkHandleForm]");
+  var cbs = form.find('input:checkbox[name=selectedFields]');
+  cbs.each(function(){
+    this.checked = false;
+  });
 */
 
 function goToIsolate(ele,type,source_id,start,end) {
   var $ = jQuery;
-	var form = $(ele).parents("form[name=checkHandleForm]");
-	var cbs = form.find('input:checkbox[name=selectedFields]:checked');
-	//alert("cbs length is " + cbs.length);
-	if(cbs.length < 2) {
-		alert("Please select at least two isolates to run ClustalW");
-		return false;
-	}
-	var url = "/cgi-bin/isolateClustalw?project_id=" + wdk.modelName() + ";type=" + type + ";sid=" + source_id + ";start=" + start + ";end=" + end + ";isolate_ids=";
-	cbs.each(function(){
-		url += $(this).val() + ",";
-	});
-	//alert(url);
-	/* code if we want to popup a new window */
-	var w = open ('', 'clustalwResult', 'width=800,height=500,titlebar=1,menubar=1,resizable=1,scrollbars=1,toolbar=1');
-	w.document.open();
-	w.location.href=url;
-	//window.location.href = url;
+  var form = $(ele).parents("form[name=checkHandleForm]");
+  var cbs = form.find('input:checkbox[name=selectedFields]:checked');
+  //alert("cbs length is " + cbs.length);
+  if(cbs.length < 2) {
+    alert("Please select at least two isolates to run ClustalW");
+    return false;
+  }
+  var url = "/cgi-bin/isolateClustalw?project_id=" + wdk.modelName() + ";type=" + type + ";sid=" + source_id + ";start=" + start + ";end=" + end + ";isolate_ids=";
+  cbs.each(function(){
+    url += $(this).val() + ",";
+  });
+  //alert(url);
+  /* code if we want to popup a new window */
+  var w = open ('', 'clustalwResult', 'width=800,height=500,titlebar=1,menubar=1,resizable=1,scrollbars=1,toolbar=1');
+  w.document.open();
+  w.location.href=url;
+  //window.location.href = url;
   // focus the window
   w.focus();
 }
 
 function goToHTSStrain(ele,type,source_id,start,end) {
   var $ = jQuery;
-	var form = $(ele).parents("form[name=checkHandleForm]");
-	var cbs = form.find('input:checkbox[name=selectedFields]:checked');
+  var form = $(ele).parents("form[name=checkHandleForm]");
+  var cbs = form.find('input:checkbox[name=selectedFields]:checked');
+  if(cbs.length < 2) {
+    alert("Please select at least two HTS isolates to show alignment.");
+    return false;
+  }
 
-	var url = "/cgi-bin/isolateClustalw?project_id=" + wdk.modelName() + ";type=" + type + ";sid=" + source_id + ";start=" + start + ";end=" + end + ";isolate_ids=";
-	cbs.each(function(){
-		url += $(this).val() + ",";
-	});
-	//alert(url);
-	/* code if we want to popup a new window */
-	var w = open ('', 'clustalwResult', 'width=800,height=500,titlebar=1,menubar=1,resizable=1,scrollbars=1,toolbar=1');
-	w.document.open();
-	w.location.href=url;
-	//window.location.href = url;
+  var url = "/cgi-bin/isolateClustalw?project_id=" + wdk.modelName() + ";type=" + type + ";sid=" + source_id + ";start=" + start + ";end=" + end + ";isolate_ids=";
+  cbs.each(function(){
+    url += $(this).val() + ",";
+  });
+  //alert(url);
+  /* code if we want to popup a new window */
+  var w = open ('', 'clustalwResult', 'width=800,height=500,titlebar=1,menubar=1,resizable=1,scrollbars=1,toolbar=1');
+  w.document.open();
+  w.location.href=url;
+  //window.location.href = url;
   // focus the window
   w.focus();
 }
