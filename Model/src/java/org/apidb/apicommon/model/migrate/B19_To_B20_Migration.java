ac369faa7f112fbce222f8f71481ef06a061b66c
Ryan Doherty <tech@conical.org>
2013-11-22 15:39:41 +0000

    Add extra logic to remove out-of-date gbrowse users
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@59163 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/migrate/B19_To_B20_Migration.java
+++ Model/src/java/org/apidb/apicommon/model/migrate/B19_To_B20_Migration.java
@@ -22,12 +22,19 @@ public class B19_To_B20_Migration {
     private static final boolean TEST = true;
     
     // SQL we use to execute the updates
-    private static String GB_SESSION_TABLE = "gbrowseusers.session_tbl";
-    private static String WDK_USER_TABLE = "userlogins4.users";
-
-    private static String SELECT_GB_USERNAMES_SQL = "select username from " + GB_SESSION_TABLE;
-    private static String GET_WDK_UID_BY_EMAIL_SQL = "select user_id from " + WDK_USER_TABLE + " where email = ?";
-    private static String UPDATE_GB_USERNAME_SQL = "update " + GB_SESSION_TABLE + " set username = ? where username = ?";
+    private static String SELECT_GB_USERNAMES_SQL = "select username, sessionid, uploadsid from gbrowseusers.session_tbl";
+    private static String GET_WDK_UID_BY_EMAIL_SQL = "select user_id from userlogins4.users where email = ?";
+    private static String UPDATE_GB_USERNAME_SQL = "update gbrowseusers.session_tbl set username = ? where username = ?";
+    private static String DELETE_OUTDATED_MAPPINGS_SQL = "delete from gbrowseusers.session_tbl where userid = ?";
+    private static String DELETE_OUTDATED_USERS_SQL = "delet from gbrowseusers.users where userid = ?"; //userid
+    private static String DELETE_OUTDATED_SESSIONS_SQL = "delete from gbrowseusers.sessions where id = ?"; // sessionid
+    
+    private static class UserMapping {
+    	int userid; String username, sessionid;
+    	public UserMapping(int userid, String username, String sessionid) {
+    		this.userid = userid; this.username = username; this.sessionid = sessionid;
+    	}
+    }
     
     public static void main(final String[] args) {
         if (args.length != 3) {
@@ -51,25 +58,32 @@ public class B19_To_B20_Migration {
 
     private static void migrateGBrowseIds(DataSource ds) {
         
-        final List<String> oldIds = new ArrayList<>();
+        final List<UserMapping> users = new ArrayList<>();
         final BasicArgumentBatch updateParams = new BasicArgumentBatch();
+        final BasicArgumentBatch userIdsToRemove = new BasicArgumentBatch();
+        final BasicArgumentBatch sessionIdsToRemove = new BasicArgumentBatch();
         
         // get current list of usernames from GBrowse sessions table
         new SQLRunner(ds, SELECT_GB_USERNAMES_SQL)
             .executeQuery(new ResultSetHandler() {
                 @Override public void handleResult(ResultSet rs) throws SQLException {
-                    while (rs.next()) oldIds.add(rs.getString(1));}});
+                	while (rs.next()) users.add(
+                		new UserMapping(rs.getInt(1), rs.getString(2), rs.getString(3)));}});
         
         // create new gbrowse id by fetching uid from email and add to update params; no, this is not very efficient
         SQLRunner querier = new SQLRunner(ds, GET_WDK_UID_BY_EMAIL_SQL);
-        for (final String oldId : oldIds) {
-            final String email = oldId.substring(0, oldId.lastIndexOf("-"));
+        for (final UserMapping user : users) {
+        	final String username = user.username;
+            final String email = username.substring(0, username.lastIndexOf("-"));
             querier.executeQuery(new String[]{ email }, new ResultSetHandler(){
                 @Override public void handleResult(ResultSet rs) throws SQLException {
                     if (rs.next()) {
-                        updateParams.add(new Object[]{ oldId, oldId.replace(email, String.valueOf(rs.getInt(1))) });
+                        updateParams.add(new Object[]{ username, username.replace(email, String.valueOf(rs.getInt(1))) });
                     } else {
-                        LOG.warn("Unable to convert GBrowse user ID: " + oldId);
+                    	// this user is out of date; remove user, session, and any uploads
+                        LOG.warn("Unable to convert GBrowse username: " + username);
+                        userIdsToRemove.add(new Object[]{ user.userid });
+                        sessionIdsToRemove.add(new Object[]{ user.sessionid });
                     }
                 }
             });
@@ -77,10 +91,21 @@ public class B19_To_B20_Migration {
         
         // update each ID to new value containing uid instead of email
         if (TEST) {
+        	System.out.println("Gbrowse usernames to update:");
             System.out.println(updateParams);
+            System.out.println("UserIds to remove");
+            System.out.println(userIdsToRemove);
+            System.out.println("SessionIds to remove");
+            System.out.println(sessionIdsToRemove);
         } else {
             new SQLRunner(ds, UPDATE_GB_USERNAME_SQL)
-                .executeBatchUpdate(updateParams);
+                .executeUpdateBatch(updateParams);
+            new SQLRunner(ds, DELETE_OUTDATED_MAPPINGS_SQL)
+            	.executeUpdateBatch(userIdsToRemove);
+            new SQLRunner(ds, DELETE_OUTDATED_USERS_SQL)
+        	    .executeUpdateBatch(userIdsToRemove);
+            new SQLRunner(ds, DELETE_OUTDATED_SESSIONS_SQL)
+        	    .executeUpdateBatch(sessionIdsToRemove);
         }
     }
 }
