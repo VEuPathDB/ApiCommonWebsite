ef4d1b9881388f64878a1532cb0f8add22bdbe50
Dave Falke <dmfalke@gmail.com>
2013-11-26 22:50:05 +0000

    fixed formatting and some documentation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@59247 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/assets/js/orthologpattern.js
+++ Site/htdocs/assets/js/orthologpattern.js
@@ -14,6 +14,16 @@ wdk.namespace('eupathdb.orthologPattern', function(ns, $) {
   ns.init = init;
   ns.toggle = toggle;
 
+  /**
+   * Initialize global variables
+   *
+   * state    - {Array} Each element is the state of the corresponding tree node
+   * children - {Array} Each element is an array of indexes which are the corresponding tree node's children
+   * parent   - {Array} Each element is the index of the corresponding tree node's parent
+   * abbrev   - {Array} Each element is the four-letter identifier of a tree node
+   * parents  - {Array} Indexes of parent nodes
+   */
+
   function init(element, attrs){
     var includes, excludes, pattern;
 
@@ -83,99 +93,99 @@ wdk.namespace('eupathdb.orthologPattern', function(ns, $) {
   }
 
   function setstate (imgidx, urlidx, dofixparent) {
-      state[imgidx] = urlidx;
-      $("img#img" + imgidx).attr('src',"images/" + urls[urlidx]);
-      for (var i = 0 ; i < children[imgidx].length ; i++) {
-    setstate(children[imgidx][i], urlidx == 3 ? 0 : urlidx, 0);
-      }
-      
-      if (dofixparent) {
-    fixparent(imgidx, urlidx);
-      }
+    state[imgidx] = urlidx;
+    $("img#img" + imgidx).attr('src',"images/" + urls[urlidx]);
+    for (var i = 0 ; i < children[imgidx].length ; i++) {
+      setstate(children[imgidx][i], urlidx == 3 ? 0 : urlidx, 0);
+    }
+
+    if (dofixparent) {
+      fixparent(imgidx, urlidx);
+    }
   }
 
   function fixparent (imgidx, urlidx) {
 
-      var parentidx = parent[imgidx];
-      if (parentidx != null) {
-    var allmatch = 1;
-    if (urlidx == null) {
+    var parentidx = parent[imgidx];
+    if (parentidx != null) {
+      var allmatch = 1;
+      if (urlidx == null) {
         allmatch = 0;
-    } else {
+      } else {
         for (var i = 0 ; i < children[parentidx].length ; i++) {
-      if (state[children[parentidx][i]] != urlidx) {
-          allmatch = 0;
-          break;
+          if (state[children[parentidx][i]] != urlidx) {
+            allmatch = 0;
+            break;
+          }
+        }
       }
-        }    
-    }
-    if (allmatch) {
+      if (allmatch) {
         state[parentidx] = urlidx;
         $("img#img" + parentidx).attr('src', "images/" + urls[urlidx]);
         fixparent(parentidx, urlidx);
-    } else {
+      } else {
         state[parentidx] = null;
         $("img#img" + parentidx).attr('src', "images/" + urls[4]);
         fixparent(parentidx, null);
-    }
       }
+    }
   }
 
   function toggle (imgidx) {
-      var urlidx = 0;
-      if (state[imgidx] != null) {
-    urlidx = (state[imgidx] + 1) % 3;
-      }
-      setstate(imgidx, urlidx, 1);
-      calctext();
+    var urlidx = 0;
+    if (state[imgidx] != null) {
+      urlidx = (state[imgidx] + 1) % 3;
+    }
+    setstate(imgidx, urlidx, 1);
+    calctext();
   }
 
   function calctext () {
-      var tree = new Array();
+    var tree = new Array();
 
-      var includeClause = new Array();
-      var excludeClause = new Array();
-      var includeClauseSQL = new Array();
-      var excludeClauseSQL = new Array();
+    var includeClause = new Array();
+    var excludeClause = new Array();
+    var includeClauseSQL = new Array();
+    var excludeClauseSQL = new Array();
 
-      tree[tree.length] = 0;
-      while (tree.length) {
-    var parent = tree.shift();
-    var leafabbrev = abbrev[parent];
-    var leaflist = new Array();
-    if (state[parent] == null) {
+    tree[tree.length] = 0;
+    while (tree.length) {
+      var parent = tree.shift();
+      var leafabbrev = abbrev[parent];
+      var leaflist = new Array();
+      if (state[parent] == null) {
         // need to walk children
         for (var j = 0 ; j < children[parent].length ; j++) {
-      tree[tree.length] = children[parent][j];
+          tree[tree.length] = children[parent][j];
         }
-    } else if (state[parent] == 1) {
+      } else if (state[parent] == 1) {
         includeClause.push(leafabbrev);
         if(children[parent].length) {
-      var childlist = listchildren(parent);
-      for (var i = 0 ; i < childlist.length ; i++) {
-          includeClauseSQL.push(childlist[i] + ":Y");
-      }
+          var childlist = listchildren(parent);
+          for (var i = 0 ; i < childlist.length ; i++) {
+            includeClauseSQL.push(childlist[i] + ":Y");
+          }
         } else {
-      includeClauseSQL.push(leafabbrev + ":Y");
+          includeClauseSQL.push(leafabbrev + ":Y");
         }
-    } else if (state[parent] == 2) {
+      } else if (state[parent] == 2) {
         excludeClause.push(leafabbrev);
         if(children[parent].length) {
-      var childlist = listchildren(parent);
-      for (var i = 0 ; i < childlist.length ; i++) {
-          excludeClauseSQL.push(childlist[i] + ":N");
-      }
+          var childlist = listchildren(parent);
+          for (var i = 0 ; i < childlist.length ; i++) {
+            excludeClauseSQL.push(childlist[i] + ":N");
+          }
         } else {
-      excludeClauseSQL.push(leafabbrev + ":N");
+          excludeClauseSQL.push(leafabbrev + ":N");
         }
-    }
+      }
 
-       // this is a remnant of orthomcl-db behavior, allowing
-       // parental "any" inclusion without specifying leaves:
-       //
-       // } else if (state[parent] == 3) { clause[clause.length] =
-       //     leafabbrev + ">=1T";
-       // }
+      // this is a remnant of orthomcl-db behavior, allowing
+      // parental "any" inclusion without specifying leaves:
+      //
+      // } else if (state[parent] == 3) { clause[clause.length] =
+      //     leafabbrev + ">=1T";
+      // }
 
       }
       var includedStr = 'n/a'; if (includeClause.length > 0) includedStr = includeClause.join(", ");
@@ -185,34 +195,34 @@ wdk.namespace('eupathdb.orthologPattern', function(ns, $) {
 
       var bothClauseSQL = includeClauseSQL.concat(excludeClauseSQL);
       $("form[name='questionForm'] input:hidden[name='value(" + profilePatternName + ")']").attr('value',
-    bothClauseSQL.length ? "%" + bothClauseSQL.sort().join("%") + "%" : "%");
+      bothClauseSQL.length ? "%" + bothClauseSQL.sort().join("%") + "%" : "%");
   }
 
   function countchildren (parent) {
-      var count = 0;
-      for (var i = 0 ; i < children[parent].length ; i++) {
-    if(children[children[parent][i]].length) {
+    var count = 0;
+    for (var i = 0 ; i < children[parent].length ; i++) {
+      if(children[children[parent][i]].length) {
         count += countchildren(children[parent][i]);
-    } else {
+      } else {
         count += 1;
-    }
       }
-      return count;
+    }
+    return count;
   }
 
   function listchildren (parent) {
-      var list = new Array();
-      for (var i = 0 ; i < children[parent].length ; i++) {
-    if(children[children[parent][i]].length) {
+    var list = new Array();
+    for (var i = 0 ; i < children[parent].length ; i++) {
+      if(children[children[parent][i]].length) {
         newlist = listchildren(children[parent][i]);
         for (var j = 0 ; j < newlist.length ; j++) {
-      list[list.length] = newlist[j];
+          list[list.length] = newlist[j];
         }
-    } else {
+      } else {
         list[list.length] = abbrev[children[parent][i]];
-    }
       }
-      return list;
+    }
+    return list;
   }
 
 });
