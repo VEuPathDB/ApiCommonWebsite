946ea901b69a850c957ab42e64defc421c8df665
Mark Heiges <mheiges@gmail.com>
2014-02-27 01:01:56 +0000

    add dashboard xml api attributes to assist virtual machine setup
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@60706 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/dashboard/lib/PrivateAPI.php
+++ Site/dashboard/lib/PrivateAPI.php
@@ -8,6 +8,8 @@ require_once dirname(__FILE__) . "/modules/CommentConfig.php";
 require_once dirname(__FILE__) . "/modules/BuildInfo.php";
 require_once dirname(__FILE__) . "/modules/ProxyInfo.php";
 require_once dirname(__FILE__) . "/modules/WdkMeta.php";
+require_once dirname(__FILE__) . "/modules/Webapp.php";
+require_once dirname(__FILE__) . "/modules/Jvm.php";
 require_once dirname(__FILE__) . "/LdapTnsNameResolver.php";
 
 /**
@@ -47,19 +49,25 @@ class PrivateAPI {
     $wdk_meta = new WdkMeta();
     $wdk_meta_attr = $wdk_meta->attributes();
 
+    $webapp = new Webapp();
+    $webapp_attr = $webapp->attributes();
+
+    $jvm = new Jvm();
+    $jvm_attr = $jvm->attributes();
+
     $build = new BuildInfo();
     $proxy = new ProxyInfo();
     $proxy_attr = $proxy->attributes();
 
     $ldap_resolver = new LdapTnsNameResolver();
 
-    $all_data = array('wdk' =>
-        array(
-            'proxy' => array(
-                'proxyapp' => $proxy_attr{'proxy_app'},
-                'proxyhost' => $proxy_attr{'proxy_host'},
-                'upstreamhost' => $proxy_attr{'upstream_server'},
-            ),
+    $all_data = array(
+        'proxy' => array(
+            'proxyapp' => $proxy_attr{'proxy_app'},
+            'proxyhost' => $proxy_attr{'proxy_host'},
+            'upstreamhost' => $proxy_attr{'upstream_server'},
+        ),
+        'wdk' => array(
             'modelname' => $wdk_meta_attr{'DisplayName'},
             'modelversion' => $wdk_meta_attr{'ModelVersion'},
             'buildnumber' => $wdk_meta_attr{'BuildNumber'},
@@ -82,8 +90,18 @@ class PrivateAPI {
             'modelconfig' => $this->normalize_keys_in_array($model_config_attr),
             'commentconfig' => $this->normalize_keys_in_array($comment_config_attr),
             'modelprop' => $this->normalize_keys_in_array($wdk_properties_attr),
-            'svn' => $this->init_svn_info($build->get_data_map()),
-        )
+        ),
+        'tomcat' => array(
+            'webapp' => $this->webapp_from_context($webapp_attr{'path'}),
+            'webappstarttime' => $webapp_attr{'startTime'},
+            'webappstarttext' => $webapp->uptime_as_text(),
+            'instance'  => $jvm_attr{'SystemProperties'}{'instance.name'},
+            'instancestarttime'  => $jvm_attr{'Uptime'},
+            'instancestarttext'  => $jvm->uptime_as_text(),
+        ),
+        'svn' => $this->init_svn_info($build->get_data_map()),
+        'vmenv' => $this->virtual_machine_environment_settings(
+              $wdk_meta_attr, $wdk_properties_attr, $webapp_attr, $adb_attr),
     );
 
     $this->api_dataset = array_merge($this->api_dataset, $all_data);
@@ -155,8 +173,7 @@ class PrivateAPI {
 
   /**
    * Restructures and formats subversion data that was extracted
-   * from the GUS .buildlog/gus-build-state.log properties file 
-   * via the BuildInfo class.
+   * from the GUS .build.info properties file via the BuildInfo class.
    * @see BuildInfo
    *
    * @param array build data
@@ -166,10 +183,12 @@ class PrivateAPI {
     $array = array(
         'locations' => array(),
         'switch' => '',
+        'checkout' => '',
     );
 
     $switch_stmts = null;
-
+    $checkout_stmts = null;
+    
     foreach ($build as $p => $v) {
       if ($trunc = strpos($p, '.svn.info')) {
         $start = strpos($v, 'Revision: ') + strlen('Revision: ');
@@ -191,22 +210,61 @@ class PrivateAPI {
         );
 
         $switch_stmts .= "svn switch -r$svnrevision $svnbranch $svnproject;\n";
+        $checkout_stmts .= "svn checkout -r$svnrevision $svnbranch $svnproject;\n";
       }
     }
     $array{'switch'} = $switch_stmts;
+    $array{'checkout'} = $checkout_stmts;
     return $array;
   }
 
+  /**
+   * bash shell variable assignments to aid virtual machine setup
+   */
+/**U=abarreto
+PRODUCT=PlasmoDB
+WEBAPP=plasmo.b19
+HOST=sa.plasmodb.org
+RELEASE=release-10.0
+LOGIN=plasmodbwww
+BUILD_NO=19
+**/
+  private function virtual_machine_environment_settings(
+        $wdk_meta_attr, $wdk_properties_attr, $webapp_attr, $adb_attr) {
+
+    $webapp = $this->webapp_from_context($webapp_attr{'path'});
+
+    // e.g. get TLD plasmodb.org from qa.plasmodb.org
+    preg_match("/[^\.\/]+\.[^\.\/]+$/", $_SERVER['SERVER_NAME'], $matches);
+    $tld = @$matches[0];
+
+    $env = '';
+    $env .= 'PRODUCT=' . $wdk_properties_attr{'PROJECT_ID'} . "\n";
+    $env .= 'HOST=' . 'sa.' . $tld . "\n";
+    $env .= 'WEBAPP=' . $webapp . "\n";
+    $env .= 'RELEASE=' . $wdk_meta_attr{'ModelVersion'} . "\n";
+    $env .= 'APP_LOGIN=' . $adb_attr{'login'} . "\n";
+    $env .= 'BUILD_NUMBER=' . $wdk_meta_attr{'BuildNumber'} . "\n";
+    return $env;
+  }
+
+  // Given '/toxo.b1' return 'toxo.b1' (strip the leading slash)
+  private function webapp_from_context($context) {
+    $webapp = substr($context, 1);
+    $webapp = ($webapp == '') ? 'ROOT' : $webapp;
+    return $webapp;
+  }
+  
   /**
    * Returns full data set as XML
    *
    * @return \DomDocument
    */
   public function get_xml() {
-    $wdkxml = new DomDocument('1.0');
-    $wdkxml->preserveWhiteSpace = false;
-    $load = $wdkxml->loadXML($this->to_xml($this->api_dataset{'wdk'}, 'wdk'));
-    return $wdkxml;
+    $xml = new DomDocument('1.0');
+    $xml->preserveWhiteSpace = false;
+    $load = $xml->loadXML($this->to_xml($this->api_dataset, 'foos'));
+    return $xml;
   }
 
   /**
