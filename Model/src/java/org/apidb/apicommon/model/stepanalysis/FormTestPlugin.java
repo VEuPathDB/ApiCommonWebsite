67d8e51969fd79bbfebe0e4ecaafdd66cac91fd7
Ryan Doherty <tech@conical.org>
2014-03-25 17:16:33 +0000

    add plugin to test form population
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/step-analysis@61095 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/stepanalysis/FormTestPlugin.java
@@ -0,0 +1,38 @@
+package org.apidb.apicommon.model.stepanalysis;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.gusdb.fgputil.FormatUtil;
+import org.gusdb.fgputil.FormatUtil.Style;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.analysis.AbstractStepAnalyzer;
+import org.gusdb.wdk.model.answer.AnswerValue;
+import org.gusdb.wdk.model.user.analysis.ExecutionStatus;
+import org.gusdb.wdk.model.user.analysis.StatusLogger;
+
+public class FormTestPlugin extends AbstractStepAnalyzer {
+
+  private static final Logger LOG = Logger.getLogger(FormTestPlugin.class);
+  
+  @Override
+  public List<String> validateFormParams(Map<String, String[]> formParams) {
+    return null;
+  }
+  
+  @Override
+  public ExecutionStatus runAnalysis(AnswerValue answerValue, StatusLogger log) throws WdkModelException {
+    Map<String,String[]> params = getParamMap();
+    Map<String,String> prettyParams = new HashMap<>();
+    for (String key : params.keySet()) {
+      prettyParams.put(key, FormatUtil.arrayToString(params.get(key)));
+    }
+    String result = FormatUtil.prettyPrint(prettyParams, Style.MULTI_LINE);
+    LOG.info("Form test plugin setting following result:\n" + result);
+    setResults(result);
+    return ExecutionStatus.COMPLETE;
+  }
+
+}
