7047d40689b8dbdca772b4cf8fc6c235d723492e
Haiming Wang <hwang@uga.edu>
2014-04-11 16:44:54 +0000

    add a cgi page to show dbp images - dic and gfp together - refs #15696
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@61552 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/perl/CgiApp/GeneImage.pm
@@ -0,0 +1,88 @@
+package ApiCommonWebsite::View::CgiApp::GeneImage;
+
+@ISA = qw( ApiCommonWebsite::View::CgiApp );
+
+use strict;
+use ApiCommonWebsite::View::CgiApp;
+
+sub run {
+  my ($self, $cgi) = @_;
+
+  my $dbh = $self->getQueryHandle($cgi);
+
+  print $cgi->header('text/html');
+
+  $self->handleImage($dbh, $cgi);
+
+  exit();
+}
+
+sub handleImage {
+  my ($self, $dbh, $cgi) = @_;
+
+  my $source_id  = $cgi->param('source_id');
+
+  my $sql = "";
+
+  $sql = <<EOSQL;
+SELECT substr(uri, 1, instr(uri,',', 1,1)-1) as dic_img_uri,
+     substr(uri, instr(uri,',', 1,1)+1) as gfp_img_uri,
+     note, source_id
+     FROM (
+     SELECT listagg(image_uri, ',') within group (order by image_type) as uri,
+     replace(note, 'Gene annotation: ', '') as note, source_id
+     FROM
+     ( SELECT distinct img.image_uri, replace(img.note, 'GO term: ', '') as note,
+     img.label_type, img.image_type, gf.source_id
+     FROM ApiDB.NAFeatureImage img, Dots.GeneFeature gf
+     WHERE img.na_feature_id = gf.na_feature_id
+     ) a
+     group by source_id, note
+     )
+     where lower(source_id) = lower('$source_id')
+EOSQL
+
+
+  my $sth = $dbh->prepare($sql);
+  $sth->execute();
+
+
+  print "<table align=center width=800>";
+  while(my ($dic_img_uri, $gfp_img_uri, $note, $source_id) = $sth->fetchrow_array()) {
+     print <<EOL
+     <tr>
+       <td colspan=2>
+         <font color="red">Warning: some Web browsers do not support viewing TIFF images. Try to use Safari</font>
+       </td>
+      </tr>
+      <tr>
+       <td colspan=2>
+         $source_id - $note
+       </td>
+     </tr>
+     <tr>
+       
+       <td>
+       <embed 
+       src="/common/GintestinalisAssemblageA/image/gassAWB_DBP_GeneImage_RSRC/$gfp_img_uri"
+       type="image/tiff"
+       negative=yes>
+
+      </td>
+
+       <td>
+       <embed 
+       src="/common/GintestinalisAssemblageA/image/gassAWB_DBP_GeneImage_RSRC/$dic_img_uri"
+       type="image/tiff"
+       negative=yes>
+
+      </td>
+     </tr>
+EOL
+  }
+  print "</td></tr>";
+  print "</table>";
+
+}
+
+1;
