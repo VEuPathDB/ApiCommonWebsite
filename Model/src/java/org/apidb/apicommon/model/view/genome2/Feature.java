e49171521ba350821356494f969a7f38b243945e
Xin Gao <xingao@upenn.edu>
2014-05-12 02:50:58 +0000

    create a new implementation of genome view, which combines features too close to each other into single spans.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@62110 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/view/genome2/Feature.java
@@ -0,0 +1,118 @@
+package org.apidb.apicommon.model.view.genome2;
+
+public class Feature implements Comparable<Feature> {
+
+  private final String sourceId;
+  private final boolean forward;
+  private final String sequenceId;
+  private long start;
+  private long end;
+  private double percentStart;
+  private double percentLength;
+  private String context;
+  private String description;
+
+  public Feature(String sourceId, String sequenceId, boolean forward) {
+    this.sourceId = sourceId;
+    this.sequenceId = sequenceId;
+    this.forward = forward;
+  }
+
+  public Feature(Feature feature) {
+    this.context = feature.context;
+    this.end = feature.end;
+    this.forward = feature.forward;
+    this.percentLength = feature.percentLength;
+    this.percentStart = feature.percentStart;
+    this.sequenceId = feature.sequenceId;
+    this.sourceId = feature.sourceId;
+    this.start = feature.start;
+    this.description = feature.description;
+  }
+
+  public String getSourceId() {
+    return sourceId;
+  }
+
+  public String getSequenceId() {
+    return sequenceId;
+  }
+
+  public boolean isForward() {
+    return forward;
+  }
+
+  public String getStrand() {
+    return forward ? "forward" : "reversed";
+  }
+  
+  public long getLength() {
+    return (end - start + 1);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public long getStart() {
+    return start;
+  }
+
+  public String getStartFormatted() {
+    return GenomeViewHandler.FORMAT.format(start);
+  }
+
+  public void setStart(long start) {
+    this.start = start;
+  }
+
+  public long getEnd() {
+    return end;
+  }
+
+  public String getEndFormatted() {
+    return GenomeViewHandler.FORMAT.format(end);
+  }
+
+  public void setEnd(long end) {
+    this.end = end;
+  }
+
+  public double getPercentStart() {
+    return percentStart;
+  }
+
+  public void setPercentStart(double percentStart) {
+    this.percentStart = percentStart;
+  }
+
+  public double getPercentLength() {
+    return percentLength;
+  }
+
+  public void setPercentLength(double percentLength) {
+    this.percentLength = percentLength;
+  }
+
+  public void setContext(String context) {
+    this.context = context;
+  }
+
+  public String getContext() {
+    return sequenceId + ":" + context;
+  }
+
+  @Override
+  public int compareTo(Feature feature) {
+    if (feature != null) {
+      int diff = (int) (this.start - feature.start);
+      return (diff == 0) ? (int) (this.end - feature.end) : diff;
+    }
+    else
+      return 1;
+  }
+}
---
+++ Model/src/java/org/apidb/apicommon/model/view/genome2/Region.java
@@ -0,0 +1,110 @@
+package org.apidb.apicommon.model.view.genome2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Region {
+
+  private final boolean forward;
+  private final List<Feature> features;
+  private double percentLength;
+  private double percentStart;
+
+  public Region(boolean forward) {
+    this.forward = forward;
+    this.features = new ArrayList<>();
+  }
+
+  public boolean isForward() {
+    return forward;
+  }
+
+  public String getStrand() {
+    return forward ? "forward" : "reversed";
+  }
+
+  public void addFeature(Feature feature) throws GenomeViewException {
+    // make sure the feature is on the same strand as the region
+    if (feature.isForward() != forward)
+      throw new GenomeViewException(
+          "The feature has to be on the same strand as the region. The region is on " + getStrand() +
+              " strand, while feature " + feature.getSourceId() + " is on " + feature.getStrand() +
+              " strand.");
+
+    features.add(feature);
+  }
+
+  public List<Feature> getFeatures() {
+    return new ArrayList<>(features);
+  }
+
+  public int getFeatureCount() {
+    return features.size();
+  }
+
+  public long getStart() {
+    return getLocation()[0];
+  }
+
+  public long getEnd() {
+    return getLocation()[1];
+  }
+
+  public long getLength() {
+    long[] location = getLocation();
+    return (location[1] - location[0] + 1);
+  }
+
+  private long[] getLocation() {
+    if (features.isEmpty())
+      return new long[] { 0, 0 };
+    long start = Long.MAX_VALUE, end = Long.MIN_VALUE;
+    for (Feature feature : features) {
+      if (feature.getStart() < start)
+        start = feature.getStart();
+      if (feature.getEnd() > end)
+        end = feature.getEnd();
+    }
+    return new long[] { start, end };
+  }
+
+  public String getEndFormatted() {
+    return GenomeViewHandler.FORMAT.format(getEnd());
+  }
+
+  public String getStartFormatted() {
+    return GenomeViewHandler.FORMAT.format(getStart());
+  }
+
+  /**
+   * @return the percentLength
+   */
+  public double getPercentLength() {
+    return percentLength;
+  }
+
+  /**
+   * @return the percentStart
+   */
+  public double getPercentStart() {
+    return percentStart;
+  }
+
+  public void computePercentSize(long maxLength) {
+    long[] location = getLocation();
+    // compute the percent size of the region relative to the sequence
+    percentStart = GenomeViewHandler.round(location[0] * 100D / (maxLength));
+    percentLength = GenomeViewHandler.round((location[1] - location[0] + 1) * 100D/ maxLength);
+    
+    // also compute the percent size of each feature relative to the region.
+    computeFeaturesPercentSize(location);
+  }
+
+  private void computeFeaturesPercentSize(long[] location) {
+    long length = location[1] - location[0] + 1;
+    for (Feature feature : features) {
+      feature.setPercentStart(GenomeViewHandler.round((feature.getStart() - location[0]) * 100D / length));
+      feature.setPercentLength(GenomeViewHandler.round(feature.getLength() * 100D / length));
+    }
+  }
+}
