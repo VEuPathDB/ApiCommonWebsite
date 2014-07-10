386d49998acb18b72a3ae0d5de399e91dda8eb6d
Xin Gao <xingao@upenn.edu>
2014-07-10 20:33:35 +0000

    remove old genome-view related files, and move the new genome view code into the original genome package.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@63275 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/view/genome/Feature.java
+++ Model/src/java/org/apidb/apicommon/model/view/genome/Feature.java
@@ -1,110 +1,118 @@
 package org.apidb.apicommon.model.view.genome;
 
-import java.text.DecimalFormat;
+public class Feature implements Comparable<Feature> {
 
-public class Feature {
+  private final String sourceId;
+  private final boolean forward;
+  private final String sequenceId;
+  private long start;
+  private long end;
+  private double percentStart;
+  private double percentLength;
+  private String context;
+  private String description;
 
-    private static final DecimalFormat FORMAT = new DecimalFormat("#,###");
+  public Feature(String sourceId, String sequenceId, boolean forward) {
+    this.sourceId = sourceId;
+    this.sequenceId = sequenceId;
+    this.forward = forward;
+  }
 
-    private final String sourceId;
-    private String sequenceId;
-    private long start;
-    private long end;
-    private boolean forward;
-    private double percentStart;
-    private double percentLength;
-    private String context;
-    private String description;
+  public Feature(Feature feature) {
+    this.context = feature.context;
+    this.end = feature.end;
+    this.forward = feature.forward;
+    this.percentLength = feature.percentLength;
+    this.percentStart = feature.percentStart;
+    this.sequenceId = feature.sequenceId;
+    this.sourceId = feature.sourceId;
+    this.start = feature.start;
+    this.description = feature.description;
+  }
 
-    public Feature(String sourceId) {
-        this.sourceId = sourceId;
+  public String getSourceId() {
+    return sourceId;
+  }
+
+  public String getSequenceId() {
+    return sequenceId;
+  }
+
+  public boolean isForward() {
+    return forward;
+  }
+
+  public String getStrand() {
+    return forward ? "forward" : "reversed";
+  }
+  
+  public long getLength() {
+    return (end - start + 1);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public long getStart() {
+    return start;
+  }
+
+  public String getStartFormatted() {
+    return GenomeViewHandler.FORMAT.format(start);
+  }
+
+  public void setStart(long start) {
+    this.start = start;
+  }
+
+  public long getEnd() {
+    return end;
+  }
+
+  public String getEndFormatted() {
+    return GenomeViewHandler.FORMAT.format(end);
+  }
+
+  public void setEnd(long end) {
+    this.end = end;
+  }
+
+  public double getPercentStart() {
+    return percentStart;
+  }
+
+  public void setPercentStart(double percentStart) {
+    this.percentStart = percentStart;
+  }
+
+  public double getPercentLength() {
+    return percentLength;
+  }
+
+  public void setPercentLength(double percentLength) {
+    this.percentLength = percentLength;
+  }
+
+  public void setContext(String context) {
+    this.context = context;
+  }
+
+  public String getContext() {
+    return sequenceId + ":" + context;
+  }
+
+  @Override
+  public int compareTo(Feature feature) {
+    if (feature != null) {
+      int diff = (int) (this.start - feature.start);
+      return (diff == 0) ? (int) (this.end - feature.end) : diff;
     }
-
-    public Feature(Feature feature) {
-      this.context = feature.context;
-      this.end = feature.end;
-      this.forward = feature.forward;
-      this.percentLength = feature.percentLength;
-      this.percentStart = feature.percentStart;
-      this.sequenceId = feature.sequenceId;
-      this.sourceId = feature.sourceId;
-      this.start = feature.start;
-      this.description = feature.description;
-    }
-
-    public String getSequenceId() {
-        return sequenceId;
-    }
-
-    public void setSequenceId(String sequenceId) {
-        this.sequenceId = sequenceId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public long getStart() {
-        return start;
-    }
-    
-    public String getStartFormatted() {
-        return FORMAT.format(start);
-    }
-
-    public void setStart(long start) {
-        this.start = start;
-    }
-
-    public long getEnd() {
-        return end;
-    }
-    
-    public String getEndFormatted() {
-        return FORMAT.format(end);
-    }
-
-    public void setEnd(long end) {
-        this.end = end;
-    }
-
-    public String getSourceId() {
-        return sourceId;
-    }
-
-    public boolean isForward() {
-        return forward;
-    }
-
-    public void setForward(boolean forward) {
-        this.forward = forward;
-    }
-
-    public double getPercentStart() {
-        return percentStart;
-    }
-
-    public void setPercentStart(double percentStart) {
-        this.percentStart = percentStart;
-    }
-
-    public double getPercentLength() {
-        return percentLength;
-    }
-
-    public void setPercentLength(double percentLength) {
-        this.percentLength = percentLength;
-    }
-
-    public void setContext(String context) {
-        this.context = context;
-    }
-
-    public String getContext() {
-        return sequenceId + ":" + context;
-    }
+    else
+      return -1;
+  }
 }
