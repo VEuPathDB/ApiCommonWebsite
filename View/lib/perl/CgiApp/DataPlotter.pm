8efcd4471eea86964f43073c57b9decd3d00e6e1
Bindu Gajria <bindu.gajria@gmail.com>
2014-07-14 19:36:00 +0000

    move PathwayGenera.pm to GraphPackage/EuPathDB/ fix DataPlotter accordingly
    refs #16698
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@63340 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/DataPlotter.pm
+++ View/lib/perl/CgiApp/DataPlotter.pm
@@ -61,6 +61,7 @@ sub run {
 
 	 my $pkg          = $Cgi->param('project_id');
 	 my $type           = $Cgi->param('type');
+	 $pkg = "EuPathDB" if ($type eq 'PathwayGenera');
 	 my $id             = $Cgi->param('id');
 	 my $sid            = $Cgi->param('sid');
 	 my $wantLogged     = $Cgi->param('wl');
@@ -83,7 +84,7 @@ sub run {
 	 my @errors;
 
 	 push(@errors, 'model must be supplied') if not defined $pkg;
-	 push(@errors, $pkg . ' is an unallowed value for Project_id arg') if ($pkg ne 'PlasmoDB' and $pkg ne 'ToxoDB' and $pkg ne 'GiardiaDB' and $pkg ne 'AmoebaDB' and $pkg ne 'TriTrypDB' and $pkg ne 'FungiDB' and $pkg ne 'CryptoDB' and $pkg ne 'PiroplasmaDB' and $pkg ne 'MicrosporidiaDB' and $pkg ne 'HostDB');
+	 push(@errors, $pkg . ' is an unallowed value for Project_id arg') if ($pkg ne 'PlasmoDB' and $pkg ne 'ToxoDB' and $pkg ne 'GiardiaDB' and $pkg ne 'AmoebaDB' and $pkg ne 'TriTrypDB' and $pkg ne 'FungiDB' and $pkg ne 'CryptoDB' and $pkg ne 'PiroplasmaDB' and $pkg ne 'MicrosporidiaDB' and $pkg ne 'HostDB' and $pkg ne 'EuPathDB');
 	 push(@errors, 'type must be supplied' ) if not defined $type;
 	 push(@errors, 'id must be supplied'   ) if not defined $id;
 
