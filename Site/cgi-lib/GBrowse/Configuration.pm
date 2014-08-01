86cb0773a7c4afe560430f519820e8c68ea4b503
John Brestelli <jbrestell@gmail.com>
2014-08-01 19:04:39 +0000

    revert citation method to b18; (refs #16491)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@63780 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/GBrowse/Configuration.pm
+++ Site/cgi-lib/GBrowse/Configuration.pm
@@ -260,59 +260,28 @@ sub wdkReference {
   }
 }
 
+
+
 sub citationFromExtDatabaseNamePattern {
   my ($self,$extdb) = @_;
 
+  if(defined $self->{citation_from_ext_database_name_pattern}) {
+    return $self->{citation_from_ext_database_name_pattern}->{$extdb};
+  }
+
   my $sql =<<EOL;
-WITH pubs as (select name, 
-                     id, 
-                     listagg(publication,',') WITHIN GROUP (order by publication) PMIDS, 
-                     contact_email 
-                from ( SELECT nvl(ds.dataset_name_pattern, ds.name) as name, 
-                              ds.dataset_presenter_id as id, 
-                              c.email as contact_email,
-                              p.pmid as publication 
-                         from ApidbTuning.DatasetPresenter ds,
-                              APIDBTUNING.datasetcontact c,
-                              APIDBTUNING.datasetpublication p
-                        where ds.dataset_presenter_id = c.dataset_presenter_id 
-                          and ds.dataset_presenter_id = p.dataset_presenter_id 
-                          and ((ds.name = '$extdb' and ds.dataset_name_pattern is null) or ds.dataset_name_pattern = '$extdb')
-                          and c.is_primary_contact =1 )
-                     group by name, id, contact_email),
-                     samples as ( select experiment, 
-                                         listagg(sample_i,chr(10)) WITHIN GROUP (order by sample) sample_table
-                                    from ( select distinct external_database_name as experiment,
-                                                  sample,
-                                                  '<p style="color:' || html_color || '">' || sample || '<\p>' as sample_i
-                                             from APIDBTUNING.MSPeptideSummary 
-                                             where internal_id like '$extdb%'
-                                          )
-                                    group by experiment
-                                           
-                                 )
-SELECT name, 
-       dbms_lob.substr(description,4000,1) ||
-       ' Primary Contact Email: '|| nvl(email,'unavailable')||
-       ' PMID: ' || publications || 
-       '<p style="color:black">Samples:<\p>' || sample_table || chr(10) ||
-       ' Please note that subtrack labels will disappear if the selected subtracks number is over 15!' as citation 
-  FROM (SELECT nvl(ds.dataset_name_pattern, ds.name) as name, 
-               ds.summary as description, 
-               pubs.contact_email as email, 
-               pubs.PMIDS as publications,
-               samples.sample_table as sample_table
-          FROM ApidbTuning.DatasetPresenter ds, 
-               pubs,
-               samples
-         where ds.dataset_presenter_id = pubs.id )
+WITH pubs as (select name, id, listagg(publication,',') WITHIN GROUP (order by publication) PMIDS, contact_email from ( SELECT nvl(ds.dataset_name_pattern, ds.name) as name, ds.dataset_presenter_id as id, c.email as contact_email, p.pmid as publication from ApidbTuning.DatasetPresenter ds, APIDBTUNING.datasetcontact c,APIDBTUNING.datasetpublication p where ds.dataset_presenter_id = c.dataset_presenter_id and ds.dataset_presenter_id = p.dataset_presenter_id and c.is_primary_contact =1 )group by name, id, contact_email)
+SELECT name, dbms_lob.substr(description,4000,1) ||' Primary Contact Email: '||nvl(email,'unavailable')||' PMID: ' || publications as citation 
+FROM (SELECT nvl(ds.dataset_name_pattern, ds.name) as name, ds.summary as description, pubs.contact_email as email, pubs.PMIDS as publications FROM ApidbTuning.DatasetPresenter ds, pubs where ds.dataset_presenter_id = pubs.id )
 EOL
   my $sth = $self->{dbh}->prepare($sql);
   $sth->execute() or $self->throw($sth->errstr);
+
   while (my ($name,$value)  = $sth->fetchrow_array) {
-    return "$value";
+    $self->{citation_from_ext_database_name_pattern}->{$name} = $value;
   }
 
+  return $self->{citation_from_ext_database_name_pattern}->{$extdb};  
 }
 
 1;
