12dceb7c2a28c885de24c5c02a5b8344f542330e
Mark Heiges <mheiges@gmail.com>
2014-08-07 00:56:40 +0000

    add UserDB connection pool data to /dashboard
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@63900 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/dashboard/view/dbConnectionPool.php
+++ Site/dashboard/view/dbConnectionPool.php
@@ -98,4 +98,76 @@ foreach ($fields as $param) {
 
 <h2>WDK-Engine/Userlogin Connection Pool</h2>
 
+<p>
+<?php print $metrics_description ?>
+</p>
+
+<p class="clickable">Summary (with stack traces, if any) &#8593;&#8595;</p>
+<div class="expandable" >
+    <p>
+    <span class='pre'><?php print $u_pool{'UnclosedConnectionInfo'}?></span>
+    </p>
+</div> <!-- div expandable -->
+</p>
+
+<p>
+<b>Metrics</b>
+<table border="0" cellspacing="3" cellpadding="2" align="">
+<tr class="secondary3">
+<th><font size="-2">Key</font></th>
+<th><font size="-2">Value</font></th>
+<th><font size="-2">Description</font></th>
+</tr>
+
+<tr class="rowLight"><td>BorrowedCount</td><td><?php print strtolower($u_pool{'BorrowedCount'})?></td>
+  <td>Running count of connections borrowed from the pool by the application. This value is managed by the web application.</td>
+</tr>
+<tr class="rowMedium"><td>ReturnedCount</td><td><?php print strtolower($u_pool{'ReturnedCount'})?></td>
+  <td>Running count of connections returned the pool by the application. This value is managed by the web application.</td>
+</tr>
+<tr class="rowLight"><td>CurrentlyOpenCount</td><td><?php print strtolower($u_pool{'CurrentlyOpenCount'})?></td>
+  <td>Number of connections currenly in use by the application. A high number might indicate a leak. This value is managed by the web application.</td>
+</tr>
+<tr class="rowMedium"><td>NumActive</td><td><?php print strtolower($u_pool{'NumActive'})?></td>
+  <td>The number of connections currently borrowed from the pool. This value is managed by the connection pool library.</td>
+</tr>
+<tr class="rowLight"><td>NumIdle</td><td><?php print strtolower($u_pool{'NumIdle'})?></td>
+  <td>The number of connections currently idle in the pool. This value is managed by the connection pool library.</td>
+</tr>
+
+</table>
+</p>
+
+
+<p>
+<b>Configuration</b>
+</p>
+<p>
+The following are current runtime values retreived through the API of the active connection pool.
+See <a href='http://commons.apache.org/proper/commons-dbcp/api-1.4/org/apache/commons/dbcp/BasicDataSource.html'>DBCP javadocs</a> 
+for explanation of parameters.
+</p>
+
+<p>
+<table border="0" cellspacing="3" cellpadding="2" align="">
+<tr class="secondary3"><th><font size="-2">Parameter</font></th><th><font size="-2">Value</font></th></tr>
+<?php
+$row = 0;
+$fields = array(
+  'MinIdle', 'MaxIdle', 'MinEvictableIdleTimeMillis',
+  'SoftMinEvictableIdleTimeMillis', 'TimeBetweenEvictionRunsMillis',
+  'TestOnBorrow', 'TestOnReturn', 'TestWhileIdle'
+);
+foreach ($fields as $param) {
+  $css_class = ($row % 2) ? "rowMedium" : "rowLight";
+?>
+<tr class="<?php print $css_class?>">
+  <td><?php print $param?></td><td><?php print strtolower(var_export($u_pool{$param}))?></td>
+</tr>
+<?php
+  $row++;
+}
+?>
+</table>
+</p>
 
