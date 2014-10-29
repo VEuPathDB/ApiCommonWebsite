edd7ee1b7bf23e07b113b3fa018381d1fa366159
Haiming Wang <hwang@uga.edu>
2014-10-29 19:13:34 +0000

    using local clustalw v2.1 instead of VBI clustalw web service, VBI is going to shut down their clustalw service by the end of Friday (10/31/2014). will notify the infrastructure group about incorporating clustalw into result analysis tool.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/api-build-22@65143 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/IsolateClustalw.pm
+++ View/lib/perl/CgiApp/IsolateClustalw.pm
@@ -3,12 +3,10 @@ package ApiCommonWebsite::View::CgiApp::IsolateClustalw;
 @ISA = qw( ApiCommonWebsite::View::CgiApp );
 
 use strict;
+use Tie::IxHash;
 use ApiCommonWebsite::View::CgiApp;
+use Data::Dumper;
 
-use SOAP::Lite;
-use CGI::Session;
-use XML::XPath;
-use XML::XPath::XMLParser;
 use Bio::Graphics::Browser2::PadAlignment;
 
 sub run {
@@ -37,11 +35,13 @@ sub processParams {
   $list =~ s/\,$//;
 
   $self->{ids} = $list;
-
 }
 
 sub handleIsolates {
   my ($self, $dbh, $cgi) = @_;
+
+  my $ROOT = $ENV{'DOCUMENT_ROOT'};
+  my $GUS_HOME = $ENV{'GUS_HOME'};
 
   my $ids = $self->{ids};
 
@@ -85,171 +85,63 @@ EOSQL
     $sequence .= ">$id\n$seq\n";
   }
 
-  # previouse url.
-  # ->service('http://ppdev.bioinformatics.vt.edu:6565/axis/services/msa?wsdl');
-  my $result = SOAP::Lite
-     ->service('http://pathport.bioinformatics.vt.edu:6565/axis/services/msagit?wsdl');
-
-  my $alignment = $result->nucleotide_Alignment("$sequence", "ALIGNED", "", "15.00", "6.66", "15.00", "6.66", "30", "0.50", "iub", "iub");
-
-  my $xml = XML::XPath->new(xml => $alignment);
-
-  my $nodes = $xml->find('/MSAML/alignment/sequence/seqname');
-  my (@names, @aligns);
-
-  foreach my $node($nodes->get_nodelist) {
-    push @names, $node->string_value();
-  }
-
-  $nodes = $xml->find('/MSAML/alignment/sequence/seq');
-
-  foreach my $node($nodes->get_nodelist) {
-    my @arr = split /\s+/, $node->string_value();
-    push @aligns, \@arr,
-  }
-
-  $nodes = $xml->find('/MSAML/alignment/scoring/param');
-
-  my @ws_params;
-  foreach my $node($nodes->get_nodelist) {
-    push @ws_params, $node;
-  }
-
-  my $size = @{$aligns[0]};
-
-  my @sequences;
-  my @segments;
-
-  $nodes = $xml->find('/MSAML/alignment/sequence/seqname');
-
-  my (@n, @s);
-
-  foreach my $node($nodes->get_nodelist) {
-    push @n, $node->string_value();
-  }
-
-  $nodes = $xml->find('/MSAML/alignment/sequence/seq');
-
-  my $length;
-
-  foreach my $node($nodes->get_nodelist) {
-    my $seq = $node->string_value();
-    $seq =~ s/\s+//g;
-    $length = length $seq;
-    push @s, $seq;
-  }
-
-  $size = @n;
-  my $ct = 0;
-
-  for(my $i = 0; $i < $size; $i++) {
-    $ct += 1;
-    push @sequences, $n[$i];
-    push @sequences, $s[$i];
-    push @segments, [$n[$i], 0, $length, 0, $length] unless ($ct == 1);
-    #push @segments, [$n[$i], 0, $length, 0, $length];
-  }
-
-  my $align = Bio::Graphics::Browser2::PadAlignment->new(\@sequences,\@segments);
-
-  my %origins = ();
-  if ($type =~ /htsSNP/i){
-     foreach my $id (split /,/, $ids) {
-        $id =~ s/'//g;
-        $id =~ s/^$sid\.// unless ($id eq $sid);
-        $origins{$id} = $start;
-     }
-  }
-
-  print "<table align=center width=800>";
-  print "<tr><td>";
-  print "<a href='#tree'><h3>To view a guide tree, click here or scroll to the bottom of the page</h3></a>" if ($type !~ /htsSNP/i);
-  print "</td></tr>";
-  print "<tr><td>";
-  print $cgi->pre($align->alignment( \%origins, { show_mismatches   => 1,
-                                                  show_similarities => 1, 
-                                                  show_matches      => 1})); 
-
-  print "</td></tr>";
-
-  if ($type !~ /htsSNP/i){  # don't show the tree if it's htsSNPs
-
-  print "<tr><td><pre><a name='tree'>Guide Tree</a></pre></td></tr>";
-  my @parts = $result->packager->parts;
-  foreach my $p (@parts) {
-    foreach(@$p) {
-      my $id = $_->head->get('Content-Id');
-      if($id =~ /tree/i) {
-        my $tree = $_->bodyhandle->as_string;
-        my $link = $self->writetree($tree);
-        $self->printApplet($link);
-        $tree =~ s/\n/ /g;
-        print "<tr><td>$tree</td></tr>";
-      }
-    }
-  }
-
-  print "<tr><td><hr><pre>";
-
-  foreach(@ws_params) { 
-    print $_->string_value, "\n"; 
-  } 
-
-  }
-  
-  print "</pre></td></tr>";
-  print "</table>";
-}
-
-sub printApplet {
-  my ($self, $link) = @_;
-
-  my $host = $ENV{'SERVER_NAME'};
-
-print <<EOF;
-<tr><td>
-<applet archive="forester.jar"
-        code="org.forester.atv.ATVe.class"
-        codebase="/"
-        width="600"
-        height="500"
-        alt="ATVe is not working on your system (requires at least Sun Java 1.5)!">
-        <param name="url_of_tree_to_load" value="http://$host/$link">
-        <param name="config_file" value="/_atv_configuration_ncbi_tax">
-</applet>
-</td></tr>
-EOF
-
-}
-
-sub writetree {
-
-  my ($self, $tree) = @_;
-
-  my $ROOT = $ENV{'DOCUMENT_ROOT'};
-
-  my $dir = "$ROOT/gbrowse/tmp/clustalw";
-
-  mkdir("$dir", 0755);
-  $self->cleanup($dir);
-
   my $range = 10000000;
   my $random = int(rand($range));
-  my $tree_file = "$dir/$random.tree";
-  
-  open(TREE, ">$tree_file");
-  print TREE $tree;
-  close(TREE); 
-  chmod 0666, $tree_file;
-  
-  return "/gbrowse/tmp/clustalw/$random.tree";
 
-}
+  my $infile  = "/tmp/isolate_seq_tmp$random.fas";
+  my $outfile = "/tmp/isolate_seq_tmp$random.aln";
+  my $dndfile = "/tmp/isolate_seq_tmp$random.dnd";
+  my $tmpfile = "/tmp/isolate_seq_tmp$random.tmp";
+  
+  open(OUT, ">$infile");
+  print OUT $sequence;
+  close OUT;
 
-sub cleanup {
-  my ($self, $dir) = @_;
-  my $cmd = "find $dir -mtime +10 -exec rm -f {} \\;";
+  my $cmd = "$GUS_HOME/bin/clustalw2 -infile=$infile -outfile=$outfile > $tmpfile";
   system($cmd);
+
+  open(O, "$outfile");
+  my %hash;
+  tie %hash, "Tie::IxHash";
+
+  while(<O>) {
+    chomp;
+    next if (/^CLUSTAL/i || /^\s+$/ || /\*{1,}+/);
+    my ($id, $seq) = split /\s+/, $_;
+    $id =~ s/\s+//g; 
+    next if $id eq ""; # not sure why empty ids are not skipped.
+    push @{$hash{$id}}, $seq;
+  }
+  close O;
+
+  my @dnas;
+  my @alignments;
+  while(my ($id, $seqs) = each %hash) {
+    my $seq = join '', @{$hash{$id}};
+    #my $length = length($seq);
+    #push @alignments, [$id, 0, $length, 0, $length]; 
+    push @dnas, $id, $seq;
+  }
+
+  my $align = Bio::Graphics::Browser2::PadAlignment->new(\@dnas,\@alignments);
+
+  my %origins = ();
+
+  print $cgi->pre("CLUSTAL 2.1 Multiple Sequence Alignments\n");
+  print $cgi->pre($align->alignment( \%origins, { show_mismatches   => 1,
+                                                   show_similarities => 1, 
+                                                   show_matches      => 1})); 
+
+     
+  open(D, "$dndfile");
+  print "<pre>";
+  print "<hr>.dnd file\n\n";
+  while(<D>) {
+    chomp;
+    print $_;
+  }
+  close D;
+  print "</pre>";
 }
 
 sub error {
