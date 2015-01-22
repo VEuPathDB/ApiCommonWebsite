0f8d7ed2e9058cf4c3bde4233f5d6280b2e24a80
Ryan Doherty <tech@conical.org>
2015-01-22 04:24:30 +0000

    #17132: Fix dashboard to use JMX refactoring to dynamically find DB names and show props for all configured DBs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@66066 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/dashboard/lib/modules/DBInstances.php
+++ Site/dashboard/lib/modules/DBInstances.php
@@ -3,20 +3,17 @@
 require_once dirname(__FILE__) . "/JolModule.php";
 
 /**
- * Superclass for database mbean access
+ * Superclass for DBInstances mbean access
  *
  * @author Mark Heiges <mheiges.edu>
  * @package Module
  * @subpackage Database
 
  */
-class Database extends JolModule {
+class DBInstances extends JolModule {
 
-  protected $role;
-
-  public function __construct($dbName) {
+  public function __construct() {
     parent::__construct();
-    $this->role = $dbName;
   }
 
   /**
@@ -36,26 +33,9 @@ class Database extends JolModule {
     return $response[0]->value();
   }
 
-  /**
-   *
-   * @return boolean TRUE if operation was successful, otherwise FALSE
-   */
-  public function refresh() {
-    $req = new JolRequest($this->jol_base_url);
-    $exec = new JolExecOperation(array(
-                'mbean' => $this->get_mbean(),
-                'operation' => 'reload',
-            ));
-    $req->add_operation($exec);
-    $response = $req->invoke();
-    return $response[0]->is_success();
-  }
-
   private function get_mbean() {
-    return 'org.gusdb.wdk:type=Database,' .
-            'role=' . $this->role .
-            ',data=Environment' .
-            ',path=' . $this->path_name;
+    return 'org.gusdb.wdk:type=Database,status=DBInstances' .
+           ',path=' . $this->path_name;
   }
 
 }
--- Site/dashboard/view/dbConnectionPool.php
+++ Site/dashboard/view/dbConnectionPool.php
@@ -4,71 +4,87 @@
  * @package View
  */
 
-require_once dirname(__FILE__) . "/../lib/modules/AppDBConnectionPool.php";
-require_once dirname(__FILE__) . "/../lib/modules/UserDBConnectionPool.php";
+require_once dirname(__FILE__) . "/../lib/modules/DBInstances.php";
+require_once dirname(__FILE__) . "/../lib/modules/ConnectionPool.php";
 
-$appdb_pool = new AppDBConnectionPool();
-$userdb_pool = new UserDBConnectionPool();
+$db_instances_obj = new DBInstances();
+$db_instances_attribs = $db_instances_obj->attributes();
+$db_instances_str = $db_instances_attribs{'DbInstanceNames'};
+$db_names = explode(',', $db_instances_str);
 
-$a_pool = $appdb_pool->attributes();
-$u_pool = $userdb_pool->attributes();
+/**
+ * Loop through available DBs by name and display attributes for each
+ */
+foreach ($db_names as $db_name) {
+
+$db_pool = new ConnectionPool($db_name);
+$db_display_name = ucfirst(strtolower($db_name));
+$pool_attribs = $db_pool->attributes();
 
 $metrics_description = <<<EOF
 The following are current runtime values retreived through the instantiated connection pool classes or 
  web application classes. The connection pool reported on here is initiated and managed by 
-<code>{$a_pool{'PoolOwnerClassname'}}</code>.
+<code>{$pool_attribs{'PoolOwnerClassname'}}</code>.
 EOF;
-
 ?>
-<h2>Application Connection Pool</h2>
 
+<h2><?php print $db_display_name ?> Connection Pool</h2>
 
 <p>
-<?php print $metrics_description ?>
+  <?php print $metrics_description ?>
 </p>
 
-
 <p>
-<b>Metrics</b>
+  <b>Metrics</b>
+</p>
+
 <table border="0" cellspacing="3" cellpadding="2" align="">
-<tr class="secondary3">
-<th><font size="-2">Key</font></th>
-<th><font size="-2">Value</font></th>
-<th><font size="-2">Description</font></th>
-</tr>
-
-<tr class="rowLight"><td>BorrowedCount</td><td><?php print strtolower($a_pool{'BorrowedCount'})?></td>
-  <td>Running count of connections borrowed from the pool by the application. This value is managed by the web application.</td>
-</tr>
-<tr class="rowMedium"><td>ReturnedCount</td><td><?php print strtolower($a_pool{'ReturnedCount'})?></td>
-  <td>Running count of connections returned the pool by the application. This value is managed by the web application.</td>
-</tr>
-<tr class="rowLight"><td>CurrentlyOpenCount</td><td><?php print strtolower($a_pool{'CurrentlyOpenCount'})?></td>
-  <td>Number of connections currenly in use by the application. A high number might indicate a leak. This value is managed by the web application.</td>
-</tr>
-<tr class="rowMedium"><td>NumActive</td><td><?php print strtolower($a_pool{'NumActive'})?></td>
-  <td>The number of connections currently borrowed from the pool. This value is managed by the connection pool library.</td>
-</tr>
-<tr class="rowLight"><td>NumIdle</td><td><?php print strtolower($a_pool{'NumIdle'})?></td>
-  <td>The number of connections currently idle in the pool. This value is managed by the connection pool library.</td>
-</tr>
-
+  <tr class="secondary3">
+    <th><font size="-2">Key</font></th>
+    <th><font size="-2">Value</font></th>
+    <th><font size="-2">Description</font></th>
+  </tr>
+  <tr class="rowLight">
+    <td>BorrowedCount</td>
+    <td><?php print strtolower($pool_attribs{'BorrowedCount'})?></td>
+    <td>Running count of connections borrowed from the pool by the application. This value is managed by the web application.</td>
+  </tr>
+  <tr class="rowMedium">
+    <td>ReturnedCount</td>
+    <td><?php print strtolower($pool_attribs{'ReturnedCount'})?></td>
+    <td>Running count of connections returned the pool by the application. This value is managed by the web application.</td>
+  </tr>
+  <tr class="rowLight">
+    <td>CurrentlyOpenCount</td>
+    <td><?php print strtolower($pool_attribs{'CurrentlyOpenCount'})?></td>
+    <td>Number of connections currenly in use by the application. A high number might indicate a leak. This value is managed by the web application.</td>
+  </tr>
+  <tr class="rowMedium">
+    <td>NumActive</td>
+    <td><?php print strtolower($pool_attribs{'NumActive'})?></td>
+    <td>The number of connections currently borrowed from the pool. This value is managed by the connection pool library.</td>
+  </tr>
+  <tr class="rowLight">
+    <td>NumIdle</td>
+    <td><?php print strtolower($pool_attribs{'NumIdle'})?></td>
+    <td>The number of connections currently idle in the pool. This value is managed by the connection pool library.</td>
+  </tr>
 </table>
-</p>
-
 
 <p>
-<b>Configuration</b>
+  <b>Configuration</b>
 </p>
 <p>
-The following are current runtime values retreived through the API of the active connection pool.
-See <a href='http://commons.apache.org/proper/commons-dbcp/api-1.4/org/apache/commons/dbcp/BasicDataSource.html'>DBCP javadocs</a> 
-for explanation of parameters.
+  The following are current runtime values retreived through the API of the active connection pool.
+  See <a href='http://commons.apache.org/proper/commons-dbcp/api-1.4/org/apache/commons/dbcp/BasicDataSource.html'>DBCP javadocs</a> 
+  for explanation of parameters.
 </p>
 
-<p>
 <table border="0" cellspacing="3" cellpadding="2" align="">
-<tr class="secondary3"><th><font size="-2">Parameter</font></th><th><font size="-2">Value</font></th></tr>
+  <tr class="secondary3">
+    <th><font size="-2">Parameter</font></th>
+    <th><font size="-2">Value</font></th>
+  </tr>
 <?php
 $row = 0;
 $fields = array(
@@ -79,80 +95,14 @@ $fields = array(
 foreach ($fields as $param) {
   $css_class = ($row % 2) ? "rowMedium" : "rowLight";
 ?>
-<tr class="<?php print $css_class?>">
-  <td><?php print $param?></td><td><?php print strtolower(var_export($a_pool{$param}))?></td>
-</tr>
+  <tr class="<?php print $css_class?>">
+    <td><?php print $param?></td>
+    <td><?php print strtolower(var_export($pool_attribs{$param}))?></td>
+  </tr>
 <?php
   $row++;
 }
 ?>
 </table>
-</p>
 
-<h2>WDK-Engine/Userlogin Connection Pool</h2>
-
-<p>
-<?php print $metrics_description ?>
-</p>
-
-<p>
-<b>Metrics</b>
-<table border="0" cellspacing="3" cellpadding="2" align="">
-<tr class="secondary3">
-<th><font size="-2">Key</font></th>
-<th><font size="-2">Value</font></th>
-<th><font size="-2">Description</font></th>
-</tr>
-
-<tr class="rowLight"><td>BorrowedCount</td><td><?php print strtolower($u_pool{'BorrowedCount'})?></td>
-  <td>Running count of connections borrowed from the pool by the application. This value is managed by the web application.</td>
-</tr>
-<tr class="rowMedium"><td>ReturnedCount</td><td><?php print strtolower($u_pool{'ReturnedCount'})?></td>
-  <td>Running count of connections returned the pool by the application. This value is managed by the web application.</td>
-</tr>
-<tr class="rowLight"><td>CurrentlyOpenCount</td><td><?php print strtolower($u_pool{'CurrentlyOpenCount'})?></td>
-  <td>Number of connections currenly in use by the application. A high number might indicate a leak. This value is managed by the web application.</td>
-</tr>
-<tr class="rowMedium"><td>NumActive</td><td><?php print strtolower($u_pool{'NumActive'})?></td>
-  <td>The number of connections currently borrowed from the pool. This value is managed by the connection pool library.</td>
-</tr>
-<tr class="rowLight"><td>NumIdle</td><td><?php print strtolower($u_pool{'NumIdle'})?></td>
-  <td>The number of connections currently idle in the pool. This value is managed by the connection pool library.</td>
-</tr>
-
-</table>
-</p>
-
-
-<p>
-<b>Configuration</b>
-</p>
-<p>
-The following are current runtime values retreived through the API of the active connection pool.
-See <a href='http://commons.apache.org/proper/commons-dbcp/api-1.4/org/apache/commons/dbcp/BasicDataSource.html'>DBCP javadocs</a> 
-for explanation of parameters.
-</p>
-
-<p>
-<table border="0" cellspacing="3" cellpadding="2" align="">
-<tr class="secondary3"><th><font size="-2">Parameter</font></th><th><font size="-2">Value</font></th></tr>
-<?php
-$row = 0;
-$fields = array(
-  'MinIdle', 'MaxIdle', 'MinEvictableIdleTimeMillis',
-  'TimeBetweenEvictionRunsMillis',
-  'TestOnBorrow', 'TestOnReturn', 'TestWhileIdle'
-);
-foreach ($fields as $param) {
-  $css_class = ($row % 2) ? "rowMedium" : "rowLight";
-?>
-<tr class="<?php print $css_class?>">
-  <td><?php print $param?></td><td><?php print strtolower(var_export($u_pool{$param}))?></td>
-</tr>
-<?php
-  $row++;
-}
-?>
-</table>
-</p>
-
+<?php } ?>
