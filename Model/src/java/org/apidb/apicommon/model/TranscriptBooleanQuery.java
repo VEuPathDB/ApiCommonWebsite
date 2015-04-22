71bff48530e1ecea2865241ae5192508282266de
Steve Fischer <sfischer@upenn.edu>
2015-04-22 23:35:49 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4-altsplice@67496 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQuery.java
+++ Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQuery.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.query.BooleanQuery;
 import org.gusdb.wdk.model.query.BooleanQueryInstance;
 import org.gusdb.wdk.model.query.Column;
@@ -12,36 +13,40 @@ import org.gusdb.wdk.model.record.RecordClass;
 import org.gusdb.wdk.model.record.attribute.PrimaryKeyAttributeField;
 import org.gusdb.wdk.model.user.User;
 import org.gusdb.wdk.model.question.DynamicAttributeSet;
+import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.record.attribute.ColumnAttributeField;
+import org.apache.log4j.Logger;
+
 
 public class TranscriptBooleanQuery extends BooleanQuery {
+
+  private static final Logger logger = Logger.getLogger(TranscriptBooleanQuery.class);
 
 	public static final String LEFT_MATCH_COLUMN = "left_match";
 	public static final String RIGHT_MATCH_COLUMN = "right_match";
 
     public TranscriptBooleanQuery() throws WdkModelException {
 	super();
-	//	addDynamicAttributeSetToQuestion();
     }
 
     protected TranscriptBooleanQuery(TranscriptBooleanQuery tbq) {
 	super(tbq);
     }
 
-    private void addDynamicAttributeSetToQuestion() {
-    	DynamicAttributeSet das = new DynamicAttributeSet();
-    	
+    private void addDynamicAttributeSetToQuestion(WdkModel wdkModel) throws WdkModelException {
+       	DynamicAttributeSet das = getContextQuestion().getDynamicAttributeSet();
+	
     	ColumnAttributeField left_af = new ColumnAttributeField();
-    	left_af.setName("LEFT_MATCH_COLUMN");
+	left_af.excludeResources(wdkModel.getProjectId());
+    	left_af.setName(LEFT_MATCH_COLUMN);
     	left_af.setDisplayName("Matches prev step");
     	das.addAttributeField(left_af);
     	
     	ColumnAttributeField right_af = new ColumnAttributeField();
-    	right_af.setName("RIGHT_MATCH_COLUMN");
+	right_af.excludeResources(wdkModel.getProjectId());
+    	right_af.setName(RIGHT_MATCH_COLUMN);
     	right_af.setDisplayName("Matches this step");
     	das.addAttributeField(right_af);
-    	
-    	getContextQuestion().addDynamicAttributeSet(das);
     }
 
     /*
@@ -53,10 +58,15 @@ public class TranscriptBooleanQuery extends BooleanQuery {
 	public BooleanQueryInstance makeInstance(User user, Map<String, String> values,
 						 boolean validate, int assignedWeight, Map<String, String> context)
 	throws WdkModelException, WdkUserException {
-		addDynamicAttributeSetToQuestion();
 	return new TranscriptBooleanQueryInstance(user, this, values, validate,
 						  assignedWeight, context);
     }	
+
+    @Override
+	public void setContextQuestion(Question contextQuestion) throws WdkModelException {
+	super.setContextQuestion(contextQuestion);
+	addDynamicAttributeSetToQuestion(wdkModel);
+    }
     
     protected void prepareColumns(RecordClass recordClass) {
 	super.prepareColumns(recordClass);
@@ -70,8 +80,7 @@ public class TranscriptBooleanQuery extends BooleanQuery {
 	column.setName(RIGHT_MATCH_COLUMN);
 	column.setQuery(this);
 	columnMap.put(RIGHT_MATCH_COLUMN, column);
-	//	addDynamicAttributeSetToQuestion();
-
+ 	
     }
 
     /*
