f9d8399af7be620f62770b627bce956932b47868
Steve Fischer <sfischer@upenn.edu>
2015-04-22 11:31:03 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4-altsplice@67477 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQuery.java
+++ Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQuery.java
@@ -7,59 +7,80 @@ import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.query.BooleanQuery;
 import org.gusdb.wdk.model.query.BooleanQueryInstance;
 import org.gusdb.wdk.model.query.Column;
+import org.gusdb.wdk.model.query.Query;
 import org.gusdb.wdk.model.record.RecordClass;
 import org.gusdb.wdk.model.record.attribute.PrimaryKeyAttributeField;
 import org.gusdb.wdk.model.user.User;
+import org.gusdb.wdk.model.question.DynamicAttributeSet;
+import org.gusdb.wdk.model.record.attribute.ColumnAttributeField;
 
 public class TranscriptBooleanQuery extends BooleanQuery {
 
 	public static final String LEFT_MATCH_COLUMN = "left_match";
 	public static final String RIGHT_MATCH_COLUMN = "right_match";
 
-    public TranscriptBooleanQuery()
-			throws WdkModelException {
-		super();
-	}
+    public TranscriptBooleanQuery() throws WdkModelException {
+	super();
+	//	addDynamicAttributeSetToQuestion();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.gusdb.wdk.model.query.Query#makeInstance()
-	 */
-	@Override
+    protected TranscriptBooleanQuery(TranscriptBooleanQuery tbq) {
+	super(tbq);
+    }
+
+    private void addDynamicAttributeSetToQuestion() {
+    	DynamicAttributeSet das = new DynamicAttributeSet();
+    	
+    	ColumnAttributeField left_af = new ColumnAttributeField();
+    	left_af.setName("LEFT_MATCH_COLUMN");
+    	left_af.setDisplayName("Matches prev step");
+    	das.addAttributeField(left_af);
+    	
+    	ColumnAttributeField right_af = new ColumnAttributeField();
+    	right_af.setName("RIGHT_MATCH_COLUMN");
+    	right_af.setDisplayName("Matches this step");
+    	das.addAttributeField(right_af);
+    	
+    	getContextQuestion().addDynamicAttributeSet(das);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wdk.model.query.Query#makeInstance()
+     */
+    @Override
 	public BooleanQueryInstance makeInstance(User user, Map<String, String> values,
-			boolean validate, int assignedWeight, Map<String, String> context)
-					throws WdkModelException, WdkUserException {
-		return new TranscriptBooleanQueryInstance(user, this, values, validate,
-				assignedWeight, context);
-	}	
+						 boolean validate, int assignedWeight, Map<String, String> context)
+	throws WdkModelException, WdkUserException {
+		addDynamicAttributeSetToQuestion();
+	return new TranscriptBooleanQueryInstance(user, this, values, validate,
+						  assignedWeight, context);
+    }	
+    
+    protected void prepareColumns(RecordClass recordClass) {
+	super.prepareColumns(recordClass);
+	
+	Column column = new Column();
+	column.setName(LEFT_MATCH_COLUMN);
+	column.setQuery(this);
+	columnMap.put(LEFT_MATCH_COLUMN, column);
 
-	protected void prepareColumns(RecordClass recordClass) {
-		super.prepareColumns(recordClass);
-		PrimaryKeyAttributeField primaryKey = recordClass.getPrimaryKeyAttributeField();
-		
-		Column column = new Column();
-		column.setName(LEFT_MATCH_COLUMN);
-		column.setQuery(this);
-		columnMap.put(LEFT_MATCH_COLUMN, column);
+	column = new Column();
+	column.setName(RIGHT_MATCH_COLUMN);
+	column.setQuery(this);
+	columnMap.put(RIGHT_MATCH_COLUMN, column);
+	//	addDynamicAttributeSetToQuestion();
 
-		column = new Column();
-		column.setName(RIGHT_MATCH_COLUMN);
-		column.setQuery(this);
-		columnMap.put(RIGHT_MATCH_COLUMN, column);
+    }
 
-	}
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.gusdb.wdk.model.query.Query#clone()
-   */
-  @Override
-  public Query clone() {
-    return new TranscriptBooleanQuery(this);
-  }
-
-
-
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wdk.model.query.Query#clone()
+     */
+    @Override
+	public Query clone() {
+	return new TranscriptBooleanQuery(this);
+    }
 }
--- Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQueryInstance.java
+++ Model/src/java/org/apidb/apicommon/model/TranscriptBooleanQueryInstance.java
@@ -7,70 +7,59 @@ import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.query.BooleanQuery;
 import org.gusdb.wdk.model.query.BooleanQueryInstance;
 import org.gusdb.wdk.model.user.User;
+import org.apache.log4j.Logger;
+
 
 public class TranscriptBooleanQueryInstance extends BooleanQueryInstance {
-	private static final String NL = System.lineSeparator();
-	  
-	public TranscriptBooleanQueryInstance(User user, BooleanQuery query,
-			Map<String, String> values, boolean validate, int assignedWeight,
-			Map<String, String> context) throws WdkModelException,
-			WdkUserException {
-		super(user, query, values, validate, assignedWeight, context);
-	}
-	
-	/*
-	   * (non-Javadoc)
-	   * 
-	   * @see org.gusdb.wdk.model.query.SqlQueryInstance#getUncachedSql()
-	   */
-	  @Override
-	  public String getUncachedSql() throws WdkModelException, WdkUserException {
-		  String booleanTranscriptsSql = super.getUncachedSql();
-                  String booleanGenesSql = "select ta.gene_source_id from (" + booleanTranscriptSql + ") bt, apidbtuning.transcriptattributes ta where bt.source_id = ta.source_id ";
-		  String 
-		  String sql = 
-				  "WITH genes as (" + geneSql + ")" + NL +
-				  "select gene_source_id, source_id, weight, sum(left_match) as " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", sum(right_match) as " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + NL +
-				  "from (" + NL +
-				  "  select left.gene_source_id, left.source_id, genes.weight, 1 as left_match, 0 as right_match" + NL +
-				  "  from genes, " + NL +
-				  "  (leftOperand) left" + NL +
-				  "  where left.gene_source_id = genes.gene_source_id" + NL +
-				  "  UNION" + NL +
-				  "  select right.gene_source_id, right.source_id, genes.weight, 1 as right_match, 0 as right_match" + NL +
-				  "  from genes, " + NL +
-				  "  (rightOperand) right" + NL +
-				  "  where right.gene_source_id = genes.gene_source_id" + NL +
-				  "  UNION" + NL +
-				  "  select ta.gene_source_id, ta.source_id, genes.weight, 0 as left_match, 0 as right_match" + NL +
-				  "  from genes, apidbtuning.transcriptattributes ta" + NL +
-				  "  where genes.gene_source_id = ta.gene_source_id) big" + NL +
-				  "group by (gene_source_id, source_id, weight)";
-		return sql;	
-		  
-	  }
+    private static final String NL = System.lineSeparator();
+  private static final Logger logger = Logger.getLogger(BooleanQueryInstance.class);
 
-	  /**
-	   * get the columns to do the boolean operation on
-	   * subclasses can override to provide custom pk columns
-	   * @return
-	   */
-	  @Override
-	  protected String[] getPkColumns() {
-		  String[] cols = {"gene_source_id"};
-		  return cols;
-	  }
+    GeneBooleanQueryInstance genebqi;
 	  
-	  /**
-	   * subclasses can override to provide custom preprocessing
-	   * @param operandSql
-	   * @return
-	   */
-	  @Override
-	  protected String preProcessOperandSql(String operandSql, String leftOrRight) {
-		  String sql = "-- reduce to genes for boolean" + NL + 
-				  "select distinct gene_source_id, weight from (" + operandSql + ") genes" + leftOrRight;
-		  return operandSql;
-	  }
+    public TranscriptBooleanQueryInstance(User user, BooleanQuery query,
+					  Map<String, String> values, boolean validate, int assignedWeight,
+					  Map<String, String> context) throws WdkModelException,
+									      WdkUserException {
+	super(user, query, values, validate, assignedWeight, context);
+	genebqi = new GeneBooleanQueryInstance(user, query, values, false, assignedWeight, context);
+    }
+	
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wdk.model.query.SqlQueryInstance#getUncachedSql()
+     */
+    @Override
+	public String getUncachedSql() throws WdkModelException, WdkUserException {
+
+	String booleanGenesSql = genebqi.getUncachedSql();
+
+	String sql = 
+	    " -- boolean of genes " + NL +
+	    "WITH genes as (" + booleanGenesSql + ")" + NL +
+	    " -- major select " + NL +
+	    "select gene_source_id, source_id, project_id, wdk_weight, sum(left_match) as " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", sum(right_match) as " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + NL +
+	    "from (" + NL +
+	    "  select left.gene_source_id, left.source_id, left.project_id, genes.wdk_weight, 1 as left_match, 0 as right_match" + NL +
+	    "  from genes, " + NL +
+	    "  (" + getLeftSql() + ") left" + NL +
+	    "  where left.gene_source_id = genes.gene_source_id" + NL +
+	    "  UNION" + NL +
+	    "  select right.gene_source_id, right.source_id, right.project_id, genes.wdk_weight, 0 as left_match, 1 as right_match" + NL +
+	    "  from genes, " + NL +
+	    "  (" + getRightSql() + ") right" + NL +
+	    "  where right.gene_source_id = genes.gene_source_id" + NL +
+	    "  UNION" + NL +
+	    "  select ta.gene_source_id, ta.source_id, genes.project_id, genes.wdk_weight, 0 as left_match, 0 as right_match" + NL +
+	    "  from genes, apidbtuning.transcriptattributes ta" + NL +
+	    "  where genes.gene_source_id = ta.gene_source_id) big" + NL +
+	    "group by (gene_source_id, source_id, project_id, wdk_weight)";
+	logger.info("TranscriptBooleanQueryInstance sql: " + NL + sql);
+
+	return sql;	
+		  
+    }
+
+   
 	  
 }
