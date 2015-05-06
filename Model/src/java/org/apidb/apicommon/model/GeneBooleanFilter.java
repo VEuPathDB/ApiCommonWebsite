bb9f67d2ae335d4d53c78eddd214d063814ea71c
Dave Falke <dmfalke@gmail.com>
2015-05-06 19:56:55 +0000

    add gene boolean filter ui
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4-altsplice@67660 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/GeneBooleanFilter.java
+++ Model/src/java/org/apidb/apicommon/model/GeneBooleanFilter.java
@@ -3,6 +3,7 @@ package org.apidb.apicommon.model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -14,109 +15,111 @@ import org.gusdb.wdk.model.answer.AnswerValue;
 import org.gusdb.wdk.model.filter.StepFilter;
 import org.gusdb.wdk.model.filter.FilterSummary;
 import org.gusdb.wdk.model.filter.ListColumnFilterSummary;
+import org.gusdb.wdk.model.user.Step;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class GeneBooleanFilter extends StepFilter {
-	
-	protected static final String COLUMN_COUNT = "count";
-	  private static final String GENE_BOOLEAN_FILTER_ARRAY_KEY = "gene_boolean_filter_array";
 
-	public GeneBooleanFilter() {
-		super("geneBooleanFilter");
-	}
-       
-        @Override
-	    public String getKey() { return GENE_BOOLEAN_FILTER_ARRAY_KEY; }
+  protected static final String COLUMN_COUNT = "count";
+  private static final String GENE_BOOLEAN_FILTER_ARRAY_KEY = "gene_boolean_filter_array";
 
-	@Override
-	public FilterSummary getSummary(AnswerValue answer, String idSql)
-			throws WdkModelException, WdkUserException {
 
-		Map<String, Integer> counts = new LinkedHashMap<>();
-		// group by the query and get a count
-				
-		// the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
-		String fullIdSql = getFullSql(answer, idSql);
+  public GeneBooleanFilter() {
+    super("geneBooleanFilter");
+  }
 
-		String sql = getSummarySql(fullIdSql);
+  @Override
+  public String getKey() { return GENE_BOOLEAN_FILTER_ARRAY_KEY; }
 
-		ResultSet resultSet = null;
-		DataSource dataSource = answer.getQuestion().getWdkModel().getAppDb().getDataSource();
-		try {
-			resultSet = SqlUtils.executeQuery(dataSource, sql, getKey() + "-summary");
-			while (resultSet.next()) {
-				String leftValue = resultSet.getString(TranscriptBooleanQuery.LEFT_MATCH_COLUMN);
-				String rightValue = resultSet.getString(TranscriptBooleanQuery.RIGHT_MATCH_COLUMN);
-				int count = resultSet.getInt(COLUMN_COUNT);
-				counts.put(leftValue + rightValue, count);
-			}
-		}
-		catch (SQLException ex) {
-			throw new WdkModelException(ex);
-		}
-		finally {
-			SqlUtils.closeResultSetAndStatement(resultSet);
-		}
-		return new ListColumnFilterSummary(counts);
-	}
-	
-	private String getSummarySql(String idSql) {
-		String sql = "select " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + ", count(*) as " + COLUMN_COUNT +
-				" from (" + idSql + ") group by " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN ;
-		
-		return sql;
-	}
-	
-	 @Override
-	  public String getDisplay() {
-	    return "dont care";   // custom view will take care of this
-	  }
+  @Override
+  public FilterSummary getSummary(AnswerValue answer, String idSql)
+  throws WdkModelException, WdkUserException {
 
-	@Override
-	public String getDisplayValue(AnswerValue answer, JSONObject jsValue)
-			throws WdkModelException, WdkUserException {
-		return "dont care";
-	}
+  Map<String, Integer> counts = new LinkedHashMap<>();
+  // group by the query and get a count
 
-	/**
-	 * Expected JSON is: { gene_boolean_filter_array:[[Y,N], [Y,Y]] }
-	 */
-	@Override
-	public String getSql(AnswerValue answer, String idSql, JSONObject jsValue)
-			throws WdkModelException, WdkUserException {
-		
-		// the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
-		String fullIdSql = getFullSql(answer, idSql);
-		
-		StringBuilder sql = new StringBuilder("select * from (" + fullIdSql + ") WHERE 1 = 0 "); // add a fake where to make the concatenation easier
+  // the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
+  String fullIdSql = getFullSql(answer, idSql);
 
-		  try {
-			  JSONArray jsArray = jsValue.getJSONArray(GENE_BOOLEAN_FILTER_ARRAY_KEY);
-			  for (int i = 0; i < jsArray.length(); i++) {
-				  JSONArray pair = jsArray.getJSONArray(i);
-				  sql.append("OR (" +  TranscriptBooleanQuery.LEFT_MATCH_COLUMN + "= " + pair.getString(0) + " AND " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + "= " + pair.getString(1));
-			  }
-		  } catch (JSONException ex) {
-			  throw new WdkModelException(ex);
-		  }
-		  return sql.toString();
-	  }
-	
-	/**
-	 *  the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
-	 * @param answer
-	 * @param idSql
-	 * @return
-	 * @throws WdkUserException 
-	 * @throws WdkModelException 
-	 */
-	private String getFullSql(AnswerValue answer, String idSql) throws WdkModelException, WdkUserException {
-		String originalIdSql = answer.getIdsQueryInstance().getSql();
-		
-		return "select idsql.* from (" + originalIdSql + ") idsql, (" + idSql + ") filteredIdSql" +
-		" where idSql.source_id = filteredIdSql.source_id and idSql.project_id = filteredIdSql.project_id";
-	}
+  String sql = getSummarySql(fullIdSql);
+
+  ResultSet resultSet = null;
+  DataSource dataSource = answer.getQuestion().getWdkModel().getAppDb().getDataSource();
+  try {
+    resultSet = SqlUtils.executeQuery(dataSource, sql, getKey() + "-summary");
+    while (resultSet.next()) {
+      String leftValue = resultSet.getString(TranscriptBooleanQuery.LEFT_MATCH_COLUMN);
+      String rightValue = resultSet.getString(TranscriptBooleanQuery.RIGHT_MATCH_COLUMN);
+      int count = resultSet.getInt(COLUMN_COUNT);
+      counts.put(leftValue + rightValue, count);
+    }
+  }
+  catch (SQLException ex) {
+    throw new WdkModelException(ex);
+  }
+  finally {
+    SqlUtils.closeResultSetAndStatement(resultSet);
+  }
+  return new ListColumnFilterSummary(counts);
+  }
+
+  private String getSummarySql(String idSql) {
+    String sql = "select " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + ", count(*) as " + COLUMN_COUNT +
+      " from (" + idSql + ") group by " + TranscriptBooleanQuery.LEFT_MATCH_COLUMN + ", " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN ;
+
+    return sql;
+  }
+
+  @Override
+  public String getDisplay() {
+    return "dont care";   // custom view will take care of this
+  }
+
+  @Override
+  public String getDisplayValue(AnswerValue answer, JSONObject jsValue)
+  throws WdkModelException, WdkUserException {
+  return "dont care";
+  }
+
+  /**
+   * Expected JSON is: { values:["10", "01", "11"] }
+   */
+  @Override
+  public String getSql(AnswerValue answer, String idSql, JSONObject jsValue)
+  throws WdkModelException, WdkUserException {
+
+  // the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
+  String fullIdSql = getFullSql(answer, idSql);
+
+  StringBuilder sql = new StringBuilder("select * from (" + fullIdSql + ") WHERE 1 = 0 "); // add a fake where to make the concatenation easier
+
+  try {
+    JSONArray jsArray = jsValue.getJSONArray("values");
+    for (int i = 0; i < jsArray.length(); i++) {
+      String value = jsArray.getString(i);
+      sql.append("OR (" +  TranscriptBooleanQuery.LEFT_MATCH_COLUMN + "= " + value.charAt(0) + " AND " + TranscriptBooleanQuery.RIGHT_MATCH_COLUMN + "= " + value.charAt(1) + ")");
+    }
+  } catch (JSONException ex) {
+    throw new WdkModelException(ex);
+  }
+  return sql.toString();
+  }
+
+  /**
+   *  the input idSql has filters applied, and they might strip off dyn columns.  join those back in using the original id sql
+   * @param answer
+   * @param idSql
+   * @return
+   * @throws WdkUserException 
+   * @throws WdkModelException 
+   */
+  private String getFullSql(AnswerValue answer, String idSql) throws WdkModelException, WdkUserException {
+    String originalIdSql = answer.getIdsQueryInstance().getSql();
+
+    return "select idsql.* from (" + originalIdSql + ") idsql, (" + idSql + ") filteredIdSql" +
+      " where idSql.source_id = filteredIdSql.source_id and idSql.project_id = filteredIdSql.project_id";
+  }
 
 }
--- Site/webapp/wdkCustomization/js/transcripts.js
+++ Site/webapp/wdkCustomization/js/transcripts.js
@@ -1,4 +1,4 @@
-wdk.namespace('eupathdb.transcripts', function(ns) {
+wdk.namespace('eupathdb.transcripts', function(ns, $) {
 
   function openTransform(stepId) {
     var currentStrategyFrontId = wdk.addStepPopup.current_Front_Strategy_Id;
@@ -17,5 +17,43 @@ wdk.namespace('eupathdb.transcripts', function(ns) {
     wdk.addStepPopup.callWizard(url,null,null,null,'next'); 
   }
 
+  function loadGeneBooleanFilter(event) {
+    var $filter = $(event.target).find('.gene-boolean-filter');
+    var data = $filter.data();
+    $filter
+      .find('.gene-boolean-filter-summary')
+      .load('getFilterSummary.do', data, function() {
+        var valuesStr = $filter.find('.gene-boolean-filter-values').html().trim();
+        if (valuesStr) {
+          var values = JSON.parse(valuesStr);
+          $filter.find('[name=values]').each(function(index, checkbox) {
+            checkbox.checked = values.values.indexOf(checkbox.value) > -1;
+          });
+        }
+      });
+  }
+
+  function applyGeneBooleanFilter(event) {
+    event.preventDefault();
+    var ctrl = wdk.strategy.controller;
+    var form = event.target;
+    var $form = $(form);
+    var $filter = $form.parent('.gene-boolean-filter');
+    var data = $filter.data();
+    var values = [].slice.call(form.values)
+      .filter(function(el) {
+        return el.checked;
+      })
+      .map(function(el) {
+        return el.value.replace(/1/g, 'Y').replace(/0/g, 'N').split('');
+      });
+
+    $.post('applyFilter.do', $form.serialize(), function() {
+      ctrl.fetchStrategies(ctrl.updateStrategies);
+    });
+  }
+
+  $(document).on('submit', '[name=apply-gene-boolean-filter]', applyGeneBooleanFilter);
+  $(document).on('wdk-results-loaded', loadGeneBooleanFilter);
   ns.openTransform = openTransform;
 });
