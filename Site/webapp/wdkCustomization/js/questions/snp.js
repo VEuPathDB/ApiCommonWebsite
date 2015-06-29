6b709f4830d63ca4b8f04e21a7b08402835ee142
Dave Falke <dmfalke@gmail.com>
2015-06-29 15:47:57 +0000

    Don't inject filter if form is being revised.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@68605 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/questions/snp.js
+++ Site/webapp/wdkCustomization/js/questions/snp.js
@@ -1,27 +1,31 @@
-jQuery(document).on('filterParamDidMount', injectDefaultFilter);
+(function($) {
+  $(document).on('filterParamDidMount', injectDefaultFilter);
 
-// Inject a filter to deselect experiment P. reichenowi by default
-// per a request from BB.
-//
-// FIXME Remove this when BB reloads SNPs.
-function injectDefaultFilter(event, filterParam) {
-  var filterService = filterParam.filterService;
+  // Inject a filter to deselect experiment P. reichenowi by default
+  // per a request from BB.
+  //
+  // FIXME Remove this when BB reloads SNPs.
+  function injectDefaultFilter(event, filterParam) {
+    var $form = $(event.target).closest('form');
+    var filterService = filterParam.filterService;
 
-  if (filterService.name !== 'ngsSnp_strain_meta') return;
-  if (filterService.filters.length > 0) return;
+    if ($form.is('.is-revise')) return;
+    if (filterService.name !== 'ngsSnp_strain_meta') return;
+    if ($('#organism:input').val() !== 'Plasmodium falciparum 3D7') return;
 
-  var field = filterService.fields.find(function(field) {
-    return field.term === 'Experiment';
-  });
-
-  filterService.getFieldDistribution(field)
-    .then(function(distribution) {
-      filterService.distributionMap[field.term] = distribution;
-      var values = distribution.reduce(function(values, item) {
-        if (!item.value.includes('P. reichenowi'))
-          values.push(item.value);
-        return values;
-      }, []);
-      filterParam.actions.addFilter(field, values);
+    var field = filterService.fields.find(function(field) {
+      return field.term === 'Experiment';
     });
-}
+
+    filterService.getFieldDistribution(field)
+      .then(function(distribution) {
+        filterService.distributionMap[field.term] = distribution;
+        var values = distribution.reduce(function(values, item) {
+          if (!item.value.includes('P. reichenowi'))
+            values.push(item.value);
+          return values;
+        }, []);
+        filterParam.actions.addFilter(field, values);
+      });
+  }
+}(jQuery));
