551e55a0f869a440b87809098ebd55d99376bdc0
Dave Falke <dmfalke@gmail.com>
2015-07-31 16:34:59 +0000

    updates to transcript record page. #12813
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@69069 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/application.js
+++ Site/webapp/wdkCustomization/js/application.js
@@ -10,24 +10,8 @@ let rootElement = document.getElementsByTagName('main')[0];
 let rootUrl = rootElement.getAttribute('data-baseUrl');
 let endpoint = rootElement.getAttribute('data-serviceUrl');
 
-let recordComponentsMap = {
-  "DatasetRecordClasses.DatasetRecordClass": DatasetRecord
-};
-
-Wdk.flux.components.Record.wrapComponent(function(Record) {
-  let RecordComponentResolver =  React.createClass({
-    render() {
-      let Component = recordComponentsMap[this.props.recordClass.fullName] || Record;
-      return (
-        <Component {...this.props}/>
-      );
-    }
-  });
-  return RecordComponentResolver;
-});
-
 Wdk.flux.components.AnswerTableCell.wrapComponent(function(AnswerTableCell) {
-  return React.createClass({
+  let ApiAnswerTableCell = React.createClass({
     render() {
       let cell = <AnswerTableCell {...this.props}/>;
 
@@ -43,6 +27,7 @@ Wdk.flux.components.AnswerTableCell.wrapComponent(function(AnswerTableCell) {
       return cell;
     }
   });
+  return ApiAnswerTableCell;
 });
 
 let TranscriptList = React.createClass({
@@ -78,30 +63,59 @@ let TranscriptList = React.createClass({
 
 });
 
-Wdk.flux.components.RecordNavigationSection.wrapComponent(function(RecordNavigationSection) {
-  return React.createClass({
-    componentDidMount() {
-      this.fetchTranscripts(this.props);
-    },
-
-    componentWillReceiveProps(nextProps) {
-      if (this.props.record !== nextProps.record) {
+Wdk.flux.components.Record.wrapComponent(function(Record) {
+  let TranscriptRecord = React.createClass({
+      componentDidMount() {
+        this.setStateFromProps(this.props);
+        this.fetchTranscripts(this.props);
+      },
+      componentWillReceiveProps(nextProps) {
+        this.setStateFromProps(nextProps);
         this.fetchTranscripts(nextProps);
+      },
+      setStateFromProps(props) {
+        if ('GeneTranscripts' in props.record.tables) {
+          this.setState(props);
+        }
+      },
+      fetchTranscripts(props) {
+        if (!('GeneTranscripts' in props.record.tables)) {
+          let { recordClass } = props;
+          let spec = {
+            primaryKey: props.record.id,
+            attributes: [],
+            tables: [ 'GeneTranscripts' ]
+          };
+          props.recordActions.fetchRecordDetails(recordClass.fullName, spec);
+        }
+      },
+      render() {
+        if (this.state === null) return null;
+        return (
+          <Record {...this.state}/>
+        );
       }
-    },
+  });
 
-    fetchTranscripts(props) {
-      let { recordClass } = props;
-      if (recordClass.fullName == 'TranscriptRecordClasses.TranscriptRecordClass') {
-        let spec = {
-          primaryKey: props.record.id,
-          attributes: [],
-          tables: [ 'GeneTranscripts' ]
-        };
-        props.recordActions.fetchRecordDetails(recordClass.fullName, spec);
+  let ApiRecord = React.createClass({
+    render() {
+      let { recordClass } = this.props;
+      switch(recordClass.fullName) {
+        case 'DatasetRecordClasses.DatasetRecordClass':
+          return <DatasetRecord {...this.props}/>
+        case 'TranscriptRecordClasses.TranscriptRecordClass':
+          return <TranscriptRecord {...this.props}/>
+        default:
+          return <Record {...this.props}/>
       }
-    },
+    }
+  });
 
+  return ApiRecord;
+});
+
+Wdk.flux.components.RecordNavigationSection.wrapComponent(function(RecordNavigationSection) {
+  let ApiRecordNavigationSection = React.createClass({
     render() {
       let { recordClass, categories } = this.props;
       if (recordClass.fullName == 'TranscriptRecordClasses.TranscriptRecordClass') {
@@ -124,8 +138,87 @@ Wdk.flux.components.RecordNavigationSection.wrapComponent(function(RecordNavigat
       }
       return <RecordNavigationSection {...this.props}/>
     }
+  });
+  return ApiRecordNavigationSection;
+});
+
+Wdk.flux.components.RecordMainSection.wrapComponent(function(RecordMainSection) {
+  let ApiRecordMainSection = React.createClass({
+
+    render() {
+      let { recordClass, categories, depth = 1 } = this.props;
+
+      if (recordClass.fullName == 'TranscriptRecordClasses.TranscriptRecordClass' && depth == 1) {
+        let uncategorized = categories.find(c => c.name === 'uncategorized');
+        categories = categories.filter(c => c !== uncategorized);
+        return(
+          <div>
+            {categories.map(this.renderCategory)}
+          </div>
+        );
+      }
+
+      return (
+        <RecordMainSection {...this.props} categories={categories}/>
+      );
+    },
+
+    renderCategory(category) {
+      if (category.name == 'gene_parent') {
+        return this.renderGeneCategory(category);
+      }
+      if (category.name == 'trans_parent') {
+        return this.renderTransCategory(category);
+      }
+      return (
+        <section id={category.name} key={category.name}>
+          <h1>{category.displayName}</h1>
+          <RecordMainSection {...this.props} categories={[category]}/>
+        </section>
+      );
+    },
+
+    renderGeneCategory(category) {
+      return (
+        <section id={category.name} key={category.name}>
+          <RecordMainSection {...this.props} categories={category.subCategories}/>
+        </section>
+      );
+    },
+
+    renderTransCategory(category) {
+      let { recordClass, record } = this.props;
+      return (
+        <div id={category.name} key={category.name}>
+          {this.props.record.tables.GeneTranscripts.map(row => {
+            let { transcript_id } = row;
+            let isActive = transcript_id === record.id.source_id;
+            let query = Object.assign({}, record.id, { source_id: transcript_id });
+            let params = { class: recordClass.fullName };
+            return isActive ? (
+              <section key={transcript_id}>
+                <h1 style={{ marginBottom: '1em' }} className="wdk-Record-sectionHeader">
+                  {'Transcript ' + transcript_id}
+                </h1>
+                <RecordMainSection {...this.props} categories={category.subCategories}/>
+              </section>
+            ) : (
+              <section key={transcript_id}>
+                <h1 style={{ opacity: '0.6', marginBottom: '1em' }} className="wdk-Record-sectionHeader">
+                  <Link to="record" params={params} query={query} style={{ color: 'inherit' }}>
+                    {'Transcript ' + transcript_id}
+                  </Link>
+                </h1>
+              </section>
+            );
+          })}
+        </div>
+      );
+    }
 
   });
+
+  return ApiRecordMainSection;
 });
 
 window._app = Wdk.flux.createApplication({
