df4a71dd78f2878aac9abb4c06246e4867ab69f4
Ja’Shon Cade <jcade@upenn.edu>
2015-09-03 18:26:59 +0000

    added Injector and graph for Llinas Microarray decay experiment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@69678 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/LinePlot.pm
+++ View/lib/perl/GraphPackage/LinePlot.pm
@@ -802,6 +802,30 @@ sub new {
    return $self;
 }
 
+#--------------------------------------------------------------------------------
+
+package ApiCommonWebsite::View::GraphPackage::LinePlot::MRNADecay;
+use base qw( ApiCommonWebsite::View::GraphPackage::LinePlot );
+use strict;
+
+sub new {
+  my $class = shift; 
+   my $self = $class->SUPER::new(@_);
+
+   my $id = $self->getId();
+
+   $self->setDefaultYMax(4);
+   $self->setDefaultYMin(0);
+   $self->setYaxisLabel('Expression Values');
+
+   $self->setPartName('exprn_val');
+   $self->setPlotTitle("Expression Values - $id");
+
+   $self->setMakeYAxisFoldInduction(0);
+   $self->setIsLogged(0);
+
+   return $self;
+}
 
 #--------------------------------------------------------------------------------
 
