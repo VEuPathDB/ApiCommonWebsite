2dbfeb2167e4550d6a9a77c6476cb091f6e83a0e
Dave Falke <dmfalke@gmail.com>
2015-09-18 14:55:29 +0000

    add comments and remove unused code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@69932 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/application.js
+++ Site/webapp/wdkCustomization/js/application.js
@@ -1,44 +1,42 @@
 import Wdk from 'wdk';
+
+// Import custom components
 import {
-  DatasetRecord,
-  Tooltip
+  DatasetRecord
 } from './records/DatasetRecordClasses.DatasetRecordClass';
 
-let recordComponentsMap = {
-  "DatasetRecordClasses.DatasetRecordClass": DatasetRecord
-};
 
+// Customize the Record component
 Wdk.client.components.Record.wrapComponent(function(Record) {
+  // Map record class names to custom components
+  function recordComponent(recordClassName) {
+    switch (recordClassName) {
+      case 'DatasetRecordClasses.DatasetRecordClass':
+        return DatasetRecord;
+
+      default:
+        return Record;
+    }
+  }
+
+  // This React component will delegate to custom components defined in the
+  // Object defined above.
   let RecordComponentResolver =  React.createClass({
     render() {
-      let Component = recordComponentsMap[this.props.recordClass.fullName] || Record;
+      let Component = recordComponent(this.props.recordClass.fullName);
       return (
         <Component {...this.props}/>
       );
     }
   });
+
   return RecordComponentResolver;
 });
 
-// Wdk.client.components.AnswerTableCell.wrapComponent(function(AnswerTableCell) {
-//   return React.createClass({
-//     render() {
-//       let cell = <AnswerTableCell {...this.props}/>;
-// 
-//       if (this.props.recordClass === "DatasetRecordClasses.DatasetRecordClass"
-//          && this.props.attribute.name === "primary_key") {
-//         return (
-//           <Tooltip text={this.props.record.attributes.description.value} witdh={this.props.width}>
-//             {cell}
-//           </Tooltip>
-//         );
-//       }
-// 
-//       return cell;
-//     }
-//   });
-// });
 
+// Bootstrap the WDK client application
+
+// getApiClientConfig() is defined in /client/index.jsp
 let config = window.getApiClientConfig();
 let app = window._app = Wdk.client.createApplication({
   rootUrl: config.rootUrl,
