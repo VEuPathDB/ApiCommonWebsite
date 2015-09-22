87598537d07b4a62080a1ebbf87885b58fd3b98e
Steve Fischer <sfischer@upenn.edu>
2015-09-22 18:46:39 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@69964 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/view/MissingTranscriptsViewHandler.java
@@ -0,0 +1,66 @@
+package org.apidb.apicommon.model.view;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gusdb.wdk.controller.summary.ResultTablePaging;
+import org.gusdb.wdk.controller.summary.SummaryTableUpdateProcessor;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.answer.AnswerValueAttributes;
+import org.gusdb.wdk.model.answer.SummaryViewHandler;
+import org.gusdb.wdk.model.jspwrap.AnswerValueBean;
+import org.gusdb.wdk.model.jspwrap.StepBean;
+import org.gusdb.wdk.model.jspwrap.UserBean;
+import org.gusdb.wdk.model.record.attribute.AttributeField;
+import org.gusdb.wdk.model.user.Step;
+import org.gusdb.wdk.model.user.User;
+
+public class MissingTranscriptsViewHandler implements SummaryViewHandler {
+
+	private static final String USER_PREFERENCE_SUFFIX = "_missingTranscriptsView";
+	  private static final String MISSING_TRANSCRIPTS_STEP = "missingTranscriptsStep";
+	@Override
+	public Map<String, Object> process(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
+			throws WdkModelException, WdkUserException {
+
+		// get answer from Transcripts step
+		UserBean userBean = new UserBean(user);
+		StepBean stepBean = new StepBean(userBean, step);
+		AnswerValueBean answer = stepBean.getAnswerValue();
+
+		// get new step, to make result view from
+		// use original step id, so that state is in this view is associated with it
+		Step newStep = new Step(wdkModel.getStepFactory(), user, step.getStepId()); 
+		
+		newStep.setQuestionName("InternalQuestions.GenesByMissingTranscriptsTransform");
+		Map<String, String> paramValues = new HashMap<String, String>();
+		paramValues.put("gene_result", "" + step.getStepId());
+		newStep.setParamValues(paramValues);
+		
+	    // override attributes so they are remembered in the step using the suffix
+	    AnswerValueAttributes attributes = answer.getAnswerValue().getAttributes();
+	    AttributeField pkField = stepBean.getQuestion().getRecordClass()
+	        .getPrimaryKeyAttribute().getPrimaryKeyAttributeField();
+	    Map<String, AttributeField> summaryFields = AnswerValueAttributes
+	        .buildSummaryAttributeFieldMap(user, step.getQuestion(), USER_PREFERENCE_SUFFIX, pkField);
+	    attributes.overrideSummaryAttributeFieldMap(summaryFields);
+
+		Map<String, Object> model = ResultTablePaging.processPaging(
+	            parameters, stepBean.getQuestion(), userBean, answer);
+
+	    // pass the new step to the JSP to be rendered instead of the normal step
+	    model.put(MISSING_TRANSCRIPTS_STEP, stepBean);
+
+	    return model;
+	}
+
+	@Override
+	public String processUpdate(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
+			throws WdkModelException, WdkUserException {
+		return SummaryTableUpdateProcessor.processUpdates(step, parameters, user, wdkModel, USER_PREFERENCE_SUFFIX);
+
+	}
+
+}
