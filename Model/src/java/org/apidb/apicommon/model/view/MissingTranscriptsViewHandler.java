28ff0853f6b9986710e590027010f4274823ad48
Ryan Doherty <tech@conical.org>
2015-09-25 02:57:15 +0000

    format file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@69996 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/view/MissingTranscriptsViewHandler.java
+++ Model/src/java/org/apidb/apicommon/model/view/MissingTranscriptsViewHandler.java
@@ -23,49 +23,49 @@ public class MissingTranscriptsViewHandler implements SummaryViewHandler {
   private static final String MISSING_TRANSCRIPTS_STEP = "missingTranscriptsStep";
   private static final String TRANSCRIPT_ID_FIELD = "source_id";
 
-	@Override
-	public Map<String, Object> process(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
-			throws WdkModelException, WdkUserException {
+  @Override
+  public Map<String, Object> process(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
+      throws WdkModelException, WdkUserException {
 
-	  // get answer from Transcripts step
-	  UserBean userBean = new UserBean(user);
-	  StepBean stepBean = new StepBean(userBean, step);
-	  AnswerValueBean answer = stepBean.getAnswerValue();
+    // get answer from Transcripts step
+    UserBean userBean = new UserBean(user);
+    StepBean stepBean = new StepBean(userBean, step);
+    AnswerValueBean answer = stepBean.getAnswerValue();
 
-	  // get new step, to make result view from
-	  // use original step id, so that state is in this view is associated with it
-	  Step newStep = new Step(wdkModel.getStepFactory(), user, step.getStepId()); 
-	  newStep.setInMemoryOnly(true);
-		
-	  newStep.setQuestionName("InternalQuestions.GenesByMissingTranscriptsTransform");
-	  Map<String, String> paramValues = new HashMap<String, String>();
-	  paramValues.put("gene_result", "" + step.getStepId());
-	  newStep.setParamValues(paramValues);
-		
-	  // override attributes so they are remembered in the step using the suffix
-	  AnswerValueAttributes attributes = answer.getAnswerValue().getAttributes();
-	  //	  AttributeField pkField = stepBean.getQuestion().getRecordClass()
-	  //	    .getPrimaryKeyAttribute().getPrimaryKeyAttributeField();
+    // get new step, to make result view from
+    // use original step id, so that state is in this view is associated with it
+    Step newStep = new Step(wdkModel.getStepFactory(), user, step.getStepId());
+    newStep.setInMemoryOnly(true);
+
+    newStep.setQuestionName("InternalQuestions.GenesByMissingTranscriptsTransform");
+    Map<String, String> paramValues = new HashMap<String, String>();
+    paramValues.put("gene_result", "" + step.getStepId());
+    newStep.setParamValues(paramValues);
+
+    // override attributes so they are remembered in the step using the suffix
+    AnswerValueAttributes attributes = answer.getAnswerValue().getAttributes();
     AttributeField pkField = step.getQuestion().getRecordClass().getAttributeFieldMap().get(TRANSCRIPT_ID_FIELD);
-	  Map<String, AttributeField> summaryFields = AnswerValueAttributes
-	    .buildSummaryAttributeFieldMap(user, step.getQuestion(), USER_PREFERENCE_SUFFIX, pkField);
-	  attributes.overrideSummaryAttributeFieldMap(summaryFields);
+    Map<String, AttributeField> summaryFields = AnswerValueAttributes.buildSummaryAttributeFieldMap(user,
+        step.getQuestion(), USER_PREFERENCE_SUFFIX, pkField);
+    attributes.overrideSummaryAttributeFieldMap(summaryFields);
 
-	  StepBean newStepBean = new StepBean(userBean, newStep);
+    StepBean newStepBean = new StepBean(userBean, newStep);
 
-	  Map<String, Object> model = ResultTablePaging.processPaging(parameters, newStepBean.getQuestion(), userBean, newStepBean.getAnswerValue());
+    Map<String, Object> model = ResultTablePaging.processPaging(parameters, newStepBean.getQuestion(),
+        userBean, newStepBean.getAnswerValue());
 
-	  // pass the new step to the JSP to be rendered instead of the normal step
-	  model.put(MISSING_TRANSCRIPTS_STEP,  newStepBean);
+    // pass the new step to the JSP to be rendered instead of the normal step
+    model.put(MISSING_TRANSCRIPTS_STEP, newStepBean);
 
-	  return model;
-	}
+    return model;
+  }
 
-	@Override
-	public String processUpdate(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
-			throws WdkModelException, WdkUserException {
-		return SummaryTableUpdateProcessor.processUpdates(step, parameters, user, wdkModel, USER_PREFERENCE_SUFFIX);
+  @Override
+  public String processUpdate(Step step, Map<String, String[]> parameters, User user, WdkModel wdkModel)
+      throws WdkModelException, WdkUserException {
+    return SummaryTableUpdateProcessor.processUpdates(step, parameters, user, wdkModel,
+        USER_PREFERENCE_SUFFIX);
 
-	}
+  }
 
 }
