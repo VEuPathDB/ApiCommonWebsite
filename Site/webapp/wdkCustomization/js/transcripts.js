8186f458b3b37e6e2343baae028dac28e4f54949
Cristina Aurrecoechea <aurreco@uga.edu>
2015-10-21 21:07:13 +0000

    latest behavior, to be reviewed in next UI meeting: - leaf step: shows icon and warning (transform) when missing transcripts - boolean step: (as it was) shows icon when either YN/NY/NN > 0
    verbiage needs work (not found vs not returned vs divergent) but consistent in using uppercase
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@70381 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/transcripts.js
+++ Site/webapp/wdkCustomization/js/transcripts.js
@@ -52,7 +52,7 @@ wdk.namespace('eupathdb.transcripts', function(ns, $) {
           $filter.css('display', 'block');
           // icon in tab
           if ( $("i#tr-warning").length == 0 ){
-            $( "li#transcript-view a span" ).append( $( "<i id='tr-warning' style='color: #0039FF;' title='This combined result contains transcripts that were not returned by one or both input searches.' class='fa fa-lg fa-exclamation-circle'></i>" ) );
+            $( "li#transcript-view a span" ).append( $( "<i id='tr-warning' style='color: #0039FF;' title='This combined result contains Transcripts that were not returned by one or both input searches.' class='fa fa-lg fa-exclamation-circle'></i>" ) );
           }
           // do not show warning sentence in genes view
           if ( $("div#genes").parent().css('display') != 'none'){
