03cf654213cad6fa719bf8f66a5219d7fa06e8c4
Ryan Doherty <tech@conical.org>
2015-10-21 17:30:16 +0000

    Add basic Shiny app 'framework'
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/prism_06252014@70366 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ View/lib/R/shiny/apps/transcriptLengthHistogram/config.R
@@ -0,0 +1,3 @@
+webappPort <- 7000
+fetchStyle <- "disk"
+dataStorageDir <- "/Users/rdoherty/work/tmpData/stepAnalysisJobs"
---
+++ View/lib/R/shiny/lib/wdkDataset.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+# A function that fetches the passed contextHash or ID of a
+# WDK step analysis, loads the data file output into a dataset,
+# and returns it.  If fetchStyle is "disk", file is read
+# directly from the step analysis storage dir (passed in). If
+# fetchStyle is "url", data is read from URL.
+
+getWdkDataset <- function(session, fetchStyle, dataStorageDir="") {
+
+  query = parseQueryString(session$clientData$url_search)
+
+  if (fetchStyle == "disk") {
+    contextHash = get("contextHash", query)
+    validate(
+      need(contextHash != "", "Must pass a contextHash query parameter"),
+      need(grepl("..",contextHash), "Hash must be sent alone")
+    )
+    dataFile <- paste0(dataStorageDir, "/", contextHash, "/data.tab")
+    print(paste0("Will read from: ", dataFile), stderr())
+    read.table(dataFile, sep="\t", header=FALSE)
+  }
+  else {
+    fetchUrl = get("dataUrl", query)
+    validate(
+        need(fetchUrl != "", "Must pass a dataUrl query paramenter")
+    )
+    print(paste0("Will read from: ", fetchUrl), stderr())
+    read.table(fetchUrl, sep="\t", header=FALSE)
+  }
+}
