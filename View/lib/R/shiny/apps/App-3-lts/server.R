385b5c7c0464a9430462e73220de40887cfb7352
sane <sane@_.com>
2015-10-30 07:21:19 +0000

    Refactored code for LTS
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/prism_06252014@70503 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/App-3-lts/server.R
+++ View/lib/R/shiny/apps/App-3-lts/server.R
@@ -1,7 +1,30 @@
 library(zoo)
-shinyServer(
-  function(input,output){
-      output$LTSChart <-renderChart({
+require(rCharts)
+require(reshape2)
+source("../../lib/wdkDataset.R")
+source("config.R")
+
+shinyServer(function(input, output, session) {
+
+  datasetFetcher <- reactive({
+    getWdkDataset(session, fetchStyle, FALSE, dataStorageDir)
+  })
+
+  output$LTSChart <- renderChart2({
+
+    LTS <- datasetFetcher()
+ 
+    names(LTS) <- substr(names(LTS),3,50) #Remove preceeding 'x.'
+    names(LTS) = gsub("\\.", "", names(LTS)) #Remove remaining periods
+   
+#coerce variables to required type:
+LTS$GeographicLocation <- as.factor(LTS$GeographicLocation)
+
+LTS.S <- subset(LTS, AnophelesCollectedtotal >0, select = c("DateOfVisit","AnophelesCollectedtotal","GeographicLocation",
+                                                            "TotalAnophelesPositive","TotalAnophelesTested","Parous",
+                                                            "Nulliparous","AFunestus","AGambiae","OtherAnopheles","CollectionBarcode"))
+      LTS.M <- melt(LTS.S, id.vars =c("DateOfVisit","CollectionBarcode","GeographicLocation") )
+      LTS.D <- dcast(LTS.M,DateOfVisit+GeographicLocation~variable, fun.aggregate = sum)
       x <-input$pvar
       y <- input$yvar
       g <- input$group
@@ -10,7 +33,8 @@ shinyServer(
       #sort dataframe by MonthYear
       d <- LTS.D[order(as.yearmon(LTS.D$DateOfVisit, "%y-%b")),]
       p1 <- rPlot(y, x, data = d, type = 'bar', color ='GeographicLocation', group='GeographicLocation' ) #Plot PD as it depends on MonthYear
-      #p1$guides(x = list(title = "", ticks = list(ym)))
+      str(p1)  
+   #p1$guides(x = list(title = "", ticks = list(ym)))
       p1$addParams(dom = 'LTSChart')
       return(p1)
     })
--- View/lib/R/shiny/apps/App-3-lts/ui.R
+++ View/lib/R/shiny/apps/App-3-lts/ui.R
@@ -1,4 +1,5 @@
 #Declare Shiny UI
+require(rCharts)
 shinyUI(
   #pageWithSidebar, has three components - head, side, main panels
   pageWithSidebar(
@@ -8,7 +9,7 @@ shinyUI(
       selectInput(inputId="pvar" ,label="Plot",choices = c("AnophelesCollectedtotal","TotalAnophelesPositive","TotalAnophelesTested","Parous",
                                          "Nulliparous","AFunestus","AGambiae","OtherAnopheles")),
       selectInput(inputId="yvar", label="against", choices = ("DateOfVisit")),
-      selectInput(inputId="facet",label="Facet",choices = ("GeographicLocation"), selected = ""),
+#      selectInput(inputId="facet",label="Facet",choices = ("GeographicLocation"), selected = ""),
       selectInput(inputId="group",label="Group", choices = ("GeographicLocation"), selected = "")
     ),
     mainPanel(
