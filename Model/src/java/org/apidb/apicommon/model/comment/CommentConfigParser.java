1ce7b44216282cfa164672a20496e24b71b30bc6
Ryan Doherty <tech@conical.org>
2015-11-13 19:40:13 +0000

    #21499: Greatly improve XML utilities API
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@70689 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/comment/CommentConfigParser.java
+++ Model/src/java/org/apidb/apicommon/model/comment/CommentConfigParser.java
@@ -5,52 +5,45 @@ import java.net.URL;
 
 import org.apache.commons.digester.Digester;
 import org.gusdb.fgputil.xml.XmlParser;
+import org.gusdb.fgputil.xml.XmlValidator;
 import org.gusdb.wdk.model.WdkModelException;
 import org.xml.sax.SAXException;
 
 /**
  * @author xingao
- * 
  */
 public class CommentConfigParser extends XmlParser {
 
-    private final String gusHome;
+  private final String _gusHome;
+  private final Digester _digester;
 
-    public CommentConfigParser(String gusHome) {
-        this.gusHome = gusHome;
+  public CommentConfigParser(String gusHome) {
+    _gusHome = gusHome;
+    _digester = configureDigester();
+  }
+
+  public CommentConfig parseConfig(String projectId) throws WdkModelException, CommentModelException {
+    try {
+      // validate the configuration file
+      XmlValidator validator = new XmlValidator(_gusHome + "/lib/rng/comment-config.rng");
+      URL configUrl = makeURL(_gusHome + "/config/" + projectId + "/comment-config.xml");
+      if (!validator.validate(configUrl)) {
+        throw new WdkModelException("Validation failed: " + configUrl.toExternalForm());
+      }
+      return (CommentConfig) _digester.parse(configUrl.openStream());
     }
-
-    /**
-     * @param projectId
-     * @return the relative path from $GUS_HOME to the config file
-     */
-    public String getConfigFile(String projectId) {
-        return "config/" + projectId + "/comment-config.xml";
+    catch (IOException | SAXException ex) {
+      throw new CommentModelException(ex);
     }
+  }
 
-    public CommentConfig parseConfig(String projectId) throws WdkModelException, CommentModelException {
-        try {
-          // validate the configuration file
-          configureValidator(gusHome + "/lib/rng/comment-config.rng");
-          URL configURL = makeURL(gusHome + "/" + getConfigFile(projectId));
-          if (!validate(configURL)) {
-            throw new WdkModelException("Validation failed: " + configURL.toExternalForm());
-          }
-          return (CommentConfig) getDigester().parse(configURL.openStream());
-        }
-        catch (IOException | SAXException ex) {
-          throw new CommentModelException(ex);
-        }
-    }
+  private static Digester configureDigester() {
+    Digester digester = new Digester();
+    digester.setValidating(false);
 
-    @Override
-    protected Digester configureDigester() {
-        Digester digester = new Digester();
-        digester.setValidating(false);
+    digester.addObjectCreate("commentConfig", CommentConfig.class);
+    digester.addSetProperties("commentConfig");
 
-        digester.addObjectCreate("commentConfig", CommentConfig.class);
-        digester.addSetProperties("commentConfig");
-
-        return digester;
-    }
+    return digester;
+  }
 }
