a1ed3b7c18323770106cc3672c770f40b8bb2dd0
Ja’Shon Cade <jcade@upenn.edu>
2015-12-20 07:56:38 +0000

    commented out bar graphs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@71368 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Caro/Ribosome.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Caro/Ribosome.pm
@@ -40,57 +40,58 @@ sub init {
   
   my $line = ApiCommonWebsite::View::GraphPackage::LinePlot->new(@_);
   $line->setProfileSets([$profileSets->[0],$profileSets->[2],$profileSets->[4],$profileSets->[6],]);
-  $line->setPartName('rpkm_line');
   $line->setYaxisLabel('RPKM');
   $line->setPointsPch($pch);
   $line->setColors([$colors->[0], $colors->[1],$colors->[2], $colors->[3],]);
   $line->setArePointsLast(1);
   $line->setElementNameMarginSize(6);
   $line->setXaxisLabel('Life Cycle Stage');
-  $line->setPartName('rpkm_time_course');
+  $line->setPartName('rpkm');
   $line->setSampleLabels($sampleLabels);
   my $id = $self->getId();
   $line->setPlotTitle("RPKM - $id - Time Course");
 
+# Removed Stack Bar plots based on request from Outreach Team.
 
-  my $partName;
-  my $stackedRibosomeSense = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stackedRibosomeSense->setProfileSets([$profileSets->[0], $profileSets->[1]]);
-  $stackedRibosomeSense->setColors([$colors->[0],$colors->[4]] );
-  $stackedRibosomeSense->setPartName('rpkm_ribosome_sense');
-  my $basePlotTitle = $stackedRibosomeSense->getPlotTitle;
-  $stackedRibosomeSense->setPlotTitle($basePlotTitle." - sense - Ribosome");
-  $stackedRibosomeSense->setSampleLabels($sampleLabels);
-  $stackedRibosomeSense->setElementNameMarginSize(6);
-
-
-  my $stackedRibosomeAnti = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stackedRibosomeAnti->setProfileSets([$profileSets->[2], $profileSets->[3]]);
-  $stackedRibosomeAnti->setColors([$colors->[1],$colors->[4]] );
-  $stackedRibosomeAnti->setPartName('rpkm_ribosome_anti');
-  my $basePlotTitle = $stackedRibosomeAnti->getPlotTitle;
-  $stackedRibosomeAnti->setPlotTitle($basePlotTitle." - antisense - Ribosome");
-  $stackedRibosomeAnti->setSampleLabels($sampleLabels);
-  $stackedRibosomeAnti->setElementNameMarginSize(6);
+    # my $partName;
+    # my $stackedRibosomeSense = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
+    # $stackedRibosomeSense->setProfileSets([$profileSets->[0], $profileSets->[1]]);
+    # $stackedRibosomeSense->setColors([$colors->[0],$colors->[4]] );
+    # $stackedRibosomeSense->setPartName('rpkm_ribosome_sense');
+    # my $basePlotTitle = $stackedRibosomeSense->getPlotTitle;
+    # $stackedRibosomeSense->setPlotTitle($basePlotTitle." - sense - Ribosome");
+    # $stackedRibosomeSense->setSampleLabels($sampleLabels);
+    # $stackedRibosomeSense->setElementNameMarginSize(6);
 
 
-  my $stackedSteadySense = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stackedSteadySense->setProfileSets([$profileSets->[4], $profileSets->[5]]);
-  $stackedSteadySense->setColors([$colors->[2],$colors->[4]] );
-  $stackedSteadySense->setPartName('rpkm_steady_sense');
-  my $basePlotTitle = $stackedSteadySense->getPlotTitle;
-  $stackedSteadySense->setPlotTitle($basePlotTitle." - sense - Steady State");
-  $stackedSteadySense->setSampleLabels($sampleLabels);
-  $stackedSteadySense->setElementNameMarginSize(6);
+    # my $stackedRibosomeAnti = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
+    # $stackedRibosomeAnti->setProfileSets([$profileSets->[2], $profileSets->[3]]);
+    # $stackedRibosomeAnti->setColors([$colors->[1],$colors->[4]] );
+    # $stackedRibosomeAnti->setPartName('rpkm_ribosome_anti');
+    # my $basePlotTitle = $stackedRibosomeAnti->getPlotTitle;
+    # $stackedRibosomeAnti->setPlotTitle($basePlotTitle." - antisense - Ribosome");
+    # $stackedRibosomeAnti->setSampleLabels($sampleLabels);
+    # $stackedRibosomeAnti->setElementNameMarginSize(6);
 
-  my $stackedSteadyAnti = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
-  $stackedSteadyAnti->setProfileSets([$profileSets->[6], $profileSets->[7]]);
-  $stackedSteadyAnti->setColors([$colors->[3],$colors->[4]] );
-  $stackedSteadyAnti->setPartName('rpkm_steady_anti');
-  my $basePlotTitle = $stackedSteadyAnti->getPlotTitle;
-  $stackedSteadyAnti->setPlotTitle($basePlotTitle." - antisense - Steady State");
-  $stackedSteadyAnti->setSampleLabels($sampleLabels);
-  $stackedSteadyAnti->setElementNameMarginSize(6);
+
+    # my $stackedSteadySense = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
+    # $stackedSteadySense->setProfileSets([$profileSets->[4], $profileSets->[5]]);
+    # $stackedSteadySense->setColors([$colors->[2],$colors->[4]] );
+    # $stackedSteadySense->setPartName('rpkm_steady_sense');
+    # my $basePlotTitle = $stackedSteadySense->getPlotTitle;
+    # $stackedSteadySense->setPlotTitle($basePlotTitle." - sense - Steady State");
+    # $stackedSteadySense->setSampleLabels($sampleLabels);
+    # $stackedSteadySense->setElementNameMarginSize(6);
+
+    # my $stackedSteadyAnti = ApiCommonWebsite::View::GraphPackage::BarPlot::RNASeqStacked->new(@_);
+    # $stackedSteadyAnti->setProfileSets([$profileSets->[6], $profileSets->[7]]);
+    # $stackedSteadyAnti->setColors([$colors->[3],$colors->[4]] );
+    # $stackedSteadyAnti->setPartName('rpkm_steady_anti');
+    # my $basePlotTitle = $stackedSteadyAnti->getPlotTitle;
+    # $stackedSteadyAnti->setPlotTitle($basePlotTitle." - antisense - Steady State");
+    # $stackedSteadyAnti->setSampleLabels($sampleLabels);
+    # $stackedSteadyAnti->setElementNameMarginSize(6);
+
 
   my $percentile = ApiCommonWebsite::View::GraphPackage::LinePlot::Percentile->new(@_);
   $percentile->setProfileSets([$percentileSets->[0],$percentileSets->[1],$percentileSets->[2],$percentileSets->[3],]);
@@ -98,13 +99,17 @@ sub init {
   $percentile->setArePointsLast(1);
   $percentile->setPartName('percentile');
   $percentile->setXaxisLabel('Hours post infection');
+  my $basePlotTitle = $percentile->getPlotTitle;
   $percentile->setPlotTitle($basePlotTitle." - percentile");
   $percentile->setSampleLabels($sampleLabels);
   $percentile->setElementNameMarginSize(6);
 
-  $self->setGraphObjects($line, $stackedRibosomeSense, $stackedRibosomeAnti,$stackedSteadySense,$stackedSteadyAnti, $percentile,);
+  #$self->setGraphObjects($line, $stackedRibosomeSense, $stackedRibosomeAnti,$stackedSteadySense,$stackedSteadyAnti, $percentile,);
+
+  $self->setGraphObjects($line, $percentile);
 
   return $self;
+
 }
 
 1;
