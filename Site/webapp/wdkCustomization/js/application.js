0ab7d6fbf206d8b45155fe27a93f480a1298a71e
Dave Falke <dmfalke@gmail.com>
2015-12-21 01:40:57 +0000

    update model and ui for expression graph table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@71375 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/application.js
+++ Site/webapp/wdkCustomization/js/application.js
@@ -116,51 +116,135 @@ let Footer = React.createClass({
   }
 });
 
+// Renders an Expression graph with the provided rowData.
+// rowData comes from an ExpressionTable record table.
+//
+// rowData will include the available gene ids (graph_ids), but the available
+// graphs for the dataset (visible_parts) has to be fetched from dataPlotter.pl.
+// This means that when we get new rowData, we first have to make a request for
+// the available parts, and then we can update the state of the Component. This
+// flow will ensure that we have a consistent state when rendering.
 let ExpressionGraph = React.createClass({
 
   getInitialState() {
     return {
-      graphId: this.props.rowData.graph_ids.split(',')[0],
-      visibleParts: this.props.rowData.visible_parts.split(',').slice(0, 1),
-      loading: true
+      loading: true,
+      imgError: false,
+      details: null,
+      graphId: null,
+      visibleParts: null
     };
   },
 
+  componentDidMount() {
+    this.setStateFromProps(this.props);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.rowData !== nextProps.rowData) {
+      this.setStateFromProps(nextProps);
+    }
+  },
+
+  // TODO Better name
+  setStateFromProps(props) {
+    this.setState({ loading: true });
+
+    let { rowData } = props;
+    let graphIds = rowData.graph_ids.split(/\s*,\s*/);
+    let graphId = this.state.graphId || graphIds[0];
+    let baseUrl = '/cgi-bin/dataPlotter.pl?' +
+      'type=' + rowData.module + '&' +
+      'project_id=' + rowData.project_id + '&' +
+      'datasetId=' + rowData.dataset_id + '&' +
+      'template=' + (rowData.is_graph_custom === 'false' ? 1 : '') + '&' +
+      'id=' + graphId;
+
+    $.get(baseUrl + '&declareParts=1').then(partsString => {
+      let parts = partsString.split(/\s*,\s*/);
+      let visibleParts = parts.slice(0, 1);
+      this.setState({
+        imgError: false,
+        visibleParts,
+        graphId,
+        details: {
+          baseUrl,
+          parts,
+          graphIds,
+          description: rowData.description,
+          x_axis: rowData.x_axis,
+          y_axis: rowData.y_axis
+        }
+      })
+    });
+  },
+
+  setGraphId(graphId) {
+    this.setState({ graphId });
+    setStateFromProps(this.props);
+  },
+
+  renderLoading() {
+    if (this.state.loading) {
+      return (
+        <Wdk.client.Components.Loading radius={4} className="eupathdb-ExpressionGraphLoading"/>
+      );
+    }
+  },
+
+  renderImgError() {
+    if (this.state.imgError) {
+      return (
+        <div className="eupathdb-ExpressGraphErrorMessage">
+          The requested graph could not be loaded.
+        </div>
+      );
+    }
+  },
+
   render() {
-    let { rowData } = this.props;
-    let displayName = rowData.display_name;
+    if (this.state.details == null) {
+      return this.renderLoading();
+    }
 
-    let graphIds = rowData.graph_ids.split(',');
-    let visibleParts = rowData.visible_parts.split(',');
+    let { visibleParts, graphId } = this.state;
 
-    let baseUrl = '/cgi-bin/dataPlotter.pl' +
-      '?type=' + rowData.module +
-      '&project_id=' + rowData.project_id +
-      '&dataset=' + rowData.dataset_name +
-      '&template=' + (rowData.is_graph_custom === 'false' ? 1 : '') +
-      '&id=' + this.state.graphId +
-      '&vp=_LEGEND,' + this.state.visibleParts
+    let {
+      baseUrl,
+      parts,
+      graphIds,
+      description,
+      x_axis,
+      y_axis
+    } = this.state.details;
 
-    let imgUrl = baseUrl + '&fmt=png';
-    let tableUrl = baseUrl + '&fmt=table';
+    let baseUrlWithState = `${baseUrl}&id=${graphId}&vp=_LEGEND,${visibleParts}`;
+
+    let imgUrl = baseUrlWithState + '&fmt=png';
+    let tableUrl = baseUrlWithState + '&fmt=table';
 
     return (
       <div className="eupathdb-ExpressionGraphContainer">
-        {this.state.loading
-          ? <Wdk.client.Components.Loading radius={4} className="eupathdb-ExpressionGraphLoading"/>
-          : null}
+
+        {this.renderLoading()}
+
         <div className="eupathdb-ExpressionGraph">
-          <img src={imgUrl} onLoad={() => this.setState({ loading: false })}/>
+          <img
+            src={imgUrl}
+            onLoad={() => this.setState({ loading: false })}
+            onError={() => this.setState({ loading: false, imgError: true })}
+          />
+          {this.renderImgError()}
         </div>
         <div className="eupathdb-ExpressionGraphDetails">
           <h4>Description</h4>
-          <div dangerouslySetInnerHTML={{__html: rowData.description}}/>
+          <div dangerouslySetInnerHTML={{__html: description}}/>
 
           <h4>X-axis</h4>
-          <div>{rowData.x_axis}</div>
+          <div dangerouslySetInnerHTML={{__html: x_axis}}/>
 
           <h4>Y-axis</h4>
-          <div>{rowData.y_axis}</div>
+          <div dangerouslySetInnerHTML={{__html: y_axis}}/>
 
           <h4>Choose gene for which to display graph</h4>
           {graphIds.map(graphId => {
@@ -169,13 +253,13 @@ let ExpressionGraph = React.createClass({
                 <input
                   type="radio"
                   checked={graphId === this.state.graphId}
-                  onChange={() => this.setState({ graphId })}
+                  onChange={() => this.setGraphId({ graphId })}
                 /> {graphId} </label>
             );
           })}
 
           <h4>Choose graph(s) to display</h4>
-          {visibleParts.map(part => {
+          {parts.map(part => {
             return (
               <label>
                 <input
