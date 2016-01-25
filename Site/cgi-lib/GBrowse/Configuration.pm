f1119091eb62ca86316a0c4aeda8eca79baa075d
John Brestelli <jbrestell@gmail.com>
2016-01-25 17:30:18 +0000

    add method to get category path as string
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@72042 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/GBrowse/Configuration.pm
+++ Site/cgi-lib/GBrowse/Configuration.pm
@@ -11,8 +11,13 @@ use DAS::Util::SynView;
 
 use HTML::Template;
 
+use LWP::UserAgent;
+use JSON qw( decode_json );
+
 require Exporter;
 
+
+use Data::Dumper;
 umask 0;
 
 # Export Static Methods
@@ -34,6 +39,10 @@ sub new {
   bless ($self, $class);
   $self->dbh($dbh);
   $self->{dbh}{InactiveDestroy} = 1;
+
+
+
+
   return $self;
 }
 
@@ -329,4 +338,60 @@ EOL
 
   return $self->{proteomics_citation_from_ext_database_name}->{$extdb};
 }
+
+
+sub getOntologyCategoryFromTrackName {
+  my ($trackName) = @_;
+
+
+  if($self->{_ontology_category_from_track_name}->{$trackName}) {
+    return $self->{_ontology_category_from_track_name}->{$trackName};
+  }
+
+  my $ua = LWP::UserAgent->new;
+  
+  my $server_endpoint = "http://$ENV{HTTP_HOST}/$ENV{CONTEXT_PATH}/service/ontology/Categories/path";
+
+  print STDERR $server_endpoint . "\n";
+ 
+# set custom HTTP request header fields
+  my $req = HTTP::Request->new(POST => $server_endpoint);
+  $req->header('content-type' => 'application/json');
+ 
+# add POST data to HTTP request body
+  my $post_data = '{ "scope": "gbrowse" }';
+  $req->content($post_data);
+ 
+  my $resp = $ua->request($req);
+  if ($resp->is_success) {
+  my $decoded_json = decode_json( $resp->decoded_content  );
+
+  my %trackNameToPathHashRef;
+  foreach my $pathArrayRef (@$decoded_json) {
+
+    my @pathLabels;
+
+    my $lastIndex = scalar @$pathArrayRef - 1;
+
+    for(my $i = 1; $i < $lastIndex; $i ++) {
+      push @pathLabels, $pathArrayRef->[$i]->{label}->[0];
+
+    }
+
+    my $pathLabelsAsString = join(" : ", @pathLabels);
+    my $trackName = $pathArrayRef->[$lastIndex]->{name}->[0];
+    $trackNameToPathHashRef{$trackName} = $pathLabelsAsString;
+  }
+  
+  $self->{_ontology_category_from_track_name} = \%trackNameToPathHashRef;
+  }
+  else {
+    print STDERR "HTTP POST error code: ", $resp->code, "\n";
+    print STDERR "HTTP POST error message: ", $resp->message, "\n";
+  }
+
+  return $self->{_ontology_category_from_track_name}->{$trackName};
+}
+
+
 1;
