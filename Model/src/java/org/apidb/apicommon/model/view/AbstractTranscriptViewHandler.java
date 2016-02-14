ef41f5eb5971c2fb2818c66a8362a916bd1b037e
Cristina Aurrecoechea <aurreco@uga.edu>
2016-02-14 21:31:52 +0000

    gene view: removes attributes from summary attributes if related to transcripts (checking geneOrTranscript property in ontology)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@72559 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/view/AbstractTranscriptViewHandler.java
+++ Model/src/java/org/apidb/apicommon/model/view/AbstractTranscriptViewHandler.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.gusdb.fgputil.functional.TreeNode;
 import org.gusdb.wdk.model.SelectableItem;
+import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.FieldTree.NameMatchPredicate;
 import org.gusdb.wdk.model.jspwrap.StepBean;
@@ -20,7 +21,7 @@ public abstract class AbstractTranscriptViewHandler extends AltSpliceViewHandler
   };
 
   @Override
-  protected void customizeAvailableAttributeTree(Step step, TreeNode<SelectableItem> root) {
+  protected void customizeAvailableAttributeTree(Step step, TreeNode<SelectableItem> root, WdkModel model) {
     root.removeAll(new NameMatchPredicate(Arrays.asList(FIELDS_TO_REMOVE)));
   }
 
