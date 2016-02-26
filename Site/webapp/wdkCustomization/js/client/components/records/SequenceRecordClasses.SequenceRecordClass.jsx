e1d4771b7a5e5a8262a4b46c03a54e802c2d3dda
Brian Brunk <brunkb@upenn.edu>
2016-02-26 18:28:48 +0000

    added gbrowse img for sequence record
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@72992 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/records/SequenceRecordClasses.SequenceRecordClass.jsx
+++ Site/webapp/wdkCustomization/js/client/components/records/SequenceRecordClasses.SequenceRecordClass.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as Gbrowse from '../common/Gbrowse';
+
 let { ComponentUtils } = Wdk.client;
 
 export function RecordAttributionSection(props) {
@@ -8,5 +10,14 @@ export function RecordAttributionSection(props) {
       {ComponentUtils.renderAttributeValue(props.record.attributes.description)}
     </div>
   )
+}
+
+export function RecordAttribute(props) {
+    let context = Gbrowse.contexts.find(context => context.gbrowse_url === props.name);
+    if (context != null) {
+      return ( <Gbrowse.GbrowseContext {...props} context={context} /> );
+  }
+
+  return ( <props.DefaultComponent {...props}/> );
 }
 
