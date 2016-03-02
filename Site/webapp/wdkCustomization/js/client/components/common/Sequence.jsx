2719b97e49df5478b16ef4a01b7a4e4f7090f55a
Dave Falke <dmfalke@gmail.com>
2016-03-02 20:59:58 +0000

    add sequences with highlighting of introns and utrs as child rows to Sequences table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@73090 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/webapp/wdkCustomization/js/client/components/common/Sequence.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import lodash from 'lodash';
+
+export default function Sequence(props) {
+  let { highlightRegions, sequence } = props;
+
+  let sequenceChars = highlightRegions.reduce((sequenceChars, highlightRegion) => {
+    let { color, start, end } = highlightRegion;
+    return [
+      ...sequenceChars.slice(0, start - 1),
+      ...sequenceChars.slice(start - 1, end).map(c =>
+        <span style={{ backgroundColor: color }}>{c}</span>
+      ),
+      ...sequenceChars.slice(end)
+    ];
+  }, sequence.split(''));
+
+  return (
+    <pre>
+      {lodash.chunk(sequenceChars, 80).map(s => <div>{s}</div>)}
+    </pre>
+  );
+}
+
+Sequence.propTypes = {
+  /** The sequence to display **/
+  sequence: React.PropTypes.string.isRequired,
+
+  /** Regions to highlight, using 1-based indexing for start and end **/
+  highlightRegions: React.PropTypes.arrayOf(React.PropTypes.shape({
+    color: React.PropTypes.string,
+    coords: React.PropTypes.shape({
+      start: React.PropTypes.number,
+      end: React.PropTypes.number
+    })
+  }))
+};
+
+Sequence.defaultProps = {
+  highlightRegions: []
+};
