35f92c6903eb3d5961a222f48d6d9603dd6ca07d
Dave Falke <dmfalke@gmail.com>
2016-03-15 15:56:55 +0000

    add comment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@73326 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/records.js
+++ Site/webapp/wdkCustomization/js/client/components/records.js
@@ -1,3 +1,8 @@
+/**
+ * Dynamically resolve `componentName` to a React Component object by checking
+ * for a an export of the same name in the recordClass module. The recordClass
+ * module may not exist, in which case we will simply return undefined.
+ */
 export function findComponent(componentName, recordClassName) {
   try {
      let record = require('./records/' + recordClassName);
