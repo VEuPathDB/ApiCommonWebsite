df4e12112dd4fd8119d35271d56098646d42fda4
Dave Falke <dmfalke@gmail.com>
2016-03-15 15:59:20 +0000

    Remove iframes and replace with a transformed gbrowse image map. Tooltips are created using the qtip library provided by wdk. This change prevents an additional image download and script downloads from gbrowse.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@73328 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/common/Gbrowse.jsx
+++ Site/webapp/wdkCustomization/js/client/components/common/Gbrowse.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import lodash from 'lodash';
+import $ from 'jquery';
 import { Components, ComponentUtils } from 'wdk-client';
 import RemoteContent from './RemoteContent';
 
@@ -70,6 +71,7 @@ export class GbrowseContext extends ComponentUtils.PureComponent {
   render() {
     let gbrowseUrl = this.props.record.attributes[this.props.name];
     let iframeUrl = gbrowseUrl + ';width=800;embed=1';
+    let mapName = this.props.name + '_map';
     return (
       <CollapsibleSection
         id={this.props.name}
@@ -79,7 +81,8 @@ export class GbrowseContext extends ComponentUtils.PureComponent {
         isCollapsed={this.state.isCollapsed}
         onCollapsedChange={this.updateCollapsed}
       >
-        <iframe src={iframeUrl} onLoad={gbrowseOnLoad} height="500"/>
+        <img src={gbrowseUrl} useMap={'#' + mapName}/>
+        <GbrowseImageMap url={iframeUrl} name={mapName}/>
         <div>
           <a href={gbrowseUrl.replace('/gbrowse_img/', '/gbrowse/')}>View in genome browser</a>
         </div>
@@ -92,53 +95,126 @@ export class GbrowseContext extends ComponentUtils.PureComponent {
 export function ProteinContext(props) {
   let url = props.rowData.ProteinPbrowseUrl;
   let divId = props.table.name + "-" + props.rowData.transcript_id
-
+  let mapName = divId + '_map';
   return (
     <div id={divId} className="eupathdb-GbrowseContext">
-      <iframe src={`${url};width=800;embed=1;genepage=1`} onLoad={pbrowseOnLoad} height="500"/>
+      <img src={url} useMap={'#' + mapName}/>
+      <GbrowseImageMap url={url + ';width=800;embed=1;genepage=1'} name={mapName}/>
+      <div>
+        <a href={url.replace('/gbrowse_img/', '/gbrowse/')}>View in genome browser</a>
+      </div>
     </div>
   );
 }
 
-let gbrowseScripts = [ '/gbrowse/apiGBrowsePopups.js', '/gbrowse/wz_tooltip.js' ];
 
-function gbrowseOnLoad(event) {
-  let iframe = event.target;
-  setBaseTarget(iframe);
-  injectGbrowseScripts(iframe);
-  resizeIframe(iframe);
-}
+// RegExps specific to Gbrowse image maps
 
-function pbrowseOnLoad(event) {
-  let iframe = event.target;
-  setBaseTarget(iframe);
-  resizeIframe(iframe);
-}
+/** Image map mouseover regexp */
+const onMouseOverRegexp = /GBubble\.showTooltip\(event,'(\w+:)?(.*)'.*$/;
+/** Regexp to get inner html of a map element */
+const areaTagsRegexp = /<map [^>]*>([\s\S]*)<\/map>/;
 
-function injectGbrowseScripts(iframe) {
-  let gbrowseWindow = iframe.contentWindow.window;
-  let gbrowseDocumentBody = iframe.contentWindow.document.body;
 
-  gbrowseWindow.wdk = wdk;
-  gbrowseWindow.jQuery = jQuery;
+/**
+ * Helper Component that loads and parses a Gbrowse image map in order to provide
+ * custom tooltips, etc. This is needed to avoid loading Prototype.js in the
+ * page, which breaks many parts of the app in subtle ways.
+ */
+class GbrowseImageMap extends ComponentUtils.PureComponent {
+  constructor(props) {
+    super(props);
+    this.mapContainerNode = null;
+    this.xhr = null;
+    this.state = { error: null };
+  }
 
-  for (let scriptUrl of gbrowseScripts) {
-    let script = document.createElement('script');
-    script.src = scriptUrl;
-    gbrowseDocumentBody.appendChild(script);
+  componentDidMount() {
+    loadGbrowseScripts().then(() => this.loadImageMap(this.props));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.url !== nextProps.url) {
+      this.xhr && this.xhr.abort();
+      this.loadImageMap(nextProps);
+    }
+  }
+
+  loadImageMap(props) {
+    this.xhr = $.get(props.url);
+    this.xhr.promise().then(
+      data => this.handleImageMapLoad(data),
+      jqXhr => this.handleError(jqXhr)
+    );
+  }
+
+  handleImageMapLoad(data) {
+    // We need to parse the area tags from the HTML so we don't load the imgs.
+    let areaTagsMatches = data.match(areaTagsRegexp);
+    if (areaTagsMatches == null) return;
+    let areaTagsHtml = areaTagsMatches[1];
+    let container = document.createElement('div');
+    container.innerHTML = areaTagsHtml;
+    let areas = container.querySelectorAll('area[onmouseover]');
+    for (let area of areas) {
+      let matches = onMouseOverRegexp.exec(area.getAttribute('onmouseover'));
+      if (matches == null) {
+        continue;
+      }
+      let [, pragma = '', content = '' ] = matches;
+      if (pragma === 'javascript:') {
+        let contentFn = new Function('"use strict"; return ' + content.replace(/^escape\((.*)\)$/, '$1').replace(/\\/g, ''));
+        area.setAttribute('title', contentFn.call(area));
+      }
+      else if (pragma === 'url:') {
+        area.setAttribute('data-url', content);
+      }
+      else {
+        area.setAttribute('title', content);
+      }
+      area.removeAttribute('onmouseover');
+      area.onmouseover = null;
+    }
+
+    $(this.mapContainerNode)
+    .empty()
+    .append(areas)
+    .find('area')
+    .wdkTooltip({
+      position: { effect: false },
+      style: {
+        classes: 'qtip-bootstrap eupathdb-GbrowseImageMapTooltip',
+        tip: { height: 12, width: 18 }
+      }
+    });
+  }
+
+  handleError(jqXHR) {
+    let { statusText } = jqXHR;
+    if (statusText === 'abort') return;
+    let error = 'Unable to load mouseover details for tracks.';
+    this.setState({ error })
+    console.error('Error: %s. %o', error, jqXHR);
+  }
+
+  renderError() {
+    if (this.state.error) {
+      return (
+        <div style={{ color: 'red', fontStyle: 'italic', padding: '12px 0' }}>Error: {this.state.error}</div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderError()}
+        <map name={this.props.name} ref={node => this.mapContainerNode = node}/>
+      </div>
+    );
   }
 }
 
-function resizeIframe(iframe) {
-  let height = Math.max(500, iframe.contentWindow.document.body.scrollHeight + 20);
-  iframe.style.height = height + 'px';
-}
-
-function setBaseTarget(iframe) {
-  let base = iframe.contentDocument.querySelector('base');
-  if (base == null) {
-    base = document.createElement('base');
-    iframe.contentDocument.head.appendChild(base);
-  }
-  base.target = '_top';
-}
+let loadGbrowseScripts = lodash.once(() => {
+  return $.getScript('/gbrowse/apiGBrowsePopups.js');
+});
