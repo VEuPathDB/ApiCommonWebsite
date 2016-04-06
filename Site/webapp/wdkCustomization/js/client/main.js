3b79d046f268d6534d4c89915b0d57e255fa4d68
Dave Falke <dmfalke@gmail.com>
2016-04-06 19:56:00 +0000

    Use updated wdk client api.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@73828 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -3,7 +3,7 @@
 
 // TODO Remove auth_tkt from url before proceeding
 
-import { run, wrapComponents, wrapStores } from 'wdk-client';
+import { initialize, wrapComponents, wrapStores } from 'wdk-client';
 
 // Import apicomm wrappers and additional routes
 import * as componentWrappers from './componentWrappers';
@@ -18,9 +18,19 @@ wrapStores(storeWrappers);
 
 // getApiClientConfig() is defined in /client/index.jsp
 let config = window.getApiClientConfig();
-let app = window._app = run({
+
+// replace '/a/' with '/${webapp}/'
+let pathname = window.location.pathname;
+let aliasUrl = config.rootUrl.replace(/^\/[^/]+\/(.*)$/, '/a/$1');
+if (pathname.indexOf(aliasUrl) === 0) {
+  window.history.replaceState(null, '', pathname.replace(aliasUrl, config.rootUrl));
+}
+
+let app = window._app = initialize({
   rootUrl: config.rootUrl,
   endpoint: config.endpoint,
   rootElement: config.rootElement,
   applicationRoutes: routes
 });
+
+app.render();
