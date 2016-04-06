4fef989b2a13f5bff56237e30e71ed7b143a3236
Steve Fischer <sfischer@upenn.edu>
2016-04-06 20:33:01 +0000

    get user from original answer value
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@73833 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/report/TranscriptTableReporter.java
+++ Model/src/java/org/apidb/apicommon/model/report/TranscriptTableReporter.java
@@ -9,13 +9,9 @@ import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
 import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.report.AttributesColumnarReporter;
-import org.gusdb.wdk.model.user.UserFactory;
 import org.json.JSONObject;
 
 public class TranscriptTableReporter extends AttributesColumnarReporter {
-  
-  public static final String PROP_USER_ID = "userId";
-  private Integer userId = null;
   
   public static final String PROP_STEP_ID = "stepId";
   private String stepId = null;
@@ -32,19 +28,16 @@ public class TranscriptTableReporter extends AttributesColumnarReporter {
   @Override
   public void configure(JSONObject config) {
     super.configure(config);
-    userId = config.getInt(PROP_USER_ID);
     stepId = config.getString(PROP_STEP_ID);
   }
   
   @Override
   public void initialize() throws WdkModelException {
-    if (userId == null) throw new WdkModelException("Missing required reporter property: " + PROP_USER_ID);
     if (stepId == null) throw new WdkModelException("Missing required reporter property: " + PROP_STEP_ID); 
     WdkModel model = baseAnswer.getQuestion().getWdkModel();
     String xformQuestionName = "GeneRecordQuestions.GenesFromTranscripts";
     Question question = model.getQuestion(xformQuestionName);
     if (question == null) throw new WdkModelException("Can't find xform with name: " + xformQuestionName);
-    UserFactory userFactory = new UserFactory(model);
     Map<String, String> params = new HashMap<String, String>();
     String paramName = "gene_result";
     if (question.getParamMap().size() != 1
@@ -52,7 +45,7 @@ public class TranscriptTableReporter extends AttributesColumnarReporter {
       throw new WdkModelException("Expected question " + xformQuestionName + " to have exactly one parameter named " + paramName);
     params.put(paramName, stepId);
     try {
-      baseAnswer = question.makeAnswerValue(userFactory.getUser(userId), params, true, 10);
+      baseAnswer = question.makeAnswerValue(baseAnswer.getUser(), params, true, 10);
     } catch (WdkUserException e) {
       throw new WdkModelException(e);
     }
