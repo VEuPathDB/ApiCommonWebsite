0f320c9b8d96b3818d3c7de7d493dd24d97f40b7
Bindu Gajria <bindu.gajria@gmail.com>
2016-04-15 18:33:00 +0000

    fix SQL refs #22228
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@74007 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/lib/perl/GoEnrichment.pm
+++ Model/lib/perl/GoEnrichment.pm
@@ -33,7 +33,7 @@ sub getAnnotatedGenesCountBgd {
 SELECT count(distinct ga.source_id)
 FROM ApidbTuning.GoTermSummary gts, ApidbTuning.GeneAttributes ga
 where ga.taxon_id = $taxonId
-  and gts.source_id = ga.source_id
+  and gts.gene_source_id = ga.source_id
   and gts.is_not is null
   and gts.displayable_source in ($self->{sources})
 --  and gts.evidence_code in ($self->{evidCodes})
@@ -49,10 +49,10 @@ sub getAnnotatedGenesCountResult {
   my ($self, $dbh, $geneResultSql) = @_;
 
   my $sql = "
-SELECT count(distinct gts.source_id)
+SELECT count(distinct gts.gene_source_id)
 FROM ApidbTuning.GoTermSummary gts,
      ($geneResultSql) r
-where gts.source_id = r.source_id
+where gts.gene_source_id = r.source_id
   and gts.is_not is null
   and gts.displayable_source in ($self->{sources})
 --  and gts.evidence_code in ($self->{evidCodes})
@@ -70,14 +70,14 @@ sub getDataSql {
 return "
 select distinct bgd.go_id, bgdcnt, resultcnt, round(100*resultcnt/bgdcnt, 1) as pct_of_bgd, bgd.name
 from
- (SELECT gt.go_id, count(distinct gts.source_id) as bgdcnt, gt.name
+ (SELECT gt.go_id, count(distinct gts.gene_source_id) as bgdcnt, gt.name
             FROM apidbtuning.geneattributes gf,
                  apidbtuning.gotermsummary gts,
                  sres.GoTerm gt,
                  sres.GoRelationship gr,
                  sres.GoRelationshipType grt
             WHERE gf.taxon_id = $taxonId
-              AND gts.source_id = gf.source_id
+              AND gts.gene_source_id = gf.source_id
               AND gts.ontology = '$self->{subOntology}'
               AND gts.displayable_source in ($self->{sources})
 --              AND gts.evidence_code in ($self->{evidCodes})
@@ -88,13 +88,13 @@ from
               AND grt.name = 'closure'
             group BY gt.go_id, gt.name
    ) bgd,
-   (SELECT gt.go_id, count(distinct gts.source_id) as resultcnt
+   (SELECT gt.go_id, count(distinct gts.gene_source_id) as resultcnt
             FROM ApidbTuning.GoTermSummary gts,
                  sres.GoTerm gt,
                  sres.GoRelationship gr,
                  sres.GoRelationshipType grt,
                  ($geneResultSql) r
-            WHERE gts.source_id = r.source_id
+            WHERE gts.gene_source_id = r.source_id
               AND gts.ontology = '$self->{subOntology}'
               AND gts.displayable_source in ($self->{sources})
 --              AND gts.evidence_code in ($self->{evidCodes})
