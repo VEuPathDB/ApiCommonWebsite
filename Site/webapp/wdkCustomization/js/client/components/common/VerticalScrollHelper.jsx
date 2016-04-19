8fc478e2f645fd91000807963bb7d6d055a4125e
Dave Falke <dmfalke@gmail.com>
2016-04-19 19:18:46 +0000

    Add buttons to scroll thumbnails
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@74051 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/webapp/wdkCustomization/js/client/components/common/VerticalScrollHelper.jsx
@@ -0,0 +1,107 @@
+import {Component, PropTypes, cloneElement} from 'react';
+import {bindAll, throttle} from 'lodash';
+import {isNodeOverflowing} from '../../utils';
+
+/**
+ * Renders buttons to scroll scroll vertically.
+ */
+export default class VerticalScrollHelper extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftButtonActive: false,
+      rightButtonActive: false
+    };
+    this.node = null;
+    bindAll(this, 'setNode', 'scrollLeft', 'scrollRight');
+    this.updateButtonState = throttle(this.updateButtonState.bind(this), 250);
+  }
+
+  componentDidMount() {
+    this.updateButtonState();
+    window.addEventListener('resize', this.updateButtonState);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateButtonState);
+  }
+
+  setNode(node) {
+    this.node = node;
+  }
+
+  updateButtonState() {
+    this.setState({
+      showButtons: isNodeOverflowing(this.node),
+      leftButtonActive: this.node.scrollLeft > 0,
+      rightButtonActive: this.node.scrollWidth - this.node.scrollLeft > this.node.clientWidth
+    });
+  }
+
+  scrollLeft(event) {
+    event.preventDefault();
+    this.updateScroll((this.node.clientWidth - 20) * -1);
+  }
+
+  scrollRight(event) {
+    event.preventDefault();
+    this.updateScroll(this.node.clientWidth - 20);
+  }
+
+  updateScroll(scrollDelta) {
+    this.node.scrollLeft += scrollDelta;
+    this.updateButtonState();
+  }
+
+  renderLeftButton() {
+    return this.renderButton( '\u25c0', this.state.leftButtonActive, this.scrollLeft);
+  }
+
+  renderRightButton() {
+    return this.renderButton('\u25b6', this.state.rightButtonActive, this.scrollRight);
+  }
+
+  renderButton(text, active, onClick) {
+    return (
+      <button
+        className="eupathdb-VerticalScrollHelperButton"
+        disabled={!active}
+        onClick={onClick}
+      > {text} </button>
+    );
+  }
+
+  renderButtons() {
+    if (this.state.showButtons) {
+      return (
+        <div className="eupathdb-VerticalScrollHelperButtonContainer">
+          {this.renderLeftButton()}
+          {this.renderRightButton()}
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="eupathdb-VerticalScrollHelper">
+        <div ref={this.setNode} style={{overflow: 'hidden'}}>
+          {this.props.children}
+        </div>
+        {this.renderButtons()}
+      </div>
+    );
+  }
+
+}
+
+
+VerticalScrollHelper.propTypes = {
+  children: PropTypes.element.isRequired,
+  scrollDelta: PropTypes.number.isRequired
+}
+
+VerticalScrollHelper.defaultProps = {
+  scrollDelta: 50
+};
