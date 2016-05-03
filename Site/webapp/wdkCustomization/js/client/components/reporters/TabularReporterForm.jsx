c4ca95eb7e66c400d6766bcc8d44f2f69bf20edd
Ryan Doherty <tech@conical.org>
2016-05-03 02:19:41 +0000

    Factor help note about excel and other reporter todos
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@74273 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/reporters/TabularReporterForm.jsx
+++ Site/webapp/wdkCustomization/js/client/components/reporters/TabularReporterForm.jsx
@@ -49,10 +49,7 @@ let TabularReporterForm = props => {
       </div>
       <hr/>
       <div style={{margin:'0.5em 2em'}}>
-        **Note: If you choose "Excel File" as Download Type, you can only download a
-        maximum 10M (in bytes) of the results and the rest will be discarded.<br/>
-        Opening a huge Excel file may crash your system. If you need to get the
-        complete results, please choose "Text File".
+        <ExcelNote/>
       </div>
       <hr/>
     </div>
