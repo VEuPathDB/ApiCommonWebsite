b255cbd7de7e0d4cef75cb995dbde37750970dd4
Dave Falke <dmfalke@gmail.com>
2016-05-26 02:02:45 +0000

    add util to get menu categories
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@74625 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/webapp/wdkCustomization/js/client/util/category.js
@@ -0,0 +1,83 @@
+import {indexBy, propertyOf, difference} from 'lodash';
+import {preorderSeq, pruneDescendantNodes} from 'wdk-client/TreeUtils';
+import {getTree} from 'wdk-client/OntologyUtils';
+import {getTargetType, getScope, getRecordClassName, isQualifying} from 'wdk-client/CategoryUtils';
+
+let isSearchMenuScope = isQualifying({ targetType: 'search', scope: 'menu' });
+
+/**
+ * Gets Category ontology and returns an array of trees, where each root node
+ * is a recordClass. Optionally, indicate record classes to include or exclude
+ * via the `options` object. If `options.include` is defined, `options.exclude`
+ * will be ignored.
+ *
+ * This is used by bubbles and query grid (and soon menus).
+ *
+ * @param {WdkService} wdkService
+ * @param {Object} options
+ * @param {string[]} options.include Record classes to include
+ * @param {string[]} options.exclude Record classes to exclude
+ * @returns Promise<RecordClassTree[]>
+ */
+export function getSearchMenuCategoryTree(wdkService, options) {
+  let ontology$ = wdk.client.runtime.wdkService.getOntology();
+  let recordClasses$ = wdk.client.runtime.wdkService.getRecordClasses();
+  return Promise.all([ ontology$, recordClasses$ ]).then(([ ontology, recordClasses ]) => {
+    let recordClassMap = indexBy(recordClasses, 'name');
+    // get searches scoped for menu
+    let categoryTree = getTree(ontology, isSearchMenuScope);
+    return groupByRecordClass(categoryTree, recordClassMap, options);
+  });
+}
+
+function groupByRecordClass(categoryTree, recordClassMap, options) {
+  let recordClassCategories = preorderSeq(categoryTree)
+  .filter(isLeafNode)
+  .map(getRecordClassName)
+  .uniq()
+  .filter(isDefined)
+  .filter(includeExclude(options))
+  .map(propertyOf(recordClassMap))
+  .map(getRecordClassTree(categoryTree))
+  .toArray();
+  return { children: recordClassCategories };
+}
+
+function isLeafNode(node) {
+  return node.children.length === 0;
+}
+
+function isDefined(maybe) {
+  return maybe !== undefined;
+}
+
+function includeExclude({ include, exclude }) {
+  return function(item) {
+    return include != null ? include.indexOf(item) > -1
+         : exclude != null ? exclude.indexOf(item) === -1
+         : true;
+  }
+}
+
+function getRecordClassTree(categoryTree) {
+  return function(recordClass) {
+    let tree = pruneDescendantNodes(isRecordClassTreeNode(recordClass), categoryTree);
+    return {
+      properties: {
+        label: [recordClass.name],
+        'EuPathDB alternative term': [recordClass.displayNamePlural]
+      },
+      // Flatten non-transcript searches. This can be removed if we decide to show
+      // those categories
+      children: recordClass.name === 'TranscriptRecordClasses.TranscriptRecordClass'
+        ? tree.children
+        : preorderSeq(tree).filter(n => n.children.length === 0).toArray()
+    };
+  }
+}
+
+function isRecordClassTreeNode(recordClass) {
+  return function(node) {
+    return node.children.length !== 0 || getRecordClassName(node) === recordClass.name;
+  }
+}
