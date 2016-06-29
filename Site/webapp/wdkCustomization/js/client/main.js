b265e0492b143c301f76d32fed8ee7fa421a7bc3
Ryan Doherty <tech@conical.org>
2016-06-29 01:01:44 +0000

    clean up store wrapping logic
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@75255 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -3,18 +3,15 @@
 
 // TODO Remove auth_tkt from url before proceeding
 
-import { initialize, wrapComponents, wrapStores } from 'wdk-client';
+import { initialize, wrapComponents } from 'wdk-client';
 
-// Import apicomm wrappers and additional routes
+// import apicomm wrappers and additional routes
 import * as componentWrappers from './componentWrappers';
 import * as storeWrappers from './storeWrappers';
 import { routes } from './routes';
 
-// apply component wrappers
+// apply component wrappers (this can be done only once for all initializations)
 wrapComponents(componentWrappers);
-
-// apply store wrappers
-wrapStores(storeWrappers);
 
 // getApiClientConfig() is defined in /client/index.jsp
 let config = window.getApiClientConfig();
@@ -26,12 +23,15 @@ if (pathname.startsWith(aliasUrl)) {
   window.history.replaceState(null, '', pathname.replace(aliasUrl, config.rootUrl));
 }
 
+// initialize the application
 let app = window._app = initialize({
   rootUrl: config.rootUrl,
   endpoint: config.endpoint,
-  applicationRoutes: routes
+  applicationRoutes: routes,
+  storeWrappers
 });
 
+// render the root element once page has completely loaded
 document.addEventListener('DOMContentLoaded', function() {
   let rootEl = document.querySelector(config.rootElement);
   if (rootEl != null) app.render(rootEl);
