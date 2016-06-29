36cff167a3907923b56dea65cb82c884b81c8f6f
Ryan Doherty <tech@conical.org>
2016-06-29 21:46:25 +0000

    fix java compiler errors
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@75274 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Service/src/java/org/apidb/apicommon/service/services/TranscriptToggleService.java
+++ Service/src/java/org/apidb/apicommon/service/services/TranscriptToggleService.java
@@ -9,7 +9,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.apache.log4j.Logger;
 import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
