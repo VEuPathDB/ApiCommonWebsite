8ba65e5fc565d2d6aec856da5bf52a417d297c03
Dave Falke <dmfalke@gmail.com>
2016-07-19 19:39:39 +0000

    wdk will render application automatically
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@75498 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -8,31 +8,26 @@ import { initialize, wrapComponents } from 'wdk-client';
 // import apicomm wrappers and additional routes
 import * as componentWrappers from './componentWrappers';
 import * as storeWrappers from './storeWrappers';
-import { routes } from './routes';
+import { routes as applicationRoutes } from './routes';
 
 // apply component wrappers (this can be done only once for all initializations)
 wrapComponents(componentWrappers);
 
 // getApiClientConfig() is defined in /client/index.jsp
-let config = window.getApiClientConfig();
+let { rootUrl, rootElement, endpoint } = window.getApiClientConfig();
 
 // replace '/a/' with '/${webapp}/'
 let pathname = window.location.pathname;
-let aliasUrl = config.rootUrl.replace(/^\/[^/]+\/(.*)$/, '/a/$1');
+let aliasUrl = rootUrl.replace(/^\/[^/]+\/(.*)$/, '/a/$1');
 if (pathname.startsWith(aliasUrl)) {
-  window.history.replaceState(null, '', pathname.replace(aliasUrl, config.rootUrl));
+  window.history.replaceState(null, '', pathname.replace(aliasUrl, rootUrl));
 }
 
 // initialize the application
-let app = window._app = initialize({
-  rootUrl: config.rootUrl,
-  endpoint: config.endpoint,
-  applicationRoutes: routes,
+window._app = initialize({
+  rootUrl,
+  rootElement,
+  endpoint,
+  applicationRoutes,
   storeWrappers
-});
-
-// render the root element once page has completely loaded
-document.addEventListener('DOMContentLoaded', function() {
-  let rootEl = document.querySelector(config.rootElement);
-  if (rootEl != null) app.render(rootEl);
 });
