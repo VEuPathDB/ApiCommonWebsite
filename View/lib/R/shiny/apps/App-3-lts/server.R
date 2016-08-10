a6f1079639befb45983d19d1c6248a213d8ca10a
Ja’Shon Cade <jcade@upenn.edu>
2016-08-10 04:08:36 +0000

    updated column names, commented out libary(zoo), which was not found
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/prism_06252014@75901 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/App-3-lts/server.R
+++ View/lib/R/shiny/apps/App-3-lts/server.R
@@ -1,4 +1,4 @@
-library(zoo)
+#library(zoo)
 require(rCharts)
 require(reshape2)
 source("../../lib/wdkDataset.R")
@@ -18,13 +18,13 @@ shinyServer(function(input, output, session) {
     names(LTS) = gsub("\\.", "", names(LTS)) #Remove remaining periods
    
 #coerce variables to required type:
-LTS$GeographicLocation <- as.factor(LTS$GeographicLocation)
+LTS$SubcountyInUganda <- as.factor(LTS$SubcountyInUganda)
 
-LTS.S <- subset(LTS, AnophelesCollectedtotal >0, select = c("DateOfVisit","AnophelesCollectedtotal","GeographicLocation",
+LTS.S <- subset(LTS, AnophelesCollectedtotal >0, select = c("DateOfVisit","SumFemaleAsInACollection","SubcountyInUganda",
                                                             "TotalAnophelesPositive","TotalAnophelesTested","Parous",
-                                                            "Nulliparous","AFunestus","AGambiae","OtherAnopheles","CollectionBarcode"))
-      LTS.M <- melt(LTS.S, id.vars =c("DateOfVisit","CollectionBarcode","GeographicLocation") )
-      LTS.D <- dcast(LTS.M,DateOfVisit+GeographicLocation~variable, fun.aggregate = sum)
+                                                            "Nulliparous","SumFemaleAsFunestusInAC","SumFemaleAsGambiaeInACo","AnophelesOther","SourceId"))
+      LTS.M <- melt(LTS.S, id.vars =c("DateOfVisit","SourceId","SubcountyInUganda"))
+      LTS.D <- dcast(LTS.M,DateOfVisit+SubcountyInUganda~variable, fun.aggregate = sum)
       x <-input$pvar
       y <- input$yvar
       g <- input$group
@@ -32,7 +32,7 @@ LTS.S <- subset(LTS, AnophelesCollectedtotal >0, select = c("DateOfVisit","Anoph
      
       #sort dataframe by MonthYear
       d <- LTS.D[order(as.yearmon(LTS.D$DateOfVisit, "%y-%b")),]
-      p1 <- rPlot(y, x, data = d, type = 'bar', color ='GeographicLocation', group='GeographicLocation' ) #Plot PD as it depends on MonthYear
+      p1 <- rPlot(y, x, data = d, type = 'bar', color ='SubcountyInUganda', group='SubcountyInUganda' ) #Plot PD as it depends on MonthYear
       str(p1)  
    #p1$guides(x = list(title = "", ticks = list(ym)))
       p1$addParams(dom = 'LTSChart')
--- View/lib/R/shiny/apps/App-3-lts/ui.R
+++ View/lib/R/shiny/apps/App-3-lts/ui.R
@@ -6,11 +6,11 @@ shinyUI(
     headerPanel("Light Trap Visualizations"),
     sidebarPanel(
       #specify options
-      selectInput(inputId="pvar" ,label="Plot",choices = c("AnophelesCollectedtotal","TotalAnophelesPositive","TotalAnophelesTested","Parous",
-                                         "Nulliparous","AFunestus","AGambiae","OtherAnopheles")),
+      selectInput(inputId="pvar" ,label="Plot",choices = c("SumFemaleAsInACollection","TotalAnophelesPositive","TotalAnophelesTested","Parous",
+                                         "Nulliparous","SumFemaleAsFunestusInAC","SumFemaleAsGambiaeInACo","AnophelesOther")),
       selectInput(inputId="yvar", label="against", choices = ("DateOfVisit")),
-#      selectInput(inputId="facet",label="Facet",choices = ("GeographicLocation"), selected = ""),
-      selectInput(inputId="group",label="Group", choices = ("GeographicLocation"), selected = "")
+#      selectInput(inputId="facet",label="Facet",choices = ("SubcountyInUganda"), selected = ""),
+      selectInput(inputId="group",label="Group", choices = ("SubcountyInUganda"), selected = "")
     ),
     mainPanel(
       showOutput("LTSChart","polycharts")
