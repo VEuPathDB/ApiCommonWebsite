e82e1a9d7a98c17eaae5d4b36951232bd6850f3e
Ryan Doherty <tech@conical.org>
2016-08-11 20:52:49 +0000

    Add new globus screenshots and massage verbiage in orientation page
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/gus4@75953 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
+++ Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
@@ -9,7 +9,6 @@ let GalaxyTerms = React.createClass({
     return (
       <div id="eupathdb-GalaxyTerms">
         <h1>Analyze My Experiment</h1>
-       
         <table id="eupathdb-GalaxyTerms-login">
           <tbody>
             <tr>
@@ -19,7 +18,7 @@ let GalaxyTerms = React.createClass({
                 <p>The EuPathDB Galaxy service is hosted by Globus Genomics, an affiate of Globus.</p>
                 <p>This first login screen will always appear:</p>
               </td>
-              <td width="40%" align="right"><Image title="Screenshot of Globus Page" src="wdkCustomization/images/globusGalaxy.png"/></td>
+              <td width="40%" align="right"><Image title="Globus Login Gateway" src="wdkCustomization/images/globus-01-log-in.jpg"/></td>
             </tr>
           </tbody>
         </table>
@@ -30,24 +29,23 @@ let GalaxyTerms = React.createClass({
         <table id="eupathdb-GalaxyTerms-initial">
           <tbody>
             <tr>
-              <td><Image title="Screenshot of Globus Page" src="wdkCustomization/images/globusGalaxy.png"/></td>
-              <td><Image title="Screenshot of Globus Page" src="wdkCustomization/images/globusGalaxy.png"/></td>
-              <td><Image title="Screenshot of Globus Page" src="wdkCustomization/images/globusGalaxy.png"/></td>
+              <td><Image title="Option to link an existing Globus Account" src="wdkCustomization/images/globus-02-link-account.jpg"/></td>
+              <td><Image title="Agree to Globus account terms" src="wdkCustomization/images/globus-03-account-terms.jpg"/></td>
+              <td><Image title="Grant permission to access your Globus account" src="wdkCustomization/images/globus-04-oauth-perms.jpg"/></td>
             </tr>
             <tr>
               <td>
-                (1) If you already have a Globus account, you can
-                link it to your new EuPathDB account.
-                <strong>Your choice.</strong> If you don't
-                have a prior Globus account, choose <strong>No Thanks.</strong>
+                (1) If you already have a Globus account, you can link it to
+                your EuPathDB account. <strong>Your choice.</strong> If you
+                don't have a prior Globus account, choose <strong>No Thanks</strong>.
               </td>
               <td>
-                (2) Their Terms and Conditions.<br />
-                <strong>Please read and click Agree</strong>
+                (2) Complete your account information and agree to Globus's
+                Terms and Conditions. Please read, make your selections, and click <strong>Continue</strong>.
               </td>
               <td>
-                (3) Permission to share identity with us.<br />
-                <strong>Please click Allow.</strong>
+                (3) Grant permission to share your Globus identity and files
+                with us. Please click <strong>Allow</strong>.
               </td>
             </tr>
           </tbody>
