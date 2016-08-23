b5cc5158025e59ef903f1fd670306afa7409ba92
Dave Falke <dmfalke@gmail.com>
2016-08-23 14:28:15 +0000

    Reorganize to make application routes more apparent.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76153 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/routes.jsx
+++ Site/webapp/wdkCustomization/js/client/routes.jsx
@@ -7,23 +7,30 @@ import QueryGridController from './components/controllers/QueryGridController';
 import GalaxyTermsController from './components/controllers/GalaxyTermsContoller';
 import SampleForm from './components/samples/SampleForm';
 
+const apidbRoutes = (
+  <Route path="/">
+    <Route path="fasta-tool" component={FastaConfigController}/>
+    <Route path="query-grid" component={QueryGridController}/>
+    <Route path="galaxy-orientation" component={GalaxyTermsController}/>
+
+    {/* test/demonstration pages */}
+    <Route path="sample-form" component={SampleForm}/>
+  </Route>
+);
+
 /**
  * Wrap WDK Routes Element
  */
 export function wrapRoutes(rootRoute) {
   let mappedChildren = Children.map(rootRoute.props.children, mapRoute);
-  return cloneElement(rootRoute, {}, (
-    <Route path="/">
-      <Route path="fasta-tool" component={FastaConfigController}/>
-      <Route path="query-grid" component={QueryGridController}/>
-      <Route path="galaxy-orientation" component={GalaxyTermsController}/>
-
-      {/* test/demonstration pages */}
-      <Route path="sample-form" component={SampleForm}/>
-    </Route>
-  ), mappedChildren);
+  return cloneElement(rootRoute, {}, apidbRoutes, mappedChildren);
 }
 
+/**
+ * Add the hideProjectId `onEnter` prop to routes that expect a primary key.
+ * The current assumption is any route which begins with 'record/:recordClass'
+ * is such a route.
+ */
 function mapRoute(route) {
   let { path } = route.props;
   return path && path.startsWith('record/:recordClass')
@@ -33,6 +40,10 @@ function mapRoute(route) {
 
 
 let projectRegExp = new RegExp('/' + projectId + '$');
+
+/**
+ * Remove projectId from the url. This is like a redirect.
+ */
 function hideProjectId(nextState, replace) {
   if (projectRegExp.test(nextState.location.pathname)) {
     replace(nextState.location.pathname.replace(projectRegExp, ''));
