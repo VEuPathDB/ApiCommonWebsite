ec6d41ba4ec97890f4de8d03cc1ca5955624e83e
Dave Falke <dmfalke@gmail.com>
2016-08-23 13:55:35 +0000

    use WdkViewController
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76152 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/controllers/QueryGridController.jsx
+++ Site/webapp/wdkCustomization/js/client/components/controllers/QueryGridController.jsx
@@ -1,46 +1,25 @@
 import React from 'react';
-import QueryGrid from "../QueryGrid";
-import { Loading } from 'wdk-client/Components';
-import { getPropertyValue, getNodeChildren } from 'wdk-client/OntologyUtils';
+import QueryGrid from '../QueryGrid';
+import { WdkViewController } from 'wdk-client/Controllers';
 import { getSearchMenuCategoryTree } from '../../util/category.js';
 
-let QueryGridController = React.createClass({
+export default class QueryGridController extends WdkViewController {
 
-  componentWillMount() {
-    this.setState({isLoading: true, grid: {}});
-    Promise.all([
-      this.props.wdkService.getOntology(),
-      this.props.wdkService.getRecordClasses()
-    ]).then(([ ontology, recordClasses ]) => getSearchMenuCategoryTree(ontology, recordClasses, {})).then(grid => {
-      this.putGeneSearchesFirst(grid);
-      this.setState({isLoading: false, grid: grid});
-    });
-  },
-
-  componentDidMount() {
-    document.title = 'Query Grid';
-  },
-
-  /**
-   * A munge to move gene searches to top of tree
-   * @param tree
-   * @returns {*}
-   */
-  putGeneSearchesFirst(tree) {
-    return getNodeChildren(tree).sort((a,b) => {
-      if (getPropertyValue('EuPathDB alternative term',a) === 'Genes' && getPropertyValue('EuPathDB alternative term',b) !== 'Genes') return -1;
-      if (getPropertyValue('EuPathDB alternative term',a) !== 'Genes' && getPropertyValue('EuPathDB alternative term',b) === 'Genes') return 1;
-      return 0;
-    });
-  },
-
-  render() {
-    if (this.state.isLoading) {
-      return ( <Loading/> );
-    }
-    return ( <QueryGrid {...this.state} /> );
+  getStoreName() {
+    return 'GlobalDataStore';
   }
 
-});
+  isRenderDataLoaded(state) {
+    return state.ontology && state.recordClasses;
+  }
 
-export default QueryGridController;
+  getTitle() {
+    return 'Query Grid';
+  }
+
+  renderView(state) {
+    let grid = getSearchMenuCategoryTree(state.ontology, state.recordClasses);
+    return ( <QueryGrid grid={grid} /> );
+  }
+
+}
