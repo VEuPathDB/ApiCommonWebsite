0c5dfe5e320a027032d44156fd7882874538742f
Dave Falke <dmfalke@gmail.com>
2016-08-23 14:31:43 +0000

    Add action creators
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76154 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Site/webapp/wdkCustomization/js/client/actioncreators/GlobalActionCreators.js
@@ -0,0 +1,53 @@
+/**
+ * Created by dfalke on 8/22/16.
+ */
+import { broadcast } from 'wdk-client/StaticDataUtils';
+
+export let actionTypes = {
+  BASKETS_LOADED: 'apidb/basket',
+  QUICK_SEARCH_LOADED: 'apidb/quick-search-loaded'
+};
+
+export function loadBasketCounts() {
+  return function run(dispatch, { wdkService }) {
+    return wdkService.getBasketCounts().then(basketCounts => {
+      return dispatch({
+        type: actionTypes.BASKETS_LOADED,
+        payload: { basketCounts }
+      });
+    })
+    .catch(serviceError => {
+      if (serviceError.status !== 403) {
+        console.error('Unexpected error while attempting to retrieve basket counts.', serviceError);
+      }
+    });
+  }
+}
+
+/**
+ * Load data for quick search
+ * @param {Array<object>} questions An array of quick search spec objects.
+ *    A spec object has two properties: `name`: the name of the questions,
+ *    and `searchParam`: the name of the parameter to use for text box.
+ * @return {run}
+ */
+export function loadQuickSearches(questions) {
+  return function run(dispatch, { wdkService }) {
+    let requests = questions.map(({ name, quickSearchParamName, quickSearchDisplayName }) => {
+      return wdkService.sendRequest({
+        method: 'GET',
+        path: '/question/' + name,
+        params: { expandParams: true },
+        useCache: true
+      }).then(question => {
+        return Object.assign({}, question, { quickSearchParamName, quickSearchDisplayName })
+      });
+    });
+    return Promise.all(requests).then(questions => {
+      return dispatch({
+        type: actionTypes.QUICK_SEARCH_LOADED,
+        payload: { questions }
+      });
+    });
+  }
+}
