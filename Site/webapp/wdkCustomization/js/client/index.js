c204e57f9e507bb5b074aeef0267a024c61d1c7d
Dave Falke <dmfalke@gmail.com>
2016-08-24 20:12:15 +0000

    Add scroll to top link for new pages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76216 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/index.js
+++ Site/webapp/wdkCustomization/js/client/index.js
@@ -5,3 +5,4 @@ import 'eupathdb/wdkCustomization/css/superfish/css/superfish.css';
 import 'eupathdb/css/AllSites.css';
 import 'apidb/css/AllApiSites.css';
 import 'apidb/wdkCustomization/css/pathway.css';
+import 'apidb/js/scroll-to-top';
