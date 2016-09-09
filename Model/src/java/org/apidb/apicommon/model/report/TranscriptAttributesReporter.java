0367abfdbb72b8ea11fba83313dc5d37099e0107
Ryan Doherty <tech@conical.org>
2016-09-09 17:20:21 +0000

    Change API for FilterOption and FilterOptionList so we can lazy load Question and Filter objects
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76621 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/report/TranscriptAttributesReporter.java
+++ Model/src/java/org/apidb/apicommon/model/report/TranscriptAttributesReporter.java
@@ -5,8 +5,6 @@ import java.util.Map;
 import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.answer.AnswerValue;
-import org.gusdb.wdk.model.filter.Filter;
-import org.gusdb.wdk.model.filter.FilterOption;
 import org.gusdb.wdk.model.filter.FilterOptionList;
 import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.report.AttributesTabularReporter;
@@ -40,22 +38,20 @@ public class TranscriptAttributesReporter extends AttributesTabularReporter {
    */
   @Override
   public void initialize() throws WdkModelException {
-    
-    if (applyFilter == null) throw new WdkModelException("Missing required reporter property: " + PROP_APPLY_FILTER); 
-    
-    if (applyFilter == false) return;
+    if (applyFilter == null)
+      throw new WdkModelException("Missing required reporter property: " + PROP_APPLY_FILTER); 
+    if (!applyFilter)
+      return; // use existing answer value
 
+    // need to create new base answervalue and apply transcript filter
     baseAnswer = new AnswerValue(getAnswerValue(), startIndex, endIndex);
     Question question = baseAnswer.getQuestion();
-    if (!question.getRecordClass().getName().equals("TranscriptRecordClass"))
-      throw new WdkModelException("Calling TranscriptAttributesReporter on invalid record class: " + question.getRecordClass().getName());
     String filterName = RepresentativeTranscriptFilter.FILTER_NAME;
-    Filter repTransFilter = question.getRecordClass().getFilter(filterName);
-    if (repTransFilter == null) throw new WdkModelException("Can't find transcript filter with name " + filterName);
+    if (question.getFilter(filterName) == null)
+      throw new WdkModelException("Can't find transcript filter with name " + filterName + " on question " + question.getFullName());
     JSONObject jsFilterValue = new JSONObject(); // this filter has no params, so this stays empty
-    FilterOption filterOpt = new FilterOption(question, repTransFilter, jsFilterValue, false);
-    FilterOptionList optionsList = new FilterOptionList(question);
-    optionsList.addFilterOption(filterOpt);
+    FilterOptionList optionsList = new FilterOptionList(wdkModel, question.getFullName());
+    optionsList.addFilterOption(filterName, jsFilterValue);
     baseAnswer.setViewFilterOptions(optionsList);
   }
   
