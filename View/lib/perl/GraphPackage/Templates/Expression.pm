acb43a5d06be27d5a551457e08030c469cdbfcca
John Brestelli <jbrestell@gmail.com>
2016-09-14 20:25:05 +0000

    add rtpcr crypto
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76726 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/Expression.pm
+++ View/lib/perl/GraphPackage/Templates/Expression.pm
@@ -958,6 +958,39 @@ sub finalProfileAdjustments {
 1;
 
 
+package ApiCommonWebsite::View::GraphPackage::Templates::Expression::DS_2bf995383e;
+use vars qw( @ISA );
+@ISA = qw( ApiCommonWebsite::View::GraphPackage::Templates::Expression );
+use ApiCommonWebsite::View::GraphPackage::Templates::Expression;
+
+use strict;
+sub getGraphType { 'bar' }
+sub getExprPlotPartModuleString { 'Standardized' }
+
+sub init {
+  my $self = shift;
+
+  $self->SUPER::init(@_);
+
+  my $colors =['#E9967A', '#8B4513','#66CDAA', '#556B2F', '#87CEEB','#008080', '#C9BE62'];
+
+  my $xAxisLabels = ['2 Hrs', '6 Hrs','12 Hrs', '24 Hrs','36 Hrs','48 Hrs','72 Hrs'];
+
+  my @profileArray = (['Cparvum_RT_PCR_Kissinger', 'values', '', '', $xAxisLabels ]);
+
+  my $profileSets = ApiCommonWebsite::View::GraphPackage::Util::makeProfileSets(\@profileArray);
+
+  my $standardized = ApiCommonWebsite::View::GraphPackage::BarPlot::Standardized->new(@_);
+  $standardized->setProfileSets($profileSets);
+  $standardized->setColors($colors);
+  $standardized->setForceHorizontalXAxis(1);
+
+  $self->setGraphObjects($standardized);
+
+  return $self;
+}
+
+1;
 
 
 # package ApiCommonWebsite::View::GraphPackage::Templates::Expression::DS_4582562a4b;
