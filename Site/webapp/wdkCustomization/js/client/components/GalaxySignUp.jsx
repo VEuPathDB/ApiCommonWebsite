cd3f0a00006e9d5ddf7a735d5807108a5a78ffc7
Dave Falke <dmfalke@gmail.com>
2016-09-19 20:03:03 +0000

    update verbiage
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@76888 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/GalaxySignUp.jsx
+++ Site/webapp/wdkCustomization/js/client/components/GalaxySignUp.jsx
@@ -9,42 +9,47 @@ export default function GalaxySignUp(props) {
       <p>
         The first time you visit EuPathDB Galaxy you will be asked to sign up
         with <a href="https://www.globus.org">Globus</a>, EuPathDB’s Galaxy
-        instance manager. This is a three-step sign-up process (screenshots
-        below).
-      </p>
-
-      <p>
-        Click <strong>“Continue to Galaxy”</strong> to sign up for EuPathDB
-        Galaxy services.
+        manager, in order to set up your private Galaxy workspace.
+        This is a <u>three-step</u> sign-up process (screenshots below), which
+        will be opened in a new tab.
       </p>
 
       <p>
         <a href="contact-us">Contact us</a> if you experience any difficulties.
       </p>
 
-      <div id="eupathdb-GalaxyTerms-initial">
-        <span className="column-left" >
+      <div className="eupathdb-GalaxySignUpScreens">
+        <div>
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            <strong>1. Create new or link existing Globus account.</strong>
+          </div>
           <img title="Option to link an existing Globus Account" src="/a/wdkCustomization/images/globus-02-link-account.jpg"/>
-          <p>
-            (1) If you already have a Globus account, you can link it to
-            your EuPathDB account. <strong>Your choice.</strong> If you
-            don't have a prior Globus account, choose <strong>No Thanks</strong>.
-          </p>
-        </span>
-        <span className="column-center">
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            If you already have a Globus account, you may choose to link it to
+            your EuPathDB account. If you don't have an existing Globus account,
+            choose <strong>No thanks, continue</strong>.
+          </div>
+        </div>
+        <div>
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            <strong>2. Register your credentials.</strong>
+          </div>
           <img title="Agree to Globus account terms" src="/a/wdkCustomization/images/globus-03-account-terms.jpg"/>
-          <p>
-            (2) Complete your account information and agree to Globus's
-            Terms and Conditions. Please read, make your selections, and click <strong>Continue</strong>.
-          </p>
-        </span>
-        <span className="column-right">
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            Tell Globus how you will use your account, read and agree to their
+            Terms and Conditions, then click <strong>Continue</strong>
+          </div>
+        </div>
+        <div>
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            <strong>3. Grant EuPathDB Galaxy access to your account.</strong>
+          </div>
           <img title="Grant permission to access your Globus account" src="/a/wdkCustomization/images/globus-04-oauth-perms.jpg"/>
-          <p>
-            (3) Grant permission to share your Globus identity and files
-            with us. Please click <strong>Allow</strong>. (We will only perform file transfers that you explicitly request, between Galaxy and other resources, including EuPathDB.)
-          </p>
-        </span>
+          <div className="eupathdb-GalaxySignUpScreenCaption">
+            Click <strong>Allow</strong> to grant permission to share the
+            account with us. We will only perform file transfers that you explicitly request.
+          </div>
+        </div>
       </div>
 
       <div className="eupathdb-GalaxyTermsContinueLink">
--- Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
+++ Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
@@ -11,27 +11,21 @@ export default function GalaxyTerms(props) {
     <div id="eupathdb-GalaxyTerms">
       <h1>Analyze My Experiment</h1>
 
-      <p className="eupath-bigItalic">Welcome to the free EuPathDB Galaxy Data Analysis Services.</p>
-
-      <p>
-        This service uses Galaxy and is provided as a collaboration between EuPathDB and 
-         <a href="https://www.globus.org/genomics"> Globus Genomics</a>, an affiliate of &nbsp;
-         <a href="https://www.globus.org">Globus</a>.
+      <p className="eupathdb-GalaxyTermsLead">
+        <em>Welcome to the free EuPathDB Galaxy Data Analysis Services.</em>
+        This service uses a dedicated Galaxy site preloaded with EuPathDB genomes and
+        workflows, and is hosted by <a href="https://www.globus.org/genomics">
+        Globus Genomics</a>, an affiliate of <a href="https://www.globus.org">Globus</a>.
       </p>
 
       <div className="eupathdb-GalaxyWelcomeGrid">
-        <div>Use Galaxy analyze:
-          <ul className="eupathdb-GalaxyWelcomeAnalysisList">
-            <li>RNA-seq</li>
-            <li>ChIP-seq</li>
-            <li>and other data sets</li>
-          </ul>
+        <div>
+          <p>Use Galaxy to analyze RNA-seq, ChIP-seq, Variants, and many other data sets.</p>
           <p>Some analysis results will be available as tracks and searches in {projectId}.</p>
         </div>
         <div>
           <img src="/a/wdkCustomization/images/globus-01-welcome-page.png"/>
         </div>
-        <div></div>
       </div>
 
       <div className="eupathdb-GalaxyTermsContinueLink">
