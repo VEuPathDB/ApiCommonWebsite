5d8873ba5bf4bd0e5f9a543a88ecd6617b6b35cf
Dave Falke <dmfalke@gmail.com>
2016-09-23 16:21:39 +0000

    move new scroll-to-top button to EuPathSiteCommon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@77018 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/index.js
+++ Site/webapp/wdkCustomization/js/client/index.js
@@ -5,4 +5,6 @@ import 'eupathdb/wdkCustomization/css/superfish/css/superfish.css';
 import 'eupathdb/css/AllSites.css';
 import 'site/css/AllApiSites.css';
 import 'site/wdkCustomization/css/pathway.css';
-import 'site/js/scroll-to-top';
+
+// include scroll to top button
+import 'eupathdb/js/scroll-to-top';
