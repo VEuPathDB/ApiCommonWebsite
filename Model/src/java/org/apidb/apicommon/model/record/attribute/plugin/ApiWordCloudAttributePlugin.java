03632e489f78190a3783a00284e89b8f66336c60
Ryan Doherty <tech@conical.org>
2016-10-12 04:40:03 +0000

    Various mods to address a collection of issues
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@77577 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/record/attribute/plugin/ApiWordCloudAttributePlugin.java
@@ -0,0 +1,22 @@
+package org.apidb.apicommon.model.record.attribute.plugin;
+
+import org.apidb.apicommon.model.TranscriptUtil;
+import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.answer.AnswerValue;
+import org.gusdb.wdk.model.record.attribute.plugin.WordCloudAttributePlugin;
+import org.gusdb.wdk.model.user.Step;
+import org.gusdb.wdk.model.user.User;
+
+public class ApiWordCloudAttributePlugin extends WordCloudAttributePlugin {
+
+  @Override
+  protected AnswerValue getAnswerValue(Step step, User user) throws WdkModelException, WdkUserException {
+    if (TranscriptUtil.isTranscriptQuestion(step.getQuestion())) {
+      // transcript question; see if we should apply one-transcript-per-gene filter
+      step = RepresentativeTranscriptFilter.applyToStepFromUserPreference(step, user);
+    }
+    return super.getAnswerValue(step, user);
+  }
+}
