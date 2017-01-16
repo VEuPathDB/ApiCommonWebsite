acb7350896d86708e99e623bd18ed6a25e59d564
Brian Brunk <brunkb@upenn.edu>
2017-01-16 17:29:18 +0000

    new shiny apps with reactive from Francislon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@79246 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/alpha_diversity/server.R
+++ View/lib/R/shiny/apps/alpha_diversity/server.R
@@ -5,61 +5,68 @@ library(reshape2)
 source("config.R")
 source("../../lib/wdkDataset.R")
 
+
 shinyServer(function(input, output, session) {
+  columns <- NULL
+  hash_sample_names<- NULL
   
-  #Change with the file with abundances
-  df_abundance <-
-    read.csv(
-#      "MicrobiomeSampleByMetadata_TaxaRelativeAbundance.txt",
-      getWdkDatasetFile('TaxaRelativeAbundance.tab', session, FALSE, dataStorageDir),
-#      "TaxaRelativeAbundance.tab",
-      sep = "\t",
-      col.names = c("Sample","Taxon", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Abundance", "Nada")
-    )
-  
-  # Change with the Metadata file
-  df_sample <-
-    read.csv(
-#      "fran_updated_Characteristics_tabfile.txt",
-      getWdkDatasetFile('Characteristics.tab', session, FALSE, dataStorageDir),
-#      "Characteristics.tab",
-      sep = "\t",
-      col.names = c("SampleName", "Source", "Property", "Value", "Type", "Filter", "Nada")
-    )
-  df_sample.formatted <- dcast(data = df_sample,formula = SampleName~Property, value.var = "Value")
-  rownames(df_sample.formatted) <- df_sample.formatted[,1]
-  columns <- colnames(df_sample.formatted)
-  
-  corrected_columns <-  make.names(columns)
-  colnames(df_sample.formatted) <- corrected_columns
-  hash_sample_names<-new.env()
-  for (i in 1:length(corrected_columns)) {
-  	hash_sample_names[[columns[i]]] <- corrected_columns[i]
-  }
-  df_abundance$Abundance <- round(df_abundance$Abundance*100)
-  
-  df_abundance.formatted <- dcast(data = df_abundance,formula = Kingdom+Phylum+Class+Order+Family+Genus+Species~Sample,fun.aggregate = sum,value.var = "Abundance")
-  OTU_MATRIX <- df_abundance.formatted[,8:ncol(df_abundance.formatted)]
-  OTU = otu_table(OTU_MATRIX, taxa_are_rows = T)
-  
-  TAX_MATRIX <- df_abundance.formatted[,1:7]
-  TAX_MATRIX <- as.matrix(TAX_MATRIX)
-  TAX <- tax_table(TAX_MATRIX)
-  SAMPLE <- sample_data(df_sample.formatted)
-  
-  physeq <- phyloseq(OTU, TAX, SAMPLE)
+  physeq <- reactive({
+    #Change with the file with abundances
+    df_abundance <-
+      read.csv(
+#        "MicrobiomeSampleByMetadata_TaxaRelativeAbundance.txt",
+        getWdkDatasetFile('TaxaRelativeAbundance.tab', session, FALSE, dataStorageDir),
+        sep = "\t",
+        col.names = c("Sample","Taxon", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Abundance", "EmptyColumn")
+      )
+    
+    # Change with the Metadata file
+    df_sample <-
+      read.csv(
+#        "fran_updated_Characteristics_tabfile.txt",
+        getWdkDatasetFile('Characteristics.tab', session, FALSE, dataStorageDir),
+        sep = "\t",
+        col.names = c("SampleName", "Source", "Property", "Value", "Type", "Filter", "EmptyColumn")
+      )
+    df_sample.formatted <- dcast(data = df_sample,formula = SampleName~Property, value.var = "Value")
+    rownames(df_sample.formatted) <- df_sample.formatted[,1]
+    columns <<- colnames(df_sample.formatted)
+    corrected_columns <-  make.names(columns)
+    colnames(df_sample.formatted) <- corrected_columns
+    names(corrected_columns) <- columns
+    
+    hash_sample_names <<- corrected_columns
+   
+    df_abundance$Abundance <- round(df_abundance$Abundance*100)
+    
+    df_abundance.formatted <- dcast(data = df_abundance,formula = Kingdom+Phylum+Class+Order+Family+Genus+Species~Sample,fun.aggregate = sum,value.var = "Abundance")
+    OTU_MATRIX <- df_abundance.formatted[,8:ncol(df_abundance.formatted)]
+    OTU = otu_table(OTU_MATRIX, taxa_are_rows = input$taxa_are_rows)
+    
+    TAX_MATRIX <- df_abundance.formatted[,1:7]
+    TAX_MATRIX <- as.matrix(TAX_MATRIX)
+    TAX <- tax_table(TAX_MATRIX)
+    SAMPLE <- sample_data(df_sample.formatted)
+
+    phyloseq(OTU, TAX, SAMPLE)
+  })
 	
 	output$abundanceChart <- renderPlot({
+	  physeqobj <- physeq()
+	  if(!is.null(input$category)){
 	    if(identical(input$category, "All Samples")){
-	      chart <- plot_richness(physeq, measures = input$measureCheckBox)+theme(
+	      chart <- plot_richness(physeqobj, measures = input$measureCheckBox)+theme(
 	        panel.grid.major.x = element_blank()
 	      )+geom_point(size = 4, alpha= 0.5)
 	    }else{
-	      chart <- plot_richness(physeq, x=hash_sample_names[[input$category]], measures = input$measureCheckBox)+theme(
+	      chart <- plot_richness(physeqobj, x=hash_sample_names[[input$category]], measures = input$measureCheckBox)+theme(
 	        panel.grid.major.x = element_blank()
 	      )+geom_point(size = 4, alpha= 0.5)
 	    }
-	    richness_default <<- chart$data
+	    richness_default <<- chart$data 
+	  }else{
+	    chart <- NULL
+	  }
 	  chart
 	})
 	
@@ -80,12 +87,12 @@ shinyServer(function(input, output, session) {
 	      (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
 	    top_pct <-
 	      (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
-	    
+
 	    left_px <-
 	      hover$range$left + left_pct * (hover$range$right - hover$range$left)
 	    top_px <-
 	      hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
-	    
+
 	    style <-
 	      paste0(
 	        "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); "
@@ -97,9 +104,10 @@ shinyServer(function(input, output, session) {
 	        "px;"
 	      )
 	    near_points <- nearPoints(richness_default, hover)
+	    physeqobj <- physeq()
 	    if(identical(input$category, "All Samples")){
 	      if(nrow(near_points) > 0){
-	        lvls <- rownames(sample_data(physeq))
+	        lvls <- rownames(sample_data(physeqobj))
 	        hover_sample <- lvls[round(hover$x)]
 	        alpha_and_sample <- ""
 	        for(i in 1:nrow(near_points)){
@@ -121,7 +129,7 @@ shinyServer(function(input, output, session) {
 	      }
 	    }else{
 	      if(nrow(near_points) > 0){
-	        category_value <- sample_data(physeq)[,hash_sample_names[[input$category]]]
+	        category_value <- sample_data(physeqobj)[,hash_sample_names[[input$category]]]
 	        lvls <- levels(as.factor(category_value[[1]]))
 	        hover_category <- lvls[round(hover$x)]
 	        alpha_and_sample <- ""
@@ -143,7 +151,7 @@ shinyServer(function(input, output, session) {
 	                  hover_category,
 	                  br(),
 	                  HTML(alpha_and_sample)
-	        ) 
+	        )
 	      }
 	    }
 	})
@@ -152,7 +160,7 @@ shinyServer(function(input, output, session) {
 		click <- input$plot_click
 		if (is.null(click$x))
 			return(NULL)
-		df <- data()
+		# df <- data()
 		# lvls <- levels(df$Sample)
 		# sample <- lvls[round(click$x)]
 		# selected_sample = subset(df, df$Sample == sample)
