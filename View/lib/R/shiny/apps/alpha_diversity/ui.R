37c504e8b51bafd74623b134c0d46d291cb352e8
Brian Brunk <brunkb@upenn.edu>
2017-01-16 17:29:18 +0000

    new shiny apps with reactive from Francislon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@79246 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/alpha_diversity/ui.R
+++ View/lib/R/shiny/apps/alpha_diversity/ui.R
@@ -1,16 +1,18 @@
 library(shiny)
 
-shinyUI( 
+shinyUI(
 	fluidPage(
-	  checkboxGroupInput("measureCheckBox", label="Measure(s):",
-	                     choices = c("Chao1" = "Chao1",
-	                                 "ACE" = "ACE",
-	                       "Shannon" = "Shannon",
-	                       "Simpson" = "Simpson",
-	                       "Fisher" = "Fisher"),
-	                     selected = c("Chao1", "Shannon"), inline=T),
+    checkboxGroupInput("measureCheckBox", label="Measure(s):",
+                     choices = c("Chao1" = "Chao1",
+                                 "ACE" = "ACE",
+                       "Shannon" = "Shannon",
+                       "Simpson" = "Simpson",
+                       "Fisher" = "Fisher"),
+                     selected = c("Chao1", "Shannon"), inline=T),
+    div(style = "display: none;",
+	     checkboxInput("taxa_are_rows", label = "", value = TRUE)
+	 ),
 	  uiOutput("category"),
-	  
     		div(
     			style = "position:relative",
     			plotOutput("abundanceChart",
