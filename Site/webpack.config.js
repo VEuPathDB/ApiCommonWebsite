651db35ac1a6587160175495aae79bf93d040e2c
Dave Falke <dmfalke@gmail.com>
2017-01-30 20:26:09 +0000

    put module aliasing in webpack config rather than package.json
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@79482 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webpack.config.js
+++ Site/webpack.config.js
@@ -8,5 +8,13 @@ module.exports = configure({
   output: {
     path: __dirname + '/dist',
     filename: 'apidb-[name].bundle.js'
+  },
+  resolve: {
+    // alias 'ciena-*' entries to '/lib' directory since the default
+    // entry is es6 code, which uglifyjs does not understand
+    alias: {
+      'ciena-dagre': 'ciena-dagre/lib',
+      'ciena-graphlib': 'ciena-graphlib/lib'
+    }
   }
 });
