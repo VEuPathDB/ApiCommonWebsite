ac2e3fb4ee6b1452f799b4b8ecc616a0b7403922
Bindu Gajria <bindu.gajria@gmail.com>
2017-01-31 21:09:28 +0000

    fix label for  atryp spliceSite graph  - refs #23775
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@79519 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/SpliceSites.pm
+++ View/lib/perl/GraphPackage/Templates/SpliceSites.pm
@@ -23,6 +23,17 @@ sub getRemainderRegex {
 
 1;
 
+package ApiCommonWebsite::View::GraphPackage::Templates::SpliceSites::DS_82d2e9dd6c;
+use base qw( ApiCommonWebsite::View::GraphPackage::Templates::Expression );
+
+sub finalProfileAdjustments {
+  my ($self, $profile) = @_;
+
+  my $legend = ['spliced leader sites '];
+  $profile->setSampleLabels($legend);
+}
+1;
+
 
 #--------------------------------------------------------------------------------
 
