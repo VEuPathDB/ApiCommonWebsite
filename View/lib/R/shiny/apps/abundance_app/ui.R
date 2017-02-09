7daf15590a57e4fa74a40c7ea5636a68eafe3562
Brian Brunk <brunkb@upenn.edu>
2017-02-09 17:48:29 +0000

    updated abundance and added beta diversity shiny apps from Francislon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@79779 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/abundance_app/ui.R
+++ View/lib/R/shiny/apps/abundance_app/ui.R
@@ -1,25 +1,20 @@
 library(shiny)
 
 shinyUI( 
-	bootstrapPage(
-	  div(class="container",
-	  fluidRow(
-	    column(11,
-		  br(),
-     selectInput(inputId="taxa.level",label="Choose the taxonomic level: ",
-                 choices=c("Kingdom/Superkingdom"=3, "Phylum"=4, "Class"=5, "Order"=6,
-                           "Family"=7,"Genus"=8,"Species"=9),
-                 selected = 2),
-		  
+	fluidPage(
+	  selectInput("taxonLevel", label = "Level: ", 
+	              choices = c("Phylum", "Class", "Order", "Family", "Genus", "Species"), selected = "Phylum"),
+	  div(style = "display: none;",
+	      checkboxInput("taxa_are_rows", label = "", value = T)),
+	  uiOutput("category"),
     		div(
     			style = "position:relative",
     			plotOutput("abundanceChart",
     								 hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce"),
     								 click = clickOpts("plot_click"), width = "100%"),
-    			uiOutput("hover_info"),
-    			dataTableOutput("sample_subset")
+    			uiOutput("hover_info")
+    			# dataTableOutput("sample_subset")
     		)
-		  )
-	  ))
-	)
+) 
+	
 ) # end shinyUI
