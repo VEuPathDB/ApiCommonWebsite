f0fa5d0b0d3f394c03794c765d6145d928fe10f8
Mark Heiges <mheiges@gmail.com>
2017-03-10 22:49:54 +0000

    move /dashboard and JMX mbeans (except CommentConfig) from ApiCommonWebsite to EuPathSiteCommon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@80450 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/jmx/mbeans/ServletVersionsMBean.java
+++
@@ -1,7 +0,0 @@
-package org.apidb.apicommon.jmx.mbeans;
-
-public interface ServletVersionsMBean {
-  public String getServerInfo();
-  public String getJspSpecVersion();
-  public String getServletApiVersion();
-}
