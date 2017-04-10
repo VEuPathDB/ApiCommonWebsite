6cae46c9759eb6d6065f1756739bb73604562833
Dave Falke <dmfalke@gmail.com>
2017-04-10 14:04:01 +0000

    add superfish files to pathway page. fixes #25721
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@80889 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/index.js
+++ Site/webapp/wdkCustomization/js/client/index.js
@@ -1,8 +1,5 @@
-// include css files
-// import 'eupathdb/wdkCustomization/css/superfish/css/superfish.css';
 import 'eupathdb/css/AllSites.css';
 import 'site/css/AllApiSites.css';
 import 'site/wdkCustomization/css/client.css';
-import 'site/wdkCustomization/css/pathway.css';
 
 import './main';
