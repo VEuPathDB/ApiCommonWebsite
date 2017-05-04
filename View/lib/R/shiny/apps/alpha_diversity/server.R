a315497529d08d8bc0d051f13685ae603d45b400
Fatima Silva Franco <frasil@_.com>
2017-05-04 11:52:46 +0000

    Shiny app alpha diversity plot with boxplot and table below when clicking
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@81559 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/alpha_diversity/server.R
+++ View/lib/R/shiny/apps/alpha_diversity/server.R
@@ -1,48 +1,67 @@
 library(shiny)
 library(ggplot2)
 library(phyloseq)
-library(reshape2)
+library(data.table)
 source("config.R")
 source("../../lib/wdkDataset.R")
 
 shinyServer(function(input, output, session) {
+  # Declaring some global variables
+  # df_abundance, df_sample and df_sample.formatted are declared global to avoid 
+  # multiple file reading in the reactive section
+  df_abundance <- NULL
+  df_sample <- NULL
+  df_sample.formatted <- NULL
+  
+  # global objects to read in more than one function
   columns <- NULL
   hash_sample_names<- NULL
   hash_count_samples <- NULL
-  richness_default <- NULL
-  plot_build <- NULL
+  ggplot_data <- NULL
+  ggplot_build_object <- NULL
+  
+  abundance_otu <- NULL
+  abundance_taxa <- NULL
   
   physeq <- reactive({
-    #Change with the file with abundances
-  	df_abundance <-
-  		read.csv(
-                        getWdkDatasetFile('TaxaRelativeAbundance.tab', session, FALSE, dataStorageDir),
-  			sep = "\t",
-  			col.names = c("Sample","Taxon", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "RelativeAbundance", "AbsoluteAbundance", "Nada")
-  		)
     
-    # Change with the Metadata file
-    df_sample <-
+    start.time <- Sys.time()
+    df_abundance <<-
+      read.csv(
+        getWdkDatasetFile('TaxaRelativeAbundance.tab', session, FALSE, dataStorageDir),
+        sep = "\t",
+        col.names = c("Sample","Taxon", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "RelativeAbundance", "AbsoluteAbundance", "EmptyColumn"),
+        colClasses = c("character", "integer", "character", "character", "character", "character", "character", "character", "character", "numeric", "integer", "character")
+      )
+    
+    # Change with the Characteristics file
+    df_sample <<-
       read.csv(
         getWdkDatasetFile('Characteristics.tab', session, FALSE, dataStorageDir),
         sep = "\t",
-        col.names = c("SampleName", "Source", "Property", "Value", "Type", "Filter", "EmptyColumn")
-      )
+        col.names = c("SampleName", "Source", "Property", "Value", "Type", "Filter", "EmptyColumn"),
+        colClasses = c("character", "character", "character", "character", "character", "character", "character")
+      ) 
     
-    df_sample.formatted <- dcast(data = df_sample,formula = SampleName~Property, value.var = "Value")
-    rownames(df_sample.formatted) <- df_sample.formatted[,1]
+    df_sample.formatted <<- dcast(data = df_sample,formula = SampleName~Property, value.var = "Value")
+    
+    end.time <- Sys.time()
+    time.taken <- end.time - start.time
+    write(paste("Elapsed time to load the files: ", time.taken), stderr())
+    
+    rownames(df_sample.formatted) <<- df_sample.formatted[,1]
     columns <<- colnames(df_sample.formatted)
     corrected_columns <-  make.names(columns)
-    colnames(df_sample.formatted) <- corrected_columns
+    colnames(df_sample.formatted) <<- corrected_columns
     names(corrected_columns) <- columns
     
     hash_sample_names <<- corrected_columns
     
     df_abundance.formatted <- dcast(data = df_abundance,formula = Kingdom+Phylum+Class+Order+Family+Genus+Species~Sample,fun.aggregate = sum,value.var = "AbsoluteAbundance")
     if(ncol(df_abundance.formatted) == 8){
-    	OTU_MATRIX <- df_abundance.formatted[,8, drop=F]
+      OTU_MATRIX <- df_abundance.formatted[,8, drop=F]
     }else{
-    	OTU_MATRIX <- df_abundance.formatted[,8:ncol(df_abundance.formatted)]
+      OTU_MATRIX <- df_abundance.formatted[,8:ncol(df_abundance.formatted)]
     }
     
     OTU = otu_table(OTU_MATRIX, taxa_are_rows = input$taxa_are_rows)
@@ -51,126 +70,243 @@ shinyServer(function(input, output, session) {
     TAX_MATRIX <- as.matrix(TAX_MATRIX)
     TAX <- tax_table(TAX_MATRIX)
     SAMPLE <- sample_data(df_sample.formatted)
-	
+    
     categories <- columns
     new_columns <- 0
     k <- 1
     merged_phyloseq<-phyloseq(OTU, TAX, SAMPLE)
     for(i in 1:length(columns)){
-    	unique_factors <- as.factor(sample_data(merged_phyloseq)[[hash_sample_names[[columns[i]]]]]) 
-    	if(length(levels(unique_factors)) > 1){
-    		new_columns[k] <- paste0(columns[i], " (",length(levels(unique_factors)), ")")
-    		hash_count_samples[[new_columns[k]]] <<- columns[i]
-    		k <- k+1
-    	}
+      unique_factors <- as.factor(sample_data(merged_phyloseq)[[hash_sample_names[[columns[i]]]]]) 
+      if(length(levels(unique_factors)) > 1){
+        new_columns[k] <- paste0(columns[i], " (",length(levels(unique_factors)), ")")
+        hash_count_samples[[new_columns[k]]] <<- columns[i]
+        k <- k+1
+      }
     }
     columns <<- new_columns
     
     merged_phyloseq
   })
-	
-	output$abundanceChart <- renderPlot({
-	  physeqobj <- physeq()
-	  if(!is.null(input$category)){
-	    if(identical(input$category, "All Samples")){
-	      chart <- plot_richness(physeqobj, measures = input$measureCheckBox)+theme(
-	        panel.grid.major.x = element_blank()
-	      )+geom_point(size = 4, alpha= 0.5)
-	    }else{
-	      chart <- plot_richness(physeqobj, x=hash_sample_names[[hash_count_samples[[input$category]]]], measures = input$measureCheckBox)+theme(
-	        panel.grid.major.x = element_blank()
-	      )+geom_point(size = 4, alpha= 0.5)
-	    }
-	    richness_default <<- chart$data 
-	  }else{
-	    chart <- NULL
-	  }
-	  chart
-	})
-	
-	output$category <- renderUI({
-	  lvls <- columns
-	  lvls[1] <- "All Samples"
-	  selectInput("category", label = "Category: ", 
-	              choices = lvls, selected = 1)
-	  
-	})
-	
-	output$hover_info <- renderUI({
-	    hover <- input$plot_hover
-	    if (is.null(hover$x))
-	      return(NULL)
-	   
-	    left_pct <-
-	      (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
-	    top_pct <-
-	      (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+  
+  output$abundanceChart <- renderPlot({
+    physeqobj <- physeq()
+    if(!is.null(input$category)){
+      if(identical(input$category, "All Samples")){
+        if(identical(input$plotTypeRadio, "dotplot")){
+          chart <- plot_richness(physeqobj, measures = input$measureCheckBox)+theme(
+            panel.grid.major.x = element_blank()
+          )+geom_point(size = 4, alpha= 0.5)+coord_fixed()
+        }else{
+          rich <- estimate_richness(physeqobj, measures = input$measureCheckBox)
+          rich$SampleName <- rownames(rich)
+          data_melted<-melt(rich, id.vars = c("SampleName"),  measure.vars=input$measureCheckBox)
+          abundance_otu <<- data_melted
+          chart<-ggplot(data_melted, aes(variable, value))+geom_boxplot()+
+            facet_wrap(~ variable, scales="free")+
+            labs(x="All Samples", y="Alpha Diversity Measure")
+          
+        }
+      }else{
+        if(identical(input$plotTypeRadio, "dotplot")){
+          chart <- plot_richness(physeqobj, x=hash_sample_names[[hash_count_samples[[input$category]]]], measures = input$measureCheckBox)+
+            theme(panel.grid.major.x = element_blank(), strip.text.y = element_text(size=18))+
+            geom_point(size = 4, alpha= 0.5) 
+        }else{
+          category<-hash_sample_names[[hash_count_samples[[input$category]]]]
+          rich <- estimate_richness(physeqobj, measures = input$measureCheckBox)
+          
+          df_sample_selected <- df_sample.formatted[,c("SampleName", category)]
+          
+          richness_merged <- cbind(df_sample_selected, rich)
+          
+          data_melted<-melt(richness_merged, id.vars = c("SampleName", category),  measure.vars=input$measureCheckBox)
+          
+          chart<-ggplot(data_melted, aes_string(category, "value"))+geom_boxplot()+
+            facet_wrap(as.formula("~ variable "), scales = "free_y")+
+            labs(x=stringi::stri_trans_totitle(hash_count_samples[[input$category]]), y="Alpha Diversity Measure")
+        }
+      }
+      ggplot_data <<- chart$data
+      ggplot_build_object <<- ggplot_build(chart)
+    }else{
+      chart <- NULL
+    }
+    output$sample_subset <- renderDataTable(NULL)
+    chart
+  })
+  
+  output$category <- renderUI({
+    lvls <- columns
+    lvls[1] <- "All Samples"
+    selectInput("category", label = "Category: ", 
+                choices = lvls, selected = 1)
+    
+  })
+  
+  output$hover_info <- renderUI({
+    hover <- input$plot_hover
+    physeqobj <- physeq()
+    lvls <- rownames(sample_data(physeqobj))
+    
+    if (is.null(hover$x) || round(hover$x) <0 || round(hover$y)<0 || is.null(hover$y))
+      return(NULL)
+    
+    left_pct <-
+      (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct <-
+      (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+    
+    left_px <-
+      hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px <-
+      hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+    
+    style <-
+      paste0(
+        "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); "
+        ,
+        "left:",
+        left_px + 2,
+        "px; top:",
+        top_px + 2,
+        "px;"
+      )
+    
+    if(identical(input$plotTypeRadio, "dotplot") ){
+      near_points <- nearPoints(ggplot_data, hover)
 
-	    left_px <-
-	      hover$range$left + left_pct * (hover$range$right - hover$range$left)
-	    top_px <-
-	      hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
-
-	    style <-
-	      paste0(
-	        "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); "
-	        ,
-	        "left:",
-	        left_px + 2,
-	        "px; top:",
-	        top_px + 2,
-	        "px;"
-	      )
-	    near_points <- nearPoints(richness_default, hover)
-	    physeqobj <- physeq()
-	    if(identical(input$category, "All Samples")){
-	      if(nrow(near_points) > 0){
-	        lvls <- rownames(sample_data(physeqobj))
-	        hover_sample <- lvls[round(hover$x)]
-	        alpha_and_sample <- ""
-	        for(i in 1:nrow(near_points)){
-	          if(!is.na(near_points[i,"se"]) && near_points[i,"se"]!=0){
-	            alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
-	                                       "<br><b>Alpha diversity: </b>", sprintf("%.3f [SE: %.5f]",near_points[i,"value"],
-	                                                                               near_points[i,"se"]),"<br>")
-	          }else{
-	            alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
-	                                       "<br><b>Alpha diversity: </b>", sprintf("%.3f",near_points[i,"value"]),"<br>")
-	          }
-	        }
-	        wellPanel(style = style,
-	                  tags$b("Measure: "),
-	                  hover$panelvar1,
-	                  br(),
-	                  HTML(alpha_and_sample)
-	        )
-	      }
-	    }else{
-	      if(nrow(near_points) > 0){
-	        category_value <- sample_data(physeqobj)[,hash_sample_names[[hash_count_samples[[input$category]]]]]
-	        lvls <- levels(as.factor(category_value[[1]]))
-	        hover_category <- lvls[round(hover$x)]
-	        alpha_and_sample <- ""
-	        for(i in 1:nrow(near_points)){
-	          if(!is.na(near_points[i,"se"]) && near_points[i,"se"]!=0){
-	            alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
-	                                       "<br><b>Alpha diversity: </b>", sprintf("%.3f [SE: %.5f]",near_points[i,"value"],
-	                                                                               near_points[i,"se"]),"<br>")
-	          }else{
-	            alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
-	                                       "<br><b>Alpha diversity: </b>", sprintf("%.3f",near_points[i,"value"]),"<br>")
-	          }
-	        }
-	        wellPanel(style = style,
-	                  tags$b("Measure: "),
-	                  hover$panelvar1,
-	                  br(),
-	                  tags$b(paste(hash_count_samples[[input$category]], ": ")),
-	                  hover_category,
-	                  br(),
-	                  HTML(alpha_and_sample)
-	        )
-	      }
-	    }
-	})
+      if(identical(input$category, "All Samples")){
+        if(nrow(near_points) > 0){
+          lvls <- rownames(sample_data(physeqobj))
+          hover_sample <- lvls[round(hover$x)]
+          alpha_and_sample <- ""
+          for(i in 1:nrow(near_points)){
+            if(!is.na(near_points[i,"se"]) && near_points[i,"se"]!=0){
+              alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
+                                         "<br><b>Alpha diversity: </b>", sprintf("%.3f [SE: %.5f]",near_points[i,"value"],
+                                                                                 near_points[i,"se"]),"<br>")
+            }else{
+              alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
+                                         "<br><b>Alpha diversity: </b>", sprintf("%.3f",near_points[i,"value"]),"<br>")
+            }
+          }
+          wellPanel(style = style,
+                    tags$b("Measure: "),
+                    hover$panelvar1,
+                    br(),
+                    HTML(alpha_and_sample)
+          )
+        }
+      }else{
+        if(nrow(near_points) > 0){
+          category_value <- sample_data(physeqobj)[,hash_sample_names[[hash_count_samples[[input$category]]]]]
+          lvls <- levels(as.factor(category_value[[1]]))
+          hover_category <- lvls[round(hover$x)]
+          alpha_and_sample <- ""
+          for(i in 1:nrow(near_points)){
+            if(!is.na(near_points[i,"se"]) && near_points[i,"se"]!=0){
+              alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
+                                         "<br><b>Alpha diversity: </b>", sprintf("%.3f [SE: %.5f]",near_points[i,"value"],
+                                                                                 near_points[i,"se"]),"<br>")
+            }else{
+              alpha_and_sample <- paste0(alpha_and_sample, "<b>Sample: </b>",near_points[i,"samples"],
+                                         "<br><b>Alpha diversity: </b>", sprintf("%.3f",near_points[i,"value"]),"<br>")
+            }
+          }
+          wellPanel(style = style,
+                    tags$b("Measure: "),
+                    hover$panelvar1,
+                    br(),
+                    tags$b(paste(hash_count_samples[[input$category]], ": ")),
+                    hover_category,
+                    br(),
+                    HTML(alpha_and_sample)
+          )
+        }
+      }
+    }else{ # end if(identical(input$plotTypeRadio, "dotplot") ){
+      gg_data <- ggplot_build_object$data[[1]]
+      subBoxplot<-subset(gg_data,hover$x>=xmin & hover$x<=xmax & hover$y>=ymin & hover$y<=ymax & PANEL==match(hover$panelvar1, input$measureCheckBox))
+      alpha_and_sample <- NULL
+      if(identical(input$category, "All Samples")){
+        if(nrow(subBoxplot)>0){
+          alpha_and_sample <- ""
+          for(i in 1:nrow(subBoxplot)){
+            alpha_and_sample <- paste0(alpha_and_sample,
+                                       sprintf("<b>Alpha diversity values:</b><br>&nbsp;&nbsp;<b>min: </b>%f - <b>max: </b>%f<br>
+                                               &nbsp;&nbsp;<b>25th percentile: </b>%f<br>&nbsp;&nbsp;<b>Median: </b>%f<br>&nbsp;&nbsp;<b>75th percentile: </b>%f<br>",
+                                               subBoxplot[i,"ymin"],subBoxplot[i,"ymax"],subBoxplot[i,"lower"],subBoxplot[i,"middle"],subBoxplot[i,"upper"]) )
+          }
+          
+        }else{
+          near_points <- nearPoints(ggplot_data, hover)
+          if(nrow(near_points) > 0){
+            alpha_and_sample <- ""
+            for(i in 1:nrow(near_points)){
+              alpha_and_sample <- paste0(alpha_and_sample,
+                                         sprintf("<b>Sample: </b>%s<br><b>Alpha diversity: </b>%.3f<br>",
+                                                 near_points[i,"SampleName"],near_points[i,"value"]))
+            }
+          }
+        }
+        # return(NULL)
+      }else{ # if(identical(input$category, "All Samples")){
+        column_category<-hash_sample_names[[hash_count_samples[[input$category]]]]
+        original_category<-hash_count_samples[[input$category]]
+        all_groups <- levels(as.factor(ggplot_data[,column_category]))
+        if(nrow(subBoxplot)>0){
+          alpha_and_sample<-""
+          for(i in 1:nrow(subBoxplot)){
+            alpha_and_sample <- paste0(alpha_and_sample,
+                                     sprintf("<b>Alpha diversity values:</b><br>&nbsp;&nbsp;<b>min: </b>%f - <b>max: </b>%f<br>
+                                             &nbsp;&nbsp;<b>25th percentile: </b>%f<br>&nbsp;&nbsp;<b>Median: </b>%f<br>&nbsp;&nbsp;<b>75th percentile: </b>%f<br><b>%s: </b>%s<br>",
+                                             subBoxplot[i,"ymin"],subBoxplot[i,"ymax"],subBoxplot[i,"lower"],subBoxplot[i,"middle"], subBoxplot[i,"upper"], original_category, all_groups[subBoxplot[i,"group"]]) )
+          }
+        }else{
+          near_points <- nearPoints(ggplot_data, hover)
+          if(nrow(near_points) > 0){
+            alpha_and_sample <- ""
+            for(i in 1:nrow(near_points)){
+              alpha_and_sample <- paste0(alpha_and_sample,
+                                         sprintf("<b>Sample: </b>%s<br><b>Alpha diversity: </b>%.3f<br><b>%s</b>: %s<br>",
+                                                 near_points[i,"SampleName"],near_points[i,"value"], original_category, near_points[i,column_category]))
+            }
+          }
+        }
+      }# else
+      if(!is.null(alpha_and_sample)){
+        wellPanel(style = style,
+                  tags$b("Measure: "),
+                  hover$panelvar1,
+                  br(),
+                  HTML(alpha_and_sample)
+        )
+      }else{
+        return(NULL)
+      }
+    }
+  })
+  
+  observeEvent(input$plot_click, {
+    click <- input$plot_click
+    if (is.null(click$y))
+      return(NULL)
+    
+    if(identical(input$category, "All Samples")){
+      output_table <- subset(ggplot_data, ggplot_data$variable==click$panelvar1, select=c("SampleName", "variable", "value"))
+      colnames(output_table)<-c("Sample", "Measure", "Alpha Diversity")
+    }else{
+      column_category<-hash_sample_names[[hash_count_samples[[input$category]]]]
+      original_category<-hash_count_samples[[input$category]]
+      
+      output_table <- subset(ggplot_data, ggplot_data$variable==click$panelvar1, select=c("SampleName", "variable", "value", column_category))
+      colnames(output_table)<-c("Sample", "Measure", "Alpha Diversity", original_category)
+    }
+    
+    output$sample_subset <- renderDataTable(output_table, 
+                                            options = list(
+                                              order = list(list(2, 'desc'))
+                                            )
+    )
+  })
 })
--- View/lib/R/shiny/apps/alpha_diversity/ui.R
+++ View/lib/R/shiny/apps/alpha_diversity/ui.R
@@ -2,24 +2,45 @@ library(shiny)
 
 shinyUI(
 	fluidPage(
-    checkboxGroupInput("measureCheckBox", label="Measure(s):",
-                     choices = c("Chao1" = "Chao1",
-                                 "ACE" = "ACE",
-                       "Shannon" = "Shannon",
-                       "Simpson" = "Simpson",
-                       "Fisher" = "Fisher"),
-                     selected = c("Chao1", "Shannon"), inline=T),
-    div(style = "display: none;",
-	     checkboxInput("taxa_are_rows", label = "", value = T)
-	 ),
-	  uiOutput("category"),
-    		div(
-    			style = "position:relative",
-    			plotOutput("abundanceChart",
-    								 hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce"),
-    								 click = clickOpts("plot_click"), width = "100%"),
-    			uiOutput("hover_info")
-    			# dataTableOutput("sample_subset")
-    		)
+	  fluidRow(
+	    column(5, 
+	           checkboxGroupInput("measureCheckBox", label="Measure(s):",
+	                              choices = c("Chao1" = "Chao1",
+	                                          "ACE" = "ACE",
+	                                          "Shannon" = "Shannon",
+	                                          "Simpson" = "Simpson",
+	                                          "Fisher" = "Fisher"),
+	                              selected = c("Chao1", "Shannon"), inline=T),
+	           div(style = "display: none;",
+	               checkboxInput("taxa_are_rows", label = "", value = T) )
+	    ),
+	    column(7,
+	           radioButtons("plotTypeRadio", label="Visualization type:",
+	                        choices = c("Boxplot" = "boxplot",
+	                                    "Dot plot" = "dotplot"),
+	                        selected = c("boxplot"), inline=T)
+	    )
+	  ),
+	  fluidRow(
+	    column(6,
+	           uiOutput("category")
+	    )
+	  ),
+	  fluidRow(
+	    column(12,
+	           div(
+	             style = "position:relative",
+	             plotOutput("abundanceChart",
+	                        hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce"),
+	                        click = clickOpts("plot_click"), width = "100%"),
+	             uiOutput("hover_info")
+	           ) 
+	    )
+	  ),
+	  fluidRow(
+	    column(12,
+	           dataTableOutput("sample_subset")
+	    )
 	  )
+	)
 ) # end shinyUI
