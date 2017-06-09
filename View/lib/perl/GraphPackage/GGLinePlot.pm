b50a5b10b62acdfbde4ac078989cc487ccbe4ca2
John Brestelli <jbrestell@gmail.com>
2017-06-09 18:25:09 +0000

    gridlines for ggplot
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@82173 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/GGLinePlot.pm
+++ View/lib/perl/GraphPackage/GGLinePlot.pm
@@ -358,12 +358,14 @@ if(is.null(profile.df.full\$LEGEND)) {
 if(is.compact) {
   gp = gp + theme_void() + theme(legend.position=\"none\");
 } else if(is.thumbnail) {
+  gp = gp + theme_bw();
   gp = gp + labs(title=\"$plotTitle\", y=\"$yAxisLabel\", x=NULL);
   gp = gp + ylim(y.min, y.max);
   gp = gp + scale_x_discrete(label=abbreviate);
   gp = gp + theme(axis.text.x  = element_text(angle=90,vjust=0.5, size=9), plot.title = element_text(colour=\"#b30000\"));
   gp = gp + theme(legend.position=\"none\");
 } else {
+  gp = gp + theme_bw();
   gp = gp + labs(title=\"$plotTitle\", y=\"$yAxisLabel\", x=\"$xAxisLabel\");
   gp = gp + ylim(y.min, y.max);
   gp = gp + theme(plot.title = element_text(colour=\"#b30000\"))
