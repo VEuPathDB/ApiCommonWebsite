4f6def408963a269de5934605c0d85a7115f0b6f
Ryan Doherty <tech@conical.org>
2017-06-16 03:31:02 +0000

    Rewrite to fix logical errors and use db links in constants
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/account_db@82295 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/migrate/B33_To_B34_Migration.java
+++ Model/src/java/org/apidb/apicommon/model/migrate/B33_To_B34_Migration.java
@@ -8,6 +8,7 @@ import org.gusdb.fgputil.db.platform.SupportedPlatform;
 import org.gusdb.fgputil.db.pool.DatabaseInstance;
 import org.gusdb.fgputil.db.pool.SimpleDbConfig;
 import org.gusdb.fgputil.db.runner.SQLRunner;
+import org.gusdb.fgputil.db.runner.SingleLongResultSetHandler;
 import org.gusdb.fgputil.db.slowquery.QueryLogger;
 
 /**
@@ -40,51 +41,64 @@ import org.gusdb.fgputil.db.slowquery.QueryLogger;
  */
 public class B33_To_B34_Migration {
 
-  private static final boolean WRITE_TO_DB = true;     // keep off to check generated SQL
+  // configuration constants
+  private static final boolean WRITE_TO_DB = false;    // keep off to check generated SQL
   private static final boolean REPLICATED_DBS = false; // keep off until testing on apicommDev
+
+  // connection information to user DBs
+  private static final String PRIMARY_USERDB_CONNECTION_URL = "jdbc:oracle:oci:@rm9972"; // to be apicommdevn
+  @SuppressWarnings("unused")
+  private static final String REPLICATED_USERDB_CONNECTION_URL = "jdbc:oracle:oci:@rm9972"; // to be apicommdevs
 
   // connection information to account DBs
   private static final String PRIMARY_ACCTDB_CONNECTION_URL = "jdbc:oracle:oci:@acctdbn";
   private static final String REPLICATED_ACCTDB_CONNECTION_URL = "jdbc:oracle:oci:@acctdbs";
 
+  // object names when operating in userDBs
+  private static final String USER_DB_SCHEMA = "userlogins5.";
+  private static final String USERS_TABLE = USER_DB_SCHEMA + "users";
+  private static final String BACKUP_USERS_TABLE = USER_DB_SCHEMA + "users_backup";
+
   // object names when operating in account DBs
   private static final String ACCOUNT_DB_SCHEMA = "useraccounts.";
-  private static final String SOURCE_USERS_TABLE = "users"; // add dblink here if needed
-  private static final String NEW_TABLE_ACCOUNTS = "accounts";
-  private static final String NEW_TABLE_ACCOUNT_PROPS = "account_properties";
-  private static final String NEW_USER_ID_SEQUENCE = NEW_TABLE_ACCOUNTS + "_PKSEQ";
+  private static final String NEW_TABLE_ACCOUNTS = ACCOUNT_DB_SCHEMA + "accounts";
+  private static final String NEW_TABLE_ACCOUNT_PROPS = ACCOUNT_DB_SCHEMA + "account_properties";
+  private static final String NEW_USER_ID_SEQUENCE = ACCOUNT_DB_SCHEMA + NEW_TABLE_ACCOUNTS + "_PKSEQ";
 
-  // connection information to user DBs
-  private static final String PRIMARY_USERDB_CONNECTION_URL = "jdbc:oracle:oci:@rm9972"; // to be apicommdevn
-  //private static final String REPLICATED_USERDB_CONNECTION_URL = "jdbc:oracle:oci:@rm9972"; // to be apicommdevs
-  private static final String USER_DB_SCHEMA = "userlogins5.";
+  // userdb objects needed to be read while creating account DB
+  private static final String ACCOUNTDB_DBLINK_TO_USERDB = ""; // add dblink suffix (including '@') to userdb here if needed
+  private static final String SOURCE_USERS_TABLE = USERS_TABLE + ACCOUNTDB_DBLINK_TO_USERDB;
+  private static final String SOURCE_USERS_BACKUP_TABLE = BACKUP_USERS_TABLE + ACCOUNTDB_DBLINK_TO_USERDB;
+  private static final String SOURCE_USER_ID_SEQUENCE = SOURCE_USERS_TABLE + "_PKSEQ" + ACCOUNTDB_DBLINK_TO_USERDB;
 
-  // object names when operating in userDBs
-  private static final String USERS_TABLE = "users";
-  private static final String BACKUP_USERS_TABLE = "users_backup";
-
-  /*============= BEGIN SQL TO BE EXECUTED ON ACCOUNT_DB=============*/
+  /*======================================================*/
+  /*          SQL to be executed on AccountDB             */
+  /*======================================================*/
 
   private static final String SEQUENCE_START_NUM_MACRO = "$$sequence_start_macro$$";
 
+  private static final String READ_OLD_USER_ID_SEQUENCE =
+      "SELECT " + SOURCE_USER_ID_SEQUENCE + ".nextval" + ACCOUNTDB_DBLINK_TO_USERDB + " from dual";
+  
   private static final String CREATE_ACCOUNT_USER_ID_SEQUENCE =
-      "CREATE SEQUENCE " + ACCOUNT_DB_SCHEMA + NEW_USER_ID_SEQUENCE +
+      "CREATE SEQUENCE " + NEW_USER_ID_SEQUENCE +
       "  MINVALUE 1 MAXVALUE 9999999999999999999999999999" +
       "  INCREMENT BY 10" +
       "  START WITH " + SEQUENCE_START_NUM_MACRO +
       "  CACHE 20 NOORDER NOCYCLE";
 
   private static final String CREATE_ACCOUNT_TABLE_SQL =
-      "create table " + ACCOUNT_DB_SCHEMA + NEW_TABLE_ACCOUNTS + " as ( " +
+      "create table " + NEW_TABLE_ACCOUNTS + " as ( " +
       "  select user_id, email, passwd, is_guest, signature, address as stable_id, register_time, last_active as last_login " +
-      "  from " + USER_DB_SCHEMA + SOURCE_USERS_TABLE +
+      "  from " + SOURCE_USERS_TABLE +
+      "  where is_guest = 0" +
       ")";
 
   private static final String SELECT_USER_PROPS_SQL_SUFFIX =
-      " from " + USER_DB_SCHEMA + SOURCE_USERS_TABLE + " where is_guest = 0 ";
+      " from " + SOURCE_USERS_TABLE + " where is_guest = 0 ";
 
   private static final String CREATE_ACCOUNT_PROPS_TABLE_SQL =
-      "create table " + ACCOUNT_DB_SCHEMA + NEW_TABLE_ACCOUNT_PROPS + " as ( " +
+      "create table " + NEW_TABLE_ACCOUNT_PROPS + " as ( " +
       "  select user_id, 'first_name' as key, first_name as value " + SELECT_USER_PROPS_SQL_SUFFIX +
       "  union " +
       "  select user_id, 'middle_name' as key, middle_name as value " + SELECT_USER_PROPS_SQL_SUFFIX +
@@ -94,34 +108,71 @@ public class B33_To_B34_Migration {
       "  select user_id, 'organization' as key, organization as value " + SELECT_USER_PROPS_SQL_SUFFIX +
       ")";
 
-  private static final String DELETE_ACCOUNTS_TABLE_SQL = "delete table " + ACCOUNT_DB_SCHEMA + NEW_TABLE_ACCOUNTS;
-  private static final String DELETE_ACCOUNT_PROPS_TABLE_SQL = "delete table " + ACCOUNT_DB_SCHEMA + NEW_TABLE_ACCOUNT_PROPS;
-  private static final String DELETE_ACCOUNT_SEQUENCE_TABLE_SQL = "delete sequence " + ACCOUNT_DB_SCHEMA + NEW_USER_ID_SEQUENCE;
+  private static final String RESIZE_PROPERTY_VALUE_COL_SQL =
+      "ALTER TABLE " + NEW_TABLE_ACCOUNT_PROPS + " MODIFY value VARCHAR2(4000)";
 
-  /*============= END SQL TO BE EXECUTED ON ACCOUNT_DB =============*/
+  private static final String DELETE_ACCOUNTS_TABLE_SQL = "delete table " + NEW_TABLE_ACCOUNTS;
+  private static final String DELETE_ACCOUNT_PROPS_TABLE_SQL = "delete table " + NEW_TABLE_ACCOUNT_PROPS;
+  private static final String DELETE_ACCOUNT_SEQUENCE_TABLE_SQL = "delete sequence " + NEW_USER_ID_SEQUENCE;
 
-  /*============= BEGIN SQL TO BE EXECUTED ON USER_DB =============*/
+  /*======================================================*/
+  /*          SQL to be executed on UserDB             */
+  /*======================================================*/
 
   private static final String BACK_UP_USERS_TABLE =
-      "create table " + USER_DB_SCHEMA + BACKUP_USERS_TABLE + " as (" +
-      "  select * from " + USER_DB_SCHEMA + USERS_TABLE +
+      "create table " + BACKUP_USERS_TABLE + " as (" +
+      "  select * from " + USERS_TABLE +
       ")";
 
   private static final String DROP_COLS_FROM_USERS_TABLE =
-      "alter table " + USER_DB_SCHEMA + USERS_TABLE + " drop (" +
+      "alter table " + USERS_TABLE + " drop (" +
       "  EMAIL, PASSWD, SIGNATURE, REGISTER_TIME, LAST_ACTIVE, LAST_NAME," +
       "  FIRST_NAME, MIDDLE_NAME, TITLE, ORGANIZATION, DEPARTMENT, ADDRESS," +
       "  CITY, STATE, ZIP_CODE, PHONE_NUMBER, COUNTRY, PREV_USER_ID, MIGRATION_ID" +
       ")";
 
-  /*============= END SQL TO BE EXECUTED ON USER_DB =============*/
+  /*===========================================================================*/
+  /* SqlGetter interface and implementations which provide various SQLs to run */
+  /*===========================================================================*/
+
+  private static interface SqlGetter {
+    public String getSql(DataSource ds) throws Exception;
+  }
+
+  private static SqlGetter doSql(String sql) {
+    return ds -> sql;
+  }
+
+  private static SqlGetter createAccountSequenceFromUserSequence() {
+    return ds -> {
+      SingleLongResultSetHandler result = new SingleLongResultSetHandler();
+      new SQLRunner(ds, READ_OLD_USER_ID_SEQUENCE).executeQuery(result);
+      return CREATE_ACCOUNT_USER_ID_SEQUENCE.replace(
+          SEQUENCE_START_NUM_MACRO, result.getRetrievedValue().toString());
+    };
+  }
+
+  private static SqlGetter conditionallyUseBackupTable(String originalSql) {
+    return ds -> new Oracle().checkTableExists(
+        ds, USER_DB_SCHEMA.substring(0, USER_DB_SCHEMA.length() - 1), SOURCE_USERS_BACKUP_TABLE) ?
+            originalSql.replace(SOURCE_USERS_TABLE, SOURCE_USERS_BACKUP_TABLE) : originalSql;
+  }
+
+  /*===================================================================*/
+  /* Groups of commands to be run depending on command and replication */
+  /*===================================================================*/
 
   private static final SqlGetter[] PRIMARY_SQLS_TO_RUN_ACCOUNT_DB = {
       // create a new sequence in account DB with the start ID of the old sequence
       createAccountSequenceFromUserSequence(),
       // create new account tables from user table
-      doSql(CREATE_ACCOUNT_TABLE_SQL),
-      doSql(CREATE_ACCOUNT_PROPS_TABLE_SQL),
+      conditionallyUseBackupTable(CREATE_ACCOUNT_TABLE_SQL),
+      conditionallyUseBackupTable(CREATE_ACCOUNT_PROPS_TABLE_SQL),
+      doSql(RESIZE_PROPERTY_VALUE_COL_SQL)
+  };
+
+  private static final SqlGetter[] REPLICATED_SQLS_TO_RUN_ACCOUNT_DB = {
+      createAccountSequenceFromUserSequence()
   };
 
   private static final SqlGetter[] PRIMARY_SQLS_TO_RUN_USER_DB = {
@@ -131,35 +182,42 @@ public class B33_To_B34_Migration {
       doSql(DROP_COLS_FROM_USERS_TABLE)
   };
 
-  private static final SqlGetter[] REPLICATED_SQLS_TO_RUN_ACCOUNT_DB = {
-      createAccountSequenceFromUserSequence()
-  };
-
   private static final SqlGetter[] DROP_ACCOUNT_DB_SQLS = {
       doSql(DELETE_ACCOUNT_PROPS_TABLE_SQL),
       doSql(DELETE_ACCOUNTS_TABLE_SQL),
       doSql(DELETE_ACCOUNT_SEQUENCE_TABLE_SQL)
   };
 
+  /*============================================*/
+  /*         main method and subroutines        */
+  /*============================================*/
+
+  private static void printUsageAndExit() {
+    System.err.println("USAGE: fgpJava " + B33_To_B34_Migration.class.getName() + " [create|drop] <db_user> <db_password>");
+    System.exit(1);
+  }
+
   public static void main(String[] args) {
-    if (args.length != 3 || (!args[0].equals("create") && !args[0].equals("drop")) ||
-        args[1].trim().isEmpty() || args[2].trim().isEmpty()) {
-      System.err.println("USAGE: fgpJava " + B33_To_B34_Migration.class.getName() + " [create|drop] <db_user> <db_password>");
-      System.exit(1);
+    if (args.length != 3 || args[1].trim().isEmpty() || args[2].trim().isEmpty()) {
+      printUsageAndExit();
     }
     String operation = args[0];
     String dbUser = args[1];
     String dbPassword = args[2];
     QueryLogger.setInactive();
-    if (operation.equals("create")) {
-      runSqls(PRIMARY_ACCTDB_CONNECTION_URL, PRIMARY_SQLS_TO_RUN_ACCOUNT_DB, dbUser, dbPassword);
-      runSqls(PRIMARY_USERDB_CONNECTION_URL, PRIMARY_SQLS_TO_RUN_USER_DB, dbUser, dbPassword);
-      if (REPLICATED_DBS) {
-        runSqls(REPLICATED_ACCTDB_CONNECTION_URL, REPLICATED_SQLS_TO_RUN_ACCOUNT_DB, dbUser, dbPassword);
-      }
-    }
-    else {
-      runSqls(PRIMARY_ACCTDB_CONNECTION_URL, DROP_ACCOUNT_DB_SQLS, dbUser, dbPassword);
+    switch (operation) {
+      case "create":
+        runSqls(PRIMARY_ACCTDB_CONNECTION_URL, PRIMARY_SQLS_TO_RUN_ACCOUNT_DB, dbUser, dbPassword);
+        runSqls(PRIMARY_USERDB_CONNECTION_URL, PRIMARY_SQLS_TO_RUN_USER_DB, dbUser, dbPassword);
+        if (REPLICATED_DBS) {
+          runSqls(REPLICATED_ACCTDB_CONNECTION_URL, REPLICATED_SQLS_TO_RUN_ACCOUNT_DB, dbUser, dbPassword);
+        }
+        break;
+      case "drop":
+        runSqls(PRIMARY_ACCTDB_CONNECTION_URL, DROP_ACCOUNT_DB_SQLS, dbUser, dbPassword);
+        break;
+      default:
+        printUsageAndExit();
     }
   }
 
@@ -179,20 +237,5 @@ public class B33_To_B34_Migration {
       System.err.println("Error while executing migration: " + FormatUtil.getStackTrace(e));
       System.exit(2);
     }
-  }
-
-  private static interface SqlGetter {
-    public String getSql(DataSource ds) throws Exception;
-  }
-
-  private static SqlGetter doSql(String sql) {
-    return ds -> sql;
-  }
-
-  private static SqlGetter createAccountSequenceFromUserSequence() {
-    return ds -> {
-      Long nextId = new Oracle().getNextId(ds, USER_DB_SCHEMA, SOURCE_USERS_TABLE);
-      return CREATE_ACCOUNT_USER_ID_SEQUENCE.replace(SEQUENCE_START_NUM_MACRO, nextId.toString());
-    };
   }
 }
