2b0759639b8de2c367434a89561d4d9db70fdd8f
Danielle Callan <dcallan@upenn.edu>
2017-09-05 14:59:22 +0000

    fix x axis label for kazura plot
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@84357 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Kazura/AbMicroarray.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Kazura/AbMicroarray.pm
@@ -31,6 +31,7 @@ sub init {
 #  my $scatter = EbrcWebsiteCommon::View::GraphPackage::ScatterPlot::ClinicalMetaData->new(@_);
   $scatter->setProfileSets($profileSets);
   $scatter->setColors($colors);
+  $scatter->setXaxisLabel("Age");
 
   $self->setGraphObjects($scatter);
 
