28eaa00219f913a1d9195c2b6c0b37a5f81eb37a
Cris Lawrence <crisl@upenn.edu>
2017-11-03 19:49:46 +0000

    Initial attempt to include user_db login as service - using JSON and not redirecting for user_db case
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@85339 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Service/src/main/java/org/apidb/apicommon/service/services/ApiSessionService.java
+++ Service/src/main/java/org/apidb/apicommon/service/services/ApiSessionService.java
@@ -3,7 +3,7 @@ package org.apidb.apicommon.service.services;
 import static org.gusdb.fgputil.FormatUtil.urlEncodeUtf8;
 
 import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.jspwrap.UserBean;
@@ -13,9 +13,10 @@ public class ApiSessionService extends SessionService {
 	
   private static final String GBROWSE_SETUP_PAGE = "/gbrowse/gbrowseSetup.html";
 
-  protected Response createSuccessResponse(String redirectUrl, UserBean userBean, NewCookie cookie) throws WdkModelException {
+  @Override
+  protected ResponseBuilder createSuccessResponse(String redirectUrl, UserBean userBean, NewCookie cookie, boolean performRedirect) throws WdkModelException {
 	String gbrowseLoginUrl = getGbrowsePathWithParams(redirectUrl, getWdkModel().getProjectId(), cookie.getMaxAge(), userBean.getFirstName());
-	return super.createSuccessResponse(gbrowseLoginUrl, userBean, cookie);
+	return super.createSuccessResponse(gbrowseLoginUrl, userBean, cookie, performRedirect);
   }
   
   private static String getGbrowsePathWithParams(String redirectUrl, String project, int wdkCookieMaxAge, String displayName) {
