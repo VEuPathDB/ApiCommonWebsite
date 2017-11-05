f17008b41f010a13f5537247f1bb9ec46a9757eb
Ryan Doherty <tech@conical.org>
2017-11-05 01:01:29 +0000

    Finish up login/logout services, including client-side code and slight server-side refactor
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@85346 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Service/src/main/java/org/apidb/apicommon/service/services/ApiSessionService.java
+++ Service/src/main/java/org/apidb/apicommon/service/services/ApiSessionService.java
@@ -2,30 +2,23 @@ package org.apidb.apicommon.service.services;
 
 import static org.gusdb.fgputil.FormatUtil.urlEncodeUtf8;
 
-import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.servlet.http.Cookie;
 
-import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.jspwrap.UserBean;
+import org.gusdb.wdk.model.user.User;
 import org.gusdb.wdk.service.service.SessionService;
 
 public class ApiSessionService extends SessionService {
-	
+
   private static final String GBROWSE_SETUP_PAGE = "/gbrowse/gbrowseSetup.html";
 
   @Override
-  protected ResponseBuilder createSuccessResponse(String redirectUrl, UserBean userBean, NewCookie cookie, boolean performRedirect) throws WdkModelException {
-	String gbrowseLoginUrl = getGbrowsePathWithParams(redirectUrl, getWdkModel().getProjectId(), cookie.getMaxAge(), userBean.getFirstName());
-	return super.createSuccessResponse(gbrowseLoginUrl, userBean, cookie, performRedirect);
+  protected String getSuccessRedirectUrl(String redirectUrl, User user, Cookie cookie) {
+    return new StringBuilder(GBROWSE_SETUP_PAGE)
+      .append("?redirectUrl=").append(urlEncodeUtf8(redirectUrl))
+      .append("&project=").append(getWdkModel().getProjectId())
+      .append("&cookieMaxAge=").append(cookie.getMaxAge())
+      .append("&userDisplayName=").append(urlEncodeUtf8(new UserBean(user).getFirstName()))
+      .toString();
   }
-  
-  private static String getGbrowsePathWithParams(String redirectUrl, String project, int wdkCookieMaxAge, String displayName) {
-    return new StringBuffer(GBROWSE_SETUP_PAGE)
-            .append("?redirectUrl=").append(urlEncodeUtf8(redirectUrl))
-            .append("&project=").append(project)
-            .append("&cookieMaxAge=").append(wdkCookieMaxAge)
-            .append("&userDisplayName=").append(urlEncodeUtf8(displayName))
-            .toString();
-  }
-
 }
