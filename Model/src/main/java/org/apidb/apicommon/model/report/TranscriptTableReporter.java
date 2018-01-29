14e4aa798e7c71bf283d5d755bfcb79827e37db3
Ryan Doherty <tech@conical.org>
2018-01-29 08:52:00 +0000

    Remove stepId requirement in transcript table tabular reporter
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@86727 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/report/TranscriptTableReporter.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/TranscriptTableReporter.java
@@ -3,15 +3,16 @@ package org.apidb.apicommon.model.report;
 import java.util.Map;
 
 import org.apidb.apicommon.model.TranscriptUtil;
-import org.gusdb.fgputil.FormatUtil;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkRuntimeException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
 import org.gusdb.wdk.model.report.TableTabularReporter;
+import org.gusdb.wdk.model.user.Step;
+import org.gusdb.wdk.model.user.StepUtilities;
 import org.json.JSONObject;
 
 public class TranscriptTableReporter extends TableTabularReporter {
-
-  private static final String PROP_STEP_ID = "stepId";
 
   private String _originalQuestionName;
 
@@ -26,15 +27,17 @@ public class TranscriptTableReporter extends TableTabularReporter {
 
   @Override
   public TranscriptTableReporter configure(JSONObject config) throws WdkUserException {
-    String stepId = config.getString(PROP_STEP_ID);
-    if (!FormatUtil.isInteger(stepId)) {
-      throw new WdkUserException("Property '" + PROP_STEP_ID + "' must be an integer.");
+    try {
+      _originalQuestionName = _baseAnswer.getQuestion().getName();
+      Step baseStep = StepUtilities.createStep(_baseAnswer.getUser(), null, _baseAnswer, false, 0);
+      _baseAnswer = TranscriptUtil.transformToGeneAnswer(_baseAnswer, baseStep.getStepId());
+      // now that base answer is a Gene answer, check and assign selected table field name
+      super.configure(config);
+      return this;
     }
-    _originalQuestionName = _baseAnswer.getQuestion().getName();
-    _baseAnswer = TranscriptUtil.transformToGeneAnswer(_baseAnswer, Integer.parseInt(stepId));
-    // now that base answer is a Gene answer, check and assign selected table field name
-    super.configure(config);
-    return this;
+    catch (WdkModelException e) {
+      throw new WdkRuntimeException("Could not create in-memory step from incoming answer spec", e);
+    }
   }
 
   @Override
