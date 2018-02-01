0cf48227182249d6707be5f641fbe7bc9df844b0
Danielle Callan <dcallan@upenn.edu>
2018-02-01 18:06:27 +0000

    make custom graph for icemr and update crompton and kazura graphs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@86917 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlasmoDB/Crompton/AbMicroarray.pm
+++ View/lib/perl/GraphPackage/PlasmoDB/Crompton/AbMicroarray.pm
@@ -19,10 +19,10 @@ sub init {
 
 #  my $colors = ['blue','white'];
   my $colors = ['blue'];
-#  my $facet = $self->getFacets();
-#  my $contXAxis = $self->getContXAxis();
-   my $facet = ['ICEMR_health_status'];
-   my $contXAxis = 'PATO_0000011';
+  my $facet = $self->getFacets();
+  my $contXAxis = $self->getContXAxis();
+#  my $facet = ['ICEMR_health_status'];
+#  my $contXAxis = 'PATO_0000011';
  
   my @profileSetArray = (['Crompton Ab Microarray Profiles','values', '', '', '', '', '', $facet, '', '', $contXAxis]);
   my $profileSets = EbrcWebsiteCommon::View::GraphPackage::Util::makeProfileSets(\@profileSetArray);
@@ -31,7 +31,7 @@ sub init {
 #  my $scatter = EbrcWebsiteCommon::View::GraphPackage::ScatterPlot::ClinicalMetaData->new(@_);
   $scatter->setProfileSets($profileSets);
   $scatter->setColors($colors);
-  $scatter->setXaxisLabel("Age");
+#  $scatter->setXaxisLabel("Age");
 
   $self->setGraphObjects($scatter);
 
