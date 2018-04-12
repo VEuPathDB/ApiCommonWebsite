1e9e904eb29251914dbffe6ada7a8f098c2f7ea4
Ryan Doherty <tech@conical.org>
2018-04-12 11:49:41 +0000

    Override common words for ApiCommon sites
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@88397 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/record/attribute/plugin/ApiWordCloudAttributePlugin.java
+++ Model/src/main/java/org/apidb/apicommon/model/record/attribute/plugin/ApiWordCloudAttributePlugin.java
@@ -2,6 +2,7 @@ package org.apidb.apicommon.model.record.attribute.plugin;
 
 import org.apidb.apicommon.model.TranscriptUtil;
 import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
+import org.gusdb.fgputil.ArrayUtil;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
@@ -11,6 +12,11 @@ import org.gusdb.wdk.model.user.User;
 
 public class ApiWordCloudAttributePlugin extends WordCloudAttributePlugin {
 
+  private static String[] SUPPLEMENTAL_COMMON_WORDS = {
+      "off", "cgi", "bin", "groupac", "href", "http", "org", "tmp",
+      "chro", "sequencelist", "orthomcl", "orthomclweb"
+  };
+
   @Override
   protected AnswerValue getAnswerValue(Step step, User user) throws WdkModelException, WdkUserException {
     if (TranscriptUtil.isTranscriptQuestion(step.getQuestion())) {
@@ -19,4 +25,10 @@ public class ApiWordCloudAttributePlugin extends WordCloudAttributePlugin {
     }
     return super.getAnswerValue(step, user);
   }
+
+  @Override
+  protected String[] getCommonWords() {
+    return ArrayUtil.concatenate(COMMON_WORDS, SUPPLEMENTAL_COMMON_WORDS);
+  }
+
 }
