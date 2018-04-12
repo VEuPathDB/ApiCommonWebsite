8936090b3abc98fd1b5456e2ea379cd481e05f6a
Ryan Doherty <tech@conical.org>
2018-04-12 12:00:25 +0000

    Move overridden word cloud words to eupath common so ortho can pick them up
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@88399 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/record/attribute/plugin/ApiWordCloudAttributePlugin.java
+++ Model/src/main/java/org/apidb/apicommon/model/record/attribute/plugin/ApiWordCloudAttributePlugin.java
@@ -2,20 +2,14 @@ package org.apidb.apicommon.model.record.attribute.plugin;
 
 import org.apidb.apicommon.model.TranscriptUtil;
 import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
-import org.gusdb.fgputil.ArrayUtil;
+import org.eupathdb.common.model.attribute.EuPathWordCloudAttributePlugin;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
-import org.gusdb.wdk.model.record.attribute.plugin.WordCloudAttributePlugin;
 import org.gusdb.wdk.model.user.Step;
 import org.gusdb.wdk.model.user.User;
 
-public class ApiWordCloudAttributePlugin extends WordCloudAttributePlugin {
-
-  private static String[] SUPPLEMENTAL_COMMON_WORDS = {
-      "off", "cgi", "bin", "groupac", "href", "http", "org", "tmp",
-      "chro", "sequencelist", "orthomcl", "orthomclweb"
-  };
+public class ApiWordCloudAttributePlugin extends EuPathWordCloudAttributePlugin {
 
   @Override
   protected AnswerValue getAnswerValue(Step step, User user) throws WdkModelException, WdkUserException {
@@ -24,11 +18,6 @@ public class ApiWordCloudAttributePlugin extends WordCloudAttributePlugin {
       step = RepresentativeTranscriptFilter.applyToStepFromUserPreference(step, user);
     }
     return super.getAnswerValue(step, user);
-  }
-
-  @Override
-  protected String[] getCommonWords() {
-    return ArrayUtil.concatenate(COMMON_WORDS, SUPPLEMENTAL_COMMON_WORDS);
   }
 
 }
