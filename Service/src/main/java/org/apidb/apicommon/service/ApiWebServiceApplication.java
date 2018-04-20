395e10b41a48bd4abc085a6227cc54347591ead7
Ryan Doherty <tech@conical.org>
2018-04-20 14:12:06 +0000

    Polish API of basket service to support gene->transcript override in Api
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@88547 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
+++ Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
@@ -6,10 +6,12 @@ import java.util.Set;
 
 import org.apidb.apicommon.service.services.ApiSessionService;
 import org.apidb.apicommon.service.services.BigWigTrackService;
+import org.apidb.apicommon.service.services.CustomBasketService;
 import org.apidb.apicommon.service.services.TranscriptToggleService;
 import org.eupathdb.common.service.EuPathServiceApplication;
 import org.gusdb.fgputil.SetBuilder;
 import org.gusdb.wdk.service.service.SessionService;
+import org.gusdb.wdk.service.service.user.BasketService;
 import org.apidb.apicommon.service.services.JBrowseService;
 
 public class ApiWebServiceApplication extends EuPathServiceApplication {
@@ -19,13 +21,16 @@ public class ApiWebServiceApplication extends EuPathServiceApplication {
     return new SetBuilder<Class<?>>()
 
       // add WDK services
-      .addAll(filter(super.getClasses(), clazz -> !clazz.getName().equals(SessionService.class.getName())))
+      .addAll(filter(super.getClasses(), clazz ->
+          !clazz.getName().equals(SessionService.class.getName()) &&
+          !clazz.getName().equals(BasketService.class.getName())))
 
       // add ApiCommon-specific services
       .add(TranscriptToggleService.class)
       .add(ApiSessionService.class)
+      .add(CustomBasketService.class)
       .add(BigWigTrackService.class)
-              .add(JBrowseService.class)
+      .add(JBrowseService.class)
 
       .toSet();
   }
