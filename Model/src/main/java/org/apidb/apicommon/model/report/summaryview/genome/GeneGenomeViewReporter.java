d98fc0a00937a283416edbe5d3319c7b3da9bcd4
Steve Fischer <sfischer@upenn.edu>
2018-05-07 17:51:12 +0000

    add new reporters to support summary views
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@88904 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/apidb/apicommon/model/report/summaryview/genome/GeneGenomeViewReporter.java
@@ -0,0 +1,40 @@
+package org.apidb.apicommon.model.report.summaryview.genome;
+
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkUserException;
+import org.gusdb.wdk.model.answer.AnswerValue;
+
+/**
+ * @author jerric
+ */
+public class GeneGenomeViewReporter extends GenomeViewReporter {
+
+    protected GeneGenomeViewReporter(AnswerValue answerValue) {
+    super(answerValue);
+  }
+
+    @Override
+    public String prepareSql(String idSql) throws WdkModelException,
+            WdkUserException {
+        StringBuilder sql = new StringBuilder("SELECT DISTINCT ");
+        sql.append("    ga.gene_source_id AS " + COLUMN_SOURCE_ID + ", ");
+        sql.append("    ga.sequence_id AS " + COLUMN_SEQUENCE_ID + ", ");
+        sql.append("    sa.length AS " + COLUMN_SEQUENCE_LENGTH + ", ");
+        sql.append("    sa.chromosome AS " + COLUMN_CHROMOSOME + ", ");
+        sql.append("    sa.organism AS " + COLUMN_ORGANISM + ", ");
+        sql.append("    ga.gene_start_min AS " + COLUMN_START + ", ");
+        sql.append("    ga.gene_end_max AS " + COLUMN_END + ", ");
+        sql.append("    ga.gene_product AS " + COLUMN_DESCRIPTION + ", ");
+        sql.append("    ga.gene_context_start || '..' || ga.gene_context_end AS " + COLUMN_CONTEXT + ", ");
+        sql.append("    CASE ga.strand WHEN 'forward' THEN 1 ELSE 0 END AS " + COLUMN_STRAND);
+        sql.append(" FROM ApidbTuning.TranscriptAttributes ga, ");
+        sql.append("      ApidbTuning.GenomicSeqAttributes sa, ");
+        sql.append("      (" + idSql + ") idq ");
+        sql.append(" WHERE ga.sequence_id = sa.source_id ");
+        sql.append("  AND ga.gene_source_id = idq.gene_source_id ");
+
+        return sql.toString();
+    }
+
+
+}
