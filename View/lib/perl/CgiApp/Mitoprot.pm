11deb12620928186f340db6cabef6d34e07bef3a
Lin Xu <linxu123@upenn.edu>
2018-09-20 16:52:08 +0000

    add mitoprot on gene pages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@91772 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/Mitoprot.pm
+++ View/lib/perl/CgiApp/Mitoprot.pm
@@ -1,80 +1,83 @@
 # Test module for CgiApp                                                                                                            
-# returns Protein Subcellular Localization Prediction and reports if                                                                      
+# returns Prediction of mitochondrial targeting sequences and reports if                                                                      
 # running in  cgi or Apache::Registry environment                                                                                   
 
-package ApiCommonWebsite::View::CgiApp::WolfPSORT;
+package ApiCommonWebsite::View::CgiApp::Mitoprot;
 @ISA = qw( EbrcWebsiteCommon::View::CgiApp );
 
 use strict;
 use URI::Escape;
 use LWP::UserAgent;
-use HTTP::Request::Common qw(POST);
+use HTTP::Request;
+#use HTTP::Request::Common qw(POST);
 use EbrcWebsiteCommon::View::CgiApp;
-
+use LWP::Protocol::https;
 use Data::Dumper;
 
 
 sub run {
-
+    
     my ($self,$cgi) = @_;
-
+    
     my $project_id = $cgi->param('project_id');
-    my $organism_type = $cgi->param('organism_type');
-    my $input_type=$cgi->param('input_type');
     my $source_ID = $cgi->param('source_ID');
     my $id_type = $cgi->param('id_type');
-
+    
+    
     my $qh = $self->getQueryHandle($cgi);
-
+    
     my $sql;
-
+    
     if($id_type eq  'protein'){
-
+	
         $sql = "select SEQUENCE from APIDBTUNING.PROTEINSEQUENCE where SOURCE_ID = '" . $source_ID . "' and PROJECT_ID= '" . $project_id . "'";
     }else{
-       
+	
 	die "cannot find the protein sequence from database based on entered source_ID and PROJECT_ID" ;
     }
-
-    my $sh = $qh->prepare($sql);
-
-    $sh->execute();
-
-    my $seq = $sh->fetchrow_array();
-
-    $sh->finish();
-
-
-    my $ua = LWP::UserAgent->new;
-
-    my $args = "CMD=Put&organism_type=$organism_type&input_type=$input_type&fasta_input=" . $seq;
-
-    my $req = new HTTP::Request POST => 'https://wolfpsort.hgc.jp/?submitted=1';
     
-
+    my $sh = $qh->prepare($sql);
+    
+    $sh->execute();
+    
+    my $seq = $sh->fetchrow_array();
+    
+    $sh->finish();
+    
+    
+#skip ssl verify &  be aware that you can turn off verification in your script on the creation of a new LWP::UserAgent instance by:
+    
+    my $ua = LWP::UserAgent->new(
+	ssl_opts => { verify_hostname => 0 },
+	);
+    
+    
+    
+# In this case, we don't need the following comment parts 
+=head
+    
+    my $args = "CMD=Put&seq=" . $seq;
+    my $req = new HTTP::Request GET => 'https://ihg.gsf.de/cgi-bin/paolo/mitofilter';
+    
     $req->content_type('application/x-www-form-urlencoded');
     $req->content($args);
 
-
     # get the response                                                                                                              
     my $response = $ua->request($req);
+=cut
 
-    #parse out the request id                                                                                                    
 
-    my $id;
 
-    if ($response->content =~ /id=(\w*)"/) {
-	$id = $1;
-    } else {
-	print "didn't match\n";
-    }
-
-   # retrieve and display results  
-    print "Location: https://wolfpsort.hgc.jp/?submitted=1&id=$id"."\n\n";
+   #retrieve and display results  
+    print "Location: https://ihg.gsf.de/cgi-bin/paolo/mitofilter?seq=$seq&seqname="."\n\n";
 
 }
 
 1;
+
+
+
+
 
 
 
