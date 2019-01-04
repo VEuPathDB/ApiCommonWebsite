1820d34d00cb3a8d2a2b8d69de007b9d2fb54085
Ryan Doherty <tech@conical.org>
2019-01-04 04:26:53 +0000

    Move beans to WDKWebsite and use 'real' WdkModel and User objects in ServletContext and HttpSession respectively
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/trunk@94238 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/controller/wizard/CustomShowQuestionStageHandler.java
+++ Model/src/main/java/org/apidb/apicommon/controller/wizard/CustomShowQuestionStageHandler.java
@@ -6,29 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.apache.struts.action.ActionServlet;
 import org.apidb.apicommon.controller.action.CustomShowQuestionAction;
-import org.gusdb.wdk.controller.form.WizardForm;
 import org.gusdb.wdk.controller.wizard.ShowQuestionStageHandler;
+import org.gusdb.wdk.controller.wizard.WizardFormIfc;
+import org.gusdb.wdk.model.WdkModel;
 
 public class CustomShowQuestionStageHandler extends ShowQuestionStageHandler {
 
     private static final Logger logger = Logger.getLogger(CustomShowQuestionStageHandler.class);
 
-    /* (non-Javadoc)
-     * @see org.gusdb.wdk.controller.wizard.ShowQuestionStageHandler#execute(org.apache.struts.action.ActionServlet, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, org.gusdb.wdk.controller.action.WizardForm)
-     */
     @Override
-    public Map<String, Object> execute(ActionServlet servlet,
+    public Map<String, Object> execute(WdkModel wdkModel,
             HttpServletRequest request, HttpServletResponse response,
-            WizardForm wizardForm) throws Exception {
+            WizardFormIfc wizardForm) throws Exception {
         logger.debug("Entering CustomShowQuestionStageHandler...");
 
         // load data sources
-        CustomShowQuestionAction.loadReferences(servlet, request);
+        CustomShowQuestionAction.loadReferences(wdkModel, request);
         
         // call execute() from parent;
-        return super.execute(servlet, request, response, wizardForm);
+        return super.execute(wdkModel, request, response, wizardForm);
     }
 
     
