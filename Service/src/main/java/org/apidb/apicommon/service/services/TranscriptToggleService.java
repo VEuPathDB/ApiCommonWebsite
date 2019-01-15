208596ac8cb92fbba986459d5522e52134096cd0
Ryan Doherty <tech@conical.org>
2019-01-15 18:08:14 +0000

    fix errors
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/strategy-loading@94484 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Service/src/main/java/org/apidb/apicommon/service/services/TranscriptToggleService.java
+++ Service/src/main/java/org/apidb/apicommon/service/services/TranscriptToggleService.java
@@ -1,21 +1,23 @@
 package org.apidb.apicommon.service.services;
 
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 import org.apidb.apicommon.model.filter.RepresentativeTranscriptFilter;
+import org.gusdb.fgputil.validation.ValidationLevel;
 import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.answer.spec.AnswerSpec;
+import org.gusdb.wdk.model.answer.spec.AnswerSpecBuilder;
 import org.gusdb.wdk.model.user.Step;
+import org.gusdb.wdk.model.user.StepFactoryHelpers.UserCache;
 import org.gusdb.wdk.service.service.AbstractWdkService;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 @Path("/step/{stepId}/transcript-view/config")
@@ -26,35 +28,35 @@ public class TranscriptToggleService extends AbstractWdkService {
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
-  public Response setTranscriptFlag(@PathParam("stepId") Integer stepId, String body) throws WdkModelException {
-    try {
-      LOG.info("Received transcript toggle POST request for step ID " + stepId + " and body " + body);
-      JSONObject input = new JSONObject(body);
-      boolean filterTurnedOn = input.getBoolean(RepresentativeTranscriptFilter.FILTER_NAME);
-      LOG.info("Action is to turn filter: " + filterTurnedOn);
-      Step step = getWdkModel().getStepFactory().getStepById(stepId);
-      if (getSessionUser().getUserId() != step.getUser().getUserId()) {
-        LOG.warn("Attempt made to edit Step " + stepId + " by non-owner (user id " +
-            getSessionUser().getUserId() + "); session expired?");
-        throw new ForbiddenException(AbstractWdkService.PERMISSION_DENIED);
-      }
-      if (filterTurnedOn) {
-        step.addViewFilterOption(RepresentativeTranscriptFilter.FILTER_NAME, new JSONObject());
-      }
-      else {
-        step.removeViewFilterOption(RepresentativeTranscriptFilter.FILTER_NAME);
-      }
-      step.writeParamFiltersToDb();
-      return Response.ok().build();
+  public void setTranscriptFlag(@PathParam("stepId") Integer stepId, String body) throws WdkModelException {
+
+    LOG.info("Received transcript toggle POST request for step ID " + stepId + " and body " + body);
+    JSONObject input = new JSONObject(body);
+
+    boolean filterTurnedOn = input.getBoolean(RepresentativeTranscriptFilter.FILTER_NAME);
+    LOG.info("Action is to turn filter: " + filterTurnedOn);
+
+    Step step = getWdkModel().getStepFactory().getStepById(stepId, ValidationLevel.SYNTACTIC)
+        .orElseThrow(() -> new NotFoundException("No step exists with ID " + stepId));
+
+    if (getSessionUser().getUserId() != step.getUser().getUserId()) {
+      LOG.warn("Attempt made to edit Step " + stepId + " by non-owner (user id " +
+          getSessionUser().getUserId() + "); session expired?");
+      throw new ForbiddenException(AbstractWdkService.PERMISSION_DENIED);
     }
-    catch (WdkModelException e) {
-      LOG.error("Unable to update transcript view flag for step " + stepId, e);
-      throw new WdkModelException(e);
+
+    AnswerSpecBuilder newSpec = AnswerSpec.builder(step.getAnswerSpec());
+    if (filterTurnedOn) {
+      newSpec.getViewFilterOptions().addFilterOption(RepresentativeTranscriptFilter.FILTER_NAME, new JSONObject());
     }
-    catch (JSONException e) {
-      LOG.error("Unable to parse input JSON", e);
-      throw new BadRequestException("JSON object required with property " +
-          RepresentativeTranscriptFilter.FILTER_NAME + " (boolean).");
+    else {
+      newSpec.getViewFilterOptions().removeAll(fo -> fo.getFilterName().equals(RepresentativeTranscriptFilter.FILTER_NAME));
     }
+
+    Step newStep = Step.builder(step).setAnswerSpec(newSpec)
+        .build(new UserCache(step.getUser()), ValidationLevel.NONE, step.getStrategy());
+
+    // since view filters do not affect downstream steps, we can just save the step here
+    getWdkModel().getStepFactory().updateStep(newStep);
   }
 }
