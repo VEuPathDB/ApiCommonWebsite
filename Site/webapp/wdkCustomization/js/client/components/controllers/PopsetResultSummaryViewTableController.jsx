12d19b505714a0a787487c199ae9e6f9696387d5
Dave Falke <dmfalke@gmail.com>
2019-03-07 21:19:10 +0000

    Use a form with POST for clustal call
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/reactResultPanel@96035 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/controllers/PopsetResultSummaryViewTableController.jsx
+++ Site/webapp/wdkCustomization/js/client/components/controllers/PopsetResultSummaryViewTableController.jsx
@@ -1,28 +1,29 @@
+import React from 'react';
 import { projectId } from '../../config';
 import { ResultTableSummaryViewPlugin } from 'wdk-client/Plugins';
 
 const title =
 `Please select at least two isolates to run Clustal Omega. Note: only isolates from a single page will be aligned. 
 The result is an alignment of the locus that was used to type the isolates.
-(Increase the page size in 'Advanced Paging' to increase the number that can be aligned).`;
+(Increase the 'Rows per page' to increase the number that can be aligned).`;
 
 
 export default ResultTableSummaryViewPlugin.withTableActions([
   {
-    element: (selectedRecords) => {
-      const ids = selectedRecords
-        .map(record => record.attributes.primary_key)
-        .join(',');
-      const href = `/cgi-bin/isolateAlignment?project_id=${projectId};type=undefined;sid=undefined;start=undefined;end=undefined;isolate_ids=${ids}`;
-      return (
+    element: (selectedRecords) => (
+      <form action="/cgi-bin/isolateAlignment" target="_blank" method="post">
+        <input type="hidden" name="project_id" value={projectId}/>
+        <input type="hidden" name="type"/>
+        <input type="hidden" name="sid"/>
+        <input type="hidden" name="start"/>
+        <input type="hidden" name="end"/>
+        <input type="hidden" name="isolate_ids" value={selectedRecords.map(record => record.attributes.primary_key).join(',')}/>
         <button
-          type="button"
           className="btn"
           disabled={selectedRecords.length < 2}
           title={title}
-          onClick={() => window.open(href, '_blank')}
         >Run Clustal Omega</button>
-      );
-    }
+      </form>
+    )
   }
 ]);
