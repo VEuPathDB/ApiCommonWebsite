a8ef079ad14d64f34234c37d38ac194787c72d0c
Dave Falke <dmfalke@gmail.com>
2019-03-11 16:36:23 +0000

    use GlobalViewFitlers feature
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/reactResultPanel@96112 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
+++ Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { requestTranscriptFilterPreference, requestTranscriptFilterUpdate } from '../../util/transcriptFilters';
+import {
+  isTranscripFilterEnabled,
+  requestTranscriptFilterUpdate
+} from '../../util/transcriptFilters';
 
 // --------------
 // GeneRecordLink
@@ -30,56 +33,49 @@ export const RecordLink = connect(mapStateToGeneRecordLinkProps)(GeneRecordLink)
 // ResultTable
 // -----------
 
-class TranscriptViewFilter extends React.Component {
-  componentDidMount() {
-    this.props.requestTranscriptFilterPreference();
-  }
-  render() {
-    // TODO When Checkbox toggled:
-    // - PATCH User preferences with transcript filter value
-    // - dispatch fulfillAnswer so that we get answer value with filter applied (or not)
-    const {
-      answer: { meta: { totalCount, displayTotalCount, viewTotalCount, displayViewTotalCount } },
-      recordClass: { displayName, displayNamePlural, nativeDisplayName, nativeDisplayNamePlural },
-      isEnabled,
-      isLoading,
-      requestTranscriptFilterUpdate
-    } = this.props;
+function TranscriptViewFilter({
+  answer: { meta: { totalCount, displayTotalCount, viewTotalCount } },
+  recordClass: { name, displayName, displayNamePlural, nativeDisplayName, nativeDisplayNamePlural },
+  globalViewFilters,
+  isEnabled,
+  isLoading,
+  requestTranscriptFilterUpdate
+}) {
+  if (totalCount === displayTotalCount) return null;
 
-    if (totalCount === displayTotalCount) return null;
-
-    const display = displayTotalCount === 1 ? displayName : displayNamePlural;
-    const nativeDisplay = totalCount === 1 ? nativeDisplayName : nativeDisplayNamePlural;
-    const hiddenCount = isEnabled ? `(hiding ${(totalCount - viewTotalCount).toLocaleString()})` : null;
-    const toggleId = "TranscriptViewFilter--Toggle";
-    return (
-      <div className="TranscriptViewFilter">
-        <div>
-          <div className="TranscriptViewFilter--Label">{display}:</div> {displayTotalCount.toLocaleString()}
-        </div>
-        <div>
-          <div className="TranscriptViewFilter--Label">{nativeDisplay}:</div> {totalCount.toLocaleString()} {hiddenCount}
-        </div>
-        <div>
-          <input
-            id={toggleId}
-            type="checkbox"
-            checked={isEnabled}
-            disabled={isLoading}
-            onChange={() => requestTranscriptFilterUpdate(!isEnabled)}
-          />
-          <label htmlFor={toggleId}>Show Only One {nativeDisplayName} Per Gene</label>
-          {isLoading && <div style={{ color: 'gray', padding: '0 1em', fontWeight: 'normal' }}>...updating results</div>}
-        </div>
+  const display = displayTotalCount === 1 ? displayName : displayNamePlural;
+  const nativeDisplay = totalCount === 1 ? nativeDisplayName : nativeDisplayNamePlural;
+  const hiddenCount = isEnabled ? `(hiding ${(totalCount - viewTotalCount).toLocaleString()})` : null;
+  const toggleId = "TranscriptViewFilter--Toggle";
+  return (
+    <div className="TranscriptViewFilter">
+      <div>
+        <div className="TranscriptViewFilter--Label">{display}:</div> {displayTotalCount.toLocaleString()}
       </div>
-    )
-  }
+      <div>
+        <div className="TranscriptViewFilter--Label">{nativeDisplay}:</div> {totalCount.toLocaleString()} {hiddenCount}
+      </div>
+      <div>
+        <input
+          id={toggleId}
+          type="checkbox"
+          checked={isEnabled}
+          disabled={isLoading}
+          onChange={() => requestTranscriptFilterUpdate(globalViewFilters[name], !isEnabled)}
+        />
+        <label htmlFor={toggleId}>Show Only One {nativeDisplayName} Per Gene</label>
+        {isLoading && <div style={{ color: 'gray', padding: '0 1em', fontWeight: 'normal' }}>...updating results</div>}
+      </div>
+    </div>
+  )
 }
 
 const ConnectedTranscriptViewFilter = connect(
-  state => state.transcriptFilters,
+  state => ({
+    isEnabled: isTranscripFilterEnabled(state),
+    globalViewFilters: state.resultTableSummaryView.globalViewFilters
+  }),
   {
-    requestTranscriptFilterPreference,
     requestTranscriptFilterUpdate
   }
 )(TranscriptViewFilter);
--- Site/webapp/wdkCustomization/js/client/util/transcriptFilters.js
+++ Site/webapp/wdkCustomization/js/client/util/transcriptFilters.js
@@ -1,130 +1,37 @@
-import { combineEpics } from 'redux-observable';
-import { ResultTableSummaryViewActions } from 'wdk-client/Actions';
-import { makeActionCreator, mergeMapRequestActionsToEpic as mrate } from 'wdk-client/ActionCreatorUtils';
+import { compose, get, negate, some } from "lodash/fp";
+import { ResultTableSummaryViewActions } from "wdk-client/Actions";
 
-export const REPRESENTATIVE_TRANSCRIPT_FILTER_NAME = 'representativeTranscriptOnly';
-export const TRANSCRIPT_RECORD_CLASS_NAME = 'TranscriptRecordClasses.TranscriptRecordClass';
-export const TRANSCRIPT_LINK_ATTRIBUTE_NAME = 'transcript_link';
+const REPRESENTATIVE_TRANSCRIPT_FILTER_NAME =
+  "representativeTranscriptOnly";
+const TRANSCRIPT_RECORD_CLASS_NAME =
+  "TranscriptRecordClasses.TranscriptRecordClass";
 
-export const wdkServiceMixin = WdkService => class TranscriptMixin extends WdkService {
+const isFilter = filter =>
+  filter.name === REPRESENTATIVE_TRANSCRIPT_FILTER_NAME;
+const isNotFilter = negate(isFilter);
 
-  async getStepAnswer(stepId, formatting, userId = 'current') {
-    const step = await this.findStep(stepId, userId);
-    return this.getAnswer(step.answerSpec, formatting);
-  }
+// selector to determine if filter is enabled
+export const isTranscripFilterEnabled = compose(
+  some(isFilter),
+  get([
+    "resultTableSummaryView",
+    "globalViewFilters",
+    TRANSCRIPT_RECORD_CLASS_NAME
+  ])
+);
 
-  async getStepAnswerJson(stepId, formatConfig, userId = 'current') {
-    const step = await this.findStep(stepId, userId);
-    return this.getAnswerJson(step.answerSpec, formatConfig);
-  }
-
-  async getAnswer(answerSpec, formatting) {
-    return super.getAnswer(await this._updateAnswerSpecIfTranscript(answerSpec), formatting);
-  }
-
-  async getAnswerJson(answerSpec, formatConfig) {
-    return super.getAnswerJson(
-      await this._updateAnswerSpecIfTranscript(answerSpec),
-      // We only need to do this for answerJson
-      await this._ensureTranscriptLinkAttribute(answerSpec, formatConfig)
-    );
-  }
-
-  async updateRepresentativeTranscriptFilter(shouldEnable) {
-    return this.patchUserPreference(
-      'project',
-      REPRESENTATIVE_TRANSCRIPT_FILTER_NAME,
-      shouldEnable ? 'true' : 'false'
-    )
-  }
-
-  async _updateAnswerSpecIfTranscript(answerSpec) {
-    if (!this._isTranscriptQuestion(answerSpec)) return answerSpec;
-    const prefs = await this.getCurrentUserPreferences();
-    const shouldAddFilter = prefs.project[REPRESENTATIVE_TRANSCRIPT_FILTER_NAME] === 'true';
-    const viewFiltersWithoutTranscriptFilter = answerSpec.viewFilters
-      ? answerSpec.viewFilters.filter(({ name }) => name !== REPRESENTATIVE_TRANSCRIPT_FILTER_NAME)
-      : undefined;
-    const viewFilters = shouldAddFilter
-      ? [ ...viewFiltersWithoutTranscriptFilter, { name: REPRESENTATIVE_TRANSCRIPT_FILTER_NAME, value: {} }]
-      : undefined;
-    return { ...answerSpec, viewFilters };
-  }
-
-  async _ensureTranscriptLinkAttribute(answerSpec, formatConfig) {
-    if (
-      !(await this._isTranscriptQuestion(answerSpec)) ||
-      formatConfig.attributes == null ||
-      formatConfig.attributes[1] === TRANSCRIPT_LINK_ATTRIBUTE_NAME
-    ) return formatConfig;
-
-    // make sure formatConfig includes transcript_link as the second attribute
-    const attributes = formatConfig.attributes.filter(a => a !== TRANSCRIPT_LINK_ATTRIBUTE_NAME);
-    attributes.splice(1, 0, TRANSCRIPT_LINK_ATTRIBUTE_NAME);
-    return { ...formatConfig, attributes };
-  }
-
-  async _isTranscriptQuestion(answerSpec) {
-    const question = await this.findQuestion(({ name }) => name === answerSpec.questionName);
-    return question.recordClassName === TRANSCRIPT_RECORD_CLASS_NAME;
-  }
-
-}
-
-
-export const requestTranscriptFilterPreference = makeActionCreator(
-  'transcript/requestTranscriptFilterPreference'
-)
-
-export const fulfillTranscriptFilterPreference = makeActionCreator(
-  'transcript/fulfillTranscriptFilterPreference',
-  isEnabled => ({ isEnabled })
-)
-
-export const requestTranscriptFilterUpdate = makeActionCreator(
-  'transcript/requestTranscriptFilterUpdate',
-  shouldEnable => ({ shouldEnable })
-)
-
-export const fulfillTranscriptFilterUpdate = makeActionCreator(
-  'transcript/fulfillTranscriptFilterUpdate',
-  isEnabled => ({ isEnabled })
-)
-
-export function reduce(state = { isLoading: false, isEnabled: false }, action) {
-  switch(action.type) {
-    case fulfillTranscriptFilterPreference.type:
-    case fulfillTranscriptFilterUpdate.type:
-      return { ...state, isEnabled: action.payload.isEnabled };
-    case requestTranscriptFilterUpdate.type:
-      return { ...state, isLoading: true };
-    case ResultTableSummaryViewActions.fulfillAnswer.type:
-      return { ...state, isLoading: false };
-    default:
-      return state;
-  }
-}
-
-async function getFulfillTranscriptFilterPreference([ requestAction ], state$, { wdkService }) {
-  const preferences = await wdkService.getCurrentUserPreferences();
-  return fulfillTranscriptFilterPreference(preferences.project[REPRESENTATIVE_TRANSCRIPT_FILTER_NAME] === 'true')
-}
-
-async function getFulfillTranscriptFilterUpdate([ requestAction ], state$, { wdkService }) {
-  await wdkService.patchUserPreference('project', REPRESENTATIVE_TRANSCRIPT_FILTER_NAME, requestAction.payload.shouldEnable ? 'true' : 'false')
-  return fulfillTranscriptFilterUpdate(requestAction.payload.shouldEnable);
-}
-
-async function getFulillAnswer([ requestAction ], state$, { wdkService }) {
-  const { stepId, answer: { meta: { sorting, attributes, pagination } } } = state$.value.resultTableSummaryView;
-  const answer = await wdkService.getStepAnswerJson(
-    stepId, { sorting, attributes, pagination }
+// Add/remove representativeTranscriptOnly from global filters for record class
+export function requestTranscriptFilterUpdate(currentViewFilters, enable) {
+  return ResultTableSummaryViewActions.updateGlobalViewFilters(
+    TRANSCRIPT_RECORD_CLASS_NAME,
+    updateTranscriptFilterValue(currentViewFilters, enable)
   );
-  return ResultTableSummaryViewActions.fulfillAnswer(stepId, { sorting, attributes }, pagination, answer);
 }
 
-export const observe = combineEpics(
-  mrate([requestTranscriptFilterPreference], getFulfillTranscriptFilterPreference),
-  mrate([requestTranscriptFilterUpdate], getFulfillTranscriptFilterUpdate),
-  mrate([fulfillTranscriptFilterUpdate], getFulillAnswer)
-)
+export function updateTranscriptFilterValue(currentViewFilters = [], enable) {
+  const viewFilters = currentViewFilters.filter(isNotFilter);
+  if (!enable) return viewFilters;
+  return viewFilters.concat([
+    { name: REPRESENTATIVE_TRANSCRIPT_FILTER_NAME, value: {} }
+  ]);
+}
