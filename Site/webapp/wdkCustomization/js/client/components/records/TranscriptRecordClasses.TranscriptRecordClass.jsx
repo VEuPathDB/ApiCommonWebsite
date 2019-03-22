2c3a2ccd212b64dc30c09a8b61b540b48c5bf361
Dave Falke <dmfalke@gmail.com>
2019-03-22 16:41:08 +0000

    fix selectors for transcript view filter
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/reactResultPanel@96473 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
+++ Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
@@ -1,3 +1,4 @@
+import { get } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -70,14 +71,16 @@ function TranscriptViewFilter({
   )
 }
 
+// FIXME Need to access viewId from Controller...
+const viewId = 'strategy';
 const ConnectedTranscriptViewFilter = connect(
-  state => ({
-    isEnabled: isTranscripFilterEnabled(state),
-    globalViewFilters: state.resultTableSummaryView.globalViewFilters
+  (state, props) => ({
+    isEnabled: isTranscripFilterEnabled(state, { viewId }),
+    globalViewFilters: get(state, ['resultTableSummaryView', viewId, 'globalViewFilters'], {})
   }),
-  {
-    requestTranscriptFilterUpdate
-  }
+  (dispatch, props) => ({
+    requestTranscriptFilterUpdate: (...args) => dispatch(requestTranscriptFilterUpdate(viewId, ...args))
+  })
 )(TranscriptViewFilter);
 
 export function ResultTable(props) {
