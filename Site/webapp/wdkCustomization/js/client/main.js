d729df108685f0caccd0e54aa59dfa94e238f8a2
Dave Falke <dmfalke@gmail.com>
2019-06-13 14:13:50 +0000

    move import of site specific css
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebsite/branches/strategy-loading@98943 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -1,12 +1,12 @@
-// import CSS files
-import 'site/wdkCustomization/css/client.scss';
-
 import { initialize } from 'ebrc-client/bootstrap';
 // import apicomm wrappers and additional routes
 import * as componentWrappers from './componentWrappers';
 import { wrapRoutes } from './routes';
 import wrapStoreModules from './wrapStoreModules';
 import pluginConfig from './pluginConfig';
+
+// import CSS files
+import 'site/wdkCustomization/css/client.scss';
 
 // Initialize the application.
 initialize({
