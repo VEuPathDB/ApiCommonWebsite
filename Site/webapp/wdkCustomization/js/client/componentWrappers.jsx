f06f4f51d749b59fb80900fa5e39d9cf71fe26a3
Dave Falke <dmfalke@gmail.com>
2019-09-17 14:40:26 -0400

    map urls in portal to component site

--- Site/webapp/wdkCustomization/js/client/componentWrappers.jsx
+++ Site/webapp/wdkCustomization/js/client/componentWrappers.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Cookies from 'js-cookie';
 import QueryString from 'querystring';
+import { projectId } from 'ebrc-client/config';
 import { emptyAction } from 'wdk-client/WdkMiddleware';
 import { CollapsibleSection, Link } from 'wdk-client/Components';
 import { getSingleRecordAnswerSpec } from 'wdk-client/WdkModel';
@@ -88,13 +89,42 @@ const RecordClassSpecificRecordlink = makeDynamicWrapper('RecordLink');
 
 /** Remove project_id from record links */
 export function RecordLink(WdkRecordLink) {
-  const ResolvedRecordLink = RecordClassSpecificRecordlink(WdkRecordLink);
+  const isPortal = projectId === 'EuPathDB';
+  const DefaultRecordLink = isPortal ? PortalRecordLink : WdkRecordLink;
+  const ResolvedRecordLink = RecordClassSpecificRecordlink(DefaultRecordLink);
   return function ApiRecordLink(props) {
-    let recordId = props.recordId.filter(p => p.name !== 'project_id');
+    let recordId = isPortal ? props.recordId : props.recordId.filter(p => p.name !== 'project_id');
     return (
       <ResolvedRecordLink {...props} recordId={recordId}/>
     );
   };
+}
+
+function PortalRecordLink(props) {
+  const { recordId, recordClass } = props;
+  const projectIdPart = recordId.find(part => part.name === 'project_id');
+  const baseUrl = getBaseUrl(projectIdPart && projectIdPart.value);
+  const pkValues = recordId.filter(p => p.name !== 'project_id').map(p => p.value).join('/');
+  return (
+    <a href={`${baseUrl}/app/record/${recordClass.urlSegment}/${pkValues}`} target="_blank">{props.children}</a>
+  );
+}
+
+function getBaseUrl(projectId) {
+  switch(projectId) {
+    case 'AmoebaDB': return 'https://amoebadb.org/amoeba';
+    case 'CryptoDB': return 'https://cryptodb.org/cryptodb';
+    case 'FungiDB': return 'https://fungidb.org/fungidb';
+    case 'GiardiaDB': return 'https://giardiadb.org/giardiadb';
+    case 'MicrosporidiaDB': return 'https://microsporidiadb.org/micro';
+    case 'PiroplasmaDB': return 'https://piroplasmadb.org/piro';
+    case 'PlasmoDB': return 'https://plasmodb.org/plasmo';
+    case 'ToxoDB': return 'https://toxodb.org/toxo';
+    case 'TrichDB': return 'https://trichdb.org/trichdb';
+    case 'TriTrypDB': return 'https://tritrypdb.org/tritrypdb';
+    case 'OrthoMCL': return 'https://orthomcl.org/orthomcl';
+    default: return 'https://eupathdb.org/eupathdb';
+  }
 }
 
 function downloadRecordTable(record, tableName) {
