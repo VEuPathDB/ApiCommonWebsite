54a5cd1406f1d402d81ee942338c52e67e70e4c3
Jamie Long <jtlong3rd@gmail.com>
2019-10-31 09:35:25 -0400

    Add Header branding + suggestions to VEuPathDB homepage

--- Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -1,21 +1,90 @@
-import React, { FunctionComponent, useCallback, useEffect } from 'react';
+import React, { FunctionComponent, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { ErrorBoundary } from 'wdk-client/Controllers';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
 
 import { Footer } from 'ebrc-client/components/homepage/Footer';
-import { Header } from 'ebrc-client/components/homepage/Header';
+import { Header, HeaderMenuItem } from 'ebrc-client/components/homepage/Header';
 import { Main } from 'ebrc-client/components/homepage/Main';
 import { NewsPane } from 'ebrc-client/components/homepage/NewsPane';
 import { SearchPane } from 'ebrc-client/components/homepage/SearchPane';
 import { combineClassNames } from 'ebrc-client/components/homepage/Utils';
 
+import { projectId } from '../../config';
+
 import './VEuPathDBHomePage.scss';
 
 const vpdbCx = makeClassNameHelper('vpdb-');
 
+const useProjectId = (): string => {
+  return projectId;
+};
+
+const headerMenuItems: HeaderMenuItem[] = [
+  {
+    key: 'searches',
+    display: 'Searches',
+    type: 'externalLink',
+    href: '#'
+  },
+  {
+    key: 'tools',
+    display: 'Tools',
+    type: 'externalLink',
+    href: '#'
+  },
+  {
+    key: 'workspace',
+    display: 'Workspace',
+    type: 'externalLink',
+    href: '#'
+  },
+  {
+    key: 'data',
+    display: 'Data',
+    type: 'externalLink',
+    href: '#'
+  },
+  {
+    key: 'community',
+    display: 'Community',
+    type: 'subMenu',
+    items: [
+      {
+        key: 'about',
+        display: 'About',
+        type: 'externalLink',
+        href: '#'
+      },
+      {
+        key: 'help',
+        display: 'Help',
+        type: 'externalLink',
+        href: '#'
+      },
+      {
+        key: 'community',
+        display: 'Community',
+        type: 'externalLink',
+        href: '#'
+      }
+    ]
+  },
+  {
+    key: 'contact',
+    display: 'Contact',
+    type: 'route',
+    route: '/contact-us',
+    target: '_blank'
+  }
+];
+
 export const VEuPathDBHomePage: FunctionComponent = props => {
-  const rootContainerClassName = vpdbCx('RootContainer');
+  const [ siteSearchSuggestions, setSiteSearchSuggestions ] = useState<string[] | undefined>(undefined);
+  const [ additionalSuggestions, setAdditionalSuggestions ] = useState<{ key: string, display: ReactNode }[]>([]);
+  const projectId = useProjectId();
+
+  const rootContainerClassName = combineClassNames(vpdbCx('RootContainer'), projectId);
   const headerClassName = combineClassNames(vpdbCx('Header'), vpdbCx('BgDark'));
   const searchPaneClassName = combineClassNames(vpdbCx('SearchPane'), vpdbCx('BgWash'));
   const mainClassName = vpdbCx('Main');
@@ -49,15 +118,54 @@ export const VEuPathDBHomePage: FunctionComponent = props => {
     };
   }, [ updateHeaderExpanded ]);
 
+  const preloadedSuggestions = useMemo(
+    () => [
+      'protein',
+      'protein motifs',
+      'proteomics',
+      'protozoa',
+    ],
+    []
+  );
+
+  const loadSuggestions = useCallback((searchTerm: string) => {
+    if (searchTerm) {
+      setSiteSearchSuggestions(
+        [
+          searchTerm,
+          ...preloadedSuggestions
+        ].sort()
+      );
+    } else {
+      setSiteSearchSuggestions(undefined);
+    }
+  }, []);
+
   return (
     <div className={rootContainerClassName}>
-      <ErrorBoundary><Header containerClassName={headerClassName} /></ErrorBoundary>
-      <ErrorBoundary><SearchPane containerClassName={searchPaneClassName} /></ErrorBoundary>
+      <ErrorBoundary>
+        <Header 
+          // FIXME: use project logos for component site branding 
+          branding={projectId}
+          menuItems={headerMenuItems} 
+          containerClassName={headerClassName} 
+          loadSuggestions={loadSuggestions}
+          siteSearchSuggestions={siteSearchSuggestions}
+          additionalSuggestions={additionalSuggestions}
+        />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <SearchPane containerClassName={searchPaneClassName} />
+      </ErrorBoundary>
       <Main containerClassName={mainClassName}>
         {props.children}
       </Main>
-      <ErrorBoundary><NewsPane containerClassName={newsPaneClassName} /> </ErrorBoundary>
-      <ErrorBoundary><Footer containerClassName={footerClassName} /></ErrorBoundary>
+      <ErrorBoundary>
+        <NewsPane containerClassName={newsPaneClassName} />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Footer containerClassName={footerClassName} />
+      </ErrorBoundary>
     </div>
   );
 }
