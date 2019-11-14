05ad2c2491a33c35b2b6376878c8b26dd4fea03a
Jamie Long <jtlong3rd@gmail.com>
2019-11-14 13:31:20 -0500

    Add PlasmoDB Page description

--- Site/webapp/wdkCustomization/js/client/components/homepage/PageDescription.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/PageDescription.tsx
@@ -4,6 +4,8 @@ import { projectId } from '../../config';
 
 import { makeVpdbClassNameHelper } from './Utils';
 
+import './PageDescription.scss';
+
 const cx = makeVpdbClassNameHelper('PageDescription');
 
 const PlasmoDB = 'PlasmoDB';
@@ -17,6 +19,14 @@ export const PageDescription = () =>
     }
     {
       projectId === PlasmoDB &&
-      null
+      <p>
+        As part of the VEuPathDB 
+        {' '}
+        <a href="https://www.niaid.nih.gov/research/bioinformatics-resource-centers" target="_blank">Bioinformatics Resource Center</a>,
+        {' '}
+        <span className={cx('ProjectId')}>PlasmoDB</span>
+        {' '} 
+        provides genomic, phenotypic, and population-centric data to the scientific community for plasmodium.
+      </p>
     }
   </div>;
