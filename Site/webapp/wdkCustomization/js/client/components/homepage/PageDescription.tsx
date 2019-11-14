20d0fefbbc4c4c18c70cce023207be578490427f
Jamie Long <jtlong3rd@gmail.com>
2019-11-14 16:22:54 -0500

    Add PageDescription verbiage for VEuPathDB portal site

--- Site/webapp/wdkCustomization/js/client/components/homepage/PageDescription.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/PageDescription.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+import { IconAlt } from 'wdk-client/Components';
+
+import { projects } from 'ebrc-client/components/homepage/Footer';
+import { ProjectLink } from 'ebrc-client/components/homepage/ProjectLink';
 
 import { projectId } from '../../config';
 
@@ -15,7 +20,7 @@ export const PageDescription = () =>
   <div className={cx()}>
     {
       projectId === VEuPathDB &&
-      null
+      <VEuPathDBDescription />
     }
     {
       projectId === PlasmoDB &&
@@ -30,3 +35,66 @@ export const PageDescription = () =>
       </p>
     }
   </div>;
+
+const VEuPathDBDescription = () => {
+  const [ isExpanded, setIsExpanded ] = useState(false);
+
+  const onClickLearnMore = useCallback((e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsExpanded(!isExpanded);
+  }, [ isExpanded ]);
+
+  return (
+    <div className={VEuPathDB}>
+      <div className={cx('Header')}>
+        <div className={cx('HeaderPrimary')}>
+          <h4>VEuPathDB</h4>
+          <div className={cx('HeaderProjectLinks')}>
+            {
+              projects.map(projectId => 
+                <ProjectLink key={projectId} projectId={projectId} />
+              )
+            }
+          </div>
+        </div>
+        <div className={cx('HeaderSecondary')}>
+          <a href="#" onClick={onClickLearnMore}>
+            Learn more <IconAlt fa="angle-double-right" />
+          </a>
+        </div>
+      </div>
+      <div className={cx('ShortSummary')}>
+        <p>
+          The VEuPathDB 
+          {' '}
+          <a href="https://www.niaid.nih.gov/research/bioinformatics-resource-centers" target="_blank">
+            Bioinformatics Resource Center
+          </a>
+          {' '}
+          provides a portal for accessing genomic-scale datasets associated with diverse eukaryotic microbes and invertebrate vectors of human pathogens.
+        </p>
+      </div>
+      {
+        isExpanded &&
+        <div className={cx('ExpandedSummary')}>
+          <p>Search for data here, or use one of the VEuPathDB component websites:</p>
+
+          <div className={cx('ExpandedSummaryProjectLinks')}>
+          {
+            projects.map(
+              projectId => (
+                <div className={cx('ExpandedSummaryProjectLinksItem')}>
+                  <ProjectLink projectId={projectId} />
+                  <div className={cx('ExpandedSummaryProjectLinksItemDescription')}>
+                    {projectId}
+                  </div>
+                </div>
+              )
+            )
+          }
+          </div>
+        </div>
+      }
+    </div>
+  );
+};
