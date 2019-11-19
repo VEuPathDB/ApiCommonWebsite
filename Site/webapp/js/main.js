d3ed94c8da741f0ebea7d08ecb9f35af086cfc78
Dave Falke <dmfalke@gmail.com>
2019-11-19 11:50:45 -0500

    Pass in proxied initializeWdk when in legacy application

--- Site/webapp/js/main.js
+++ Site/webapp/js/main.js
@@ -2,7 +2,11 @@
 // Those starting with '!!script!' are loaded with script semantics
 // rather than ES module semantics (i.e., code is executed in the global scope).
 
-import 'site/wdkCustomization/js/client/main';
+import { initialize as initializeWdk } from 'wdk/js/index.js';
+import { initialize } from 'site/wdkCustomization/js/client/bootstrap';
+
+initialize(initializeWdk);
+
 import 'eupathdb/wdkCustomization/js/common';
 
 // home page bubbles
--- Site/webapp/wdkCustomization/js/client/main.js
+++ Site/webapp/wdkCustomization/js/client/main.js
@@ -1,17 +1,3 @@
-import { initialize } from 'ebrc-client/bootstrap';
-// import apicomm wrappers and additional routes
-import * as componentWrappers from './componentWrappers';
-import { wrapRoutes } from './routes';
-import wrapStoreModules from './wrapStoreModules';
-import pluginConfig from './pluginConfig';
+import { initialize } from './bootstrap';
 
-// import CSS files
-import 'site/wdkCustomization/css/client.scss';
-
-// Initialize the application.
-initialize({
-  componentWrappers,
-  wrapRoutes,
-  wrapStoreModules,
-  pluginConfig
-})
+initialize();
