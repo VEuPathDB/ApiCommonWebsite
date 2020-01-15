fad94e167ccef8aeb704c4949a4008b304599359
Jamie Long <jtlong3rd@gmail.com>
2020-01-15 15:41:00 -0500

    Remove "View All" link from Featured Resources and Tools

--- Site/webapp/wdkCustomization/js/client/components/homepage/FeaturedTools.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/FeaturedTools.tsx
@@ -93,7 +93,6 @@ export const FeaturedTools = () => {
     <div className={cx()}>
       <div className={cx('Header')}>
         <h3>Featured Resources and Tools</h3>
-        <a href="">View all <IconAlt fa="angle-double-right" /></a>
       </div>
       {
         !toolMetadata 
