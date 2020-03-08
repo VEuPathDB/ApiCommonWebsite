202ed38bc23aaa0ed8e071c298d5d4b4a9611b1c
Jamie Long <jtlong3rd@gmail.com>
2020-03-07 21:37:51 -0500

    Remove stale comment

--- Site/webapp/wdkCustomization/js/client/components/controllers/FastaConfigController.tsx
+++ Site/webapp/wdkCustomization/js/client/components/controllers/FastaConfigController.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { PageController } from 'wdk-client/Controllers';
 import { Srt } from '../Srt';
 
-/**
- * This will eventually be the view controller for the SRT page (srt.jsp).  It
- * should use the download form React components for FASTA written for the
- * step download page.  However, since this is working ok for now, we will
- * have duplicate functionality for a "limited" time. :)
- */
 export default class FastaConfigController extends PageController {
   renderView() {
     return <Srt />;
