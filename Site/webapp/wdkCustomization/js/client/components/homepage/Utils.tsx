d41b3db79c4e3ae831b95be7cb035065c7dd5183
Jamie Long <jtlong3rd@gmail.com>
2020-04-05 23:22:18 -0400

    Remove unused parameter

--- Site/webapp/wdkCustomization/js/client/components/homepage/Utils.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/Utils.tsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
 import { RootState } from 'wdk-client/Core/State/Types';
 
-export const makeVpdbClassNameHelper = (suffix: string, ...modifiers: any[]) => makeClassNameHelper(`vpdb-${suffix}`);
+export const makeVpdbClassNameHelper = (suffix: string) => makeClassNameHelper(`vpdb-${suffix}`);
 
 export const useCommunitySiteUrl = (): string | undefined => {
   const communitySiteUrl = useSelector(
