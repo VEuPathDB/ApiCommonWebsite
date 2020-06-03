7fd98481f7af0cf945e2c08ae292309c8f8c6dfd
Danielle Callan <dcallan@upenn.edu>
2020-06-03 13:04:52 -0400

    set abmicroarray plots to use legacy

--- View/lib/perl/GraphPackage/EuPathDB/Helb/AbMicroarray.pm
+++ View/lib/perl/GraphPackage/EuPathDB/Helb/AbMicroarray.pm
@@ -5,7 +5,7 @@ use vars qw( @ISA);
 
 @ISA = qw( EbrcWebsiteCommon::View::GraphPackage::MixedPlotSet);
 use EbrcWebsiteCommon::View::GraphPackage::MixedPlotSet;
-use EbrcWebsiteCommon::View::GraphPackage::GGScatterPlot;
+use EbrcWebsiteCommon::View::GraphPackage::LegacyGGScatterPlot;
 use Data::Dumper;
 
 sub init {
@@ -34,7 +34,7 @@ sub init {
   my @profileSetArray = (['Recent exposure AB array profiles','values', '', '', '', '', '', $facet, '', '', $contXAxis]);
   my $profileSets = EbrcWebsiteCommon::View::GraphPackage::Util::makeProfileSets(\@profileSetArray);
 
-  my $scatter = EbrcWebsiteCommon::View::GraphPackage::GGScatterPlot::LogRatio->new(@_);
+  my $scatter = EbrcWebsiteCommon::View::GraphPackage::LegacyGGScatterPlot::LogRatio->new(@_);
   $scatter->setProfileSets($profileSets);
   $scatter->setColors($colors);
 
--- View/lib/perl/GraphPackage/EuPathDB/Kazura/AbMicroarray.pm
+++ View/lib/perl/GraphPackage/EuPathDB/Kazura/AbMicroarray.pm
@@ -5,7 +5,7 @@ use vars qw( @ISA);
 
 @ISA = qw( EbrcWebsiteCommon::View::GraphPackage::MixedPlotSet);
 use EbrcWebsiteCommon::View::GraphPackage::MixedPlotSet;
-use EbrcWebsiteCommon::View::GraphPackage::GGScatterPlot;
+use EbrcWebsiteCommon::View::GraphPackage::LegacyGGScatterPlot;
 use Data::Dumper;
 
 sub init {
@@ -33,7 +33,7 @@ sub init {
   my @profileSetArray = (['Kazura Reinfection Ab Microarray Profiles','values', '', '', '', '', '', $facet, '', '', $contXAxis]);
   my $profileSets = EbrcWebsiteCommon::View::GraphPackage::Util::makeProfileSets(\@profileSetArray);
 
-  my $scatter = EbrcWebsiteCommon::View::GraphPackage::GGScatterPlot::LogRatio->new(@_);
+  my $scatter = EbrcWebsiteCommon::View::GraphPackage::LegacyGGScatterPlot::LogRatio->new(@_);
   $scatter->setProfileSets($profileSets);
   $scatter->setColors($colors);
 
