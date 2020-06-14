b2a914e92beb7da3936cc1f93f4360c8bcdfa337
Jamie Long <jtlong3rd@gmail.com>
2020-06-14 17:22:18 -0400

    Fix bug that was causing, when navigating from a non-home-page -> the home page in Firefox, the "thick" footer to appear

--- Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -102,7 +102,7 @@ const VEuPathDBHomePageView: FunctionComponent<Props> = props => {
     const scrollHeight = document.documentElement?.scrollHeight || document.body.scrollHeight;
     const scrolledToBottom = scrollTop + window.innerHeight >= scrollHeight;
 
-    setFooterThin(!scrolledToBottom);
+    setFooterThin(scrollTop === 0 || !scrolledToBottom);
   }, []);
   
   useEffect(() => {
