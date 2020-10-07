ba09b6a5ccf580e55479642412c0bf0287b9ca56
Ryan Doherty <rdoherty@upenn.edu>
2020-10-06 21:51:54 -0400

    Remove GBrowse-specific login option and GBrowse build

--- Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
+++ Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
@@ -4,19 +4,17 @@ import java.util.Set;
 
 import org.apidb.apicommon.service.services.ApiBasketService;
 import org.apidb.apicommon.service.services.ApiProjectService;
-import org.apidb.apicommon.service.services.ApiSessionService;
 import org.apidb.apicommon.service.services.ApiStepService;
 import org.apidb.apicommon.service.services.BigWigTrackService;
 import org.apidb.apicommon.service.services.TranscriptToggleService;
 import org.apidb.apicommon.service.services.comments.AttachmentsService;
 import org.apidb.apicommon.service.services.comments.UserCommentsService;
+import org.apidb.apicommon.service.services.dataPlotter.ProfileSetService;
 import org.apidb.apicommon.service.services.jbrowse.JBrowseService;
 import org.apidb.apicommon.service.services.jbrowse.JBrowseUserDatasetsService;
-import org.apidb.apicommon.service.services.dataPlotter.ProfileSetService;
 import org.eupathdb.common.service.EuPathServiceApplication;
 import org.gusdb.fgputil.SetBuilder;
 import org.gusdb.wdk.service.service.ProjectService;
-import org.gusdb.wdk.service.service.SessionService;
 import org.gusdb.wdk.service.service.user.BasketService;
 import org.gusdb.wdk.service.service.user.StepService;
 public class ApiWebServiceApplication extends EuPathServiceApplication {
@@ -29,7 +27,6 @@ public class ApiWebServiceApplication extends EuPathServiceApplication {
       .addAll(super.getClasses())
 
       // replace overridden services with ApiCommon versions
-      .replace(SessionService.class, ApiSessionService.class)
       .replace(BasketService.class, ApiBasketService.class)
       .replace(StepService.class, ApiStepService.class)
       .replace(ProjectService.class, ApiProjectService.class)
