c1d90076444c0a55bdd4501384c8ccc587760be0
Danielle Callan <dcallan@upenn.edu>
2020-10-14 10:55:17 -0400

    update barrett metabolite plots

--- View/lib/perl/GraphPackage/EuPathDB/Barrett/amphotericinB.pm
+++ View/lib/perl/GraphPackage/EuPathDB/Barrett/amphotericinB.pm
@@ -15,73 +15,75 @@ sub init {
 
   $self->SUPER::init(@_);
 
-  $self->setFacets(["peak"]);
   $self->setPlotWidth(600);
 
   my $compoundId = $self->getId();
 
   my $colors = ['dodgerblue', 'slateblue', 'forestgreen', '#2F4F4F', 'salmon'];
 
-  my $elementNames = ['AmBRA-cl1|peak', 'AmBRB-cl2|peak', 'AmBRC-cl3|peak', 'AmBRD-cl2|peak', 'WT|peak'];
-
-
-
   my $url = $self->getBaseUrl() . '/a/service/profileSet/CompoundPeaksIdentifierAmoB/' . $compoundId;
   my $content = get($url);
   my $json = from_json($content);
 
-  my $jsonForService;
+  my @graphObjects;
 
   foreach my $result (@$json) {
-
-    my $compoundpeaks = $result->{'COMPOUNDPEAKSIDENTIFIER'};
+    my $jsonForService;
+    my $compoundpeak = $result->{'COMPOUNDPEAKSIDENTIFIER'};
     
-    if ($compoundpeaks) {
-      if (defined $jsonForService) {
-        $jsonForService = $jsonForService . ",{\"profileSetName\":\"Barrett_AmphotericinB_Resistant [metaboliteProfiles]\",\"profileType\":\"values\",\"idOverride\":\"$compoundId|$compoundpeaks\",\"name\":\"$compoundId|$compoundpeaks\"}";
-      } else {
-        $jsonForService = "{\"profileSetName\":\"Barrett_AmphotericinB_Resistant [metaboliteProfiles]\",\"profileType\":\"values\",\"idOverride\":\"$compoundId|$compoundpeaks\",\"name\":\"$compoundId|$compoundpeaks\"}";
-      }
+    if ($compoundpeak) {
+      $jsonForService = "{\"profileSetName\":\"Barrett_AmphotericinB_Resistant [metaboliteProfiles]\",\"profileType\":\"values\",\"idOverride\":\"$compoundId|$compoundpeak\",\"name\":\"$compoundId|$compoundpeak\"}";
+    } else {
+      $jsonForService = "{\"profileSetName\":\"Barrett_AmphotericinB_Resistant [metaboliteProfiles]\",\"profileType\":\"values\",\"name\":\"$compoundId\"}";
     }
-  }
 
-  if (!defined $jsonForService) {
-    $jsonForService = "{\"profileSetName\":\"Barrett_AmphotericinB_Resistant [metaboliteProfiles]\",\"profileType\":\"values\",\"name\":\"$compoundId\"}";
-  }
-    
-  my $profileSets = EbrcWebsiteCommon::View::GraphPackage::ProfileSet->new("DUMMY");
-  $profileSets->setJsonForService($jsonForService);
-  $profileSets->setSqlName("Profile");
-
-  my $massSpec = EbrcWebsiteCommon::View::GraphPackage::GGBarPlot::MassSpec->new(@_);
-  my $rAdjustString = <<'RADJUST';
-    profile.df.full <- profile.df.full[!grepl("blank", profile.df.full$NAME),]
-    profile.df.full$LEGEND=matrix(unlist(strsplit(as.character(profile.df.full$NAME), fixed=T, split=c("|"))), ncol=2, byrow=T)[,1];
-    profile.df.full$peak=matrix(unlist(strsplit(as.character(profile.df.full$NAME), fixed=T, split=c("|"))), ncol=2, byrow=T)[,2];
-    profile.df.full$STACK=matrix(unlist(strsplit(as.character(profile.df.full$DISPLAY_NAME), fixed=T, split=c("|"))), ncol=2, byrow=T) [,2];
-    profile.df.full$STACK[which(profile.df.full$STACK == "C12")] = "C12-0";
-    profile.df.full$order=matrix(unlist(strsplit(as.character(profile.df.full$STACK), fixed=T, split=c("-"))), ncol=2, byrow=T)[,2];
-    profile.df.full$STACK[which(profile.df.full$STACK == "C12-0")] = "C12";
-    profile.df.full$NAME = factor(profile.df.full$LEGEND, levels=legend.label);
-    profile.df.full$LEGEND = factor(profile.df.full$LEGEND, levels=legend.label);
-    profile.df.full <- profile.df.full[order(as.numeric(profile.df.full$order)),]; 
-    profile.df.full$STACK = factor(profile.df.full$STACK, levels=rev(unique(profile.df.full$STACK)));
-    hideLegend <- FALSE;
+    my $profileSets = EbrcWebsiteCommon::View::GraphPackage::ProfileSet->new("DUMMY");
+    $profileSets->setJsonForService($jsonForService);
+    $profileSets->setSqlName("Profile");
+  
+    my $massSpec = EbrcWebsiteCommon::View::GraphPackage::GGBarPlot::MassSpec->new(@_);
+    my $rAdjustString = <<'RADJUST';
+      hideLegend <- FALSE;
 RADJUST
-  $massSpec->setAdjustProfile($rAdjustString);
-  $massSpec->setProfileSets([$profileSets]);
-  $massSpec->setColors($colors);
-  $massSpec->setDefaultYMax(100);
-  $massSpec->setSampleLabels($elementNames);
-
-  $massSpec->setIsStacked(1);
-  $massSpec->setHideXAxisLabels(1);
-  $massSpec->setLegendColors($colors);
-  $massSpec->setLegendLabels(['AmBRA-cl1', 'AmBRB-cl2', 'AmBRC-cl3', 'AmBRD-cl2', 'WT']);
-
-  $self->setGraphObjects($massSpec);
+    $massSpec->setAdjustProfile($rAdjustString);
+    $massSpec->setProfileSets([$profileSets]);
+    $massSpec->setColors($colors);
+    $massSpec->setDefaultYMax(100);
+  
+    $massSpec->setIsStacked(1);
+    $massSpec->setHideXAxisLabels(1);
+    $massSpec->setLegendColors($colors);
+    $massSpec->setLegendLabels(['AmBRA-cl1', 'AmBRB-cl2', 'AmBRC-cl3', 'AmBRD-cl2', 'WT']);
+  
+    $massSpec->setPartName('mass_spec_' . $compoundpeak);
+    my @peakInfo = split('\|', $compoundpeak);
+    $massSpec->setPlotTitle("Profile - $compoundId; mass - @peakInfo[0]; ret time - @peakInfo[1]");
+    push @graphObjects, $massSpec;
+  }
+ 
+  $self->setGraphObjects(@graphObjects);
   return $self;
 }
 
+sub declareParts {
+  my ($self) = @_;
+
+  my $arrayRef = $self->SUPER::declareParts();
+  my $width = @{$arrayRef}[0]->{width};
+  my $plotCount = scalar @{$arrayRef};
+  my $height = @{$arrayRef}[0]->{height} * $plotCount;
+
+  my $newVisiblePart;
+  foreach my $plotPart (@{$arrayRef}) {
+    if ($newVisiblePart) {
+      $newVisiblePart = $newVisiblePart . "," . $plotPart->{visible_part};
+    } else {
+      $newVisiblePart = $plotPart->{visible_part};
+    }
+  }
+
+  my @newParts = ({height => $height, width => $width, visible_part => $newVisiblePart});
+  return \@newParts;
+}
 
 1;
