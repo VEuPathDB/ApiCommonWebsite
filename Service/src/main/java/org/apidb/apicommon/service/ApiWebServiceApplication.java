55a6d02c1dfb57b346b764dea0470e2cbe6c1d58
Ryan Doherty <rdoherty@upenn.edu>
2021-01-30 19:00:41 -0500

    Rename and combine some request/response filters

--- Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
+++ Service/src/main/java/org/apidb/apicommon/service/ApiWebServiceApplication.java
@@ -2,7 +2,7 @@ package org.apidb.apicommon.service;
 
 import java.util.Set;
 
-import org.apidb.apicommon.service.filter.ApiCheckLoginRequestFilter;
+import org.apidb.apicommon.service.filter.ApiCheckLoginFilter;
 import org.apidb.apicommon.service.services.ApiBasketService;
 import org.apidb.apicommon.service.services.ApiProjectService;
 import org.apidb.apicommon.service.services.ApiStepService;
@@ -15,7 +15,7 @@ import org.apidb.apicommon.service.services.jbrowse.JBrowseService;
 import org.apidb.apicommon.service.services.jbrowse.JBrowseUserDatasetsService;
 import org.eupathdb.common.service.EuPathServiceApplication;
 import org.gusdb.fgputil.SetBuilder;
-import org.gusdb.wdk.service.filter.CheckLoginRequestFilter;
+import org.gusdb.wdk.service.filter.CheckLoginFilter;
 import org.gusdb.wdk.service.service.ProjectService;
 import org.gusdb.wdk.service.service.user.BasketService;
 import org.gusdb.wdk.service.service.user.StepService;
@@ -33,7 +33,7 @@ public class ApiWebServiceApplication extends EuPathServiceApplication {
       .replace(BasketService.class, ApiBasketService.class)
       .replace(StepService.class, ApiStepService.class)
       .replace(ProjectService.class, ApiProjectService.class)
-      .replace(CheckLoginRequestFilter.class, ApiCheckLoginRequestFilter.class)
+      .replace(CheckLoginFilter.class, ApiCheckLoginFilter.class)
 
       // add ApiCommon-specific services
       .add(AttachmentsService.class)
