25de1d577ec3beb60e967f799a8adc4afef04ef4
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2021-02-22 19:59:36 -0500

    remove beta in header

--- Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -163,7 +163,7 @@ const VEuPathDBHomePageView: FunctionComponent<Props> = props => {
         </div>
       </Link>
       <div className={vpdbCx('HeaderBrandingSuperscript')}>
-        {props.buildNumber && <span>Release {props.buildNumber} <em>beta</em></span>}
+        {props.buildNumber && <span>Release {props.buildNumber}</span>}
         <br />
         {props.releaseDate && formatReleaseDate(props.releaseDate)}
       </div>
