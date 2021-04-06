14a4a948fe95d4ef684d3940a5e9d6e340b014b3
Jamie Long <jtlong3rd@gmail.com>
2021-04-06 00:16:05 -0400

    Initial commit of PreferredOrganismsLink component + styling

---
+++ Site/webapp/wdkCustomization/js/client/components/PreferredOrganismsLink.tsx
@@ -0,0 +1,28 @@
+import React, { Suspense } from 'react';
+
+import { Link, IconAlt } from '@veupathdb/wdk-client/lib/Components';
+
+import { useAvailableOrganisms, usePreferredOrganismsState } from '@veupathdb/preferred-organisms/lib/hooks/preferredOrganisms';
+
+import './PreferredOrganismsLink.scss';
+
+export function PreferredOrganismsLink() {
+  return (
+    <Link className="PreferredOrganismsLink" to="/preferred-organisms">
+      <IconAlt fa="gear" />
+      {' '}
+      My Organism Preferences
+      {' '}
+      <Suspense fallback={null}>
+        <PreferredOrganismsCount />
+      </Suspense>
+    </Link>
+  );
+}
+
+function PreferredOrganismsCount() {
+  const availableOrganisms = useAvailableOrganisms();
+  const [ preferredOrganisms ] = usePreferredOrganismsState();
+
+  return <>({preferredOrganisms.length} of {availableOrganisms.length})</>;
+}
