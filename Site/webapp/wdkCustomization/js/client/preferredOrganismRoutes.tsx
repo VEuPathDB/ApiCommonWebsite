a84fe7d209a7bef5a01c998f3b6f60e1c810ad82
Jamie Long <jtlong3rd@gmail.com>
2021-08-17 13:46:05 -0400

    Preferred Organisms Config page: add PreferredOrganismsPageLoading as a fallback

--- Site/webapp/wdkCustomization/js/client/preferredOrganismRoutes.tsx
+++ Site/webapp/wdkCustomization/js/client/preferredOrganismRoutes.tsx
@@ -4,11 +4,13 @@ import { RouteEntry } from '@veupathdb/wdk-client/lib/Core/RouteEntry';
 
 import { PreferredOrganismsConfigController } from '@veupathdb/preferred-organisms/lib/controllers/PreferredOrganismsConfigController';
 
+import { PreferredOrganismsPageLoading } from './components/common/PreferredOrganismsPageLoading';
+
 export const preferredOrganismsRoutes: RouteEntry[] = [
   {
     path: '/preferred-organisms',
     component: () => (
-      <Suspense fallback={null}>
+      <Suspense fallback={<PreferredOrganismsPageLoading />}>
         <PreferredOrganismsConfigController />
       </Suspense>
     ),
