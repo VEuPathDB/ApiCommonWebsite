250bd2a99db1c0914f6f2edc542acecb4af53ff3
Ryan Doherty <rdoherty@upenn.edu>
2021-11-08 23:39:24 -0500

    Improvements suggested by Paul

--- Model/src/main/java/org/apidb/apicommon/model/report/singlegeneformats/ApolloGoTermFormat.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/singlegeneformats/ApolloGoTermFormat.java
@@ -11,6 +11,7 @@ import org.gusdb.wdk.model.record.TableValueRow;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+
 public class ApolloGoTermFormat implements Format {
 
   private static final String GO_TERM_TABLE_NAME = "GOTerms";
@@ -29,15 +30,37 @@ public class ApolloGoTermFormat implements Format {
   public JSONObject writeJson(RecordInstance recordInstance) throws WdkModelException, WdkUserException {
     JSONArray annotations = new JSONArray();
     for (TableValueRow row : recordInstance.getTableValue(GO_TERM_TABLE_NAME)) {
+      String ontologyValue = row.getAttributeValue("ontology").getValue();
+      String goId = row.getAttributeValue("go_id").getValue();
+      String evidenceCode = row.getAttributeValue("evidence_code").getValue();
       annotations.put(new JSONObject()
-        .put("goTerm", row.getAttributeValue("ontology").getValue())
-        .put("evidenceCodeLabel", row.getAttributeValue("evidence_code_parameter").getValue())
-        .put("negate", row.getAttributeValue("is_not").getValue())
-        .put("goTermLabel", row.getAttributeValue("go_term_name").getValue())
-        .put("evidenceCode", row.getAttributeValue("evidence_code").getValue())
-        .put("id", row.getAttributeValue("go_id").getValue()));
+        .put("goTerm", goId)
+        .put("goTermLabel", goId)
+        .put("aspect", getAspect(ontologyValue))
+        .put("geneRelationship", getRoValue(ontologyValue))
+        .put("evidenceCode", evidenceCode)
+        .put("evidenceCodeLabel", evidenceCode)
+        .put("negate", row.getAttributeValue("is_not").getValue()));
     }
     return new JSONObject().put("go_annotations", annotations);
+  }
+
+  private static String getAspect(String ontologyValue) {
+    switch (ontologyValue) {
+      case "Biological Process": return "BP";
+      case "Cellular Component": return "CC";
+      case "Molecular Function": return "MF";
+      default: return ontologyValue;
+    }
+  }
+
+  private static String getRoValue(String ontologyValue) {
+    switch (ontologyValue) {
+      case "Biological Process": return "RO:0002331";
+      case "Cellular Component": return "RO:0002432";
+      case "Molecular Function": return "RO:0002327";
+      default: return ontologyValue;
+    }
   }
 
 }
