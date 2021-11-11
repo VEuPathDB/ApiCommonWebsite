5a1c1f2954a6cc5b7bc69645907dd3f5d37a7a93
Paul Wilkinson <pwilkinson@ash.pcbi.upenn.edu>
2021-11-11 13:25:17 -0500

    commit of ApolloGoTermFormat.java - added lookup for EC codes

--- Model/src/main/java/org/apidb/apicommon/model/report/singlegeneformats/ApolloGoTermFormat.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/singlegeneformats/ApolloGoTermFormat.java
@@ -33,14 +33,18 @@ public class ApolloGoTermFormat implements Format {
       String ontologyValue = row.getAttributeValue("ontology").getValue();
       String goId = row.getAttributeValue("go_id").getValue();
       String evidenceCode = row.getAttributeValue("evidence_code").getValue();
+      String negateValue = "false";
+      String noteArray = "[]";
+      String goLabel = row.getAttributeValue("go_term_name").getValue();
       annotations.put(new JSONObject()
         .put("goTerm", goId)
-        .put("goTermLabel", goId)
+        .put("goTermLabel", goLabel)
         .put("aspect", getAspect(ontologyValue))
         .put("geneRelationship", getRoValue(ontologyValue))
-        .put("evidenceCode", evidenceCode)
+        .put("evidenceCode", getEcCode(evidenceCode))
         .put("evidenceCodeLabel", evidenceCode)
-        .put("negate", row.getAttributeValue("is_not").getValue()));
+        .put("negate", negateValue )
+        .put("notes", noteArray));
     }
     return new JSONObject().put("go_annotations", annotations);
   }
@@ -60,6 +64,38 @@ public class ApolloGoTermFormat implements Format {
       case "Cellular Component": return "RO:0002432";
       case "Molecular Function": return "RO:0002327";
       default: return ontologyValue;
+    }
+  }
+
+  private static String getEcCode(String evidenceCode) {
+    switch (evidenceCode) {
+      case "EXP": return "ECO:0000269";
+      case "HDA": return "ECO:0007005";
+      case "HEP": return "ECO:0007007";
+      case "HGI": return "ECO:0007003";
+      case "HMP": return "ECO:0007001";
+      case "HTP": return "ECO:0006056";
+      case "IBA": return "ECO:0000318";
+      case "IBD": return "ECO:0000319";
+      case "IC": return "ECO:0000305";
+      case "IDA": return "ECO:0000314";
+      case "IEA": return "ECO:0000501";
+      case "IEP": return "ECO:0000270";
+      case "IGC": return "ECO:0000317";
+      case "IGI": return "ECO:0000316";
+      case "IKR": return "ECO:0000320";
+      case "IMP": return "ECO:0000315";
+      case "IPI": return "ECO:0000353";
+      case "IRD": return "ECO:0000321";
+      case "ISA": return "ECO:0000247";
+      case "ISM": return "ECO:0000255";
+      case "ISO": return "ECO:0000266";
+      case "ISS": return "ECO:0000250";
+      case "NAS": return "ECO:0000303";
+      case "ND": return "ECO:0000307";
+      case "RCA": return "ECO:0000245";
+      case "TAS": return "ECO:0000304";
+      default: return evidenceCode;
     }
   }
 
