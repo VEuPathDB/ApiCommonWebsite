d13129e56b1d8a41dc2bb9fcc8766bb4657b39e5
Jamie Long <jtlong@upenn.edu>
2022-01-28 17:02:43 -0500

    Add "incrementOrganismCount" method to WdkService instances used in genomics sites

--- Site/webapp/wdkCustomization/js/client/wrapWdkService.tsx
+++ Site/webapp/wdkCustomization/js/client/wrapWdkService.tsx
@@ -1,11 +1,49 @@
-import { BlastCompatibleWdkService, blastCompatibleWdkServiceWrappers } from '@veupathdb/multi-blast/lib/utils/wdkServiceIntegration';
 import { WdkService } from '@veupathdb/wdk-client/lib/Core';
+import { ok } from '@veupathdb/wdk-client/lib/Utils/Json';
 
-type ApiCommonWdkService = BlastCompatibleWdkService;
+import { blastCompatibleWdkServiceWrappers } from '@veupathdb/multi-blast/lib/utils/wdkServiceIntegration';
 
-export function wrapWdkService(wdkService: WdkService): ApiCommonWdkService {
+export type GenomicsService = WdkService & {
+  [K in keyof GenomicsServiceWrappers]: ReturnType<GenomicsServiceWrappers[K]>;
+};
+
+type GenomicsServiceWrappers = typeof genomicsServiceWrappers;
+
+export const genomicsServiceWrappers = {
+  ...blastCompatibleWdkServiceWrappers,
+  incrementOrganismCount: (wdkService: WdkService) => function(
+    organismBearingEntity:
+      string | { recordClassUrlSegment: string, primaryKeyValues: string[] }
+  ): Promise<void> {
+    const queryParams = new URLSearchParams();
+
+    if (typeof organismBearingEntity === 'string') {
+      queryParams.append('organism', organismBearingEntity)
+    } else {
+      queryParams.append('recordType', organismBearingEntity.recordClassUrlSegment);
+      queryParams.append('primaryKey', organismBearingEntity.primaryKeyValues.join(','));
+    }
+
+    return wdkService.sendRequest(
+      ok,
+      {
+        method: 'get',
+        path: `/system/metrics/organism?${queryParams}`
+      }
+    );
+  }
+};
+
+export function wrapWdkService(wdkService: WdkService): GenomicsService {
   return {
     ...wdkService,
-    getBlastParamInternalValues: blastCompatibleWdkServiceWrappers.getBlastParamInternalValues(wdkService)
+    getBlastParamInternalValues: genomicsServiceWrappers.getBlastParamInternalValues(wdkService),
+    incrementOrganismCount: genomicsServiceWrappers.incrementOrganismCount(wdkService),
   };
+}
+
+export function isGenomicsService(wdkService: WdkService): wdkService is GenomicsService  {
+  return Object.keys(genomicsServiceWrappers).every(
+    genomicsServiceWrapperKey => genomicsServiceWrapperKey in wdkService
+  );
 }
