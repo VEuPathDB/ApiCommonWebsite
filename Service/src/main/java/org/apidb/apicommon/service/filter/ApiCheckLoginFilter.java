587d7979916cc1973d71e2ad862b64f8f54bf13b
Dan Galdi <dmgaldi@upenn.edu>
2022-03-29 17:03:15 -0400

    Fix up javax ref

--- Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
+++ Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
@@ -2,7 +2,7 @@ package org.apidb.apicommon.service.filter;
 
 import java.io.IOException;
 
-import javax.annotation.Priority;
+import jakarta.annotation.Priority;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.PreMatching;
 
