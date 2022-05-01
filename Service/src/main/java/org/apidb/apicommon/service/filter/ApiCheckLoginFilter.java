48b86abb2650a50b1ea1c932972454ffd52d7840
Ryan Doherty <tech@conical.org>
2022-05-01 07:08:38 -0400

    Fixes around guest creation during request filtering

--- Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
+++ Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
@@ -1,29 +1,15 @@
 package org.apidb.apicommon.service.filter;
 
-import java.io.IOException;
-
 import javax.annotation.Priority;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.PreMatching;
 
-import org.gusdb.fgputil.web.RequestData;
-import org.gusdb.wdk.controller.ContextLookup;
 import org.gusdb.wdk.service.filter.CheckLoginFilter;
 
-@PreMatching
-@Priority(200)
+@Priority(30)
 public class ApiCheckLoginFilter extends CheckLoginFilter {
 
   @Override
-  public void filter(ContainerRequestContext requestContext) throws IOException {
-
-    // gather request data to check URI
-    RequestData request = ContextLookup.getRequest(_servletRequest.get(), _grizzlyRequest.get());
-
-    // skip this filter if request is to jbrowse service
-    if (!request.getFullRequestUri().contains("service/jbrowse")) {
-      super.filter(requestContext);
-    }
-
+  protected boolean isPathToSkip(String path) {
+    return super.isPathToSkip(path) || path.startsWith("jbrowse");
   }
+
 }
