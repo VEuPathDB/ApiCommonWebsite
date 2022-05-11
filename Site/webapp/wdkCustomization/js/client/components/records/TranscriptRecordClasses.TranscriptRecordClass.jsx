3e8e220eff7757c9c12ed310768491831975c7fe
Jamie Long <jtlong@upenn.edu>
2022-05-11 16:10:38 -0400

    Cleanup: remove unnecessary white space

--- Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
+++ Site/webapp/wdkCustomization/js/client/components/records/TranscriptRecordClasses.TranscriptRecordClass.jsx
@@ -197,7 +197,7 @@ export function ResultTable(props) {
     ],
     [onSelectBasketExport, onSelectGeneListExport]
   );
-  
+
   const renderToolbarContent = useCallback(({
     addColumnsNode,
     downloadLinkNode,
