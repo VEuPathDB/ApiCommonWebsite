449f344af5226cc27a4a385af6f05ab20f4c120b
Jamie Long <jtlong@upenn.edu>
2022-06-21 18:43:56 -0400

    Remove now-unused summaryViewStoreModules file

--- Site/webapp/wdkCustomization/js/client/storeModules/summaryViewStoreModules.ts
+++
@@ -1,7 +0,0 @@
-import * as globalData from '@veupathdb/wdk-client/lib/StoreModules/GlobalData';
-import * as genomeSummaryView from './GenomeSummaryViewStoreModule';
-
-export default {
-  globalData,
-  genomeSummaryView,
-};
