687c0cfbf6af5b9418f94f12bb38d0c5ab7e5d30
Ryan Doherty <tech@conical.org>
2022-08-15 11:52:51 -0400

    Comply with new WDK APIs; should fix memory problem here

--- Model/src/main/java/org/apidb/apicommon/model/report/FastaReporter.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/FastaReporter.java
@@ -1,24 +1,28 @@
 package org.apidb.apicommon.model.report;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.io.OutputStreamWriter;
+import java.net.URLEncoder;
 
-import javax.ws.rs.client.Client;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.log4j.Logger;
 import org.gusdb.fgputil.IoUtil;
+import org.gusdb.fgputil.iterator.IteratorUtil;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.WdkModelException;
-import org.gusdb.wdk.model.answer.AnswerValue;
+import org.gusdb.wdk.model.WdkRuntimeException;
+import org.gusdb.wdk.model.record.PrimaryKeyIterator;
 import org.gusdb.wdk.model.report.AbstractReporter;
 import org.gusdb.wdk.model.report.config.StandardConfig;
 import org.json.JSONObject;
@@ -49,35 +53,26 @@ public abstract class FastaReporter extends AbstractReporter {
 
   @Override
   public void write(OutputStream out) throws WdkModelException {
-    try {
-     Map<String,String> inputFields = buildFormData(_configuration, _wdkModel.getProjectId(), _baseAnswer);
-     //LOG.debug("\n\n\n**************" + FormatUtil.prettyPrint(inputFields, Style.MULTI_LINE));
-     transferFormResult(inputFields, out);
-    }
-    catch (IOException e) {
-      throw new WdkModelException("Cannot output FASTA reporter data", e);
-    }
-  }
-
-  private void transferFormResult(Map<String, String> inputFields, OutputStream out)
-      throws WdkModelException, IOException {
-    Client client = ClientBuilder.newClient();
-
-    // prepare the form
-    Form form = new Form();
-    for (Entry<String,String> input : inputFields.entrySet()) {
-      form.param(input.getKey(), input.getValue());
-    }
-
     Response response = null;
     try {
+      // translate reporter request into a SRT tool request
+
+      // build SRT tool URL
       String baseUrl = _wdkModel.getProperties().get("LOCALHOST");
       String srtToolUri = getSrtToolUri();
-      String srtUrl = baseUrl + (srtToolUri.startsWith("/") ? srtToolUri : "/" + srtToolUri);
+      String srtUrl = baseUrl + (srtToolUri.startsWith("/") ? "" : "/") + srtToolUri;
       LOG.info("Submitting form to " + srtUrl);
-      response = client.target(srtUrl).request(
-          MediaType.APPLICATION_OCTET_STREAM_TYPE).post(
-              Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+
+      // make request
+      response = ClientBuilder.newClient()
+          .target(srtUrl)
+          .request(MediaType.APPLICATION_OCTET_STREAM_TYPE) // response type
+          .post(Entity.entity(
+              new SrtRequestBodyStream(),
+              MediaType.APPLICATION_FORM_URLENCODED_TYPE // request type
+          ));
+
+      // read response status and stream response body if successful, else throw 500
       int status = response.getStatus();
       if (status >= 400)
         throw new WdkModelException("Request failed with status code: " + status);
@@ -85,19 +80,56 @@ public abstract class FastaReporter extends AbstractReporter {
       InputStream in = response.readEntity(InputStream.class);
       IoUtil.transferStream(out, in);
     }
+    catch (IOException e) {
+      throw new WdkModelException("Cannot output FASTA reporter data", e);
+    }
     finally {
       if (response != null) response.close();
     }
   }
 
-  private static Map<String, String> buildFormData(JSONObject configuration, String projectId, AnswerValue answer)
-      throws WdkModelException {
-    Map<String,String> formInputs = new HashMap<>();
-    formInputs.put("project_id", projectId);
-    for (String key : JsonUtil.getKeys(configuration)) {
-      formInputs.put(key, configuration.get(key).toString());
+  private class SrtRequestBodyStream implements StreamingOutput {
+
+    @Override
+    public void write(OutputStream output) throws IOException, WebApplicationException {
+
+      try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output))) {
+
+        // write project ID
+        writer.write("project_id=" + _baseAnswer.getWdkModel().getProjectId());
+
+        // write user inputs
+        for (String key : JsonUtil.getKeys(_configuration)) {
+          writer.write('&');
+          writer.write(URLEncoder.encode(key, UTF_8));
+          writer.write('=');
+          writer.write(URLEncoder.encode(_configuration.get(key).toString(), UTF_8));
+        }
+
+        // write IDs
+        writer.write("&ids=");
+        try (PrimaryKeyIterator ids = _baseAnswer.getAllIds()) {
+          // SRT expects IDs in the following URL-encoded format:
+          //   Each '\n'-delimited line contains one record, whose
+          //   primary keys are joined and delimited by a comma
+          boolean firstRow = true;
+          for (String[] pk : IteratorUtil.toIterable(ids)) {
+            // write each PK
+            StringBuilder row = new StringBuilder(String.join(", ", pk));
+            if (firstRow) {
+              firstRow = false;
+            }
+            else {
+              row.append('\n');
+            }
+            writer.write(URLEncoder.encode(row.toString(), UTF_8));
+          }
+        }
+      }
+      catch (Exception e) {
+        throw new WdkRuntimeException("Unable to stream answer IDs as SRT tool request.", e);
+      }
     }
-    formInputs.put("ids", answer.getAllIdsAsString());
-    return formInputs;
   }
+
 }
