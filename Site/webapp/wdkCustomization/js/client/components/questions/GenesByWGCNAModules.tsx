9d761eef9ecf1c0075b049ddbbfb626102496b6c
asizemore <annsize@seas.upenn.edu>
2022-08-15 21:30:11 -0400

    add eigengene image in visibleHelp test

--- Site/webapp/wdkCustomization/js/client/components/questions/GenesByWGCNAModules.tsx
+++ Site/webapp/wdkCustomization/js/client/components/questions/GenesByWGCNAModules.tsx
@@ -6,11 +6,14 @@ import { Props } from '@veupathdb/wdk-client/lib/Views/Question/Params/Utils';
 
 export const GenesByWGCNAModules = (props: Props<Parameter>) => {
   useEffect(() => {
-    const eigengeneImage = document.getElementById('tfbs_image') as HTMLImageElement;
+    const eigengeneImage = document.getElementById('wgcna_image') as HTMLImageElement;
     console.log(props.value);
 
     if (eigengeneImage) {
-      eigengeneImage.src = `/a/images/pf_tfbs/${props.value}.png`;
+      // eigengeneImage.src = `/a/images/pf_tfbs/${props.value}.png`;
+      eigengeneImage.src = `/a/images/cgi-bin/dataPlotter.pl?project_id=PlasmoDB&id=P2_I5_M9&type=WGCNA::Eigengene&model=plasmo&fmt=png&datasetId=DS_b1ac1e329c`;
+
+      console.log(eigengeneImage.src);
     }
     console.log("hi ann!!");
   }, [ props.value ]);
