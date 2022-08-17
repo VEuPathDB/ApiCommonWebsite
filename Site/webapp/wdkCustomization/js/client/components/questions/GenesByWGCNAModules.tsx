c65ec3de9c19dc1bd0dbf62bd948aa30b38ca99d
asizemore <annsize@seas.upenn.edu>
2022-08-16 21:15:51 -0400

    prevent image change based on correlation param

--- Site/webapp/wdkCustomization/js/client/components/questions/GenesByWGCNAModules.tsx
+++ Site/webapp/wdkCustomization/js/client/components/questions/GenesByWGCNAModules.tsx
@@ -7,16 +7,10 @@ import { Props } from '@veupathdb/wdk-client/lib/Views/Question/Params/Utils';
 export const GenesByWGCNAModules = (props: Props<Parameter>) => {
   useEffect(() => {
     const eigengeneImage = document.getElementById('wgcna_image') as HTMLImageElement;
-    console.log(props.value);
-
-    if (eigengeneImage) {
+    if (eigengeneImage && props.value[0]  === 'P') {
       const cleanPropValue = props.value.split(" [")[0];
-      console.log(cleanPropValue);
       eigengeneImage.src = `/cgi-bin/dataPlotter.pl?project_id=PlasmoDB&id=${cleanPropValue}&type=WGCNA::Eigengene&model=plasmo&fmt=png&datasetId=DS_b1ac1e329c`;
-
-      console.log(eigengeneImage.src);
     }
-    console.log("hi ann!!");
   }, [ props.value ]);
 
   return <ParameterComponent {...props} />;
