12c56488a851661463204a1793aabd8965b520d2
Dave Falke <dfalke@uga.edu>
2022-10-04 12:58:01 -0400

    Add study access API to WDK dependencies

--- Site/webapp/wdkCustomization/js/client/bootstrap.js
+++ Site/webapp/wdkCustomization/js/client/bootstrap.js
@@ -1,3 +1,4 @@
+import { partial } from 'lodash';
 import { initialize as initializeEbrc } from '@veupathdb/web-common/lib/bootstrap';
 // import apicomm wrappers and additional routes
 import * as componentWrappers from './componentWrappers';
@@ -6,11 +7,16 @@ import wrapStoreModules from './wrapStoreModules';
 import { wrapWdkService } from './wrapWdkService';
 import pluginConfig from './pluginConfig';
 
+import { reduxMiddleware } from '@veupathdb/study-data-access/lib/data-restriction/DataRestrictionUtils';
+import { wrapWdkDependencies } from '@veupathdb/study-data-access/lib/shared/wrapWdkDependencies';
+
+
 // import CSS files
 import '@veupathdb/web-common/lib/styles/client.scss';
 import '@veupathdb/preferred-organisms/lib/components/OrganismNode.scss';
 import 'site/css/AllApiSites.css';
 import 'site/wdkCustomization/css/client.scss';
+import { edaServiceUrl } from '@veupathdb/web-common/lib/config';
 
 // Initialize the application.
 export const initialize =  initializeWdk => initializeEbrc({
@@ -19,5 +25,7 @@ export const initialize =  initializeWdk => initializeEbrc({
   wrapRoutes,
   wrapStoreModules,
   wrapWdkService,
-  pluginConfig
+  wrapWdkDependencies: partial(wrapWdkDependencies, edaServiceUrl),
+  pluginConfig,
+  additionalMiddleware: [ reduxMiddleware ],
 })
