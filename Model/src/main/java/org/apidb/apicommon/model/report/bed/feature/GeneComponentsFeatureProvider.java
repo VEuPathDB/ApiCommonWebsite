8997b803d15bf8d9d355fe84a80e998c674890e8
Wojtek Bazant <wbazant@vet.upenn.edu>
2023-01-05 12:00:59 +0000

    Remove workaround for broken dumper. Add CDS option
    See https://github.com/VEuPathDB/ApiCommonModel/pull/15

--- Model/src/main/java/org/apidb/apicommon/model/report/bed/feature/GeneComponentsFeatureProvider.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/bed/feature/GeneComponentsFeatureProvider.java
@@ -24,29 +24,23 @@ import org.apidb.apicommon.model.report.bed.util.BedLine;
 
 public class GeneComponentsFeatureProvider implements BedFeatureProvider {
 
-  private enum GeneComponentAsRequested {
+  private enum GeneComponent {
     five_prime_utr,
     three_prime_utr,
     exon,
+    cds,
     intron;
-  }
-
-  private enum GeneComponentAsStored {
-    UTR,
-    CDS,
-    Exon,
-    Intron;
   }
 
   private static final String ATTR_ORGANISM = "organism";
   private static final String TABLE_GENE_MODEL_DUMP = "GeneModelDump";
 
   private final RequestedDeflineFields _requestedDeflineFields;
-  private final Set<GeneComponentAsRequested> _requestedComponents;
+  private final Set<GeneComponent> _requestedComponents;
 
   public GeneComponentsFeatureProvider(JSONObject config){
     _requestedDeflineFields = new RequestedDeflineFields(config);
-    _requestedComponents = config.getJSONArray("geneComponents").toList().stream().map(o -> GeneComponentAsRequested.valueOf(o.toString())).collect(Collectors.toSet());
+    _requestedComponents = config.getJSONArray("geneComponents").toList().stream().map(o -> GeneComponent.valueOf(o.toString())).collect(Collectors.toSet());
 
   }
   @Override
@@ -72,50 +66,15 @@ public class GeneComponentsFeatureProvider implements BedFeatureProvider {
       Map<String, List<Integer>> endsByT = new HashMap<>();
       TableValue geneModelDumpRows = record.getTableValue(TABLE_GENE_MODEL_DUMP);
 
-      /*
-       * Iterate twice. First get starts and ends for each transcript - we need them to orient UTRs
-       */
-      for (Map<String, AttributeValue> geneModelDumpRow : geneModelDumpRows) {
-        Integer gmStart = Integer.valueOf(geneModelDumpRow.get("gm_start").toString());
-        Integer gmEnd = Integer.valueOf(geneModelDumpRow.get("gm_end").toString());
-        for(String transcriptId: geneModelDumpRow.get("transcript_ids").toString().split(",")){
-          if(!startsByT.containsKey(transcriptId)){
-            startsByT.put(transcriptId, new ArrayList<Integer>());
-            endsByT.put(transcriptId, new ArrayList<Integer>());
-          }
-          startsByT.get(transcriptId).add(gmStart);
-          endsByT.get(transcriptId).add(gmEnd);
-        }
-      }
-      Map<String, Integer> tStartsByT = startsByT.entrySet().stream()
-                                    .collect(Collectors.toMap(
-                                      entry -> entry.getKey(),
-                                      entry -> Collections.min(entry.getValue())));
-      Map<String, Integer> tEndsByT = endsByT.entrySet().stream()
-                                    .collect(Collectors.toMap(
-                                      entry -> entry.getKey(),
-                                      entry -> Collections.max(entry.getValue())));
-
       for (Map<String, AttributeValue> geneModelDumpRow : geneModelDumpRows) {
         String sourceId = geneModelDumpRow.get("source_id").toString();
         String chrom = geneModelDumpRow.get("sequence_id").toString();
-        GeneComponentAsStored geneComponentAsStored = GeneComponentAsStored.valueOf(geneModelDumpRow.get("type").toString());
+        GeneComponent geneComponent = GeneComponent.valueOf(geneModelDumpRow.get("type").toString().toLowerCase());
         Integer segmentStart = Integer.valueOf(geneModelDumpRow.get("gm_start").toString());
         Integer segmentEnd = Integer.valueOf(geneModelDumpRow.get("gm_end").toString());
 
         StrandDirection strand = StrandDirection.fromSign(geneModelDumpRow.get("strand").toString());
 
-        boolean startMatchesTranscriptStart = Arrays.asList(geneModelDumpRow.get("transcript_ids").toString().split(","))
-          .stream()
-          .map(transcriptId -> tStartsByT.get(transcriptId).equals(segmentStart))
-          .reduce(Boolean.FALSE, Boolean::logicalOr);
-
-        boolean endMatchesTranscriptEnd = Arrays.asList(geneModelDumpRow.get("transcript_ids").toString().split(","))
-          .stream()
-          .map(transcriptId -> tEndsByT.get(transcriptId).equals(segmentEnd))
-          .reduce(Boolean.FALSE, Boolean::logicalOr);
-
-        GeneComponentAsRequested geneComponent = asRequestedComponent(geneComponentAsStored, strand, startMatchesTranscriptStart, endMatchesTranscriptEnd);
         if(_requestedComponents.contains(geneComponent)){
           DeflineBuilder defline = new DeflineBuilder(sourceId + "::" + geneComponent.toString());
           if(_requestedDeflineFields.contains("organism")){
@@ -143,10 +102,12 @@ public class GeneComponentsFeatureProvider implements BedFeatureProvider {
     }
   }
 
-  private String prettyString(GeneComponentAsRequested geneComponent){
+  private String prettyString(GeneComponent geneComponent){
     switch(geneComponent){
       case exon:
         return "Exon";
+      case cds:
+        return "CDS";
       case intron:
         return "Intron";
       case five_prime_utr:
@@ -157,25 +118,7 @@ public class GeneComponentsFeatureProvider implements BedFeatureProvider {
         throw new WdkRuntimeException("Unsupported gene component: " + geneComponent.toString());
     }
   }
-  private GeneComponentAsRequested asRequestedComponent(GeneComponentAsStored geneComponentAsStored, StrandDirection strand, boolean startMatchesTranscriptStart, boolean endMatchesTranscriptEnd){
-    switch(geneComponentAsStored){
-      case Exon:
-      case CDS:
-        return GeneComponentAsRequested.exon;
-      case Intron:
-        return GeneComponentAsRequested.intron;
-      case UTR:
-        switch (strand){
-          case forward:
-            return startMatchesTranscriptStart ? GeneComponentAsRequested.five_prime_utr : GeneComponentAsRequested.three_prime_utr;
-          case reverse:
-            return endMatchesTranscriptEnd ? GeneComponentAsRequested.five_prime_utr: GeneComponentAsRequested.three_prime_utr;
-          case none:
-            return null;
-        }
-    }
-    return null;
-  }
+
 }
 
 
