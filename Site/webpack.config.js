0a86228e53996785048276f36d98dbe8ae490961
Dave Falke <dfalke@uga.edu>
2023-02-25 00:21:31 -0500

    upgrade veupathdb packages

--- Site/webpack.config.js
+++ Site/webpack.config.js
@@ -4,6 +4,19 @@ const additionalConfig = {
   entry: {
     'site-client': __dirname + '/webapp/wdkCustomization/js/client/main.js',
   },
+  module: {
+    rules: [
+      // Apply babel to react-leaflet code.
+      // This can be removed when we upgrade to webpack@5.
+      {
+        test: /\.jsx?$/,
+        include: /node_modules\/@?react-leaflet/,
+        use: [
+          { loader: 'babel-loader', options: { configFile: './.babelrc' } }
+        ]
+      },
+    ],
+  },
   resolve: {
     // alias 'ciena-*' entries to '/lib' directory since the default
     // entry is es6 code, which uglifyjs does not understand
