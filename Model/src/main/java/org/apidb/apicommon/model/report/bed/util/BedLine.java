760e6fbe3b50aa146c85ad4f17e2474cc7eaf7bc
Ryan Doherty <tech@conical.org>
2023-12-06 00:28:03 -0500

    Remove unused import

--- Model/src/main/java/org/apidb/apicommon/model/report/bed/util/BedLine.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/bed/util/BedLine.java
@@ -1,8 +1,7 @@
 package org.apidb.apicommon.model.report.bed.util;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
