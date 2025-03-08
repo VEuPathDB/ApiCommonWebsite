d18887d178dd048580e0c6446d5278462996b0fb
Bob MacCallum <uncoolbob@gmail.com>
2025-03-07 21:38:51 -0500

    stagger AI API calls by 500ms

--- Model/src/main/java/org/apidb/apicommon/model/report/ai/expression/AiExpressionCache.java
+++ Model/src/main/java/org/apidb/apicommon/model/report/ai/expression/AiExpressionCache.java
@@ -387,6 +387,11 @@ public class AiExpressionCache {
           }))
 
         ), exec));
+
+	// Add a delay of 500 ms between job submissions.
+	// We're assuming that blocking the main thread is OK because we're waiting
+	// for the results immediately after the loop is done.
+	Thread.sleep(500);
       }
 
       // wait for all threads, filling lists along the way
