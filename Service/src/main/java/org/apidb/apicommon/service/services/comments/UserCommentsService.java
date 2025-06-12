6f2e6a95d57d403c1b1bc6521101958b531c00b8
steve-fischer-200 <sfischer@upenn.edu>
2025-06-12 16:22:16 -0400

    debug

--- Service/src/main/java/org/apidb/apicommon/service/services/comments/UserCommentsService.java
+++ Service/src/main/java/org/apidb/apicommon/service/services/comments/UserCommentsService.java
@@ -31,6 +31,7 @@ import org.gusdb.wdk.service.annotation.InSchema;
 import org.gusdb.wdk.service.annotation.OutSchema;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.apache.log4j.Logger;
 
 @Path(UserCommentsService.BASE_PATH)
 public class UserCommentsService extends AbstractUserCommentService {
@@ -42,6 +43,8 @@ public class UserCommentsService extends AbstractUserCommentService {
   public static final String SOURCE_EMAIL     = "annotator@apidb.org";
   public static final String ANNOTATORS_EMAIL = "EUPATHDB_ANNOTATORS@lists.upenn.edu";
   public static final String REDMINE_EMAIL    = "redmine@apidb.org";
+
+  private static final Logger LOG = Logger.getLogger(UserCommentsService.class);
 
   @Context
   protected UriInfo _uriInfo;
@@ -180,7 +183,7 @@ public class UserCommentsService extends AbstractUserCommentService {
   private JSONArray validatePubmedIds(Set<String> pubmedIds)
           throws WdkModelException {
     return pubmedIds.stream().filter(a-> !a.matches("\\d+"))
-            .map(id -> id + "\" is not a valid pubmed id.")
+            .map(id -> "\"" + id + "\" is not a valid pubmed id.")
             .collect(JSONArray::new, JSONArray::put, (x, y) -> y.forEach(x::put));
   }
 
