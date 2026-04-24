d8d9000568883ef8ec7285ad7e2b65b65ddb5a0c
Ryan Doherty <tech@conical.org>
2026-04-24 00:57:47 -0400

    Use utility in new fgputil release and remove temporary method copy

--- Service/src/main/java/org/apidb/apicommon/service/services/ApiRecordService.java
+++ Service/src/main/java/org/apidb/apicommon/service/services/ApiRecordService.java
@@ -1,20 +1,15 @@
 package org.apidb.apicommon.service.services;
 
 import static org.gusdb.fgputil.functional.Functions.f0Swallow;
-import static org.gusdb.fgputil.functional.Functions.swallowAndGet;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -22,7 +17,7 @@ import org.apidb.apicommon.controller.SiteSpecificTmpFileCache;
 import org.apidb.apicommon.controller.SiteSpecificTmpFileCache.CacheName;
 import org.gusdb.fgputil.Timer;
 import org.gusdb.fgputil.runtime.GusHome;
-import org.gusdb.fgputil.runtime.ThreadUtil;
+import org.gusdb.fgputil.runtime.RuntimeUtil;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkRuntimeException;
@@ -76,10 +71,13 @@ public class ApiRecordService extends RecordService {
         String gusHome = Paths.get(GusHome.getGusHome())
             .getParent().getParent().getParent().resolve("gus_home").toString();
         LOG.info("Using GUS_HOME = " + gusHome);
-        executeAndLogOutput(
+        RuntimeUtil.executeSubprocessAndLogOutput(
             List.of("perl", gusHome + "/bin/fgpJava", ApiRecordService.class.getName(), wdkModel.getProjectId()),
-            Map.of("GUS_HOME", gusHome), Optional.empty(),
-            LOG, Level.INFO, Optional.empty(), Optional.of(Duration.ofMinutes(1)), true);
+            Map.of("GUS_HOME", gusHome),               // subprocess environment 
+            Optional.empty(),                          // don't override stdin
+            LOG, Level.INFO,                           // logger and level
+            Optional.empty(),                          // combine stdout/stderr and log
+            Optional.of(Duration.ofMinutes(1)), true); // use timeout
       }
       else {
         ApiRecordService obj = new ApiRecordService();
@@ -101,103 +99,6 @@ public class ApiRecordService extends RecordService {
       throw new IllegalArgumentException("This tool requires a single argument, project_id");
     try (WdkModel wdkModel = WdkModel.construct(args[0], GusHome.getGusHome())) {
       cacheExpandedRecordClassesJson(wdkModel, false);
-    }
-  }
-
-  // FIXME: This method also exists in FgpUtil's RuntimeUtil class; use as soon as it is available via upgrade
-  public static Optional<Integer> executeAndLogOutput(
-      List<String> command,
-      Map<String,String> environment,
-      Optional<File> stdinFile,
-      Logger logger,
-      Level logLevel,
-      Optional<File> stdoutFile,
-      Optional<Duration> processTimeout,
-      boolean killOnTimeout) {
-    Thread logMonitorThread = null;
-    try {
-      LOG.info("Starting subprocess with command: " + String.join(" ", command));
-
-      // configure the process
-      ProcessBuilder processBuilder = new ProcessBuilder().command(command);
-
-      // if stdin file is passed, redirect stdin to it
-      if (stdinFile.isPresent())
-        processBuilder.redirectInput(stdinFile.get());
-
-      // if stdout file is passed, write stdout to the file; otherwise merge with stderr
-      if (stdoutFile.isPresent())
-        processBuilder.redirectInput(stdoutFile.get());
-      else
-        processBuilder.redirectErrorStream(true);
-
-      // set passed environment
-      processBuilder.environment().putAll(environment);
-
-      // start the process
-      Process process = processBuilder.start();
-
-      // start a thread to stream output to the passed Logger (if level allows)
-      InputStream streamToLog = stdoutFile.isPresent() ? process.getErrorStream() : process.getInputStream();
-      logMonitorThread = new Thread(() -> {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(streamToLog))) {
-          String line;
-          while ((line = reader.readLine()) != null) {
-            logger.log(logLevel, ">> " + line);
-          }
-        }
-        catch (IOException e) {
-          LOG.error("Parent process warning: could not read subprocess output", e);
-        }
-      });
-      logMonitorThread.start();
-
-      // wait for process to finish in this thread
-      boolean exitWithoutTimeout = processTimeout
-          .map(duration -> swallowAndGet(() -> process.waitFor(duration.toMillis(), TimeUnit.MILLISECONDS)))
-          .orElse(process.waitFor() - process.exitValue() == 0); // always true
-
-      // wait for the thread to finish processing the subprocess's output
-      logMonitorThread.join();
-
-      int exitValue = process.exitValue();
-      if (exitWithoutTimeout) {
-        LOG.info("Subprocess exited with exit code: " + exitValue);
-        return Optional.of(exitValue);
-      }
-      else {
-        // subprocess timed out before completion; kill if requested
-        if (killOnTimeout) {
-          int gracefulShutdownWindow = 500;
-          LOG.info("Subprocess timed out before completion.  Attempting to shut down gracefully...");
-          process.destroy();
-          ThreadUtil.sleep(gracefulShutdownWindow);
-          if (process.isAlive()) {
-            LOG.info("Subprocess did not shut down gracefully after " + gracefulShutdownWindow + "ms.  Forcibly terminating.");
-            process.destroyForcibly();
-          }
-        }
-        LOG.warn("Subprocess timed out before completion");
-        return Optional.empty();
-      }
-    }
-    catch (RuntimeException e) {
-      if (e.getCause() != null && e.getCause() instanceof InterruptedException) {
-        throw new RuntimeException("This thread or logger thread was interrupted before subprocess handling could complete", e.getCause());
-      }
-      throw e;
-    }
-    catch (InterruptedException e) {
-      throw new RuntimeException("This thread or logger thread was interrupted before subprocess handling could complete", e);
-    }
-    catch (IOException e) {
-      throw new RuntimeException("Error occurred while executing subprocess", e);
-    }
-    finally {
-      // kill log monitor thread if still active
-      if (logMonitorThread != null && logMonitorThread.isAlive()) {
-        logMonitorThread.interrupt();
-      }
     }
   }
 }
