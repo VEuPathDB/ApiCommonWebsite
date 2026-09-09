5eea3a956d06dc50def09ac2ea7c0512e481a67e
steve-fischer-200 <sfischer@upenn.edu>
2026-09-09 09:12:41 -0400

    add user-comment record to whitelist
    (cherry picked from commit 3d160ba10af19678fa5b481e2357fb528c11490b)

--- Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
+++ Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
@@ -39,7 +39,7 @@ public class ApiCheckLoginFilter extends CheckLoginFilter {
 		    "transcript","gene","organism","genomic-sequence","genomic-segment","strain-genomic-segment","est",
       "pathway","compound","blast-est-ontology","blast-orf-ontology","file","build",
       "dfile","metrics","jbrowse-gene","dataset-release-notes","legacy-dataset",
-      "dataset","datasource","userdataset","junction", "variation"
+      "dataset","datasource","userdataset","junction", "variation", "user-comment"
     ).stream().map(s -> "record-types/" + s).collect(Collectors.toList()))
     .toList();
 
