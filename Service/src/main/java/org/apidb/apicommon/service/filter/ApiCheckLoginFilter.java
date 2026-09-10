b24cd1694d0ca4dec8fb4cace8f5398306cfd7e0
Ryan Doherty <tech@conical.org>
2026-09-10 09:49:21 -0400

    Make all payment endpoints open without token

--- Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
+++ Service/src/main/java/org/apidb/apicommon/service/filter/ApiCheckLoginFilter.java
@@ -21,7 +21,6 @@ public class ApiCheckLoginFilter extends CheckLoginFilter {
       "user-profile-vocabularies",
       "subscription-groups",
       "oauth/state-token",
-      "payment-form-content",
       "users",
       "user-password-reset",
       "users/current",
@@ -46,7 +45,8 @@ public class ApiCheckLoginFilter extends CheckLoginFilter {
   private static final List<String> OPEN_PATH_PREFIXES = List.of(
       "system/metrics/count-page-view",
       "temporary-files",
-      "temporary-results"
+      "temporary-results",
+      "payment"
   );
 
   private static final String ADDITIONAL_MESSAGE_TEMPLATE =
